fecfef95ace91ee2741c482f58cd50698805a290
bouzuya <m@bouzuya.net>
2019-06-21 19:13:41 +0900

    abc128_b update

--- abc128_b/src/main.rs
+++ abc128_b/src/main.rs
@@ -12,31 +12,23 @@ fn read<T: std::str::FromStr>(
     s.parse().unwrap_or_else(|_| panic!("read"))
 }
 
-struct Restaurant {
-    i: i32,
-    s: String,
-    p: i32,
-}
-
 fn main() {
     let stdin = std::io::stdin();
     let mut stdin_lock = stdin.lock();
     let mut buf: Vec<u8> = Vec::new();
-    let n: i32 = read(&mut stdin_lock, &mut buf, b'\n');
-    let mut ts: Vec<Restaurant> = Vec::new();
+    let n: usize = read(&mut stdin_lock, &mut buf, b'\n');
+    let mut ts = vec![("".to_owned(), 0, 0); n];
     for i in 0..n {
-        ts.push(Restaurant {
-            i: i + 1,
-            s: read(&mut stdin_lock, &mut buf, b' '),
-            p: read(&mut stdin_lock, &mut buf, b'\n'),
-        });
+        ts[i].0 = read(&mut stdin_lock, &mut buf, b' ');
+        ts[i].1 = read(&mut stdin_lock, &mut buf, b'\n');
+        ts[i].2 = i + 1;
     }
-    ts.sort_by(|a, b| match a.s.cmp(&b.s) {
+    ts.sort_by(|a, b| match a.0.cmp(&b.0) {
         Ordering::Less => Ordering::Less,
         Ordering::Greater => Ordering::Greater,
-        Ordering::Equal => b.p.cmp(&a.p),
+        Ordering::Equal => b.1.cmp(&a.1),
     });
     for t in ts.iter() {
-        println!("{}", t.i);
+        println!("{}", t.2);
     }
 }
