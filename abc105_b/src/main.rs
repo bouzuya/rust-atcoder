9a6962cff8505088a0115aa9effc2434d8b8559c
bouzuya <m@bouzuya.net>
2019-06-27 22:12:13 +0900

    abc105_b

---
+++ abc105_b/src/main.rs
@@ -0,0 +1,28 @@
+fn read<T: std::str::FromStr>(
+    stdin_lock: &mut std::io::StdinLock,
+    buf: &mut Vec<u8>,
+    delimiter: u8,
+) -> T {
+    buf.clear();
+    let l = std::io::BufRead::read_until(stdin_lock, delimiter, buf).unwrap();
+    buf.truncate(l - 1); // remove delimiter
+    let s = unsafe { std::str::from_utf8_unchecked(&buf) };
+    s.parse().unwrap_or_else(|_| panic!("read"))
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let mut buf: Vec<u8> = Vec::new();
+    let n: i32 = read(&mut stdin_lock, &mut buf, b'\n');
+    let mut ans = false;
+    for x in 0..(n / 7 + 1) {
+        let n4 = n - 7 * x;
+        for y in 0..(n4 / 4 + 1) {
+            if n4 - 4 * y == 0 {
+                ans = true;
+            }
+        }
+    }
+    println!("{}", if ans { "Yes" } else { "No" });
+}
