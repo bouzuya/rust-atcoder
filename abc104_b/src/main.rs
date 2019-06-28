252790203fba8f1040cbc2b10cae4d2b568bff39
bouzuya <m@bouzuya.net>
2019-06-28 23:13:30 +0900

    abc104_b update

--- abc104_b/src/main.rs
+++ abc104_b/src/main.rs
@@ -15,31 +15,13 @@ fn main() {
     let mut stdin_lock = stdin.lock();
     let mut buf: Vec<u8> = Vec::new();
     let s: String = read(&mut stdin_lock, &mut buf, b'\n');
-    let mut cs: Vec<char> = s.chars().collect();
-    let mut ans = true;
-    if cs[0] != 'A' {
-        ans = false;
-    } else {
-        cs[0] = 'a';
-    }
-    let mut c_count = 0;
-    for i in 2..(cs.len() - 1) {
-        if cs[i] == 'C' {
-            cs[i] = 'c';
-            c_count += 1;
-        }
-    }
-    if c_count != 1 {
-        ans = false;
-    }
-    if cs
-        .into_iter()
-        .filter(|c| !c.is_lowercase())
-        .collect::<Vec<char>>()
-        .len()
-        != 0
-    {
-        ans = false;
-    }
+    let cs: Vec<char> = s.chars().collect();
+    let ans = cs[0] == 'A'
+        && cs[2..(cs.len() - 1)].iter().filter(|&&c| c == 'C').count() == 1
+        && (cs[1].is_lowercase()
+            && cs[2..(cs.len() - 1)]
+                .iter()
+                .all(|&c| c == 'C' || c.is_lowercase())
+            && cs[cs.len() - 1].is_lowercase());
     println!("{}", if ans { "AC" } else { "WA" });
 }
