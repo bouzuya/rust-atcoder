5a2b7147857e531b3c75713cfc65f6efa56619a2
bouzuya <m@bouzuya.net>
2019-06-29 07:31:23 +0900

    abc101_b

---
+++ abc101_b/src/main.rs
@@ -0,0 +1,27 @@
+fn read<T: std::str::FromStr>(
+    stdin_lock: &mut std::io::StdinLock,
+    buf: &mut Vec<u8>,
+    delimiter: u8,
+) -> T {
+    buf.clear();
+    let l = std::io::BufRead::read_until(stdin_lock, delimiter, buf).unwrap();
+    buf.truncate(l - 1); // remove delimiter
+    let s = unsafe { std::str::from_utf8_unchecked(&buf) };
+    s.parse().unwrap_or_else(|_| panic!("read"))
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let mut buf: Vec<u8> = Vec::new();
+    let n: i32 = read(&mut stdin_lock, &mut buf, b'\n');
+    let ns: String = format!("{}", n);
+    let ncs: Vec<char> = ns.chars().collect();
+    let mut sn: i32 = 0;
+    for &c in ncs.iter() {
+        let x: i32 = format!("{}", c).parse().unwrap();
+        sn += x;
+    }
+    let ans = if n % sn == 0 { "Yes" } else { "No" };
+    println!("{}", ans);
+}
