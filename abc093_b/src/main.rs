b0503c842e77a6f163e88c72769f247de8b76fe6
bouzuya <m@bouzuya.net>
2019-06-29 15:33:26 +0900

    abc093_b

---
+++ abc093_b/src/main.rs
@@ -0,0 +1,29 @@
+use std::cmp::{max, min};
+
+fn read<T: std::str::FromStr>(
+    stdin_lock: &mut std::io::StdinLock,
+    buf: &mut Vec<u8>,
+    delimiter: u8,
+) -> T {
+    buf.clear();
+    let l = std::io::BufRead::read_until(stdin_lock, delimiter, buf).unwrap();
+    buf.truncate(l - 1); // remove delimiter
+    let s = unsafe { std::str::from_utf8_unchecked(&buf) };
+    s.parse().unwrap_or_else(|_| panic!("read"))
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let mut buf: Vec<u8> = Vec::new();
+    let a: i32 = read(&mut stdin_lock, &mut buf, b' ');
+    let b: i32 = read(&mut stdin_lock, &mut buf, b' ');
+    let k: i32 = read(&mut stdin_lock, &mut buf, b'\n');
+    let l = max(a, min(a + k - 1, b));
+    for i in a..l + 1 {
+        println!("{}", i);
+    }
+    for i in min(max(l + 1, b - k + 1), b + 1)..(b + 1) {
+        println!("{}", i);
+    }
+}
