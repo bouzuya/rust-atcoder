8e8a6d9d44aec8e94c59c2536e2aec5a19650e02
bouzuya <m@bouzuya.net>
2019-07-01 21:35:21 +0900

    dp_a

---
+++ dp_a/src/main.rs
@@ -0,0 +1,35 @@
+use std::cmp::min;
+
+fn read<T: std::str::FromStr>(
+    stdin_lock: &mut std::io::StdinLock,
+    buf: &mut Vec<u8>,
+    delimiter: u8,
+) -> T {
+    buf.clear();
+    let l = std::io::BufRead::read_until(stdin_lock, delimiter, buf).unwrap();
+    buf.truncate(l - 1); // remove delimiter
+    let s = unsafe { std::str::from_utf8_unchecked(&buf) };
+    s.parse().unwrap_or_else(|_| panic!("read"))
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let mut buf: Vec<u8> = Vec::new();
+    let n: usize = read(&mut stdin_lock, &mut buf, b'\n');
+    let mut hv = vec![0i32; n];
+    for i in 0..n - 1 {
+        hv[i] = read(&mut stdin_lock, &mut buf, b' ');
+    }
+    hv[n - 1] = read(&mut stdin_lock, &mut buf, b'\n');
+
+    let mut cv = vec![0; n];
+    cv[0] = 0;
+    cv[1] = (hv[1] - hv[0]).abs();
+    for i in 2..n {
+        let c2 = cv[i - 2] + (hv[i] - hv[i - 2]).abs();
+        let c1 = cv[i - 1] + (hv[i] - hv[i - 1]).abs();
+        cv[i] = min(c1, c2);
+    }
+    println!("{}", cv[n - 1]);
+}
