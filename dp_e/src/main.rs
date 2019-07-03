1f9c66ea43982e1d9327b2d304fbdd64d9dfbb14
bouzuya <m@bouzuya.net>
2019-07-03 20:12:41 +0900

    dp_e fix WA

--- dp_e/src/main.rs
+++ dp_e/src/main.rs
@@ -18,27 +18,19 @@ fn main() {
     let mut buf: Vec<u8> = Vec::new();
     let n: usize = read(&mut stdin_lock, &mut buf, b' ');
     let w: i64 = read(&mut stdin_lock, &mut buf, b'\n');
-    let w_max = 10000000000i64;
-    let v_max = 1000;
+    let w_max = 10_000_000_000i64;
+    let v_max = 1_000 * n;
     let mut ans = vec![vec![w_max; v_max + 1]; n + 1];
     ans[0][0] = 0;
-    for i in 1..n + 1 {
+    for i in 0..n {
         let wi: i64 = read(&mut stdin_lock, &mut buf, b' ');
         let vi: usize = read(&mut stdin_lock, &mut buf, b'\n');
-        ans[i][0] = 0;
-        for j in 1..v_max + 1 {
-            ans[i][j] = min(
-                ans[i - 1][j],
-                if j < vi {
-                    w_max
-                } else {
-                    ans[i - 1][j - vi] + wi
-                },
-            );
+        for j in 0..v_max + 1 {
+            ans[i + 1][j] = min(ans[i][j], if j < vi { wi } else { ans[i][j - vi] + wi });
         }
     }
     let mut ans_v = 0;
-    for j in 1..v_max + 1 {
+    for j in 0..v_max + 1 {
         if ans[n][j] <= w {
             ans_v = j;
         }
