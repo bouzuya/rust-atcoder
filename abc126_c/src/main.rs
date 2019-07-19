1f9e6e58ea4180ac6ae21bfbc6797886dfab9768
bouzuya <m@bouzuya.net>
2019-07-19 22:25:52 +0900

    abc126_c

---
+++ abc126_c/src/main.rs
@@ -0,0 +1,30 @@
+fn read<T: std::str::FromStr>(
+    stdin_lock: &mut std::io::StdinLock,
+    buf: &mut Vec<u8>,
+    delimiter: u8,
+) -> T {
+    buf.clear();
+    let l = std::io::BufRead::read_until(stdin_lock, delimiter, buf).unwrap();
+    buf.truncate(l - 1); // remove delimiter
+    let s = unsafe { std::str::from_utf8_unchecked(&buf) };
+    s.parse().unwrap_or_else(|_| panic!("read"))
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let mut buf: Vec<u8> = Vec::new();
+    let n: i32 = read(&mut stdin_lock, &mut buf, b' ');
+    let k: i32 = read(&mut stdin_lock, &mut buf, b'\n');
+    let mut ans = 0f64;
+    for i in 1..n + 1 {
+        let mut p = 1f64;
+        let mut s = 1;
+        while s * i < k {
+            s *= 2;
+            p /= 2f64;
+        }
+        ans += p / f64::from(n);
+    }
+    println!("{:.12}", ans);
+}
