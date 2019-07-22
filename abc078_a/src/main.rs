b7f2eed01b8e6844d8c563591a417dd6c5d24f9b
bouzuya <m@bouzuya.net>
2019-07-22 23:22:36 +0900

    abc078_a

---
+++ abc078_a/src/main.rs
@@ -0,0 +1,28 @@
+fn read<T: std::str::FromStr>(
+    stdin_lock: &mut std::io::StdinLock,
+    buf: &mut Vec<u8>,
+    delimiter: u8,
+) -> T {
+    buf.clear();
+    let l = std::io::BufRead::read_until(stdin_lock, delimiter, buf).unwrap();
+    buf.truncate(l - 1); // remove delimiter
+    let s = unsafe { std::str::from_utf8_unchecked(&buf) };
+    s.parse().unwrap_or_else(|_| panic!("read"))
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let mut buf: Vec<u8> = Vec::new();
+    let x: char = read(&mut stdin_lock, &mut buf, b' ');
+    let y: char = read(&mut stdin_lock, &mut buf, b'\n');
+
+    let ans = if x < y {
+        "<"
+    } else if x > y {
+        ">"
+    } else {
+        "="
+    };
+    println!("{}", ans);
+}
