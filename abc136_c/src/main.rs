a38090389a98dd7ce0380de0528f29c56fd0a481
bouzuya <m@bouzuya.net>
2019-08-04 21:16:49 +0900

    abc136_c

---
+++ abc136_c/src/main.rs
@@ -0,0 +1,39 @@
+fn read<T: std::str::FromStr>(
+    stdin_lock: &mut std::io::StdinLock,
+    buf: &mut Vec<u8>,
+    delimiter: u8,
+) -> T {
+    buf.clear();
+    let l = std::io::BufRead::read_until(stdin_lock, delimiter, buf).unwrap();
+    buf.truncate(l - 1); // remove delimiter
+    let s = unsafe { std::str::from_utf8_unchecked(&buf) };
+    s.parse().unwrap_or_else(|_| panic!("read"))
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let mut buf: Vec<u8> = Vec::new();
+    let n: usize = read(&mut stdin_lock, &mut buf, b'\n');
+    let mut hv = vec![0i32; n];
+    for i in 0..n - 1 {
+        hv[i] = read(&mut stdin_lock, &mut buf, b' ');
+    }
+    hv[n - 1] = read(&mut stdin_lock, &mut buf, b'\n');
+
+    hv.reverse();
+
+    let mut ans = true;
+    let mut p = hv[0];
+    for i in 0..n {
+        if hv[i] <= p {
+            continue;
+        } else if hv[i] > p + 1 {
+            ans = false;
+            break;
+        } else {
+            p = hv[i] - 1;
+        }
+    }
+    println!("{}", if ans { "Yes" } else { "No" });
+}
