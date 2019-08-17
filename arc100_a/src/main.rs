233b5e324a64e81964f2f0a39c4ea7ff5798ace2
bouzuya <m@bouzuya.net>
2019-08-17 22:22:03 +0900

    abc102_c (arc100_a)

---
+++ arc100_a/src/main.rs
@@ -0,0 +1,35 @@
+fn read<T: std::str::FromStr>(
+    stdin_lock: &mut std::io::StdinLock,
+    buf: &mut Vec<u8>,
+    delimiter: u8,
+) -> T {
+    buf.clear();
+    let l = std::io::BufRead::read_until(stdin_lock, delimiter, buf).unwrap();
+    buf.truncate(l - 1); // remove delimiter
+    let s = unsafe { std::str::from_utf8_unchecked(&buf) };
+    s.parse().unwrap_or_else(|_| panic!("read"))
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let mut buf: Vec<u8> = Vec::new();
+    let n: usize = read(&mut stdin_lock, &mut buf, b'\n');
+    let mut av = vec![0i32; n];
+    for i in 0..n - 1 {
+        av[i] = read(&mut stdin_lock, &mut buf, b' ');
+    }
+    av[n - 1] = read(&mut stdin_lock, &mut buf, b'\n');
+
+    let mut bv = vec![0i32; n];
+    for i in 0..n {
+        bv[i] = av[i] - (i as i32 + 1);
+    }
+    bv.sort();
+    let m = bv[n / 2];
+    let mut ans = 0i32;
+    for i in 0..n {
+        ans += (av[i] - (m + (i as i32 + 1))).abs();
+    }
+    println!("{}", ans);
+}
