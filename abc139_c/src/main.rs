a99f9371ee06e2ae9b433b862022eb3ac6a0d7fc
bouzuya <m@bouzuya.net>
2019-09-01 21:13:27 +0900

    abc139_c

---
+++ abc139_c/src/main.rs
@@ -0,0 +1,37 @@
+use std::cmp::max;
+
+fn read<T: std::str::FromStr>(
+    stdin_lock: &mut std::io::StdinLock,
+    buf: &mut Vec<u8>,
+    delimiter: u8,
+) -> T {
+    buf.clear();
+    let l = std::io::BufRead::read_until(stdin_lock, delimiter, buf).unwrap();
+    buf.truncate(l - 1); // remove delimiter
+    let s = unsafe { std::str::from_utf8_unchecked(&buf) };
+    s.parse().unwrap_or_else(|_| panic!("read"))
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let mut buf: Vec<u8> = Vec::new();
+    let n: usize = read(&mut stdin_lock, &mut buf, b'\n');
+    let mut hv = vec![0i32; n];
+    for i in 0..n - 1 {
+        hv[i] = read(&mut stdin_lock, &mut buf, b' ');
+    }
+    hv[n - 1] = read(&mut stdin_lock, &mut buf, b'\n');
+
+    let mut ans = 0;
+    let mut count = 0;
+    for i in 0..n - 1 {
+        if hv[i] >= hv[i + 1] {
+            count += 1;
+            ans = max(ans, count);
+        } else {
+            count = 0;
+        }
+    }
+    println!("{}", ans);
+}
