7b03b592260239d034bd9d8502060efc9cddaa23
bouzuya <m@bouzuya.net>
2019-09-01 21:31:53 +0900

    abc139_d

---
+++ abc139_d/src/main.rs
@@ -0,0 +1,23 @@
+fn read<T: std::str::FromStr>(
+    stdin_lock: &mut std::io::StdinLock,
+    buf: &mut Vec<u8>,
+    delimiter: u8,
+) -> T {
+    buf.clear();
+    let l = std::io::BufRead::read_until(stdin_lock, delimiter, buf).unwrap();
+    buf.truncate(l - 1); // remove delimiter
+    let s = unsafe { std::str::from_utf8_unchecked(&buf) };
+    s.parse().unwrap_or_else(|_| panic!("read"))
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let mut buf: Vec<u8> = Vec::new();
+    let n: usize = read(&mut stdin_lock, &mut buf, b'\n');
+    let mut ans = 0;
+    for i in 1..n + 1 {
+        ans += i - 1;
+    }
+    println!("{}", ans);
+}
