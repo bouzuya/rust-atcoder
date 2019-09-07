ee0f68daf3dbb2fd8af5ef8b025bb60c15dc432d
bouzuya <m@bouzuya.net>
2019-09-07 21:15:26 +0900

    abc140_b

---
+++ abc140_b/src/main.rs
@@ -0,0 +1,42 @@
+fn read<T: std::str::FromStr>(
+    stdin_lock: &mut std::io::StdinLock,
+    buf: &mut Vec<u8>,
+    delimiter: u8,
+) -> T {
+    buf.clear();
+    let l = std::io::BufRead::read_until(stdin_lock, delimiter, buf).unwrap();
+    buf.truncate(l - 1); // remove delimiter
+    let s = unsafe { std::str::from_utf8_unchecked(&buf) };
+    s.parse().unwrap_or_else(|_| panic!("read"))
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let mut buf: Vec<u8> = Vec::new();
+    let n: usize = read(&mut stdin_lock, &mut buf, b'\n');
+    let mut av = vec![0usize; n];
+    for i in 0..n - 1 {
+        av[i] = read(&mut stdin_lock, &mut buf, b' ');
+    }
+    av[n - 1] = read(&mut stdin_lock, &mut buf, b'\n');
+    let mut bv = vec![0i32; n];
+    for i in 0..n - 1 {
+        bv[i] = read(&mut stdin_lock, &mut buf, b' ');
+    }
+    bv[n - 1] = read(&mut stdin_lock, &mut buf, b'\n');
+    let mut cv = vec![0i32; n - 1];
+    for i in 0..n - 2 {
+        cv[i] = read(&mut stdin_lock, &mut buf, b' ');
+    }
+    cv[n - 2] = read(&mut stdin_lock, &mut buf, b'\n');
+
+    let mut ans = 0;
+    for i in 0..n - 1 {
+        let curr = av[i] - 1;
+        let next = av[i + 1] - 1;
+        ans += bv[curr] + if next == curr + 1 { cv[curr] } else { 0 };
+    }
+    ans += bv[av[n - 1] - 1];
+    println!("{}", ans);
+}
