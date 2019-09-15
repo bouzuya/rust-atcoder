93bad213aa4b9ed9f3e36e1c25f7bfdaf3706c31
bouzuya <m@bouzuya.net>
2019-09-15 21:26:36 +0900

    abc141_d

---
+++ abc141_d/src/main.rs
@@ -0,0 +1,42 @@
+use std::collections::BinaryHeap;
+
+fn read<T: std::str::FromStr>(
+    stdin_lock: &mut std::io::StdinLock,
+    buf: &mut Vec<u8>,
+    delimiter: u8,
+) -> T {
+    buf.clear();
+    let l = std::io::BufRead::read_until(stdin_lock, delimiter, buf).unwrap();
+    buf.truncate(l - 1); // remove delimiter
+    let s = unsafe { std::str::from_utf8_unchecked(&buf) };
+    s.parse().unwrap_or_else(|_| panic!("read"))
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let mut buf: Vec<u8> = Vec::new();
+    let n: usize = read(&mut stdin_lock, &mut buf, b' ');
+    let m: usize = read(&mut stdin_lock, &mut buf, b'\n');
+    let mut av = vec![0usize; n];
+    for i in 0..n - 1 {
+        av[i] = read(&mut stdin_lock, &mut buf, b' ');
+    }
+    av[n - 1] = read(&mut stdin_lock, &mut buf, b'\n');
+
+    let mut heap = BinaryHeap::new();
+    for i in 0..n {
+        heap.push(av[i]);
+    }
+
+    for _ in 0..m {
+        let a = heap.pop().unwrap();
+        heap.push(a / 2);
+    }
+
+    let mut ans = 0;
+    while !heap.is_empty() {
+        ans += heap.pop().unwrap();
+    }
+    println!("{}", ans);
+}
