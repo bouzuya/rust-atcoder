497849c9220d40a384f74ddce67219cee52e7c92
bouzuya <m@bouzuya.net>
2019-09-28 21:16:36 +0900

    abc142_c

---
+++ abc142_c/src/main.rs
@@ -0,0 +1,34 @@
+fn read<T: std::str::FromStr>(
+    stdin_lock: &mut std::io::StdinLock,
+    buf: &mut Vec<u8>,
+    delimiter: u8,
+) -> T {
+    buf.clear();
+    let l = std::io::BufRead::read_until(stdin_lock, delimiter, buf).unwrap();
+    buf.truncate(l - 1); // remove delimiter
+    let s = unsafe { std::str::from_utf8_unchecked(&buf) };
+    s.parse().unwrap_or_else(|_| panic!("read"))
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let mut buf: Vec<u8> = Vec::new();
+    let n: usize = read(&mut stdin_lock, &mut buf, b'\n');
+    let mut av = vec![0i32; n];
+    for i in 0..n - 1 {
+        av[i] = read(&mut stdin_lock, &mut buf, b' ');
+    }
+    av[n - 1] = read(&mut stdin_lock, &mut buf, b'\n');
+
+    let mut cv = vec![(0i32, 0usize); n];
+    for i in 0..n {
+        cv[i].0 = av[i];
+        cv[i].1 = i + 1;
+    }
+    cv.sort();
+    for i in 0..n - 1 {
+        print!("{} ", cv[i].1)
+    }
+    println!("{}", cv[n - 1].1)
+}
