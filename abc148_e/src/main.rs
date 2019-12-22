7e610f883de7a5586fcdcbafcf4718550f3792bd
bouzuya <m@bouzuya.net>
2019-12-22 22:14:29 +0900

    abc148_e WA

---
+++ abc148_e/src/main.rs
@@ -0,0 +1,24 @@
+fn read<T: std::str::FromStr>(
+    stdin_lock: &mut std::io::StdinLock,
+    buf: &mut Vec<u8>,
+    delimiter: u8,
+) -> T {
+    buf.clear();
+    let l = std::io::BufRead::read_until(stdin_lock, delimiter, buf).unwrap();
+    buf.truncate(l - 1); // remove delimiter
+    let s = unsafe { std::str::from_utf8_unchecked(&buf) };
+    s.parse().unwrap_or_else(|_| panic!("read"))
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let mut buf: Vec<u8> = Vec::new();
+    let n: usize = read(&mut stdin_lock, &mut buf, b'\n');
+
+    if n % 2 != 0 {
+        println!("{}", 0);
+    } else {
+        unimplemented!();
+    };
+}
