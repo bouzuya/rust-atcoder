5f0bb5747a5fb326d9807aa06ebd77e8810b55c4
bouzuya <m@bouzuya.net>
2020-01-24 06:45:37 +0900

    abc152_e

--- abc152_e/src/main.rs
+++ abc152_e/src/main.rs
@@ -1,3 +1,4 @@
+use std::convert::From;
 use std::ops;
 
 fn read<T: std::str::FromStr>(
@@ -18,9 +19,13 @@ const MODP: u64 = 1_000_000_007;
 struct ModU64(u64);
 
 impl ModU64 {
+    fn new(x: u64) -> Self {
+        ModU64(x % MODP)
+    }
+
     fn pow(self, mut x: u64) -> Self {
         let mut b = self;
-        let mut a = ModU64(1);
+        let mut a: ModU64 = 1.into();
         while x > 1 {
             if x & 1 == 1 {
                 a = a * b;
@@ -35,11 +40,17 @@ impl ModU64 {
     }
 }
 
+impl From<u64> for ModU64 {
+    fn from(x: u64) -> Self {
+        ModU64::new(x)
+    }
+}
+
 impl ops::Add<ModU64> for ModU64 {
     type Output = ModU64;
 
     fn add(self, rhs: ModU64) -> Self::Output {
-        ModU64((self.0 + rhs.0) % MODP)
+        ModU64::new(self.0 + rhs.0)
     }
 }
 
@@ -47,7 +58,7 @@ impl ops::Mul<ModU64> for ModU64 {
     type Output = ModU64;
 
     fn mul(self, rhs: ModU64) -> Self::Output {
-        ModU64(self.0 % MODP * rhs.0 % MODP)
+        ModU64::new(self.0 * rhs.0)
     }
 }
 
@@ -73,25 +84,24 @@ fn main() {
     }
     av[n - 1] = read(&mut stdin_lock, &mut buf, b'\n');
 
-    let mut pv = vec![0u64; 1_000_000 + 1];
-    for i in 0..pv.len() {
-        pv[i] = i as u64;
-    }
-    for i in 2..pv.len() {
-        let mut j = i;
-        while j < pv.len() {
-            if pv[j] % i as u64 == 0 {
-                pv[j] = i as u64;
-            }
+    let mut sieve: Vec<u64> = (0..1_000_000 + 1).map(|i| i as u64).collect();
+    for ri in 2..sieve.len() {
+        let i = sieve.len() - ri + 1;
+        // for j in ((i + 1)..sieve.len()).step_by(i) { ... }
+        let mut j = i + i;
+        while j < sieve.len() {
+            sieve[j] = i as u64;
             j += i;
         }
     }
+
+    assert_eq!(sieve[0..10], [0, 1, 2, 3, 2, 5, 2, 7, 2, 3]);
 
     let mut apv: Vec<HashMap<u64, u64>> = vec![HashMap::new(); n];
     for i in 0..av.len() {
         let mut j = av[i];
         while j != 1 {
-            let p = pv[j as usize];
+            let p = sieve[j as usize];
             *apv[i].entry(p).or_insert(0) += 1;
             j /= p;
         }
@@ -99,25 +109,25 @@ fn main() {
 
     let mut l: HashMap<u64, u64> = HashMap::new();
     for a in apv.iter() {
-        for (&k, &v) in a.iter() {
-            if let Some(lv) = l.get_mut(&k) {
-                *lv = max(*lv, v);
+        for (k, v) in a.iter() {
+            if let Some(lv) = l.get_mut(k) {
+                *lv = max(*lv, *v);
                 continue;
             }
-            l.insert(k, v);
+            l.insert(*k, *v);
         }
     }
 
-    let mut lcm = ModU64(1);
+    let mut lcm: ModU64 = 1.into();
     for (&k, &v) in l.iter() {
         for _ in 0..v {
-            lcm = lcm * ModU64(k as u64);
+            lcm = lcm * (k as u64).into();
         }
     }
 
-    let mut ans = ModU64(0);
+    let mut ans: ModU64 = 0.into();
     for &a in av.iter() {
-        let b = lcm / ModU64(a);
+        let b = lcm / a.into();
         ans = ans + b;
     }
     println!("{}", ans.0);
