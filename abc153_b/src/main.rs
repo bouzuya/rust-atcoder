f1d5625cdb1c2596d97424210f269d28af42d931
bouzuya <m@bouzuya.net>
2020-01-26 21:07:44 +0900

    abc153_b

---
+++ abc153_b/src/main.rs
@@ -0,0 +1,34 @@
+fn read<T: std::str::FromStr>(
+    stdin_lock: &mut std::io::StdinLock,
+    buf: &mut Vec<u8>,
+    delimiter: u8,
+) -> T {
+    buf.clear();
+    let l = std::io::BufRead::read_until(stdin_lock, delimiter, buf).unwrap();
+    buf.truncate(l - 1); // remove delimiter
+    let s = unsafe { std::str::from_utf8_unchecked(&buf) };
+    s.parse().unwrap_or_else(|_| panic!("read"))
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let mut buf: Vec<u8> = Vec::new();
+
+    // input
+    let h: usize = read(&mut stdin_lock, &mut buf, b' ');
+    let n: usize = read(&mut stdin_lock, &mut buf, b'\n');
+    let mut av = vec![0usize; n];
+    for i in 0..n - 1 {
+        av[i] = read(&mut stdin_lock, &mut buf, b' ');
+    }
+    av[n - 1] = read(&mut stdin_lock, &mut buf, b'\n');
+
+    let mut sum_a = 0;
+    for a in av {
+        sum_a += a;
+    }
+
+    let ans = if h <= sum_a { "Yes" } else { "No" };
+    println!("{}", ans);
+}
