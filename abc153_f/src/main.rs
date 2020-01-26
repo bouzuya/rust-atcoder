6e0ecb71331ffb06629a237dbc60b4d0e8bdc881
bouzuya <m@bouzuya.net>
2020-01-26 22:22:05 +0900

    abc153_f TLE & WA

---
+++ abc153_f/src/main.rs
@@ -0,0 +1,52 @@
+fn read<T: std::str::FromStr>(
+    stdin_lock: &mut std::io::StdinLock,
+    buf: &mut Vec<u8>,
+    delimiter: u8,
+) -> T {
+    buf.clear();
+    let l = std::io::BufRead::read_until(stdin_lock, delimiter, buf).unwrap();
+    buf.truncate(l - 1); // remove delimiter
+    let s = unsafe { std::str::from_utf8_unchecked(&buf) };
+    s.parse().unwrap_or_else(|_| panic!("read"))
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let mut buf: Vec<u8> = Vec::new();
+
+    // input
+    let n: usize = read(&mut stdin_lock, &mut buf, b' ');
+    let d: usize = read(&mut stdin_lock, &mut buf, b' ');
+    let a: usize = read(&mut stdin_lock, &mut buf, b'\n');
+    let mut xhv = vec![(0usize, 0usize); n];
+    for i in 0..n {
+        xhv[i].0 = read(&mut stdin_lock, &mut buf, b' ');
+        xhv[i].1 = read(&mut stdin_lock, &mut buf, b'\n');
+    }
+
+    xhv.sort();
+
+    let mut ans = 0;
+    let mut i = 0;
+    while i < n {
+        let (x, h) = xhv[i];
+        let c = h / a;
+        let mut streak = true;
+        let mut next_i = i + 1;
+        for j in i + 1..xhv.len() {
+            let (jx, jh) = xhv[j];
+            if jx > x + 2 * d {
+                break;
+            }
+            xhv[j].1 = if jh < c * a { 0 } else { jh - c * a };
+            if streak && xhv[j].1 > 0 {
+                streak = false;
+                next_i = j;
+            }
+        }
+        i = next_i;
+        ans += c;
+    }
+    println!("{}", ans);
+}
