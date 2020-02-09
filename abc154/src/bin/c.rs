dab6fc1744a1c5482e675b04cc00dcb8b1ee0fb9
bouzuya <m@bouzuya.net>
2020-02-10 06:41:29 +0900

    abc154_c use sort & dedup

--- abc154/src/bin/c.rs
+++ abc154/src/bin/c.rs
@@ -6,13 +6,7 @@ fn main() {
         mut av: [usize; n]
     };
     av.sort();
-    let mut same = false;
-    for i in 1..n {
-        if av[i - 1] == av[i] {
-            same = true;
-            break;
-        }
-    }
-    let ans = if !same { "YES" } else { "NO" };
+    av.dedup();
+    let ans = if av.len() == n { "YES" } else { "NO" };
     println!("{}", ans);
 }
