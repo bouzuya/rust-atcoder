9e39219e26323028d84d38cc6a9a4111b6ee91eb
bouzuya <m@bouzuya.net>
2020-02-12 18:51:09 +0900

    abc154_e digit dp

--- abc154/src/bin/e.rs
+++ abc154/src/bin/e.rs
@@ -3,19 +3,33 @@ use proconio::marker::Bytes;
 
 fn main() {
     input! {
-        mut n: Bytes,
-        k: usize
+        mut s: Bytes,
+        nzc: usize
     };
-    for i in 0..n.len() {
-        n[i] -= b'0';
+    for i in 0..s.len() {
+        s[i] -= b'0';
     }
-    let ans = match k {
-        1 => 9 * (n.len() - 1) + n[0] as usize,
-        2 => {
-            let c = 0;
-            (n.len() * 2) * 81 + (n.len() * 9) * (n[0] as usize - 1)
+    let mut dp = vec![vec![vec![0usize; 2]; nzc + 1]; s.len() + 1];
+    dp[0][0][0] = 1;
+    for i in 0..s.len() {
+        let d = s[i];
+        for j in 0..=nzc {
+            for k in 0..=1 {
+                for l in 0..=9 {
+                    let ni = i + 1;
+                    let nj = if l != 0 { j + 1 } else { j };
+                    let nk = if k == 0 && l < d { 1 } else { k };
+                    if nj > nzc {
+                        continue;
+                    }
+                    if k == 0 && l > d {
+                        continue;
+                    }
+                    dp[ni][nj][nk] += dp[i][j][k];
+                }
+            }
         }
-        _ => 0,
-    };
+    }
+    let ans = dp[s.len()][nzc][0] + dp[s.len()][nzc][1];
     println!("{}", ans);
 }
