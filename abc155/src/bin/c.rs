2037344b2ed3eb2d467fb89f412ce3ebcf39b1be
bouzuya <m@bouzuya.net>
2020-02-18 06:23:50 +0900

    abc155_c

--- abc155/src/bin/c.rs
+++ abc155/src/bin/c.rs
@@ -1,5 +1,5 @@
 use proconio::input;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 fn main() {
     input! {
@@ -7,19 +7,12 @@ fn main() {
         sv: [String; n]
     };
 
-    let mut map = HashMap::new();
+    let mut om: BTreeMap<String, usize> = BTreeMap::new();
     for s in sv {
-        *map.entry(s).or_insert(0) += 1;
+        *om.entry(s).or_insert(0) += 1;
     }
-    let max = *map.values().max().unwrap();
-    let mut maxs = Vec::new();
-    for (s, count) in map {
-        if count == max {
-            maxs.push(s);
-        }
-    }
-    maxs.sort();
-    for s in maxs {
+    let max_v = *om.values().max().unwrap();
+    for s in om.into_iter().filter(|&(_, v)| v == max_v).map(|(k, _)| k) {
         println!("{}", s);
     }
 }
