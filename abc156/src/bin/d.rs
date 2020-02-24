c18584f7f0bdbeb21164ef93788e6ed41ed39c52
bouzuya <m@bouzuya.net>
2020-02-24 09:01:19 +0900

    abc156_d

--- abc156/src/bin/d.rs
+++ abc156/src/bin/d.rs
@@ -1,90 +1,12 @@
+use self::mod_u32::ModU32;
 use proconio::input;
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct ModI64(i64);
-
-const MODP: i64 = 1_000_000_007;
-
-impl ModI64 {
-    pub fn inv(self) -> Self {
-        self.pow(MODP as u32 - 2)
-    }
-
-    pub fn new(x: i64) -> Self {
-        Self(x % MODP)
-    }
-
-    pub fn pow(self, exp: u32) -> Self {
-        let mut b: ModI64 = self;
-        let mut a: ModI64 = 1i64.into();
-        let mut x = exp;
-        while x > 1 {
-            if x & 1 == 1 {
-                a = a * b;
-            }
-            x = x / 2;
-            b = b * b;
-        }
-        if x == 1 {
-            a = a * b;
-        }
-        a
-    }
-}
-
-impl From<i64> for ModI64 {
-    fn from(x: i64) -> Self {
-        ModI64::new(x)
-    }
-}
-
-impl std::fmt::Display for ModI64 {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-impl std::ops::Add<ModI64> for ModI64 {
-    type Output = ModI64;
-
-    fn add(self, rhs: ModI64) -> Self::Output {
-        (self.0 + rhs.0).into()
-    }
-}
-
-impl std::ops::Sub<ModI64> for ModI64 {
-    type Output = ModI64;
-
-    fn sub(self, rhs: ModI64) -> Self::Output {
-        let x = self.0 - rhs.0;
-        (if x.is_negative() { MODP + x } else { x }).into()
-    }
-}
-
-impl std::ops::Mul<ModI64> for ModI64 {
-    type Output = ModI64;
-
-    fn mul(self, rhs: ModI64) -> Self::Output {
-        (self.0 * rhs.0).into()
-    }
-}
-
-impl std::ops::Div<ModI64> for ModI64 {
-    type Output = ModI64;
-
-    fn div(self, rhs: ModI64) -> Self::Output {
-        self * rhs.inv()
-    }
-}
-
-fn comb(n: i64, r: i64) -> ModI64 {
-    let mut a = ModI64::new(1);
-    for i in (n - r + 1)..=n {
-        a = a * ModI64::new(i);
-    }
-    let mut b = ModI64::new(1);
-    for i in 1..=r {
-        b = b * ModI64::new(i);
+fn choose(n: usize, r: usize) -> ModU32 {
+    let mut a = ModU32::from(n);
+    let mut b = ModU32::from(r);
+    for i in 1..r {
+        a *= ModU32::from(n - i);
+        b *= ModU32::from(i);
     }
     a / b
 }
@@ -96,11 +18,172 @@ fn main() {
         b: usize,
     };
 
-    if n == 2 {
-        println!("{}", 0);
-    } else {
-        let u = ModI64::new(2).pow(n as u32) - ModI64::new(1);
-        let ans = u - comb(n as i64, a as i64) - comb(n as i64, b as i64);
-        println!("{}", ans);
+    let u = ModU32::new(2).pow(n as u32) - ModU32::new(1);
+    let ans = u - choose(n, a) - choose(n, b);
+    println!("{}", ans);
+}
+
+mod mod_u32 {
+    #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+    pub struct ModU32(u32);
+
+    const MODP: u32 = 1_000_000_007;
+
+    impl ModU32 {
+        pub fn inv(self) -> Self {
+            if self.0 == 0 {
+                panic!()
+            };
+            self.pow(MODP - 2)
+        }
+
+        pub fn new(x: u64) -> Self {
+            Self((x % (MODP as u64)) as u32)
+        }
+
+        pub fn pow(self, exp: u32) -> Self {
+            let mut b: ModU32 = self;
+            let mut a: ModU32 = Self::new(1);
+            let mut x = exp;
+            while x > 1 {
+                if x & 1 == 1 {
+                    a *= b;
+                }
+                x /= 2;
+                b *= b;
+            }
+            if x == 1 {
+                a *= b;
+            }
+            a
+        }
+    }
+
+    impl From<i32> for ModU32 {
+        fn from(x: i32) -> Self {
+            ModU32::from(x as i64)
+        }
+    }
+
+    impl From<i64> for ModU32 {
+        fn from(x: i64) -> Self {
+            let n = x % (MODP as i64);
+            ModU32::new(if n.is_negative() { MODP as i64 + n } else { n } as u64)
+        }
+    }
+
+    impl From<u32> for ModU32 {
+        fn from(x: u32) -> Self {
+            ModU32::from(x as u64)
+        }
+    }
+
+    impl From<u64> for ModU32 {
+        fn from(x: u64) -> Self {
+            ModU32::new(x)
+        }
+    }
+
+    impl From<usize> for ModU32 {
+        fn from(x: usize) -> Self {
+            ModU32::from(x as u64)
+        }
+    }
+
+    impl Into<i32> for ModU32 {
+        fn into(self) -> i32 {
+            self.0 as i32
+        }
+    }
+
+    impl Into<i64> for ModU32 {
+        fn into(self) -> i64 {
+            self.0 as i64
+        }
+    }
+
+    impl Into<u32> for ModU32 {
+        fn into(self) -> u32 {
+            self.0
+        }
+    }
+
+    impl Into<u64> for ModU32 {
+        fn into(self) -> u64 {
+            self.0 as u64
+        }
+    }
+
+    impl Into<usize> for ModU32 {
+        fn into(self) -> usize {
+            self.0 as usize
+        }
+    }
+
+    impl std::fmt::Display for ModU32 {
+        fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+            write!(f, "{}", self.0)
+        }
+    }
+
+    impl std::ops::Add<ModU32> for ModU32 {
+        type Output = ModU32;
+
+        fn add(self, rhs: ModU32) -> Self::Output {
+            (self.0 as u64 + rhs.0 as u64).into()
+        }
+    }
+
+    impl std::ops::AddAssign<ModU32> for ModU32 {
+        fn add_assign(&mut self, rhs: ModU32) {
+            self.0 = (*self + rhs).0
+        }
+    }
+
+    impl std::ops::Sub<ModU32> for ModU32 {
+        type Output = ModU32;
+
+        fn sub(self, rhs: ModU32) -> Self::Output {
+            (if self.0 < rhs.0 {
+                self.0 + (MODP - rhs.0)
+            } else {
+                self.0 - rhs.0
+            })
+            .into()
+        }
+    }
+
+    impl std::ops::SubAssign<ModU32> for ModU32 {
+        fn sub_assign(&mut self, rhs: ModU32) {
+            self.0 = (*self - rhs).0
+        }
+    }
+
+    impl std::ops::Mul<ModU32> for ModU32 {
+        type Output = ModU32;
+
+        fn mul(self, rhs: ModU32) -> Self::Output {
+            Self::new((self.0 as u64) * (rhs.0 as u64))
+        }
+    }
+
+    impl std::ops::MulAssign<ModU32> for ModU32 {
+        fn mul_assign(&mut self, rhs: ModU32) {
+            self.0 = (*self * rhs).0
+        }
+    }
+
+    impl std::ops::Div<ModU32> for ModU32 {
+        type Output = ModU32;
+
+        fn div(self, rhs: ModU32) -> Self::Output {
+            self * rhs.inv()
+        }
+    }
+
+    impl std::ops::DivAssign<ModU32> for ModU32 {
+        fn div_assign(&mut self, rhs: ModU32) {
+            self.0 = (*self / rhs).0
+        }
     }
 }
