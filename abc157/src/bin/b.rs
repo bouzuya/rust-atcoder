4a5af32cff90e4ef2bf1e83ead642890a2376771
bouzuya <m@bouzuya.net>
2020-03-02 07:02:15 +0900

    abc157_b

--- abc157/src/bin/b.rs
+++ abc157/src/bin/b.rs
@@ -6,23 +6,12 @@ fn main() {
         n: usize,
         bv: [usize; n]
     };
-    let mut cvv = vec![vec![false; 3]; 3];
+    let mut ans = false;
     for i in 0..3 {
-        for j in 0..3 {
-            for &b in bv.iter() {
-                if avv[i][j] == b {
-                    cvv[i][j] = true;
-                }
-            }
-        }
+        ans |= (0..3).all(|j| bv.contains(&avv[i][j]));
+        ans |= (0..3).all(|j| bv.contains(&avv[j][i]));
     }
-    let ans = (cvv[0][0] && cvv[0][1] && cvv[0][2])
-        || (cvv[1][0] && cvv[1][1] && cvv[1][2])
-        || (cvv[2][0] && cvv[2][1] && cvv[2][2])
-        || (cvv[0][0] && cvv[1][0] && cvv[2][0])
-        || (cvv[0][1] && cvv[1][1] && cvv[2][1])
-        || (cvv[0][2] && cvv[1][2] && cvv[2][2])
-        || (cvv[0][0] && cvv[1][1] && cvv[2][2])
-        || (cvv[2][0] && cvv[1][1] && cvv[2][0]);
+    ans |= (0..3).all(|i| bv.contains(&avv[i][i]));
+    ans |= (0..3).all(|i| bv.contains(&avv[3 - (i + 1)][i]));
     println!("{}", if ans { "Yes" } else { "No" });
 }
