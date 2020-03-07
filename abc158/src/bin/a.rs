38931be0ea5c2202c2473f23bedee9688bc292bc
bouzuya <m@bouzuya.net>
2020-03-08 06:38:51 +0900

    abc158_a

--- abc158/src/bin/a.rs
+++ abc158/src/bin/a.rs
@@ -4,7 +4,10 @@ fn main() {
     input! {
         s: String
     };
-    let ac = s.chars().filter(|&c| c == 'A').fold(0, |acc, _| acc + 1);
-    let ans = if 1 <= ac && ac < 3 { "Yes" } else { "No" };
+    let ans = if s.contains("A") && s.contains("B") {
+        "Yes"
+    } else {
+        "No"
+    };
     println!("{}", ans);
 }
