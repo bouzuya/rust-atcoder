5fd97c96f3400a9d47c1e8f2f42fa41ad75482d6
bouzuya <m@bouzuya.net>
2020-03-08 09:43:43 +0900

    abc158_d VecDeque

--- abc158/src/bin/d.rs
+++ abc158/src/bin/d.rs
@@ -1,12 +1,14 @@
 use proconio::input;
+use proconio::marker::Chars;
+use std::collections::VecDeque;
 
 fn main() {
     input! {
-        s: String,
+        s: Chars,
         q: usize,
     }
     let mut r = false;
-    let mut v = vec![vec![]; 2];
+    let mut d = VecDeque::from(s);
     for _ in 0..q {
         input! { t: usize }
         match t {
@@ -14,16 +16,30 @@ fn main() {
             2 => {
                 input! { f: usize, c: char }
                 match f {
-                    1 => v[if !r { 0 } else { 1 }].push(c),
-                    2 => v[if !r { 1 } else { 0 }].push(c),
+                    1 => {
+                        if !r {
+                            d.push_front(c)
+                        } else {
+                            d.push_back(c)
+                        }
+                    }
+                    2 => {
+                        if !r {
+                            d.push_back(c)
+                        } else {
+                            d.push_front(c)
+                        }
+                    }
                     _ => unreachable!(),
                 }
             }
             _ => unreachable!(),
         }
     }
-    let f: String = v[if !r { 0 } else { 1 }].iter().rev().collect();
-    let c: String = if !r { s } else { s.chars().rev().collect() };
-    let b: String = v[if !r { 1 } else { 0 }].iter().collect();
-    println!("{}{}{}", f, c, b);
+    let ans: String = if !r {
+        d.iter().collect()
+    } else {
+        d.iter().rev().collect()
+    };
+    println!("{}", ans);
 }
