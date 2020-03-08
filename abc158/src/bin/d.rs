9f9f9dd66cc97fb5aca5899e0d1908b9aa84ca6a
bouzuya <m@bouzuya.net>
2020-03-08 09:29:30 +0900

    abc158_d rev flag

--- abc158/src/bin/d.rs
+++ abc158/src/bin/d.rs
@@ -5,33 +5,25 @@ fn main() {
         s: String,
         q: usize,
     }
-    let mut d = 0;
+    let mut r = false;
     let mut v = vec![vec![]; 2];
     for _ in 0..q {
         input! { t: usize }
         match t {
-            1 => {
-                d = if d == 0 { 1 } else { 0 };
-            }
+            1 => r = !r,
             2 => {
                 input! { f: usize, c: char }
                 match f {
-                    1 => v[d].push(c),
-                    2 => v[if d == 0 { 1 } else { 0 }].push(c),
+                    1 => v[if !r { 0 } else { 1 }].push(c),
+                    2 => v[if !r { 1 } else { 0 }].push(c),
                     _ => unreachable!(),
                 }
             }
             _ => unreachable!(),
         }
     }
-    let f: String = v[d].iter().rev().collect();
-    let c: String = if d % 2 == 0 {
-        s.chars().collect()
-    } else {
-        s.chars().rev().collect()
-    };
-    let b: String = v[if d == 0 { 1 } else { 0 }].iter().collect();
-    print!("{}", f);
-    print!("{}", c);
-    println!("{}", b);
+    let f: String = v[if !r { 0 } else { 1 }].iter().rev().collect();
+    let c: String = if !r { s } else { s.chars().rev().collect() };
+    let b: String = v[if !r { 1 } else { 0 }].iter().collect();
+    println!("{}{}{}", f, c, b);
 }
