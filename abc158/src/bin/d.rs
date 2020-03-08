d162e96522e32cf7a1556202e3ed0115da081cb9
bouzuya <m@bouzuya.net>
2020-03-08 09:24:58 +0900

    abc158_d proconio

--- abc158/src/bin/d.rs
+++ abc158/src/bin/d.rs
@@ -1,50 +1,27 @@
-fn read<T: std::str::FromStr>(
-    stdin_lock: &mut std::io::StdinLock,
-    buf: &mut Vec<u8>,
-    delimiter: u8,
-) -> T {
-    buf.clear();
-    let l = std::io::BufRead::read_until(stdin_lock, delimiter, buf).unwrap();
-    buf.truncate(l - 1); // remove delimiter
-    let s = unsafe { std::str::from_utf8_unchecked(&buf) };
-    s.parse().unwrap_or_else(|_| panic!("read"))
-}
+use proconio::input;
 
 fn main() {
-    let stdin = std::io::stdin();
-    let mut stdin_lock = stdin.lock();
-    let mut buf: Vec<u8> = Vec::new();
-    let s: String = read(&mut stdin_lock, &mut buf, b'\n');
-    let q: usize = read(&mut stdin_lock, &mut buf, b'\n');
-    let mut qv: Vec<String> = Vec::new();
-    for _ in 1..=q - 1 {
-        let x: String = read(&mut stdin_lock, &mut buf, b'\n');
-        qv.push(x);
+    input! {
+        s: String,
+        q: usize,
     }
-    {
-        buf.clear();
-        let mut x = String::new();
-        std::io::BufRead::read_line(&mut stdin_lock, &mut x).unwrap();
-        qv.push(x);
-    }
-    let mut d = 0; // 0 なら 先頭→末尾、 1 なら先頭←末尾
-    let mut v = vec![vec![]; 2]; // 0 (先頭側) は逆順に格納する
-    for s in qv.iter() {
-        let cv: Vec<char> = s.chars().collect();
-        if cv[0] == '1' {
-            // 反転
-            d = if d == 0 { 1 } else { 0 };
-        } else {
-            // 追加
-            let f = cv[2];
-            let c = cv[4];
-            if f == '1' {
-                // 先頭に追加
-                v[d].push(c);
-            } else {
-                // 末尾に追加
-                v[if d == 0 { 1 } else { 0 }].push(c);
+    let mut d = 0;
+    let mut v = vec![vec![]; 2];
+    for _ in 0..q {
+        input! { t: usize }
+        match t {
+            1 => {
+                d = if d == 0 { 1 } else { 0 };
             }
+            2 => {
+                input! { f: usize, c: char }
+                match f {
+                    1 => v[d].push(c),
+                    2 => v[if d == 0 { 1 } else { 0 }].push(c),
+                    _ => unreachable!(),
+                }
+            }
+            _ => unreachable!(),
         }
     }
     let f: String = v[d].iter().rev().collect();
