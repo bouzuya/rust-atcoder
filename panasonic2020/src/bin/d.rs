4726947dd454fee56b9103e6fd949708d54ab9c9
bouzuya <m@bouzuya.net>
2020-03-15 07:37:18 +0900

    panasonic2020_d

--- panasonic2020/src/bin/d.rs
+++ panasonic2020/src/bin/d.rs
@@ -1,18 +1,18 @@
 use proconio::input;
 
-fn f(s: &mut String, n: usize, mb: u8) {
+fn dfs(s: &mut String, n: usize, mb: u8) {
     if s.len() == n {
         println!("{}", s);
         return;
     }
     for b in b'a'..=mb {
         s.push(b as char);
-        f(s, n, mb + if b == mb { 1 } else { 0 });
+        dfs(s, n, mb + if b == mb { 1 } else { 0 });
         s.pop();
     }
 }
 
 fn main() {
     input! { n: usize }
-    f(&mut String::new(), n, b'a');
+    dfs(&mut String::new(), n, b'a');
 }
