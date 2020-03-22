07f44a3e0eca4129647c2486c82693596a7fb0df
bouzuya <m@bouzuya.net>
2020-03-23 06:28:40 +0900

    abc159_b

--- abc159/src/bin/b.rs
+++ abc159/src/bin/b.rs
@@ -1,29 +1,21 @@
 use proconio::input;
 use proconio::marker::Chars;
 
+fn is_palindrome(s: &[char]) -> bool {
+    for i in 0..s.len() / 2 {
+        if s[i] != s[s.len() - i - 1] {
+            return false;
+        }
+    }
+    return true;
+}
+
 fn main() {
     input! {
         s: Chars
     };
-    for i in 0..(s.len() - 1) / 2 {
-        if s[i] != s[s.len() - i - 1] {
-            println!("No");
-            return;
-        }
-    }
-    let t = &s[0..(s.len() - 1) / 2];
-    for i in 0..(t.len() - 1) / 2 + 1 {
-        if t[i] != t[t.len() - i - 1] {
-            println!("No");
-            return;
-        }
-    }
-    let u = &s[(s.len() + 3) / 2 - 1..s.len()];
-    for i in 0..(u.len() - 1) / 2 + 1 {
-        if u[i] != u[u.len() - i - 1] {
-            println!("No");
-            return;
-        }
-    }
-    println!("Yes");
+    let ans = is_palindrome(&s)
+        && is_palindrome(&s[0..(s.len() - 1) / 2])
+        && is_palindrome(&s[(s.len() + 3) / 2 - 1..s.len()]);
+    println!("{}", if ans { "Yes" } else { "No" });
 }
