1f6af2e4a9913d8da44be6f07476ac8f9b5e6889
bouzuya <m@bouzuya.net>
2020-03-23 06:33:50 +0900

    abc159_b

--- abc159/src/bin/b.rs
+++ abc159/src/bin/b.rs
@@ -2,12 +2,7 @@ use proconio::input;
 use proconio::marker::Chars;
 
 fn is_palindrome(s: &[char]) -> bool {
-    for i in 0..s.len() / 2 {
-        if s[i] != s[s.len() - i - 1] {
-            return false;
-        }
-    }
-    return true;
+    (0..s.len() / 2).all(|i| s[i] == s[s.len() - i - 1])
 }
 
 fn main() {
@@ -15,7 +10,7 @@ fn main() {
         s: Chars
     };
     let ans = is_palindrome(&s)
-        && is_palindrome(&s[0..(s.len() - 1) / 2])
-        && is_palindrome(&s[(s.len() + 3) / 2 - 1..s.len()]);
+        && is_palindrome(&s[..(s.len() - 1) / 2])
+        && is_palindrome(&s[(s.len() + 3) / 2 - 1..]);
     println!("{}", if ans { "Yes" } else { "No" });
 }
