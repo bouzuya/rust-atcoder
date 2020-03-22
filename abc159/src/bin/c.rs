ff3b59110c2cd638baa7c2502c1577e711b92b6b
bouzuya <m@bouzuya.net>
2020-03-23 06:41:43 +0900

    abc159_c

--- abc159/src/bin/c.rs
+++ abc159/src/bin/c.rs
@@ -1,20 +1,6 @@
 use proconio::input;
 
 fn main() {
-    input! {
-        l: usize
-    };
-    let x = l as f64 / 3.0f64;
-    let mut ans: f64 = x * x * x;
-    for a in 1..=l {
-        for b in 1..=a {
-            if l < a + b {
-                continue;
-            }
-            let c = l - a - b;
-            let x = (a * b * c) as f64;
-            ans = if ans < x { x } else { ans };
-        }
-    }
-    println!("{}", ans);
+    input! { l: f64 };
+    println!("{}", (l / 3.0).powi(3));
 }
