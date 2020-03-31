98b575abc9cb694d981651bfc1ee08c8e9ebda0e
bouzuya <m@bouzuya.net>
2020-03-31 21:47:57 +0900

    abc160_e (2)

--- abc160/src/bin/e.rs
+++ abc160/src/bin/e.rs
@@ -15,21 +15,35 @@ fn main() {
     bv.sort_by(|a, b| b.cmp(&a));
     cv.sort_by(|a, b| b.cmp(&a));
 
-    let mut vv = vec![0_usize; x + y + x + y];
+    let mut ans = 0_usize;
     for i in 0..x {
-        vv[i] = av[i];
+        ans += av[i];
     }
     for i in 0..y {
-        vv[x + i] = bv[i];
+        ans += bv[i];
     }
-    for i in 0..std::cmp::min(cv.len(), x + y) {
-        vv[x + y + i] = cv[i];
+    let n_c = std::cmp::min(cv.len(), x + y);
+    for i in 0..n_c {
+        ans += cv[i];
     }
-    vv.sort_by(|a, b| b.cmp(&a));
-
-    let mut ans = 0;
-    for i in 0..x + y {
-        ans += vv[i];
+    let inf = 1_000_000_000 + 1;
+    let mut i_a = x;
+    let mut i_b = y;
+    let mut i_c = n_c;
+    for _ in 0..n_c {
+        let p = if i_a >= 1 { av[i_a - 1] } else { inf };
+        let q = if i_b >= 1 { bv[i_b - 1] } else { inf };
+        let r = if i_c >= 1 { cv[i_c - 1] } else { inf };
+        ans -= if p <= q && p <= r {
+            i_a -= 1;
+            p
+        } else if q <= p && q <= r {
+            i_b -= 1;
+            q
+        } else {
+            i_c -= 1;
+            r
+        };
     }
     println!("{}", ans);
 }
