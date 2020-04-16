8af4efcbbc96b6c10ab839e5860016ee80830cfd
bouzuya <m@bouzuya.net>
2020-04-16 14:36:42 +0900

    abc162_f

--- abc162/src/bin/f.rs
+++ abc162/src/bin/f.rs
@@ -1,42 +1,35 @@
+// Original: https://atcoder.jp/contests/abc162/submissions/11852555
 use proconio::input;
-
-fn dpj(i: usize, j_count: usize) -> Option<usize> {
-    let j0_count = (i / 2 + 1) - 1;
-    if (j0_count..j0_count + 3).contains(&j_count) {
-        Some(j_count - j0_count)
-    } else {
-        None
-    }
-}
 
 fn main() {
     input! {
-        n: usize,
-        a: [i64; n],
-    }
-
-    let inf: i64 = 1_000_000_000_000_000_005_i64;
-    let mut dp = vec![vec![-inf; 3]; n];
-    for i in 0..n {
-        if let Some(j) = dpj(i, 1) {
-            dp[i][j] = a[i];
-        }
-        let j0_count = (i / 2 + 1) - 1;
-        for j_count in j0_count..j0_count + 3 {
-            if let Some(j) = dpj(i, j_count) {
-                for i2 in i + 2..=std::cmp::min(n - 1, i + 4) {
-                    if let Some(j2) = dpj(i2, j_count + 1) {
-                        dp[i2][j2] = std::cmp::max(dp[i2][j2], dp[i][j] + a[i2]);
-                    }
-                }
-            }
-        }
-    }
+        n :usize,
+        av: [i64; n],
+    };
 
     let ans = if n % 2 == 0 {
-        std::cmp::max(dp[n - 1][1], dp[n - 2][0])
+        let mut dp = (0_i64, 0_i64);
+        for (i, &a) in av.iter().enumerate() {
+            if i % 2 == 0 {
+                dp.0 = dp.0 + a;
+            } else {
+                dp.1 = std::cmp::max(dp.0, dp.1 + a);
+            }
+        }
+        dp.1
     } else {
-        dp[n - 1][0]
+        let mut dp = (0_i64, 0_i64, 0_i64);
+        for (i, &a) in av.iter().enumerate() {
+            if i % 2 == 0 {
+                dp.0 = dp.0 + a;
+                if i != 0 {
+                    dp.2 = std::cmp::max(dp.1, dp.2 + a);
+                }
+            } else {
+                dp.1 = std::cmp::max(dp.0, dp.1 + a);
+            }
+        }
+        dp.2
     };
     println!("{}", ans);
 }
