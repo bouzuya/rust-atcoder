ed7e6b4c342137aa214acf850469015947bd9429
bouzuya <m@bouzuya.net>
2020-04-18 09:58:55 +0900

    abc162_f

--- abc162/src/bin/f.rs
+++ abc162/src/bin/f.rs
@@ -1,4 +1,3 @@
-// Original: https://atcoder.jp/contests/abc162/submissions/11852555
 use proconio::input;
 
 fn main() {
@@ -6,30 +5,25 @@ fn main() {
         n :usize,
         av: [i64; n],
     };
-
-    let ans = if n % 2 == 0 {
-        let mut dp = (0_i64, 0_i64);
-        for (i, &a) in av.iter().enumerate() {
-            if i % 2 == 0 {
-                dp.0 = dp.0 + a;
-            } else {
-                dp.1 = std::cmp::max(dp.0, dp.1 + a);
+    let inf = 1_000_000_000_000_000_i64;
+    let m = if n % 2 == 0 { 1 } else { 2 };
+    // dp[i][j] = x
+    // i (0<=i<=n) までで j (0<=j<=m) 個を飛ばしたときの最大値が x
+    let mut dp = vec![vec![-inf; m + 1]; n + 1];
+    dp[0][0] = 0;
+    for (i, &a) in av.iter().enumerate() {
+        for j in 0..=m {
+            // 飛ばす場合
+            if j + 1 <= m {
+                dp[i + 1][j + 1] = std::cmp::max(dp[i + 1][j + 1], dp[i][j]);
             }
+            // 飛ばさない場合
+            dp[i + 1][j] = std::cmp::max(
+                dp[i + 1][j],
+                dp[i][j] + if (i + j) % 2 == 0 { a } else { 0 },
+            );
         }
-        dp.1
-    } else {
-        let mut dp = (0_i64, 0_i64, 0_i64);
-        for (i, &a) in av.iter().enumerate() {
-            if i % 2 == 0 {
-                dp.0 = dp.0 + a;
-                if i != 0 {
-                    dp.2 = std::cmp::max(dp.1, dp.2 + a);
-                }
-            } else {
-                dp.1 = std::cmp::max(dp.0, dp.1 + a);
-            }
-        }
-        dp.2
-    };
+    }
+    let ans = dp[n][m];
     println!("{}", ans);
 }
