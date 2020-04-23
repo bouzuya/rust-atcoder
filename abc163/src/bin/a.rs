14cb0fa7987b647b3ba37c559085b002295b2809
bouzuya <m@bouzuya.net>
2020-04-24 06:19:26 +0900

    abc163_a

--- abc163/src/bin/a.rs
+++ abc163/src/bin/a.rs
@@ -4,6 +4,6 @@ fn main() {
     input! {
         r: f64,
     };
-    let ans = 2_f64 * 3.14159265358979323846264338327950288_f64 * r;
+    let ans = 2.0 * std::f64::consts::PI * r;
     println!("{}", ans);
 }
