1cce3e9638c88f46a833a1d88516ac86d1552409
bouzuya <m@bouzuya.net>
2020-04-26 16:11:39 +0900

    abc023_c AC

--- abc023/src/bin/c.rs
+++ abc023/src/bin/c.rs
@@ -9,21 +9,32 @@ fn main() {
         n: usize,
         rcs: [(Usize1, Usize1); n],
     };
-    let mut tbl = vec![vec![0_usize; c]; r];
-    let mut rc = vec![0_usize; r];
-    let mut cc = vec![0_usize; c];
+    let mut xss = vec![vec![]; r];
+    let mut cs = vec![0_usize; c];
     for &(r_i, c_i) in rcs.iter() {
-        rc[r_i] += 1;
-        cc[c_i] += 1;
-        tbl[r_i][c_i] += 1;
+        xss[r_i].push(c_i);
+        cs[c_i] += 1;
+    }
+    let mut ccc = vec![0_usize; n + 1];
+    for &c in cs.iter() {
+        ccc[c] += 1;
     }
     let mut count = 0;
-    for y in 0..r {
-        for x in 0..c {
-            if rc[y] + cc[x] - tbl[y][x] == k {
-                count += 1;
+    for xs in xss.iter() {
+        let cr = xs.len();
+        if cr > k {
+            continue;
+        }
+        let mut sum = ccc[k - cr];
+        for &x in xs.iter() {
+            if cs[x] == k - cr {
+                sum -= 1;
+            }
+            if cs[x] == k - cr + 1 {
+                sum += 1;
             }
         }
+        count += sum;
     }
     let ans = count;
     println!("{}", ans);
