e3e9c542973c39296803b78253e292ed31de9d26
bouzuya <m@bouzuya.net>
2020-04-29 13:31:47 +0900

    abc164_d

--- abc164/src/bin/d.rs
+++ abc164/src/bin/d.rs
@@ -3,20 +3,31 @@ use proconio::marker::Bytes;
 
 fn main() {
     input! { s: Bytes };
-    let ds: Vec<u8> = s.iter().map(|b| b - b'0').collect();
-    let n = ds.len();
-    let mut count = 0;
-    let mut b = 1_usize;
-    let mut r = 0_usize;
-    let mut dp = vec![0_usize; 2019];
-    dp[0] = 1;
-    for i in (0..n).rev() {
-        let d = ds[i] as usize;
-        r = (d * b + r) % 2019;
-        b = (b * 10) % 2019;
-        count += dp[r];
-        dp[r] += 1;
+    let m = 2019;
+    // ある桁の数値は 10^i * s[i] と表現できる。
+    // 10^i 部分も mod 2019 を取ることで扱える桁数になる。
+    // rs[i] 右から i 桁目までの mod ((s[0] + s[1] + ... s[i]) % 2019)
+    let rs = std::iter::once(0_usize).chain(s.iter().rev().map(|b| (b - b'0') as usize).scan(
+        (1_usize, 0_usize),
+        |acc, d| {
+            let (b, r) = *acc;
+            acc.0 = (b * 10) % m;
+            acc.1 = ((d * b) % m + r) % m;
+            Some(acc.1)
+        },
+    ));
+    // [i, j] において mod 2019 が 0 になるためには
+    // rs[j] - rs[i - 1] = 0 にすれば良い
+    // rs[i] = rs[j - 1] になる個数を求めれば良い
+    //
+    // cs[r] 余り r ごとの rs における出現回数
+    let mut cs = vec![0_usize; m];
+    for r in rs {
+        cs[r] += 1;
     }
-    let ans = count;
+    let ans = cs
+        .iter()
+        .map(|&c| c * c.saturating_sub(1) / 2)
+        .sum::<usize>();
     println!("{}", ans);
 }
