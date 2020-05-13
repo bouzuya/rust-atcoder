359bf88d1dcb105f32b640dfdd6a4a482481d4d7
bouzuya <m@bouzuya.net>
2020-05-13 15:11:24 +0900

    abc073_b

--- abc073/src/bin/b.rs
+++ abc073/src/bin/b.rs
@@ -1,23 +1,10 @@
 use proconio::input;
-use proconio::marker::Usize1;
 
 fn main() {
     input! {
         n: usize,
-        lr: [(Usize1, Usize1); n],
+        lr: [(u64, u64); n],
     };
-    let mut c = vec![0_i64; 100_000 + 1];
-    for &(l, r) in lr.iter() {
-        c[l] += 1;
-        c[r + 1] -= 1;
-    }
-    let s = c
-        .iter()
-        .scan(0, |acc, &c_i| {
-            *acc += c_i;
-            Some(*acc)
-        })
-        .collect::<Vec<i64>>();
-    let ans = s.len() - s.iter().filter(|&&c| c == 0).count();
+    let ans = lr.iter().map(|&(l, r)| r - l + 1).sum::<u64>();
     println!("{}", ans);
 }
