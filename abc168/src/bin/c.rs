92e6b87eeaa9f5a7e8ec13965441fe5c27423ddb
bouzuya <m@bouzuya.net>
2020-05-18 06:27:21 +0900

    abc168_c

--- abc168/src/bin/c.rs
+++ abc168/src/bin/c.rs
@@ -4,17 +4,13 @@ fn main() {
     input! {
         a: f64,
         b: f64,
-        h: i64,
-        m: i64,
+        h: f64,
+        m: f64,
     };
-    let a1 = (-h % 12) as f64 * 360_f64 / 12_f64
-        + ((-m % 60) as f64 * 360_f64 / 60_f64 / 12_f64)
-        + 90_f64;
-    let b1 = (-m % 60) as f64 * 360_f64 / 60_f64 + 90_f64;
-    let a2 = a1 * (2_f64 * std::f64::consts::PI) / 360_f64;
-    let b2 = b1 * (2_f64 * std::f64::consts::PI) / 360_f64;
-    let x = a2.cos() * a - b2.cos() * b;
-    let y = a2.sin() * a - b2.sin() * b;
-    let ans = (x * x + y * y).sqrt();
+    let th = (h * 60_f64 + m) / (60_f64 * 12_f64) * 2_f64 * std::f64::consts::PI;
+    let tm = m / 60_f64 * 2_f64 * std::f64::consts::PI;
+    let dx = th.cos() * a - tm.cos() * b;
+    let dy = th.sin() * a - tm.sin() * b;
+    let ans = (dx * dx + dy * dy).sqrt();
     println!("{}", ans);
 }
