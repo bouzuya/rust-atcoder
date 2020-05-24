8c5d908381e99cebf53c4e5e8339f324816ebaf3
bouzuya <m@bouzuya.net>
2020-05-24 20:44:11 +0900

    past201912 i

---
+++ past201912-open/src/bin/i.rs
@@ -0,0 +1,37 @@
+use proconio::input;
+use proconio::marker::Chars;
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        sc: [(Chars, i64); m]
+    };
+    let inf = 1_000_000_000_000_i64;
+    let bc = sc
+        .iter()
+        .map(|(s, c)| {
+            let mut bits = 0_usize;
+            for i in 0..n {
+                if s[i] == 'Y' {
+                    bits |= 1 << (n - 1 - i);
+                }
+            }
+            (bits, *c)
+        })
+        .collect::<Vec<(usize, i64)>>();
+    let mut dp = vec![vec![inf; 1 << n]; m + 1];
+    dp[0][0] = 0;
+    for (i, &(b_i, c_i)) in bc.iter().enumerate() {
+        for j in 0..1 << n {
+            dp[i + 1][j] = std::cmp::min(dp[i + 1][j], dp[i][j]);
+            dp[i + 1][j | b_i] = std::cmp::min(dp[i + 1][j | b_i], dp[i][j] + c_i);
+        }
+    }
+    let mut mask = 0;
+    for i in 0..n {
+        mask |= 1 << (n - 1 - i);
+    }
+    let ans = dp[m][mask];
+    println!("{}", if ans == inf { -1 } else { ans });
+}
