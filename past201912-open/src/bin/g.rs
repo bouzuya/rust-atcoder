18f13f1c22210741c88a820df6acca231cd5c714
bouzuya <m@bouzuya.net>
2020-05-26 22:02:46 +0900

    past201912_g (2)

--- past201912-open/src/bin/g.rs
+++ past201912-open/src/bin/g.rs
@@ -1,31 +1,5 @@
 use proconio::input;
 
-fn sum(a: &Vec<Vec<i64>>, g: &Vec<Vec<usize>>) -> i64 {
-    let mut res = 0_i64;
-    for g_i in g.iter() {
-        for i in 0..g_i.len() {
-            for j in i + 1..g_i.len() {
-                res += a[g_i[i]][g_i[j]];
-            }
-        }
-    }
-    res
-}
-
-fn dfs(inf: i64, a: &Vec<Vec<i64>>, g: &mut Vec<Vec<usize>>, n: usize, i: usize) -> i64 {
-    if i == n {
-        return sum(a, g);
-    }
-
-    let mut res = inf;
-    for g_i in 0..g.len() {
-        g[g_i].push(i);
-        res = std::cmp::max(res, dfs(res, a, g, n, i + 1));
-        g[g_i].pop();
-    }
-    res
-}
-
 fn main() {
     input! { n: usize };
     let mut a = vec![vec![0_i64; n]; n];
@@ -37,11 +11,26 @@ fn main() {
         }
     }
 
-    let inf = -1_000_000_000_000_000;
-    let mut ans = inf;
-    ans = dfs(ans, &a, &mut vec![vec![]; 1], n, 0);
-    ans = dfs(ans, &a, &mut vec![vec![]; 2], n, 0);
-    ans = dfs(ans, &a, &mut vec![vec![]; 3], n, 0);
+    let mut ans = -1_000_000_000_000_000;
+    let n_g = 3_usize;
+    for ds in 0..n_g.pow(n as u32) {
+        let mut x = ds;
+        let mut g = vec![0; n];
+        for i in (0..n).rev() {
+            g[i] = x % n_g;
+            x /= n_g;
+        }
+
+        let mut sum = 0;
+        for i in 0..n {
+            for j in i + 1..n {
+                if g[i] == g[j] {
+                    sum += a[i][j];
+                }
+            }
+        }
+        ans = std::cmp::max(ans, sum);
+    }
 
     println!("{}", ans);
 }
