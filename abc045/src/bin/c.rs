53a46177fd346ec8c37430db19f59077324a44d9
bouzuya <m@bouzuya.net>
2020-05-30 07:49:15 +0900

    abc045_c

--- abc045/src/bin/c.rs
+++ abc045/src/bin/c.rs
@@ -1,29 +1,12 @@
 use proconio::input;
 use proconio::marker::Chars;
 
-fn dfs(p: &mut Vec<bool>, n: usize, d: &Vec<u64>) -> u64 {
-    if p.len() == n - 1 {
-        let mut xs = vec![];
-        xs.push(d[0]);
-        for (&p_i, &d_i) in p.iter().zip(d.iter().skip(1)) {
-            if p_i {
-                xs.push(d_i);
-            } else {
-                let n_x = xs.len();
-                let x = xs[n_x - 1];
-                xs[n_x - 1] = x * 10 + d_i;
-            }
-        }
-        return xs.iter().sum::<u64>();
+fn dfs(d: &Vec<u64>, i: usize, s: u64, c: u64) -> u64 {
+    if i < d.len() {
+        dfs(d, i + 1, s + c, d[i]) + dfs(d, i + 1, s, c * 10 + d[i])
+    } else {
+        s + c
     }
-    let mut sum = 0;
-    let b = vec![false, true];
-    for &b_i in b.iter() {
-        p.push(b_i);
-        sum += dfs(p, n, d);
-        p.pop();
-    }
-    sum
 }
 
 fn main() {
@@ -34,7 +17,6 @@ fn main() {
         .iter()
         .map(|&c| c.to_digit(10).unwrap() as u64)
         .collect::<Vec<u64>>();
-    let n = d.len();
-    let ans = dfs(&mut vec![], n, &d);
+    let ans = dfs(&d, 1, 0, d[0]);
     println!("{}", ans);
 }
