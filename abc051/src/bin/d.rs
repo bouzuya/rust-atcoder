0e315c5e45591c4e61014857addad6dc14df16a2
bouzuya <m@bouzuya.net>
2020-05-29 15:38:01 +0900

    abc051_d (bellman_ford)

--- abc051/src/bin/d.rs
+++ abc051/src/bin/d.rs
@@ -1,47 +1,49 @@
 use proconio::input;
 use proconio::marker::Usize1;
 
-fn adjacency_list(n: usize, uvc: &Vec<(usize, usize, u64)>) -> Vec<Vec<(usize, u64)>> {
+fn adjacency_list(n: usize, uvw: &Vec<(usize, usize, i64)>) -> Vec<Vec<(usize, i64)>> {
     let mut e = vec![vec![]; n];
-    for &(u, v, c) in uvc.iter() {
-        e[u].push((v, c));
-        e[v].push((u, c));
+    for &(u, v, w) in uvw.iter() {
+        e[u].push((v, w));
+        e[v].push((u, w));
     }
     e
 }
 
-fn dijkstra(n: usize, inf: u64, e: &Vec<Vec<(usize, u64)>>, s: usize) -> Vec<u64> {
+fn bellman_ford(n: usize, inf: i64, e: &Vec<Vec<(usize, i64)>>, s: usize) -> Option<Vec<i64>> {
     let mut d = vec![inf; n];
-    let mut pq = std::collections::BinaryHeap::new();
     d[s] = 0;
-    pq.push(std::cmp::Reverse((0, s)));
-    while let Some(std::cmp::Reverse((c_u, u))) = pq.pop() {
-        if c_u > d[u] {
-            continue;
-        }
-        for &(v, c_v) in e[u].iter() {
-            let c = c_u + c_v;
-            if c < d[v] {
-                d[v] = c;
-                pq.push(std::cmp::Reverse((c, v)));
+    for i in 0..n {
+        for (u, e_u) in e.iter().enumerate() {
+            for &(v, w) in e_u.iter() {
+                if d[u] + w < d[v] {
+                    d[v] = d[u] + w;
+                    if i == n - 1 {
+                        return None;
+                    }
+                }
             }
         }
     }
-    d
+    Some(d)
 }
 
 fn main() {
     input! {
         n: usize,
         m: usize,
-        abc: [(Usize1, Usize1, u64); m],
+        abc: [(Usize1, Usize1, i64); m],
     };
-    let inf = 1_000_000_000;
+
     let e = adjacency_list(n, &abc);
+    let inf = 1_000_000_000;
     // d[u][v]: u から v への最短距離
     let mut d = vec![];
     for u in 0..n {
-        d.push(dijkstra(n, inf, &e, u));
+        match bellman_ford(n, inf, &e, u) {
+            Some(d_u) => d.push(d_u),
+            None => unreachable!(),
+        }
     }
     let mut ans = 0;
     for &(u, v, c) in abc.iter() {
