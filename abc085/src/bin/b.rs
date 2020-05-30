3baa932de36ce07cc48e77aca42d982839783ea3
bouzuya <m@bouzuya.net>
2020-05-30 09:15:41 +0900

    abc085_b

--- abc085/src/bin/b.rs
+++ abc085/src/bin/b.rs
@@ -3,16 +3,9 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        mut d: [u64; n],
+        d: [u64; n],
     };
-    d.sort();
-    let mut x = 0;
-    let mut ans = 0;
-    for &d_i in d.iter() {
-        if d_i > x {
-            x = d_i;
-            ans += 1;
-        }
-    }
+    let set = d.iter().collect::<std::collections::BTreeSet<_>>();
+    let ans = set.len();
     println!("{}", ans);
 }
