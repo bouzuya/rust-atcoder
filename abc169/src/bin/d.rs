89c5901719d97eb1626b8ccde73c87e4bdd6f9c3
bouzuya <m@bouzuya.net>
2020-06-01 20:41:12 +0900

    abc169_d

--- abc169/src/bin/d.rs
+++ abc169/src/bin/d.rs
@@ -1,41 +1,42 @@
 use proconio::input;
 
+fn prime_factorization(n: usize) -> std::collections::BTreeMap<usize, usize> {
+    let mut p = std::collections::BTreeMap::new();
+    if n < 2 {
+        return p;
+    }
+    let mut n = n; // shadowing
+    for i in 2.. {
+        if i * i > n {
+            break;
+        }
+        while n % i == 0 {
+            *p.entry(i).or_insert(0) += 1;
+            n /= i;
+        }
+    }
+    if n != 1 {
+        *p.entry(n).or_insert(0) += 1;
+    }
+    p
+}
+
 fn main() {
     input! {
         n: u64,
     };
-
-    let mut p = vec![];
-    let mut m = n;
-    for i in 2.. {
-        if i * i > m {
-            break;
-        }
-        let mut count = 0;
-        while m % i == 0 {
-            count += 1;
-            m /= i;
-        }
-        p.push((i, count));
-    }
-    if m != 1 {
-        p.push((n, 1));
-    }
+    let pq = prime_factorization(n as usize);
     let mut count = 0;
-    for i in 1.. {
-        let mut used = false;
-        for p_i in p.iter_mut() {
-            if p_i.1 >= i {
-                p_i.1 -= i;
-                used = true;
-                count += 1;
+    for (&_, &q_i) in pq.iter() {
+        let mut c = 0;
+        for i in 1.. {
+            c += i;
+            if c > q_i {
+                break;
             }
-        }
-        if !used {
-            break;
+            count += 1;
         }
     }
-
     let ans = count;
     println!("{}", ans);
 }
