35a2230891a9cfadb7b41ac47c633571cbce22d6
bouzuya <m@bouzuya.net>
2020-06-01 12:35:06 +0900

    abc169_b

--- abc169/src/bin/b.rs
+++ abc169/src/bin/b.rs
@@ -3,19 +3,19 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        a: [u128; n],
+        a: [u64; n],
     };
     if a.contains(&0) {
-        println!("{}", 0);
-    } else {
-        let mut ans = 1_u128;
-        for &a_i in a.iter() {
-            ans = ans * a_i;
-            if ans > 1_000_000_000_000_000_000_u128 {
-                println!("-1");
-                return;
-            }
-        }
-        println!("{}", ans);
+        println!("0");
+        return;
     }
+    let mut ans = 1_u64;
+    for &a_i in a.iter() {
+        if a_i > 1_000_000_000_000_000_000_u64 / ans {
+            println!("-1");
+            return;
+        }
+        ans = ans * a_i;
+    }
+    println!("{}", ans);
 }
