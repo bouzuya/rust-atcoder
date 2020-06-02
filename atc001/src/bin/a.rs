29be1df8dc6672cedd1b63ee00a88c1b8d03201f
bouzuya <m@bouzuya.net>
2020-06-02 13:14:23 +0900

    atc001_a (stack)

--- atc001/src/bin/a.rs
+++ atc001/src/bin/a.rs
@@ -3,33 +3,33 @@ use proconio::marker::Chars;
 
 fn dfs(
     used: &mut Vec<Vec<bool>>,
-    c: &Vec<Vec<char>>,
     h: usize,
     w: usize,
     s: (usize, usize),
     g: (usize, usize),
 ) -> bool {
-    if s == g {
-        return true;
-    }
-    let (x, y) = s;
-    if used[y][x] {
-        return false;
-    }
-    used[y][x] = true;
     let d = vec![(-1, 0), (0, -1), (0, 1), (1, 0)];
-    for &(d_x, d_y) in d.iter() {
-        let x_n = x as i64 + d_x;
-        let y_n = y as i64 + d_y;
-        if (0..h as i64).contains(&y_n) && (0..w as i64).contains(&x_n) {
-            let (x_n, y_n) = (x_n as usize, y_n as usize);
-            if dfs(used, c, h, w, (x_n, y_n), g) {
-                return true;
+    let mut stack = vec![];
+    stack.push(s);
+    while let Some((x, y)) = stack.pop() {
+        if (x, y) == g {
+            return true;
+        }
+        if used[y][x] {
+            continue;
+        }
+        used[y][x] = true;
+        for &(d_x, d_y) in d.iter() {
+            let (x_n, y_n) = (x as i64 + d_x, y as i64 + d_y);
+            if (0..h as i64).contains(&y_n) && (0..w as i64).contains(&x_n) {
+                let (x_n, y_n) = (x_n as usize, y_n as usize); // shadowing
+                stack.push((x_n, y_n));
             }
         }
     }
     false
 }
+
 fn main() {
     input! {
         h: usize,
@@ -50,6 +50,6 @@ fn main() {
             }
         }
     }
-    let ans = dfs(&mut used, &c, h, w, s, g);
+    let ans = dfs(&mut used, h, w, s, g);
     println!("{}", if ans { "Yes" } else { "No" });
 }
