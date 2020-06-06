3d9e3a79bbbdf050d1259f06a5b70db187b74843
bouzuya <m@bouzuya.net>
2020-06-06 09:09:54 +0900

    abc038_d

--- abc038/src/bin/d.rs
+++ abc038/src/bin/d.rs
@@ -1,53 +1,24 @@
 use proconio::input;
+use superslice::*;
 
 fn main() {
     input! {
         n: usize,
-        wh: [(i64, i64); n],
+        mut wh: [(i64, i64); n],
     };
-    let mut iwh = wh
-        .iter()
-        .enumerate()
-        .map(|(i, &(w, h))| (i, w, h))
-        .collect::<Vec<_>>();
-    iwh.sort_by_key(|&(_, w, _)| w);
-    let mut iw = iwh
-        .iter()
-        .scan((0, 0), |acc, &(i, w, _)| {
-            if acc.0 < w {
-                acc.0 = w;
-                acc.1 += 1;
-            }
-            Some((i, acc.1))
-        })
-        .collect::<Vec<_>>();
-    iw.sort();
-    iwh.sort_by_key(|&(_, _, h)| h);
-    let mut ih = iwh
-        .iter()
-        .scan((0, 0), |acc, &(i, _, h)| {
-            if acc.0 < h {
-                acc.0 = h;
-                acc.1 += 1;
-            }
-            Some((i, acc.1))
-        })
-        .collect::<Vec<_>>();
-    ih.sort();
-    let mut cwh = iw
-        .iter()
-        .zip(ih.iter())
-        .map(|(&(_, w), &(_, h))| (w, h))
-        .collect::<Vec<_>>();
-    cwh.sort_by_key(|&(w, h)| std::cmp::Reverse((w, h)));
-    let mut c = 0;
-    let mut wh_p = (1_000_000, 1_000_000);
-    for &(w, h) in cwh.iter() {
-        if w < wh_p.0 && h < wh_p.1 {
-            c += 1;
-            wh_p = (w, h);
+    wh.sort_by_key(|&(w, h)| (w, -h));
+
+    // LIS: Longest Increasing Subsequence の長さを求める
+    let mut l = vec![wh[0].1];
+    for &(_, h) in wh.iter().skip(1) {
+        if l[l.len() - 1] < h {
+            l.push(h);
+        } else {
+            let i = l.lower_bound(&h);
+            l[i] = h;
         }
     }
-    let ans = c;
+
+    let ans = l.len();
     println!("{}", ans);
 }
