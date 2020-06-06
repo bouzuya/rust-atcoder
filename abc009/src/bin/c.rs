c4af98833b5be724cda97a7288460eb9bafa705b
bouzuya <m@bouzuya.net>
2020-06-07 08:48:16 +0900

    abc009_c WA

--- abc009/src/bin/c.rs
+++ abc009/src/bin/c.rs
@@ -4,33 +4,33 @@ use proconio::marker::Chars;
 fn main() {
     input! {
         n: usize,
-        mut k: i64,
+        k: i64,
         mut s: Chars,
     };
 
+    let mut c = 0;
     let mut u = vec![false; n];
     for i in 0..n {
-        if k > 0 {
-            let mut min_index = i;
-            for j in i + 1..n {
-                if s[j] < s[min_index] {
-                    min_index = j;
-                }
+        let mut min_index = i;
+        for j in (i + 1..n).rev() {
+            if s[j] < s[min_index] {
+                min_index = j;
             }
-            if min_index != i {
-                k -= if u[min_index] && u[i] {
-                    0
-                } else if u[min_index] || u[i] {
-                    1
-                } else {
-                    2
-                };
+        }
+        if min_index != i {
+            let x = if u[min_index] && u[i] {
+                0
+            } else if u[min_index] || u[i] {
+                1
+            } else {
+                2
+            };
+            if c + x <= k {
+                c += x;
                 u[min_index] = true;
-                if k >= 0 {
-                    let c = s[i];
-                    s[i] = s[min_index];
-                    s[min_index] = c;
-                }
+                let c = s[i];
+                s[i] = s[min_index];
+                s[min_index] = c;
             }
         }
     }
