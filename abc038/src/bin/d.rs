43feb996f4d96138ffd3742f28148914a4040044
bouzuya <m@bouzuya.net>
2020-06-06 21:00:24 +0900

    abc038_d

--- abc038/src/bin/d.rs
+++ abc038/src/bin/d.rs
@@ -1,5 +1,15 @@
 use proconio::input;
-use superslice::*;
+
+fn lis_length(a: &Vec<i64>, inf: i64) -> usize {
+    let mut len = 0;
+    let mut l = vec![inf; a.len()];
+    for &a_i in a.iter() {
+        let i = l.binary_search(&a_i).map_or_else(|i| i, |i| i);
+        l[i] = a_i;
+        len = std::cmp::max(len, i + 1);
+    }
+    len
+}
 
 fn main() {
     input! {
@@ -7,18 +17,7 @@ fn main() {
         mut wh: [(i64, i64); n],
     };
     wh.sort_by_key(|&(w, h)| (w, -h));
-
-    // LIS: Longest Increasing Subsequence の長さを求める
-    let mut l = vec![wh[0].1];
-    for &(_, h) in wh.iter().skip(1) {
-        if l[l.len() - 1] < h {
-            l.push(h);
-        } else {
-            let i = l.lower_bound(&h);
-            l[i] = h;
-        }
-    }
-
-    let ans = l.len();
+    let a = wh.iter().map(|&(_, h)| h).collect::<Vec<_>>();
+    let ans = lis_length(&a, std::i64::MAX);
     println!("{}", ans);
 }
