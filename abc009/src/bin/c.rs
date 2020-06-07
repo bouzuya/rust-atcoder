7d02303b8af9a3641a5f646754dff14f8508e456
bouzuya <m@bouzuya.net>
2020-06-07 12:34:00 +0900

    abc009_c

--- abc009/src/bin/c.rs
+++ abc009/src/bin/c.rs
@@ -4,36 +4,47 @@ use proconio::marker::Chars;
 fn main() {
     input! {
         n: usize,
-        k: i64,
-        mut s: Chars,
+        k: usize,
+        s: Chars,
     };
+    let mut map = std::collections::BTreeMap::new();
+    for &s_i in s.iter() {
+        *map.entry(s_i).or_insert(0) += 1;
+    }
 
-    let mut c = 0;
-    let mut u = vec![false; n];
+    let mut t = vec![];
     for i in 0..n {
-        let mut min_index = i;
-        for j in (i + 1..n).rev() {
-            if s[j] < s[min_index] {
-                min_index = j;
+        let map_o = map.clone();
+        for (&ch, v) in map.iter_mut() {
+            if *v == 0 {
+                continue;
             }
-        }
-        if min_index != i {
-            let x = if u[min_index] && u[i] {
-                0
-            } else if u[min_index] || u[i] {
-                1
-            } else {
-                2
-            };
-            if c + x <= k {
-                c += x;
-                u[min_index] = true;
-                let c = s[i];
-                s[i] = s[min_index];
-                s[min_index] = c;
+            t.push(ch);
+            *v -= 1;
+            let mut m = map_o.clone();
+            *m.entry(ch).or_default() -= 1;
+            let mut u = vec![false; n];
+            for j in i + 1..n {
+                let c = m.entry(s[j]).or_default();
+                if *c > 0 {
+                    *c -= 1;
+                    u[j] = true;
+                }
             }
+            let count = s[0..=i]
+                .iter()
+                .zip(t[0..=i].iter())
+                .filter(|(s_j, t_j)| s_j != t_j)
+                .count()
+                + u[i + 1..].iter().filter(|&&b| !b).count();
+            if count <= k {
+                break;
+            }
+            t.pop();
+            *v += 1;
         }
     }
-    let ans = s.iter().collect::<String>();
+
+    let ans = t.iter().collect::<String>();
     println!("{}", ans);
 }
