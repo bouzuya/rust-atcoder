ff6801ccac8bcd20853d4108c23c883d62018359
bouzuya <m@bouzuya.net>
2020-06-10 06:35:58 +0900

    past202005_c (2)

--- past202005-open/src/bin/c.rs
+++ past202005-open/src/bin/c.rs
@@ -2,14 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
-        a: u64,
-        r: u64,
-        n: u64,
+        a: u32,
+        r: u32,
+        n: u32,
     };
+    if r == 1 {
+        println!("{}", a);
+        return;
+    }
+    if n > 1_000_000_000_u32.next_power_of_two().trailing_zeros() {
+        println!("large");
+        return;
+    }
     let mut ans = a;
-    for _ in 1..n {
+    for _ in 1..=n - 1 {
         ans *= r;
-        if ans > 1_000_000_000_u64 {
+        if ans > 1_000_000_000_u32 {
             println!("large");
             return;
         }
