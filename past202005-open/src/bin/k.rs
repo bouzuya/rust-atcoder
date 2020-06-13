6d4c40203bab0d3aef0ad7d03e2281b6a34e3b81
bouzuya <m@bouzuya.net>
2020-06-13 11:08:53 +0900

    past202005_k

--- past202005-open/src/bin/k.rs
+++ past202005-open/src/bin/k.rs
@@ -7,22 +7,41 @@ fn main() {
         q: usize,
         ftx: [(Usize1, Usize1, Usize1); q],
     };
-    let mut x = vec![std::collections::LinkedList::new(); n];
-    for (i, x_i) in x.iter_mut().enumerate() {
-        x_i.push_back(i);
+    // コンテナを 0..n 、机を n..2n で表現する。
+    let mut e = vec![(0, 0); 2 * n]; // (from, to)
+    for i in 0..n {
+        e[i].0 = n + i;
+        e[i].1 = n + i;
+    }
+    for i in 0..n {
+        e[n + i].0 = i;
+        e[n + i].1 = i;
     }
     for &(f_i, t_i, x_i) in ftx.iter() {
-        let p = x[f_i].iter().position(|&x_j| x_j == x_i).unwrap();
-        let mut x_s = x[f_i].split_off(p);
-        x[t_i].append(&mut x_s);
+        let x_f = x_i;
+        let x_t = e[n + f_i].0;
+
+        e[x_t].1 = n + t_i;
+        e[n + f_i].0 = e[x_f].0;
+        let t = e[x_f].0;
+        e[t].1 = n + f_i;
+
+        e[x_f].0 = e[n + t_i].0;
+        e[x_t].1 = n + t_i;
+
+        let t = e[n + t_i].0;
+        e[t].1 = x_f;
+        e[n + t_i].0 = x_t;
     }
     let mut ans = vec![0; n];
-    for (i, x_i) in x.iter().enumerate() {
-        for &x_ij in x_i.iter() {
-            ans[x_ij] = i;
+    for i in n..2 * n {
+        let mut c = e[i].1;
+        while c != i {
+            ans[c] = i - n + 1;
+            c = e[c].1;
         }
     }
     for &a_i in ans.iter() {
-        println!("{}", a_i + 1);
+        println!("{}", a_i);
     }
 }
