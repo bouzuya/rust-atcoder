1e46bda98a61778399caa523c03763ee912deabc
bouzuya <m@bouzuya.net>
2020-06-15 06:30:47 +0900

    abc170_c

--- abc170/src/bin/c.rs
+++ abc170/src/bin/c.rs
@@ -1,23 +1,31 @@
 use proconio::input;
 
+macro_rules! chmin {
+    ($min_v: expr, $v: expr) => {
+        if $v < $min_v {
+            $min_v = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
 fn main() {
     input! {
         x: i64,
         n: usize,
         p: [i64; n],
     };
-    let mut c = vec![true; 101 + 1];
-    for &p_i in p.iter() {
-        c[p_i as usize] = false;
-    }
-    let mut min_abs = (x + 1).abs();
-    let mut ans = -1_i64;
+    let mut min_a_i = x;
+    let mut ans = 0;
     for i in 0..=101 {
-        if c[i] {
-            if (x - i as i64).abs() < min_abs {
-                min_abs = (x - i as i64).abs();
-                ans = i as i64;
-            }
+        if p.contains(&i) {
+            continue;
+        }
+        let a_i = (x - i as i64).abs();
+        if chmin!(min_a_i, a_i) {
+            ans = i;
         }
     }
     println!("{}", ans);
