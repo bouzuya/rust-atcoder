3d0544ec57e8bd914db9287da8a7396c4b6d30ae
bouzuya <m@bouzuya.net>
2020-06-20 07:12:29 +0900

    dp_c

--- dp/src/bin/c.rs
+++ dp/src/bin/c.rs
@@ -16,6 +16,9 @@ fn main() {
         n: usize,
         abc: [(i64, i64, i64); n],
     };
+    // abc[i]: i - 1 日目の選択肢
+    // dp[i][j]: i 日目までで最後に j を選んでいたときに得る幸福度の総和の最大値
+    // j は 0..3 で 0: A, 1: B, 2: C
     let mut dp = vec![vec![0; 3]; n + 1];
     for (i, &(a_i, b_i, c_i)) in abc.iter().enumerate() {
         chmax!(dp[i + 1][0], std::cmp::max(dp[i][1] + a_i, dp[i][2] + a_i));
