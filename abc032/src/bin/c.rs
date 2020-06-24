b1f5f275147a9a2b53c505c8eee6b47709c09c69
bouzuya <m@bouzuya.net>
2020-06-25 06:39:38 +0900

    abc032_c TLE

--- abc032/src/bin/c.rs
+++ abc032/src/bin/c.rs
@@ -21,18 +21,15 @@ fn main() {
         println!("{}", n);
         return;
     }
-    let c = std::iter::once(1_i64)
-        .chain(s.iter().scan(1_i64, |acc, &s_i| {
-            *acc *= s_i;
-            Some(*acc)
-        }))
-        .collect::<Vec<_>>();
     let mut ans = 0;
-    for l in 0..=n {
-        for r in l + 1..=n {
-            if c[r] / c[l] <= k {
-                chmax!(ans, r - l);
+    for l in 0..n {
+        let mut c = 1;
+        for r in l..n {
+            c *= s[r];
+            if c > k {
+                break;
             }
+            chmax!(ans, r - l + 1);
         }
     }
     println!("{}", ans);
