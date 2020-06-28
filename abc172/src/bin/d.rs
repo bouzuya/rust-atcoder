bc71adf72316153fe9652bd415284edb7281fd12
bouzuya <m@bouzuya.net>
2020-06-28 09:07:02 +0900

    abc172_d

--- abc172/src/bin/d.rs
+++ abc172/src/bin/d.rs
@@ -4,12 +4,10 @@ fn main() {
     input! {
         n: usize,
     };
-    let mut c = vec![1_usize; n + 1];
-    for i in 2..=n {
-        for j in (i..=n).step_by(i) {
-            c[j] += 1;
-        }
+    let mut ans = 0;
+    for i in 1..=n {
+        let m = n / i;
+        ans += i * (m * (m + 1) / 2);
     }
-    let ans = (1..=n).fold(0_usize, |acc, i| acc + i * c[i]);
     println!("{}", ans);
 }
