232b9fa70bd872bacf82c1bd80e8345fb7b52ed5
bouzuya <m@bouzuya.net>
2020-07-03 22:36:16 +0900

    abc060_b

--- abc060/src/bin/b.rs
+++ abc060/src/bin/b.rs
@@ -1,21 +1,16 @@
 use proconio::input;
 
-fn gcd(n: u64, m: u64) -> u64 {
-    if n < m {
-        gcd(m, n)
-    } else if m == 0 {
-        n
-    } else {
-        gcd(m, n % m)
-    }
-}
-
 fn main() {
     input! {
         a: u64,
         b: u64,
         c: u64,
     };
-    let ans = c % gcd(a, b) == 0;
-    println!("{}", if ans { "YES" } else { "NO" });
+    for x in 1..b {
+        if (x * a) % b == c {
+            println!("YES");
+            return;
+        }
+    }
+    println!("NO");
 }
