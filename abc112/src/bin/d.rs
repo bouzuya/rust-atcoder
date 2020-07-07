0e58e3589d6e7b5c7116cecafeb2bd1e14d607b3
bouzuya <m@bouzuya.net>
2020-07-08 06:13:16 +0900

    abc112_d

--- abc112/src/bin/d.rs
+++ abc112/src/bin/d.rs
@@ -1,25 +1,29 @@
 use proconio::input;
 
+fn divisors(n: usize) -> Vec<usize> {
+    let mut d = vec![];
+    for i in 1.. {
+        if i * i > n {
+            break;
+        }
+        if n % i == 0 {
+            d.push(i);
+            if i != n / i {
+                d.push(n / i);
+            }
+        }
+    }
+    // d.sort();
+    d
+}
+
 fn main() {
     input! {
         n: usize,
         m: usize,
     };
 
-    // n の約数を列挙する
-    let mut d = vec![];
-    for i in 1.. {
-        if i * i > m {
-            break;
-        }
-        if m % i == 0 {
-            d.push(i);
-            if i != m / i {
-                d.push(m / i);
-            }
-        }
-    }
-
+    let d = divisors(m);
     let mut ans = 1;
     for &d_i in d.iter() {
         if n * d_i <= m {
