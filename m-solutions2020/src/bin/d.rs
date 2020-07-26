65e35e75bb49c8208a0ffbfda9885a2778b0da81
bouzuya <m@bouzuya.net>
2020-07-26 11:45:52 +0900

    m-solutions2020 d

--- m-solutions2020/src/bin/d.rs
+++ m-solutions2020/src/bin/d.rs
@@ -8,22 +8,12 @@ fn main() {
     let mut s = 0;
     let mut m = 1_000_i64;
     for i in 0..n {
-        if i == n - 1 {
-            m += a[i] * s;
-        }
-        for j in i + 1..n {
-            if a[i] == a[j] {
-                continue;
-            } else if a[i] < a[j] {
-                if s == 0 {
-                    s = m / a[i];
-                    m -= a[i] * s;
-                }
-                break;
-            } else if a[i] > a[j] {
-                m += a[i] * s;
-                s = 0;
-                break;
+        m += a[i] * s;
+        s = 0;
+        if i + 1 < n {
+            if a[i] < a[i + 1] {
+                s = m / a[i];
+                m -= s * a[i];
             }
         }
     }
