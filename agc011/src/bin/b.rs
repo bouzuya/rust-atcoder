ee9483ebfb9c876b3ecf6571a52741f4404b5a93
bouzuya <m@bouzuya.net>
2020-08-22 16:49:20 +0900

    agc011_b

--- agc011/src/bin/b.rs
+++ agc011/src/bin/b.rs
@@ -6,20 +6,15 @@ fn main() {
         mut a: [i64; n],
     };
     a.sort();
-    let s = a
-        .iter()
-        .scan(0, |acc, a_i| {
+    let s = std::iter::once(0)
+        .chain(a.iter().scan(0, |acc, a_i| {
             *acc += a_i;
             Some(*acc)
-        })
+        }))
         .collect::<Vec<_>>();
     let mut c = 0;
-    let mut a_p = a[0];
-    for (i, &a_i) in a.iter().enumerate().skip(1) {
-        if a_p * 2 >= a_i {
-            a_p += a_i;
-        } else {
-            a_p = s[i];
+    for (i, (&a_i, &s_i)) in a.iter().zip(s.iter()).enumerate() {
+        if s_i * 2 < a_i {
             c = i;
         }
     }
