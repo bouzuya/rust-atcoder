137ad74969f361ca1217889ebf753ef9c34424bd
bouzuya <m@bouzuya.net>
2020-10-10 11:42:31 +0900

    indeednow-quala a

--- indeednow-quala/src/bin/a.rs
+++ indeednow-quala/src/bin/a.rs
@@ -1,24 +1,10 @@
 use proconio::input;
 
-fn digits(n: usize) -> Vec<u8> {
-    let mut d = vec![];
-    let mut m = n;
-    loop {
-        d.push((m % 10) as u8);
-        m /= 10;
-        if m == 0 {
-            break;
-        }
-    }
-    d.reverse();
-    d
-}
-
 fn main() {
     input! {
         a: usize,
         b: usize,
     };
-    let ans = digits(a).len() * digits(b).len();
+    let ans = a.to_string().len() * b.to_string().len();
     println!("{}", ans);
 }
