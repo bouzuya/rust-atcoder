69db3bb7c1fe1285bdd9af2a46159acab02f0214
bouzuya <m@bouzuya.net>
2020-10-17 15:09:46 +0900

    arc042

--- arc042/src/bin/a.rs
+++ arc042/src/bin/a.rs
@@ -1,25 +1,20 @@
 use proconio::input;
-use proconio::marker::Usize1;
 
 fn main() {
     input! {
         n: usize,
         m: usize,
-        a: [Usize1; m],
+        a: [i64; m],
     };
-    let mut ans = vec![];
-    let mut set = std::collections::BTreeSet::new();
+    let mut set = std::collections::HashSet::new();
     for &a_i in a.iter().rev() {
         if set.insert(a_i) {
-            ans.push(a_i);
+            println!("{}", a_i);
         }
     }
-    for i in 0..n {
+    for i in 1..=n as i64 {
         if set.insert(i) {
-            ans.push(i);
+            println!("{}", i);
         }
-    }
-    for &ans_i in ans.iter() {
-        println!("{}", ans_i + 1);
     }
 }
--- arc042/src/bin/b.rs
+++ arc042/src/bin/b.rs
@@ -1,11 +1,44 @@
 use proconio::input;
-use proconio::marker::Usize1;
+
+struct P {
+    x: i64,
+    y: i64,
+}
+
+fn distance(p0: &P, p1: &P, p2: &P) -> f64 {
+    (((p2.y - p1.y) * p0.x - (p2.x - p1.x) * p0.y + p2.x * p1.y - p2.y * p1.x).abs() as f64)
+        / (((p2.y - p1.y).pow(2) + (p2.x - p1.x).pow(2)) as f64).sqrt()
+}
 
 fn main() {
     input! {
+        x: i64,
+        y: i64,
         n: usize,
-        a: [Usize1; n],
+        xy: [(i64, i64); n],
     };
-    let ans = n - a.len();
+    let p0 = P { x, y };
+    let mut min_d = distance(
+        &p0,
+        &P {
+            x: xy[0].0,
+            y: xy[0].1,
+        },
+        &P {
+            x: xy[n - 1].0,
+            y: xy[n - 1].1,
+        },
+    );
+    for w in xy.windows(2) {
+        let (p1_x, p1_y) = w[0];
+        let (p2_x, p2_y) = w[1];
+        let p1 = P { x: p1_x, y: p1_y };
+        let p2 = P { x: p2_x, y: p2_y };
+        let d = distance(&p0, &p1, &p2);
+        if d < min_d {
+            min_d = d;
+        }
+    }
+    let ans = min_d;
     println!("{}", ans);
 }
