d91a48282898772111e66ab0b56d78b29568354a
bouzuya <m@bouzuya.net>
2020-11-30 07:12:34 +0900

    arc109_c

--- arc109/src/bin/c.rs
+++ arc109/src/bin/c.rs
@@ -1,11 +1,28 @@
 use proconio::input;
-use proconio::marker::Usize1;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        k: usize,
+        s: Chars,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut t = s.clone();
+    for _ in 0..k {
+        t = (0..n)
+            .map(|i| match (t[(i * 2) % n], t[(i * 2 + 1) % n]) {
+                ('R', 'R') => 'R',
+                ('R', 'S') => 'R',
+                ('R', 'P') => 'P',
+                ('S', 'R') => 'R',
+                ('S', 'S') => 'S',
+                ('S', 'P') => 'S',
+                ('P', 'R') => 'P',
+                ('P', 'S') => 'S',
+                ('P', 'P') => 'P',
+                _ => unreachable!(),
+            })
+            .collect();
+    }
+    println!("{}", t[0]);
 }
