5d81e6976387fe7bd189c6a99b694be083515104
bouzuya <m@bouzuya.net>
2021-01-14 19:50:04 +0900

    abc021

--- abc021/src/bin/a.rs
+++ abc021/src/bin/a.rs
@@ -4,20 +4,8 @@ fn main() {
     input! {
         n: usize,
     };
-    let mut m = n;
-    let mut xs = vec![];
-    while m > 0 {
-        if m % 2 == 0 {
-            m -= 2;
-            xs.push(2);
-        } else {
-            m -= 1;
-            xs.push(1);
-        }
-    }
-    xs.sort();
-    println!("{}", xs.len());
-    for x_i in xs {
-        println!("{}", x_i);
+    println!("{}", n);
+    for _ in 0..n {
+        println!("1");
     }
 }
