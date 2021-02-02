9cc9ed5dd5f93f12f5f7b7774245a58691821cdf
bouzuya <m@bouzuya.net>
2021-02-02 22:41:55 +0900

    abc190 f

--- abc190/src/bin/f.rs
+++ abc190/src/bin/f.rs
@@ -1,11 +1,88 @@
+// 解説 AC
 use proconio::input;
-use proconio::marker::Usize1;
+use fenwicktree::*;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut count = 0;
+    let mut ft = FenwickTree::new(n, 0);
+    for &a_i in a.iter() {
+        count += ft.sum(a_i, n);
+        ft.add(a_i, 1);
+    }
+
+    for &a_i in a.iter() {
+        println!("{}", count);
+        count -= a_i;
+        count += n - a_i - 1;
+    }
+}
+
+//https://github.com/rust-lang-ja/ac-library-rs
+
+pub mod fenwicktree {
+    // Reference: https://en.wikipedia.org/wiki/Fenwick_tree
+    pub struct FenwickTree<T> {
+        n: usize,
+        ary: Vec<T>,
+        e: T,
+    }
+
+    impl<T: Clone + std::ops::AddAssign<T>> FenwickTree<T> {
+        pub fn new(n: usize, e: T) -> Self {
+            FenwickTree {
+                n,
+                ary: vec![e.clone(); n],
+                e,
+            }
+        }
+        pub fn accum(&self, mut idx: usize) -> T {
+            let mut sum = self.e.clone();
+            while idx > 0 {
+                sum += self.ary[idx - 1].clone();
+                idx &= idx - 1;
+            }
+            sum
+        }
+        /// performs data[idx] += val;
+        pub fn add<U: Clone>(&mut self, mut idx: usize, val: U)
+        where
+            T: std::ops::AddAssign<U>,
+        {
+            let n = self.n;
+            idx += 1;
+            while idx <= n {
+                self.ary[idx - 1] += val.clone();
+                idx += idx & idx.wrapping_neg();
+            }
+        }
+        /// Returns data[l] + ... + data[r - 1].
+        pub fn sum(&self, l: usize, r: usize) -> T
+        where
+            T: std::ops::Sub<Output = T>,
+        {
+            self.accum(r) - self.accum(l)
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn fenwick_tree_works() {
+            let mut bit = FenwickTree::new(5, 0i64);
+            // [1, 2, 3, 4, 5]
+            for i in 0..5 {
+                bit.add(i, i as i64 + 1);
+            }
+            assert_eq!(bit.sum(0, 5), 15);
+            assert_eq!(bit.sum(0, 4), 10);
+            assert_eq!(bit.sum(1, 3), 5);
+        }
+    }
 }
