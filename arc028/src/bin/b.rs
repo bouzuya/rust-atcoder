8cbb52efadf0891cfb24ac4ff92d311d45d48a88
bouzuya <m@bouzuya.net>
2021-02-21 21:49:59 +0900

    arc028 b

--- arc028/src/bin/b.rs
+++ arc028/src/bin/b.rs
@@ -1,32 +1,5 @@
-use std::collections::BTreeMap;
-
 use proconio::input;
-
-fn printk(
-    curr: &mut Option<(u64, usize)>,
-    map: &BTreeMap<u64, usize>,
-    i: usize,
-    x_i: u64,
-    k: usize,
-) {
-    if i + 1 == k {
-        let (&a, &r) = map.iter().rev().nth(0).unwrap();
-        *curr = Some((a, r));
-        let (_, curr_rank) = curr.unwrap();
-        println!("{}", curr_rank);
-    } else {
-        let (curr_age, curr_rank) = curr.unwrap();
-        if x_i < curr_age {
-            let (&a, &r) = map.range(0..curr_age + 1).into_iter().rev().nth(1).unwrap();
-            *curr = Some((a, r));
-            let (_, curr_rank) = curr.unwrap();
-            println!("{}", curr_rank);
-        } else {
-            assert!(curr_age < x_i);
-            println!("{}", curr_rank);
-        }
-    }
-}
+use std::collections::BTreeMap;
 
 fn main() {
     input! {
@@ -34,13 +7,24 @@ fn main() {
         k: usize,
         x: [u64; n],
     };
-    let mut curr = None;
     let mut map: BTreeMap<u64, usize> = BTreeMap::new(); // order by age asc
     for (i, &x_i) in x.iter().enumerate() {
-        map.entry(x_i).or_insert(i + 1);
-        if i + 1 < k {
+        let rank_i = i + 1;
+        if rank_i < k {
+            map.entry(x_i).or_insert(rank_i);
             continue;
         }
-        printk(&mut curr, &map, i, x_i, k);
+        if rank_i == k {
+            map.entry(x_i).or_insert(rank_i);
+        } else {
+            assert!(rank_i > k);
+            let (&age_k, _) = map.iter().rev().next().unwrap();
+            if x_i < age_k {
+                map.entry(x_i).or_insert(rank_i);
+                map.remove(&age_k);
+            }
+        }
+        let (_, &rank_k) = map.iter().rev().next().unwrap();
+        println!("{}", rank_k);
     }
 }
