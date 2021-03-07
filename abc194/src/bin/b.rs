b38410b28aaa91842675798af5415c146e725412
bouzuya <m@bouzuya.net>
2021-03-07 20:34:43 +0900

    abc194 b, c

--- abc194/src/bin/b.rs
+++ abc194/src/bin/b.rs
@@ -7,54 +7,18 @@ fn main() {
         ab: [(i64, i64); n],
     };
 
-    // same
-    let mut ans1 = 1_000_000_000;
-    for &(a_i, b_i) in ab.iter() {
-        ans1 = min(ans1, a_i + b_i);
+    let mut ans = 1_000_000_000;
+    for a in 0..n {
+        for b in 0..n {
+            ans = min(
+                ans,
+                if a == b {
+                    ab[a].0 + ab[b].1
+                } else {
+                    max(ab[a].0, ab[b].1)
+                },
+            );
+        }
     }
-
-    // not same
-    let ans2 = {
-        let mut min_a = ab[0].0;
-        let mut min_i = 0;
-        for (i, &(a_i, _)) in ab.iter().enumerate() {
-            if a_i < min_a {
-                min_i = i;
-                min_a = a_i;
-            }
-        }
-        let mut min_b = 1_000_000_000;
-        for (i, &(_, b_i)) in ab.iter().enumerate() {
-            if i == min_i {
-                continue;
-            }
-            if b_i < min_b {
-                min_b = b_i;
-            }
-        }
-        max(min_a, min_b)
-    };
-    let ans3 = {
-        let mut min_b = ab[0].1;
-        let mut min_i = 0;
-        for (i, &(_, b_i)) in ab.iter().enumerate() {
-            if b_i < min_b {
-                min_i = i;
-                min_b = b_i;
-            }
-        }
-        let mut min_a = 1_000_000_000;
-        for (i, &(a_i, _)) in ab.iter().enumerate() {
-            if i == min_i {
-                continue;
-            }
-            if a_i < min_a {
-                min_a = a_i;
-            }
-        }
-        max(min_a, min_b)
-    };
-
-    let &ans = vec![ans1, ans2, ans3].iter().min().unwrap();
     println!("{}", ans);
 }
--- abc194/src/bin/c.rs
+++ abc194/src/bin/c.rs
@@ -5,17 +5,35 @@ fn main() {
         n: usize,
         a: [i64; n],
     };
-    let mut sum = 0_i64;
+
+    let offset = 200;
+    let mut count: Vec<i64> = vec![0; 200 + 1 + 200 + 1];
     for &a_i in a.iter() {
-        sum += a_i.pow(2) * (n as i64 - 1);
+        count[(a_i + offset) as usize] += 1;
     }
-
-    let mut c = 0;
-    for i in (1..n).rev() {
-        c += a[i];
-        sum -= 2 * a[i - 1] * c;
+    let mut sum = 0_i64;
+    for a_i in -200..=200 {
+        let i = (a_i + offset) as usize;
+        for a_j in -200..=200 {
+            let j = (a_j + offset) as usize;
+            if a_i > a_j {
+                continue;
+            }
+            sum += count[i] * count[j] * (a_j - a_i).pow(2);
+        }
     }
-
     let ans = sum;
     println!("{}", ans);
+
+    // let mut sum = 0_i64;
+    // for &a_i in a.iter() {
+    //     sum += a_i.pow(2) * (n as i64 - 1);
+    // }
+    // let mut c = 0;
+    // for i in (1..n).rev() {
+    //     c += a[i];
+    //     sum -= 2 * a[i - 1] * c;
+    // }
+    // let ans = sum;
+    // println!("{}", ans);
 }
