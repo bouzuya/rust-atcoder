1858c243fd980fb32430992ce66201c4f385d50a
bouzuya <m@bouzuya.net>
2021-03-11 20:37:56 +0900

    dwacon5th-prelims a

--- dwacon5th-prelims/src/bin/a.rs
+++ dwacon5th-prelims/src/bin/a.rs
@@ -1,4 +1,3 @@
-use ordered_float::OrderedFloat;
 use proconio::input;
 
 fn main() {
@@ -6,13 +5,12 @@ fn main() {
         n: usize,
         a: [i64; n],
     };
-    let sum = a.iter().sum::<i64>();
-    let avg = sum as f64 / n as f64;
+    let avg = a.iter().sum::<i64>();
     let mut b = a
         .iter()
         .enumerate()
-        .map(|(i, &a_i)| (OrderedFloat((avg - a_i as f64).abs()), i))
-        .collect::<Vec<(OrderedFloat<f64>, usize)>>();
+        .map(|(i, &a_i)| ((avg - n as i64 * a_i).abs(), i))
+        .collect::<Vec<(i64, usize)>>();
     b.sort();
     let ans = b[0].1;
     println!("{}", ans);
