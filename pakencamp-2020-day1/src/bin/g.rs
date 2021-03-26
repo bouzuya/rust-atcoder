619a1a4364b8503c66e6fb7bb338ead4fb305f5b
bouzuya <m@bouzuya.net>
2021-03-26 21:28:35 +0900

    pakencamp_2020_day1 g

--- pakencamp-2020-day1/src/bin/g.rs
+++ pakencamp-2020-day1/src/bin/g.rs
@@ -1,11 +1,34 @@
 use proconio::input;
 use proconio::marker::Usize1;
+use std::cmp::max;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        lrx: [(Usize1, Usize1, usize); m],
     };
-    let ans = n - a.len();
+    let mut max_bit_count = -1;
+    for bits in 0..1 << n {
+        let mut bit_count = 0;
+        let mut c = vec![0; n + 1];
+        for i in 0..n {
+            let b = (bits >> i) & 1 == 1;
+            c[i + 1] = c[i] + if b { 1 } else { 0 };
+            if b {
+                bit_count += 1;
+            }
+        }
+        let mut ok = true;
+        for &(l_i, r_i, x_i) in lrx.iter() {
+            if c[r_i + 1] - c[l_i] != x_i {
+                ok = false;
+            }
+        }
+        if ok {
+            max_bit_count = max(max_bit_count, bit_count);
+        }
+    }
+    let ans = max_bit_count;
     println!("{}", ans);
 }
