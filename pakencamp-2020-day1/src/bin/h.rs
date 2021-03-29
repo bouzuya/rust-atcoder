f11d1a5d359871f0c96d40673391042c956b21e0
bouzuya <m@bouzuya.net>
2021-03-29 22:39:10 +0900

    pakencamp_2020_day1 h

--- pakencamp-2020-day1/src/bin/h.rs
+++ pakencamp-2020-day1/src/bin/h.rs
@@ -1,11 +1,13 @@
+// <https://atcoder.jp/contests/pakencamp-2020-day1/editorial/516>
 use proconio::input;
-use proconio::marker::Usize1;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        t: usize,
+        abc: [(u64, u64, u64); t],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    for &(a, b, c) in abc.iter() {
+        let ans = ((a & b) == b) && (((b & c) == b) || ((b & c) == 0)) && ((a | c) == a);
+        println!("{}", if ans { "Yes" } else { "No" });
+    }
 }
