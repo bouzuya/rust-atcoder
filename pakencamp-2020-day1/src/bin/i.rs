dfbdcfb34df8dd8c2255d41d14db1fe5260f86f7
bouzuya <m@bouzuya.net>
2021-03-30 21:14:25 +0900

    pakencamp_2020_day1 i

--- pakencamp-2020-day1/src/bin/i.rs
+++ pakencamp-2020-day1/src/bin/i.rs
@@ -1,11 +1,52 @@
 use proconio::input;
-use proconio::marker::Usize1;
+use proconio::marker::{Chars, Usize1};
+use std::collections::VecDeque;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        h: usize,
+        w: usize,
+        sx: Usize1,
+        sy: Usize1,
+        gx: Usize1,
+        gy: Usize1,
+        s: [Chars; h],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let inf = h * w + 1;
+    let mut d = vec![vec![vec![inf; 2]; w]; h];
+    let mut q = VecDeque::new();
+    q.push_back((sx, sy, 0)); // v = 0 // |
+    q.push_back((sx, sy, 1)); // v = 1 // -
+    d[sx][sy][0] = 0;
+    d[sx][sy][1] = 0;
+    while let Some((x, y, v)) = q.pop_front() {
+        let dir = vec![(-1, 0), (0, -1), (1, 0), (0, 1)];
+        for (i, &(dx, dy)) in dir.iter().enumerate() {
+            match (v == 0, i % 2 == 0) {
+                (false, false) | (true, true) => {
+                    let (nx, ny) = (x as i64 + dx, y as i64 + dy);
+                    if !(0..h as i64).contains(&nx) {
+                        continue;
+                    }
+                    if !(0..w as i64).contains(&ny) {
+                        continue;
+                    }
+                    let (nx, ny, nv) = (nx as usize, ny as usize, (v + 1) % 2);
+                    if s[nx][ny] == '#' {
+                        continue;
+                    }
+                    if d[nx][ny][nv] != inf {
+                        continue;
+                    }
+                    d[nx][ny][nv] = d[x][y][v] + 1;
+                    q.push_back((nx, ny, nv));
+                }
+                _ => {
+                    continue;
+                }
+            }
+        }
+    }
+    let ans = *d[gx][gy].iter().min().unwrap();
+    println!("{}", if ans == inf { -1 } else { ans as i64 });
 }
