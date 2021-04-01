d7e7bac8b0cffc0ffbaff19c9bdf33d049502a44
bouzuya <m@bouzuya.net>
2021-04-01 22:14:17 +0900

    utpc2011 b

--- utpc2011/src/bin/b.rs
+++ utpc2011/src/bin/b.rs
@@ -1,11 +1,34 @@
 use proconio::input;
-use proconio::marker::Usize1;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        s: Chars,
     };
-    let ans = n - a.len();
+    let n = s.len();
+    if n == 0 {
+        println!("{}", 0);
+        return;
+    }
+    let mut count = if n % 2 == 0 {
+        0
+    } else {
+        if vec!['i', 'w'].contains(&s[n / 2]) {
+            0
+        } else {
+            1
+        }
+    };
+    for i in 0..n / 2 {
+        let j = n - i - 1;
+        match (s[i], s[j]) {
+            ('i', 'i') => continue,
+            ('w', 'w') => continue,
+            ('(', ')') => continue,
+            (')', '(') => continue,
+            _ => count += 1,
+        }
+    }
+    let ans = count;
     println!("{}", ans);
 }
