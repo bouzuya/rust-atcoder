9c49138dd668a59f0d93fa3190f005f3c2e7b141
bouzuya <m@bouzuya.net>
2021-04-05 22:13:40 +0900

    past202012-open f

--- past202012-open/src/bin/f.rs
+++ past202012-open/src/bin/f.rs
@@ -1,11 +1,37 @@
 use proconio::input;
 use proconio::marker::Usize1;
+use std::cmp::max;
+use std::collections::BTreeSet;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        abc: [(Usize1, Usize1, Usize1); m],
     };
-    let ans = n - a.len();
+    let mut max_count = 0;
+    for bits in 0..1 << n {
+        let select = (0..n).map(|i| (bits >> i) & 1 == 1).collect::<Vec<bool>>();
+        let mut is_ok = true;
+        let mut set = BTreeSet::new();
+        for &(a_i, b_i, c_i) in abc.iter() {
+            let a = select[a_i];
+            let b = select[b_i];
+            let c = select[c_i];
+            if a && b && c {
+                is_ok = false;
+            } else if a && b && !c {
+                set.insert(c_i);
+            } else if a && !b && c {
+                set.insert(b_i);
+            } else if !a && b && c {
+                set.insert(a_i);
+            }
+        }
+        if is_ok {
+            max_count = max(max_count, set.len());
+        }
+    }
+    let ans = max_count;
     println!("{}", ans);
 }
