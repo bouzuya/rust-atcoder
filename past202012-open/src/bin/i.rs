71f0fbb5ab642e271d7308ca49642069c214c4a6
bouzuya <m@bouzuya.net>
2021-04-08 21:48:58 +0900

    past202012-open i

--- past202012-open/src/bin/i.rs
+++ past202012-open/src/bin/i.rs
@@ -1,11 +1,48 @@
+use std::cmp::min;
+
 use proconio::input;
 use proconio::marker::Usize1;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        k: usize,
+        h: [u64; n],
+        c: [Usize1; k],
+        ab: [(Usize1, Usize1); m],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut e_in = vec![vec![]; n];
+    for &(a_i, b_i) in ab.iter() {
+        if h[a_i] > h[b_i] {
+            e_in[b_i].push(a_i);
+        } else if h[a_i] < h[b_i] {
+            e_in[a_i].push(b_i);
+        } else {
+            unreachable!("invalid input");
+        }
+    }
+
+    let inf = m + 1;
+    let mut d = vec![inf; n];
+    for &c_i in c.iter() {
+        d[c_i] = 0;
+    }
+
+    let mut hi = h
+        .iter()
+        .enumerate()
+        .map(|(i, &h_i)| (h_i, i))
+        .collect::<Vec<(u64, usize)>>();
+    hi.sort();
+    for &(_, v) in hi.iter() {
+        for &u in e_in[v].iter() {
+            d[u] = min(d[u], d[v] + 1);
+        }
+    }
+
+    for &d_i in d.iter() {
+        println!("{}", if d_i == inf { -1 } else { d_i as i64 });
+    }
 }
