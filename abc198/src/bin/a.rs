02d2a5082a98bbebe606d64639bf4aab7ca01452
bouzuya <m@bouzuya.net>
2021-04-13 22:01:34 +0900

    abc198 a, e

--- abc198/src/bin/a.rs
+++ abc198/src/bin/a.rs
@@ -4,13 +4,6 @@ fn main() {
     input! {
         n: usize,
     };
-    let mut count = 0;
-    for i in 1..=n {
-        let j = n - i;
-        if i > 0 && j > 0 {
-            count += 1;
-        }
-    }
-    let ans = count;
+    let ans = n - 1;
     println!("{}", ans);
 }
--- abc198/src/bin/e.rs
+++ abc198/src/bin/e.rs
@@ -1,11 +1,43 @@
 use proconio::input;
 use proconio::marker::Usize1;
 
+fn adjacency_list(n: usize, uv: &Vec<(usize, usize)>) -> Vec<Vec<usize>> {
+    let mut e = vec![vec![]; n];
+    for &(u, v) in uv.iter() {
+        e[u].push(v);
+        e[v].push(u);
+    }
+    e
+}
+
+fn dfs(e: &Vec<Vec<usize>>, c: &Vec<usize>, color: &mut Vec<usize>, good: &mut Vec<bool>, u: usize, p: usize) {
+    if color[c[u]] > 0 {
+        good[u] = false;
+    }
+    color[c[u]] += 1;
+    for &v in e[u].iter() {
+        if v != p {
+            dfs(e, c, color, good, v, u);
+        }
+    }
+    color[c[u]] -= 1;
+}
+
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        c: [Usize1; n],
+        ab: [(Usize1, Usize1); n - 1],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let e = adjacency_list(n, &ab);
+    let mut good = vec![true; n];
+    let max_c = 100_000_usize;
+    let mut color = vec![0; max_c];
+    dfs(&e, &c, &mut color, &mut good, 0, 0);
+    for (i, &is_good) in good.iter().enumerate() {
+        if is_good {
+            println!("{}", i + 1);
+        }
+    }
 }
