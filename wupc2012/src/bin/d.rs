e3cdbe43b4e8a5d3546477e3fedcca3d3af875fa
bouzuya <m@bouzuya.net>
2021-04-26 19:45:35 +0900

    wupc2012 d

--- wupc2012/src/bin/d.rs
+++ wupc2012/src/bin/d.rs
@@ -1,11 +1,21 @@
 use proconio::input;
-use proconio::marker::Usize1;
+use std::cmp::max;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
     };
-    let ans = n - a.len();
+    let mut dp = vec![0];
+    for i in 0..n {
+        let p = dp.clone();
+        input! {
+            a_i: [u64; i + 1],
+        }
+        for (j, &a_ij) in a_i.iter().enumerate() {
+            dp[j] = max(if j > 0 { p[j - 1] + a_ij } else { 0 }, p[j] + a_ij);
+        }
+        dp.push(a_i[a_i.len() - 1]);
+    }
+    let ans = *dp.iter().max().unwrap();
     println!("{}", ans);
 }
