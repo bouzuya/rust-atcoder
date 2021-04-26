599cb983ef3ea7982ccd68a0ce53b074c8ffc6c6
bouzuya <m@bouzuya.net>
2021-04-26 21:33:23 +0900

    typical90 002, 003

--- typical90/src/bin/002.rs
+++ typical90/src/bin/002.rs
@@ -1,11 +1,35 @@
 use proconio::input;
-use proconio::marker::Usize1;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut s = vec![];
+    for bits in 0..1 << n {
+        let ps = (0..n)
+            .map(|i| if (bits >> i) & 1 == 1 { 1 } else { -1 })
+            .collect::<Vec<i64>>();
+        let mut ok = true;
+        let mut sum = 0;
+        for &p_i in ps.iter() {
+            sum += p_i;
+            if sum < 0 {
+                ok = false;
+            }
+        }
+        if sum != 0 {
+            ok = false;
+        }
+        if ok {
+            let t = ps
+                .iter()
+                .map(|&p_i| if p_i == 1 { '(' } else { ')' })
+                .collect::<String>();
+            s.push(t);
+        }
+    }
+    s.sort();
+    for s_i in s.iter() {
+        println!("{}", s_i);
+    }
 }
--- typical90/src/bin/003.rs
+++ typical90/src/bin/003.rs
@@ -1,11 +1,48 @@
+use std::collections::VecDeque;
+
 use proconio::input;
 use proconio::marker::Usize1;
+
+fn adjacency_list(n: usize, uv: &Vec<(usize, usize)>) -> Vec<Vec<usize>> {
+    let mut e = vec![vec![]; n];
+    for &(u, v) in uv.iter() {
+        e[u].push(v);
+        e[v].push(u);
+    }
+    e
+}
+
+fn shortest_path(n: usize, e: &Vec<Vec<usize>>, s: usize) -> Vec<Option<usize>> {
+    let mut d = vec![None; n];
+    let mut q = VecDeque::new();
+    d[s] = Some(0);
+    q.push_back((s, s));
+    while let Some((u, p)) = q.pop_front() {
+        for &v in e[u].iter() {
+            if v == p {
+                continue;
+            }
+            if d[v].is_none() {
+                let d_v = d[u].unwrap() + 1;
+                d[v] = Some(d_v);
+                q.push_back((v, u));
+            }
+        }
+    }
+    d
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        ab: [(Usize1, Usize1); n - 1],
     };
-    let ans = n - a.len();
+    let e = adjacency_list(n, &ab);
+    let d = shortest_path(n, &e, 0);
+    let max_d = d.iter().max_by_key(|d_i| d_i.unwrap()).unwrap().unwrap();
+    let s = d.iter().position(|d_i| d_i.unwrap() == max_d).unwrap();
+    let d = shortest_path(n, &e, s);
+    let max_d = d.iter().max_by_key(|d_i| d_i.unwrap()).unwrap().unwrap();
+    let ans = max_d + 1;
     println!("{}", ans);
 }
