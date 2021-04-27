b47b0327aa26d47d88151750b5aa88dd24593740
bouzuya <m@bouzuya.net>
2021-04-27 20:59:28 +0900

    typical90 004, 006

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -1,11 +1,35 @@
 use proconio::input;
-use proconio::marker::Usize1;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        h: usize,
+        w: usize,
+        a: [[i64; w]; h],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut s_h = vec![];
+    for i in 0..h {
+        let mut s = 0;
+        for j in 0..w {
+            s += a[i][j];
+        }
+        s_h.push(s);
+    }
+    let mut s_w = vec![];
+    for j in 0..w {
+        let mut s = 0;
+        for i in 0..h {
+            s += a[i][j];
+        }
+        s_w.push(s);
+    }
+
+    for i in 0..h {
+        for j in 0..w {
+            print!(
+                "{}{}",
+                s_h[i] + s_w[j] - a[i][j],
+                if j == w - 1 { "\n" } else { " " }
+            );
+        }
+    }
 }
--- typical90/src/bin/006.rs
+++ typical90/src/bin/006.rs
@@ -1,11 +1,31 @@
 use proconio::input;
-use proconio::marker::Usize1;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        k: usize,
+        s: Chars,
     };
-    let ans = n - a.len();
+    let chars = (0..26).map(|i| (b'a' + i) as char).collect::<Vec<char>>();
+    let mut p = vec![vec![n; chars.len()]; n + 1];
+    for (i, &s_i) in s.iter().enumerate().rev() {
+        for (j, &c) in chars.iter().enumerate() {
+            p[i][j] = if c == s_i { i } else { p[i + 1][j] };
+        }
+    }
+
+    let mut i = 0;
+    let mut t = vec![];
+    while t.len() < k {
+        for j in 0..26 {
+            if p[i][j] <= n - (k - t.len()) {
+                t.push((b'a' + j as u8) as char);
+                i = p[i][j] + 1;
+                break;
+            }
+        }
+    }
+    let ans = t.iter().collect::<String>();
     println!("{}", ans);
 }
