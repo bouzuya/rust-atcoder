412a85fad5671939d9c720f10f297835c47a2c9e
bouzuya <m@bouzuya.net>
2021-04-29 13:03:10 +0900

    typical90 010, 011

--- typical90/src/bin/010.rs
+++ typical90/src/bin/010.rs
@@ -4,8 +4,26 @@ use proconio::marker::Usize1;
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        cp: [(Usize1, i64); n],
+        q: usize,
+        lr: [(Usize1, Usize1); q],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut c = vec![vec![0; n + 1]; 2];
+    for (i, &(c_i, p_i)) in cp.iter().enumerate() {
+        c[c_i][i + 1] = p_i;
+    }
+    for i in 0..n {
+        for j in 0..2 {
+            c[j][i + 1] += c[j][i];
+        }
+    }
+
+    let mut s = String::new();
+    for (l_i, r_i) in lr {
+        let sum1 = c[0][r_i + 1] - c[0][l_i];
+        let sum2 = c[1][r_i + 1] - c[1][l_i];
+        s += &format!("{} {}\n", sum1, sum2);
+    }
+    print!("{}", s);
 }
--- typical90/src/bin/011.rs
+++ typical90/src/bin/011.rs
@@ -1,11 +1,41 @@
+// 解説 AC <https://twitter.com/e869120/status/1381064464049401856>
 use proconio::input;
-use proconio::marker::Usize1;
+
+macro_rules! chmax {
+    ($max_v: expr, $v: expr) => {
+        if $v > $max_v {
+            $max_v = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        dcs: [(usize, usize, u64); n],
     };
-    let ans = n - a.len();
+    let max_d = 5_000;
+    let mut dcs = dcs
+        .iter()
+        .filter(|&(d_i, c_i, _)| c_i <= d_i)
+        .cloned()
+        .collect::<Vec<(usize, usize, u64)>>();
+    dcs.sort_by_key(|&(d_i, _, _)| d_i);
+
+    let mut dp = vec![vec![0_u64; 2]; max_d + 1];
+    for (d_i, c_i, s_i) in dcs.iter() {
+        for d in (0..max_d).rev() {
+            if d <= d_i - c_i {
+                chmax!(dp[d + c_i][0], dp[d][0] + s_i);
+            }
+        }
+        for d in 0..max_d {
+            chmax!(dp[d + 1][0], dp[d][0]);
+        }
+    }
+    let ans = dp[max_d][0];
     println!("{}", ans);
 }
