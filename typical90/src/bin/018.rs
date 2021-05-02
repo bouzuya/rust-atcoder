c93feb8eb09f21a3cf80b004e8d68fbdf5526378
bouzuya <m@bouzuya.net>
2021-05-02 14:15:48 +0900

    typical90 018, 019

--- typical90/src/bin/018.rs
+++ typical90/src/bin/018.rs
@@ -1,11 +1,22 @@
 use proconio::input;
-use proconio::marker::Usize1;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        t: f64,
+        l: f64,
+        x: f64,
+        y: f64,
+        q: usize,
+        e: [f64; q],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    for e_i in e {
+        let r = l / 2_f64;
+        let x1 = 0_f64;
+        let y1 = -r * ((e_i / t) * 2_f64 * std::f64::consts::PI).sin();
+        let z1 = r - r * ((e_i / t) * 2_f64 * std::f64::consts::PI).cos();
+
+        let d = ((x - x1) * (x - x1) + (y - y1) * (y - y1)).sqrt();
+        let ans = z1.atan2(d) * 360_f64 / 2_f64 / std::f64::consts::PI;
+        println!("{}", ans);
+    }
 }
--- typical90/src/bin/019.rs
+++ typical90/src/bin/019.rs
@@ -1,11 +1,38 @@
+// 区間 DP
+// 解説 AC <https://twitter.com/e869120/status/1384638694162780166>
 use proconio::input;
-use proconio::marker::Usize1;
+
+macro_rules! chmin {
+    ($min_v: expr, $v: expr) => {
+        if $v < $min_v {
+            $min_v = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [i64; 2 * n],
     };
-    let ans = n - a.len();
+    let inf = 1_000_000_000_i64;
+    let mut dp = vec![vec![inf; 2 * n]; 2 * n];
+    // l = 1
+    for i in 0..2 * n - 1 {
+        dp[i][i + 1] = (a[i] - a[i + 1]).abs();
+    }
+    for len in (3..2 * n).step_by(2) {
+        for l in 0..2 * n - len {
+            let r = l + len;
+            chmin!(dp[l][r], dp[l + 1][r - 1] + (a[l] - a[r]).abs());
+            for k in l + 1..r {
+                chmin!(dp[l][r], dp[l][k] + dp[k + 1][r]);
+            }
+        }
+    }
+    let ans = dp[0][2 * n - 1];
     println!("{}", ans);
 }
