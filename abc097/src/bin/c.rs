d443756ed9c4f99857c88e3362f66b4d70d679f9
bouzuya <m@bouzuya.net>
2021-05-04 19:08:39 +0900

    abc097 c

--- abc097/src/bin/c.rs
+++ abc097/src/bin/c.rs
@@ -1,44 +1,20 @@
 use std::collections::BTreeSet;
 
-use proconio::input;
-
-fn dfs(chars: &Vec<char>, l: usize, b: &mut String, res: &mut Vec<String>) {
-    if b.len() == l {
-        res.push(b.clone());
-        return;
-    }
-    for &c in chars {
-        b.push(c);
-        dfs(chars, l, b, res);
-        b.pop();
-    }
-}
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        s: String,
+        s: Chars,
         k: usize,
     };
-    let chars = s
-        .chars()
-        .collect::<BTreeSet<char>>()
-        .iter()
-        .cloned()
-        .collect::<Vec<char>>();
-    let mut t = vec![];
-    for l in 1..=k {
-        dfs(&chars, l, &mut String::new(), &mut t);
-    }
-    t.sort();
-
-    let mut c = 0;
-    for t_i in t.iter() {
-        if s.contains(t_i.as_str()) {
-            c += 1;
-            if c == k {
-                println!("{}", t_i);
-                break;
-            }
+    let mut t = BTreeSet::new();
+    for l in 1..=5 {
+        if s.len() < l {
+            continue;
+        }
+        for i in 0..s.len() - l + 1 {
+            t.insert(s[i..i + l].iter().cloned().collect::<String>());
         }
     }
+    println!("{}", t.iter().skip(k - 1).next().unwrap());
 }
