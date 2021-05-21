13775835faba236ec766548c2e2551efbf27c72d
bouzuya <m@bouzuya.net>
2021-05-21 21:28:36 +0900

    abc146 f

--- abc146/src/bin/f.rs
+++ abc146/src/bin/f.rs
@@ -1,10 +1,33 @@
-use proconio::{input, marker::Usize1};
+use std::cmp;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        _n: usize,
+        m: usize,
+        mut s: Chars,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    s.reverse();
+    let mut steps = vec![];
+    let mut i = 0;
+    'outer: loop {
+        for j in (i + 1..cmp::min(i + 1 + m, s.len())).rev() {
+            if s[j] == '0' {
+                steps.push(j - i);
+                i = j;
+                if j == s.len() - 1 {
+                    break 'outer;
+                } else {
+                    continue 'outer;
+                }
+            }
+        }
+        println!("-1");
+        return;
+    }
+    steps.reverse();
+    for (i, &s_i) in steps.iter().enumerate() {
+        print!("{}{}", s_i, if i == steps.len() - 1 { "\n" } else { " " });
+    }
 }
