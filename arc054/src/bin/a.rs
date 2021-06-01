4bd7e6913b61de36b847f4582ff063381681d62c
bouzuya <m@bouzuya.net>
2021-06-01 21:35:30 +0900

    arc054 a, b

--- arc054/src/bin/a.rs
+++ arc054/src/bin/a.rs
@@ -2,29 +2,28 @@ use proconio::input;
 
 fn main() {
     input! {
-        l: i64,
-        x: i64,
-        y: i64,
-        s: i64,
-        d: i64,
+        l: usize,
+        x: usize,
+        y: usize,
+        s: usize,
+        d: usize,
     };
-    let max = l as f64 / x as f64;
-    let ans = if s <= d {
-        let cw = (d - s) as f64 / (x + y) as f64;
-        let ccw = if x >= y {
-            max
-        } else {
-            (s + l - d) as f64 / (y - x) as f64
-        };
-        cw.min(ccw)
-    } else {
-        let cw = (l - s + d) as f64 / (x + y) as f64;
-        let ccw = if x >= y {
-            max
-        } else {
-            (s - d) as f64 / (y - x) as f64
-        };
-        cw.min(ccw)
-    };
-    println!("{:0.10}", ans);
+    if d == s {
+        println!("0");
+        return;
+    }
+    let mut ans = l as f64 / x as f64;
+    if x < y {
+        // 反時計回り
+        let da = y - x;
+        let la = if d < s { s - d } else { l + s - d };
+        let ta = la as f64 / da as f64;
+        ans = if ta < ans { ta } else { ans };
+    }
+    // 時計回り
+    let db = x + y;
+    let lb = if d < s { l - s + d } else { d - s };
+    let tb = lb as f64 / db as f64;
+    ans = if tb < ans { tb } else { ans };
+    println!("{}", ans);
 }
--- arc054/src/bin/b.rs
+++ arc054/src/bin/b.rs
@@ -1,11 +1,21 @@
 use proconio::input;
-use proconio::marker::Usize1;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        p: f64,
     };
-    let ans = n - a.len();
+    let f = |x: f64| x + p / f64::powf(2_f64, x / 1.5_f64);
+    let mut l = 0_f64;
+    let mut r = p;
+    for _ in 0..200 {
+        let p1 = l + (r - l) / 3_f64;
+        let p2 = r - (r - l) / 3_f64;
+        if f(p1) < f(p2) {
+            r = p2;
+        } else {
+            l = p1;
+        }
+    }
+    let ans = f(l);
     println!("{}", ans);
 }
