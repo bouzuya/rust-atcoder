8d9a1bf016391287740fad78e3edbf8a90c52e75
bouzuya <m@bouzuya.net>
2021-06-24 20:17:50 +0900

    agc024 c

--- agc024/src/bin/c.rs
+++ agc024/src/bin/c.rs
@@ -1,11 +1,24 @@
 use proconio::input;
-use proconio::marker::Usize1;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
+    let mut count = 0;
+    let mut a_p = 0;
+    for (i, &a_i) in a.iter().enumerate() {
+        if a_i > i || (a_i as i64 - a_p as i64) > 1 {
+            println!("-1");
+            return;
+        }
+        if a_i == a_p + 1 {
+            count += 1;
+        } else {
+            count += a_i;
+        }
+        a_p = a_i;
+    }
+    let ans = count;
     println!("{}", ans);
 }
