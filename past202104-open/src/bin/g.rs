a7e6e235983feff77aaf5e462526d5c5fe6aa495
bouzuya <m@bouzuya.net>
2021-06-29 22:44:36 +0900

    past202104-open g, h

--- past202104-open/src/bin/g.rs
+++ past202104-open/src/bin/g.rs
@@ -1,10 +1,51 @@
+use std::{
+    cmp,
+    collections::{BTreeSet, BinaryHeap},
+};
+
 use proconio::{input, marker::Usize1};
+
+fn adjacency_list(n: usize, uvw: &[(usize, usize, u64)]) -> Vec<Vec<(usize, u64)>> {
+    let mut e = vec![vec![]; n];
+    for &(u, v, w) in uvw.iter() {
+        e[u].push((v, w));
+        e[v].push((u, w));
+    }
+    e
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        q: usize,
+        abc: [(Usize1, Usize1, u64); m],
+        x: [u64; q],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let e = adjacency_list(n, &abc);
+    let mut heap = BinaryHeap::new();
+    let mut set = BTreeSet::new();
+    set.insert(0);
+    for &(v, w) in e[0].iter() {
+        heap.push((cmp::Reverse(w), v));
+    }
+    for x_i in x {
+        let mut next = vec![];
+        while let Some((cmp::Reverse(w), v)) = heap.pop() {
+            if w > x_i {
+                heap.push((cmp::Reverse(w), v));
+                break;
+            }
+            if set.insert(v) {
+                for &(nv, nw) in e[v].iter() {
+                    next.push((cmp::Reverse(nw), nv));
+                }
+            }
+        }
+        for x in next {
+            heap.push(x);
+        }
+        println!("{}", set.len());
+    }
 }
--- past202104-open/src/bin/h.rs
+++ past202104-open/src/bin/h.rs
@@ -1,10 +1,57 @@
-use proconio::{input, marker::Usize1};
+use std::cmp;
+
+use proconio::input;
+
+fn cumsum(a: &[usize]) -> Vec<usize> {
+    std::iter::once(0)
+        .chain(a.iter().scan(0, |acc, &i| {
+            *acc += i;
+            Some(*acc)
+        }))
+        .collect()
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        k: usize,
+        q: usize,
+        pt: [(usize, usize); n],
     };
-    let ans = n - a.len();
+    let a = {
+        let mut a = pt
+            .iter()
+            .filter(|(_, t_i)| *t_i == 0)
+            .map(|(p_i, _)| *p_i)
+            .collect::<Vec<usize>>();
+        a.sort();
+        a.into_iter().take(m).collect::<Vec<usize>>()
+    };
+    let b = {
+        let mut b = pt
+            .iter()
+            .filter(|(_, t_i)| *t_i == 1)
+            .map(|(p_i, _)| *p_i)
+            .collect::<Vec<usize>>();
+        b.sort();
+        b.into_iter().take(m).collect::<Vec<usize>>()
+    };
+
+    let min_i = if a.len() < m { m - a.len() } else { 0 };
+    let mut s_a = cumsum(&a);
+    for i in a.len()..m {
+        s_a.push(s_a[i] + b[i - a.len()]);
+    }
+    let mut s_b = cumsum(&b);
+    while s_b.len() <= m {
+        s_b.push(1_000_000_000_000_000);
+    }
+    let mut min = 1_000_000_000_000_000;
+    for i in min_i..=m {
+        min = cmp::min(min, s_a[m - i] + s_b[i] + (i + k - 1) / k * q);
+    }
+
+    let ans = min;
     println!("{}", ans);
 }
