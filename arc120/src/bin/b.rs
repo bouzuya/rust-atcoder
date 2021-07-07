a2fe4e4d491cabf10c6d1f8dc8aad1b91531bd17
bouzuya <m@bouzuya.net>
2021-07-07 21:56:18 +0900

    arc120 b

--- arc120/src/bin/b.rs
+++ arc120/src/bin/b.rs
@@ -1,10 +1,34 @@
-use proconio::{input, marker::Usize1};
+use std::cmp;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        h: usize,
+        w: usize,
+        s: [Chars; h],
     };
-    let ans = n - a.len();
+    let p = 998_244_353;
+    let mut ans = 1_usize;
+    for d in 0..h + w - 1 {
+        let mut b = (false, false);
+        for i in 0..=cmp::min(d, h - 1) {
+            let j = d - i;
+            if i < h && j < w {
+                match s[i][j] {
+                    'B' => b.0 = true,
+                    'R' => b.1 = true,
+                    '.' => {}
+                    _ => unreachable!(),
+                }
+            }
+        }
+        match b {
+            (true, true) => ans *= 0,
+            (true, false) | (false, true) => ans *= 1,
+            (false, false) => ans *= 2,
+        }
+        ans %= p;
+    }
     println!("{}", ans);
 }
