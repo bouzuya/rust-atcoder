ae768668f22eb209e0372bc3cfddfa101bc58500
bouzuya <m@bouzuya.net>
2021-07-10 15:06:51 +0900

    typical90 081

--- typical90/src/bin/081.rs
+++ typical90/src/bin/081.rs
@@ -1,11 +1,38 @@
+use std::cmp;
+
 use proconio::input;
-use proconio::marker::Usize1;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        k: usize,
+        ab: [(usize, usize); n],
     };
-    let ans = n - a.len();
+    let max = 5_000;
+    let mut s = vec![vec![0; max + 1 + 1]; max + 1 + 1];
+    for (a_i, b_i) in ab.iter().copied() {
+        s[a_i][b_i] += 1;
+    }
+
+    for i in 0..max + 1 + 1 {
+        for j in 0..max + 1 {
+            s[i][j + 1] += s[i][j];
+        }
+    }
+    for j in 0..max + 1 + 1 {
+        for i in 0..max + 1 {
+            s[i + 1][j] += s[i][j];
+        }
+    }
+
+    let mut ans = 0;
+    for i in 1..max + 1 + 1 {
+        for j in 1..max + 1 + 1 {
+            let (t, b) = (i - 1, cmp::min(i + k, max + 1));
+            let (l, r) = (j - 1, cmp::min(j + k, max + 1));
+            let a = s[t][l] + s[b][r] - s[t][r] - s[b][l];
+            ans = cmp::max(ans, a);
+        }
+    }
     println!("{}", ans);
 }
