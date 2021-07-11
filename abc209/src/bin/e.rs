6c9f8a4741c18c3e19a95834292f7238f0728a55
bouzuya <m@bouzuya.net>
2021-07-11 16:25:37 +0900

    abc209 e

--- abc209/src/bin/e.rs
+++ abc209/src/bin/e.rs
@@ -1,41 +1,12 @@
-// WA
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet, VecDeque};
 
 use proconio::{input, marker::Chars};
 
-fn adjacency_list(n: usize, uv: &[(usize, usize)]) -> Vec<Vec<usize>> {
-    let mut e = vec![vec![]; n];
-    for &(u, v) in uv.iter() {
-        e[u].push(v);
-    }
-    e
-}
-
-fn dfs(e: &[Vec<usize>], r: &mut Vec<usize>, used: &mut Vec<usize>, u: usize) {
-    if e[u].is_empty() {
-        r[u] = 2; // lose
-        return;
-    }
-    if used[u] > 2 {
-        return;
-    }
-    if used[u] == 1 {
-        r[u] = 3; // draw
-    }
-    used[u] += 1;
-
-    let mut win = false;
-    for v in e[u].iter().copied() {
-        dfs(e, r, used, v);
-
-        if r[v] == 2 {
-            r[u] = 1; // win
-            win = true;
-        }
-    }
-    if r[u] == 3 && win {
-        r[u] = 1;
-    }
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum R {
+    Win,
+    Lose,
+    Draw,
 }
 
 fn main() {
@@ -43,40 +14,77 @@ fn main() {
         n: usize,
         s: [Chars; n],
     };
-    let mut s2 = vec![(n, n); n];
+    let s = s
+        .into_iter()
+        .map(|s_i| {
+            let head = &s_i[0..3];
+            let tail = &s_i[s_i.len() - 3..s_i.len()];
+            (
+                head.iter().collect::<String>(),
+                tail.iter().collect::<String>(),
+            )
+        })
+        .collect::<Vec<(String, String)>>();
+
+    let mut set = BTreeSet::new();
+    for (head, tail) in s.iter() {
+        set.insert(head.as_str());
+        set.insert(tail.as_str());
+    }
     let mut map = BTreeMap::new();
-    for (i, s_i) in s.iter().enumerate() {
-        let head = &s_i[0..3];
-        let tail = &s_i[s_i.len() - 3..s_i.len()];
-        let len = map.len();
-        let eh = *map.entry(head).or_insert(len);
-        let len = map.len();
-        let et = *map.entry(tail).or_insert(len);
-        s2[i] = (eh, et);
-    }
-    let v_len = map.len();
-    let mut v = vec![String::new(); v_len];
-    for (k, j) in map {
-        v[j] = k.iter().collect::<String>();
+    for (i, &k) in set.iter().enumerate() {
+        map.insert(k, i);
     }
 
-    let e = adjacency_list(v_len, &s2);
+    let mut e_inv = vec![vec![]; map.len()];
+    let mut e_count = vec![0; map.len()];
+    for (head, tail) in s.iter() {
+        let u = map[head.as_str()];
+        let v = map[tail.as_str()];
+        e_inv[v].push(u);
+        e_count[u] += 1;
+    }
 
-    let mut r = vec![0; v_len];
-    let mut used = vec![0; v_len];
-    dfs(&e, &mut r, &mut used, 0);
-    // eprintln!("s2 = {:?}", s2);
-    // eprintln!("v  = {:?}", v);
-    // eprintln!("e  = {:?}", e);
+    let mut r = vec![R::Draw; map.len()];
+    let mut q = VecDeque::new();
+    for u in 0..map.len() {
+        if e_count[u] == 0 {
+            r[u] = R::Lose;
+            q.push_back(u);
+        }
+    }
+    while let Some(v) = q.pop_front() {
+        match r[v] {
+            R::Lose => {
+                for u in e_inv[v].iter().copied() {
+                    if r[u] == R::Draw {
+                        r[u] = R::Win;
+                        q.push_back(u);
+                    }
+                }
+            }
+            R::Win => {
+                for u in e_inv[v].iter().copied() {
+                    if r[u] == R::Draw {
+                        e_count[u] -= 1;
+                        if e_count[u] == 0 {
+                            r[u] = R::Lose;
+                            q.push_back(u);
+                        }
+                    }
+                }
+            }
+            R::Draw => unreachable!(),
+        }
+    }
 
-    for i in 0..n {
-        let u = s2[i].1;
+    for (_, tail) in s.iter() {
         println!(
             "{}",
-            match r[u] {
-                1 => "Aoki",
-                2 => "Takahashi",
-                _ => "Draw",
+            match r[map[tail.as_str()]] {
+                R::Win => "Aoki",
+                R::Lose => "Takahashi",
+                R::Draw => "Draw",
             }
         );
     }
