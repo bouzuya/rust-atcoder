00c974d08efea5cebbe6da6d953dcf712d35d564
bouzuya <m@bouzuya.net>
2021-07-21 21:49:34 +0900

    arc119 b

--- arc119/src/bin/b.rs
+++ arc119/src/bin/b.rs
@@ -1,10 +1,33 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        _n: usize,
+        s: Chars,
+        t: Chars,
     };
-    let ans = n - a.len();
+    let s = s
+        .iter()
+        .enumerate()
+        .filter(|(_, &s_i)| s_i == '0')
+        .map(|(i, _)| i)
+        .collect::<Vec<usize>>();
+    let t = t
+        .iter()
+        .enumerate()
+        .filter(|(_, &t_i)| t_i == '0')
+        .map(|(i, _)| i)
+        .collect::<Vec<usize>>();
+    if s.len() != t.len() {
+        println!("-1");
+        return;
+    }
+    let mut count = 0;
+    for (s_i, t_i) in s.iter().copied().zip(t.iter().copied()) {
+        if s_i != t_i {
+            count += 1;
+        }
+    }
+    let ans = count;
     println!("{}", ans);
 }
