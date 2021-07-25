b4cd5732697108e5052316fe7711ef1f41cf10f9
bouzuya <m@bouzuya.net>
2021-07-25 22:39:45 +0900

    pakencamp-2020-day2 b

--- pakencamp-2020-day2/src/bin/b.rs
+++ pakencamp-2020-day2/src/bin/b.rs
@@ -1,10 +1,38 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
+
+macro_rules! chmin {
+    ($min_v: expr, $v: expr) => {
+        if $v < $min_v {
+            $min_v = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        h: usize,
+        w: usize,
+        c: [Chars; h],
     };
-    let ans = n - a.len();
+    let inf = h * w + 1;
+    let mut dp = vec![vec![inf; w]; h];
+    for i in (0..h).rev() {
+        for j in (0..w).rev() {
+            if i == h - 1 && j == w - 1 {
+                dp[h - 1][w - 1] = if c[h - 1][w - 1] == 'E' { 0 } else { 1 };
+            } else {
+                if i + 1 < h {
+                    chmin!(dp[i][j], dp[i + 1][j] + if c[i][j] == 'E' { 1 } else { 0 });
+                }
+                if j + 1 < w {
+                    chmin!(dp[i][j], dp[i][j + 1] + if c[i][j] == 'E' { 0 } else { 1 });
+                }
+            }
+        }
+    }
+    let ans = dp[0][0];
     println!("{}", ans);
 }
