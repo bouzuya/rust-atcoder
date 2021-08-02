5121bee6489687501372b20460e7085df763cad4
bouzuya <m@bouzuya.net>
2021-08-02 21:59:24 +0900

    dwacon5th-prelims b

--- dwacon5th-prelims/src/bin/b.rs
+++ dwacon5th-prelims/src/bin/b.rs
@@ -1,11 +1,43 @@
 use proconio::input;
-use proconio::marker::Usize1;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        k: usize,
+        a: [u64; n],
     };
-    let ans = n - a.len();
+
+    let b = {
+        let s = std::iter::once(0)
+            .chain(a.iter().scan(0, |acc, &i| {
+                *acc += i;
+                Some(*acc)
+            }))
+            .collect::<Vec<u64>>();
+        let mut b = vec![];
+        for i in 0..=n {
+            for j in i + 1..=n {
+                b.push(s[j] - s[i]);
+            }
+        }
+        b
+    };
+
+    let mut res = 0_u64;
+    let mut c = b;
+    for i in (0..64).rev() {
+        let mut next = vec![];
+        for c_i in c.iter().copied() {
+            if ((c_i >> i) & 1) == 1 {
+                next.push(c_i);
+            }
+        }
+        if next.len() >= k {
+            c = next;
+            res += 2_u64.pow(i);
+        }
+    }
+
+    let ans = res;
     println!("{}", ans);
 }
