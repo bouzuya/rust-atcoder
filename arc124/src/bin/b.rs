ec611070419e13b9d6bbd708d1773f67c6ee4c8b
bouzuya <m@bouzuya.net>
2021-08-04 22:13:13 +0900

    arc124 b

--- arc124/src/bin/b.rs
+++ arc124/src/bin/b.rs
@@ -1,10 +1,33 @@
-use proconio::{input, marker::Usize1};
+use std::collections::BTreeSet;
+
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [u64; n],
+        mut b: [u64; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    b.sort();
+
+    let mut set = BTreeSet::new();
+    let a_0 = a[0];
+    for b_j in b.clone() {
+        let x = a_0 ^ b_j;
+        set.insert(x);
+    }
+
+    let mut ans = vec![];
+    for x in set {
+        let mut c = a.iter().copied().map(|a_i| a_i ^ x).collect::<Vec<u64>>();
+        c.sort();
+        if b == c {
+            ans.push(x);
+        }
+    }
+
+    println!("{}", ans.len());
+    for x in ans {
+        println!("{}", x);
+    }
 }
