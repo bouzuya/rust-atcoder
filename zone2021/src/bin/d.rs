ed5912ec426a13edcd39500e7e21477624a7568b
bouzuya <m@bouzuya.net>
2021-08-13 23:00:43 +0900

    zone2021 d

--- zone2021/src/bin/d.rs
+++ zone2021/src/bin/d.rs
@@ -1,10 +1,43 @@
-use proconio::{input, marker::Usize1};
+use std::collections::VecDeque;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        s: Chars,
     };
-    let ans = n - a.len();
+    let mut b = true;
+    let mut t = VecDeque::new();
+    for s_i in s {
+        match s_i {
+            'R' => b = !b,
+            _ => {
+                if b {
+                    if let Some(x) = t.pop_back() {
+                        if x != s_i {
+                            t.push_back(x);
+                            t.push_back(s_i);
+                        }
+                    } else {
+                        t.push_back(s_i);
+                    }
+                } else {
+                    if let Some(x) = t.pop_front() {
+                        if x != s_i {
+                            t.push_front(x);
+                            t.push_front(s_i);
+                        }
+                    } else {
+                        t.push_front(s_i);
+                    }
+                }
+            }
+        }
+    }
+    let ans = if b {
+        t.iter().collect::<String>()
+    } else {
+        t.iter().rev().collect::<String>()
+    };
     println!("{}", ans);
 }
