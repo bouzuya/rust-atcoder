93ec7a8b42034381e8bf0c805cd797a52b247060
bouzuya <m@bouzuya.net>
2021-08-15 11:07:40 +0900

    abc214 c, d, e

--- abc214/src/bin/c.rs
+++ abc214/src/bin/c.rs
@@ -1,35 +1,42 @@
-use std::cmp;
-
 use proconio::input;
 
-macro_rules! chmin {
-    ($min_v: expr, $v: expr) => {
-        if $v < $min_v {
-            $min_v = $v;
-            true
-        } else {
-            false
+fn dijkstra(n: usize, inf: u64, e: &[Vec<(usize, u64)>], s: usize) -> Vec<u64> {
+    let mut d = vec![inf; n];
+    let mut pq = std::collections::BinaryHeap::new();
+    d[s] = 0;
+    pq.push(std::cmp::Reverse((0, s)));
+    while let Some(std::cmp::Reverse((w_u, u))) = pq.pop() {
+        if w_u > d[u] {
+            continue;
         }
-    };
+        for &(v, w_v) in e[u].iter() {
+            let w = w_u + w_v;
+            if w < d[v] {
+                d[v] = w;
+                pq.push(std::cmp::Reverse((w, v)));
+            }
+        }
+    }
+    d
 }
 
 fn main() {
     input! {
         n: usize,
-        s: [i64; n],
-        t: [i64; n],
+        s: [u64; n],
+        t: [u64; n],
     };
-    let s = s.iter().chain(s.iter()).copied().collect::<Vec<i64>>();
-    let t = t.iter().chain(t.iter()).copied().collect::<Vec<i64>>();
-    let inf = 1_000_000_000;
-    let mut m = vec![inf; n];
-    let mut c = t[0];
-    for i in 0..2 * n {
-        c = cmp::min(t[i], c);
-        chmin!(m[i % n], c);
-        c += s[i];
+    let mut e = vec![vec![]; n + 1];
+    for (u, s_i) in s.iter().copied().enumerate() {
+        let v = if u + 1 == n { 0 } else { u + 1 };
+        e[u].push((v, s_i));
     }
-    for m_i in m {
-        println!("{}", m_i);
+    for (v, t_i) in t.iter().copied().enumerate() {
+        e[n].push((v, t_i));
+    }
+    let inf = 1_000_000_000_000_000;
+    let d = dijkstra(e.len(), inf, &e, n);
+    for d_i in d.iter().copied().take(n) {
+        println!("{}", d_i);
     }
 }
--- abc214/src/bin/e.rs
+++ abc214/src/bin/e.rs
@@ -1,10 +1,39 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 use proconio::{input, marker::Usize1};
+
+fn solve() -> bool {
+    input! {
+        n: usize,
+        mut lr: [(Usize1, Usize1); n],
+    };
+    lr.sort();
+    let inf = 1_000_000_001;
+    lr.push((inf, inf));
+    let mut p = 0;
+    let mut pq = BinaryHeap::new();
+    for (l_i, r_i) in lr {
+        while let Some(Reverse(r_j)) = pq.pop() {
+            if p >= l_i {
+                pq.push(Reverse(r_j));
+                break;
+            }
+            if r_j < p {
+                return false;
+            }
+            p += 1;
+        }
+        p = l_i;
+        pq.push(Reverse(r_i));
+    }
+    true
+}
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        t: usize,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    for _ in 0..t {
+        println!("{}", if solve() { "Yes" } else { "No" });
+    }
 }
