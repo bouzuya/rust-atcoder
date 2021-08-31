f5a1abb038b5b8323e4dd5ad2c0a1156f46d156f
bouzuya <m@bouzuya.net>
2021-08-31 22:17:21 +0900

    past202107-open c

--- past202107-open/src/bin/c.rs
+++ past202107-open/src/bin/c.rs
@@ -1,10 +1,28 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        s: Chars,
+        l: usize,
+        r: usize,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    if s[0] == '0' && s.len() != 1 {
+        println!("No");
+        return;
+    }
+    let mut x = 0_usize;
+    for s_i in s.iter().copied() {
+        let d = (s_i as u8 - b'0') as usize;
+        match x.checked_mul(10).and_then(|x| x.checked_add(d)) {
+            None => {
+                println!("No");
+                return;
+            }
+            Some(next) => {
+                x = next;
+            }
+        }
+    }
+    let ans = (l..=r).contains(&x);
+    println!("{}", if ans { "Yes" } else { "No" });
 }
