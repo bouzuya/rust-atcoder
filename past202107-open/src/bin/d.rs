44960db7a10c3f4142f01ce89be93f260c16f28a
bouzuya <m@bouzuya.net>
2021-09-01 21:48:11 +0900

    past202107-open d

--- past202107-open/src/bin/d.rs
+++ past202107-open/src/bin/d.rs
@@ -1,10 +1,20 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        mut s: Chars,
     };
-    let ans = n - a.len();
+    for i in 1..n - 1 {
+        if s[i] == 'x' && s[i - 1] == s[i + 1] {
+            let c = s[i - 1];
+            if c == 'a' || c == 'i' || c == 'u' || c == 'e' || c == 'o' {
+                s[i - 1] = '.';
+                s[i] = '.';
+                s[i + 1] = '.';
+            }
+        }
+    }
+    let ans = s.into_iter().collect::<String>();
     println!("{}", ans);
 }
