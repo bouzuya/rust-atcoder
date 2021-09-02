29ce753c8498d9a70f3fc02b40cd964a9ac7d2e7
bouzuya <m@bouzuya.net>
2021-09-02 19:50:09 +0900

    past202107-open e

--- past202107-open/src/bin/e.rs
+++ past202107-open/src/bin/e.rs
@@ -1,10 +1,40 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    for k in 1..=30 {
+        let mut x = 1_usize;
+
+        for i in 0..30 {
+            if i == k {
+                match x.checked_add(1) {
+                    Some(x_plus_1) => {
+                        x = x_plus_1;
+                    }
+                    None => continue,
+                }
+            }
+            match x.checked_mul(3) {
+                Some(next) => {
+                    x = next;
+                }
+                None => break,
+            }
+        }
+        if 30 == k {
+            match x.checked_add(1) {
+                Some(x_plus_1) => {
+                    x = x_plus_1;
+                }
+                None => continue,
+            }
+        }
+        if x == n {
+            println!("{}", k);
+            return;
+        }
+    }
+    println!("-1");
 }
