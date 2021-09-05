ace489380ec9088d6f0b09fc9702022fc98d8833
bouzuya <m@bouzuya.net>
2021-09-05 23:01:02 +0900

    past202107-open g

--- past202107-open/src/bin/g.rs
+++ past202107-open/src/bin/g.rs
@@ -1,10 +1,40 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        n: i64,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut t = vec![];
+    let mut sum = 0;
+    for i in 0.. {
+        let v = 3_i64.pow(i);
+        sum += v;
+        t.push((sum, v));
+        if sum >= n {
+            break;
+        }
+    }
+    let mut ans = vec![];
+    let mut x = n;
+    while x != 0 {
+        for (t_i, v) in t.iter().copied() {
+            if t_i >= x.abs() {
+                let v = v * x.signum();
+                x -= v;
+                ans.push(v);
+                break;
+            }
+        }
+    }
+
+    ans.sort();
+    println!("{}", ans.len());
+    println!(
+        "{}",
+        ans.iter()
+            .map(|a| a.to_string())
+            .collect::<Vec<String>>()
+            .join(" ")
+    );
 }
