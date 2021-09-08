497a555866e4470191bf1eaa013d75774e82763f
bouzuya <m@bouzuya.net>
2021-09-08 21:15:23 +0900

    code-formula-2014-quala b

--- code-formula-2014-quala/src/bin/b.rs
+++ code-formula-2014-quala/src/bin/b.rs
@@ -1,10 +1,22 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        a: usize,
+        b: usize,
+        p: [usize; a],
+        q: [usize; b],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut pin = vec!['x'; 10];
+    for p_i in p {
+        pin[(p_i + 10 - 1) % 10] = '.';
+    }
+    for q_i in q {
+        pin[(q_i + 10 - 1) % 10] = 'o';
+    }
+
+    println!("{} {} {} {}", pin[6], pin[7], pin[8], pin[9]);
+    println!(" {} {} {}", pin[3], pin[4], pin[5]);
+    println!("  {} {}", pin[1], pin[2]);
+    println!("   {}", pin[0]);
 }
