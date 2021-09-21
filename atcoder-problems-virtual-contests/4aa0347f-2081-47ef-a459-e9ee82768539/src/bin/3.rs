84325246018b3de7f002b3b970f33ddf0102b46c
bouzuya <m@bouzuya.net>
2021-09-21 22:17:04 +0900

    4aa0347f-2081-47ef-a459-e9ee82768539 3

--- atcoder-problems-virtual-contests/4aa0347f-2081-47ef-a459-e9ee82768539/src/bin/3.rs
+++ atcoder-problems-virtual-contests/4aa0347f-2081-47ef-a459-e9ee82768539/src/bin/3.rs
@@ -3,8 +3,16 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        mut a: [i64; n],
     }
-    todo!();
-    let ans = n;
+    a.sort();
+    let mut prev = 0;
+    let mut product = 1_i64;
+    for a_i in a.iter().copied() {
+        product *= a_i - prev + 1;
+        product %= 1_000_000_007;
+        prev = a_i;
+    }
+    let ans = product;
     println!("{}", ans);
 }
