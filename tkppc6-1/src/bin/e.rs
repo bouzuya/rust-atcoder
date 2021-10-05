1d05540c293e0287ffcc8a8e7c17b283e3d6b4b1
bouzuya <m@bouzuya.net>
2021-10-05 21:44:29 +0900

    tkppc6-1 e

--- tkppc6-1/src/bin/e.rs
+++ tkppc6-1/src/bin/e.rs
@@ -1,10 +1,31 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
+
+    let mut js = vec![true; n];
+    for i in 0..32 {
+        let mut next = js.clone();
+        for (j, a_i) in a.iter().copied().enumerate() {
+            if !js[j] {
+                continue;
+            }
+            if ((a_i >> (32 - 1 - i)) & 1) != 1 {
+                next[j] = false;
+            }
+        }
+        if next.iter().filter(|&&b| b).count() >= 2 {
+            js = next;
+        }
+    }
+    let is = js
+        .iter()
+        .enumerate()
+        .filter_map(|(u, &b)| if b { Some(u) } else { None })
+        .collect::<Vec<usize>>();
+    let ans = (a[is[0]] & a[is[1]]) << 1;
     println!("{}", ans);
 }
