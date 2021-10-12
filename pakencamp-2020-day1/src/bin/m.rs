fb38f34da626fe31c25e00dd8130b6b25d2a8672
bouzuya <m@bouzuya.net>
2021-10-12 22:06:55 +0900

    pakencamp-2020-day1 m

--- pakencamp-2020-day1/src/bin/m.rs
+++ pakencamp-2020-day1/src/bin/m.rs
@@ -1,11 +1,22 @@
 use proconio::input;
-use proconio::marker::Usize1;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
+    let mut dp = vec![0; 1 << 20];
+    for a_i in a {
+        dp[a_i] = a_i;
+    }
+    let mut ans = 0_usize;
+    for bits in 1..1 << 20 {
+        if dp[bits] == bits {
+            ans += 1;
+        }
+        for i in 0..20 {
+            dp[bits | (1 << i)] |= dp[bits];
+        }
+    }
     println!("{}", ans);
 }
