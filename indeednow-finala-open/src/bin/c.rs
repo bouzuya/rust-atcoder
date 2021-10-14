e689e799ffb4e1288519d531d4702be6818c3b67
bouzuya <m@bouzuya.net>
2021-10-14 23:29:11 +0900

    indeednow-finala c

--- indeednow-finala-open/src/bin/c.rs
+++ indeednow-finala-open/src/bin/c.rs
@@ -1,10 +1,29 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        mut abcw: [(usize, usize, usize, usize); n],
+        abc: [(usize, usize, usize); m],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut dp = vec![vec![vec![0_usize; 100 + 1]; 100 + 1]; 100 + 1];
+    abcw.sort_by_key(|&(_, _, _, w)| w);
+    for (a, b, c, w) in abcw.into_iter().rev() {
+        for i in a..=100 {
+            for j in b..=100 {
+                for k in c..=100 {
+                    if dp[i][j][k] == 0 {
+                        dp[i][j][k] = w;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    for (a, b, c) in abc {
+        println!("{}", dp[a][b][c]);
+    }
 }
