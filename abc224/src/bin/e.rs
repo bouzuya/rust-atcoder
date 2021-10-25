036244926150d5106a8d80adbcf7d6d60edc3254
bouzuya <m@bouzuya.net>
2021-10-25 22:49:00 +0900

    abc224 e

--- abc224/src/bin/e.rs
+++ abc224/src/bin/e.rs
@@ -1,10 +1,17 @@
-// TLE
-use std::{
-    cmp::Reverse,
-    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet},
-};
+use std::{cmp::Reverse, collections::BTreeMap};
 
 use proconio::{input, marker::Usize1};
+
+macro_rules! chmax {
+    ($max_v: expr, $v: expr) => {
+        if $v > $max_v {
+            $max_v = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
 
 fn main() {
     input! {
@@ -13,66 +20,30 @@ fn main() {
         n: usize,
         rca: [(Usize1, Usize1, usize); n],
     };
-    let mut col_map = HashMap::new();
-    let mut row_map = HashMap::new();
+
+    let mut a2ps = BTreeMap::new();
+    let mut p2i = BTreeMap::new();
     for (i, (r, c, a)) in rca.iter().copied().enumerate() {
-        row_map
-            .entry(r)
-            .or_insert(BTreeMap::new())
-            .entry(a)
-            .or_insert(vec![])
-            .push(i);
-        col_map
-            .entry(c)
-            .or_insert(BTreeMap::new())
-            .entry(a)
-            .or_insert(vec![])
-            .push(i);
+        a2ps.entry(Reverse(a))
+            .or_insert_with(|| vec![])
+            .push((r, c));
+        p2i.entry((r, c)).or_insert(i);
     }
 
-    let mut pq = BinaryHeap::new();
-    let mut max = 0;
-    for (_, _, a) in rca.iter().copied() {
-        max = a.max(max);
-    }
-    for (i, (_, _, a)) in rca.iter().copied().enumerate() {
-        pq.push((Reverse(a), Reverse(0), i));
-    }
-
-    let mut dp = BTreeMap::new();
-    while let Some((Reverse(_), Reverse(dp_i), i)) = pq.pop() {
-        let (r, c, a) = rca[i];
-        let v = *dp.get(&(r, c)).unwrap_or(&0);
-        if v > dp_i {
-            continue;
+    let mut i2d = vec![0; n];
+    let mut r2x = vec![0; h];
+    let mut r2n = vec![0; w];
+    for (_, ps) in a2ps {
+        for (r, c) in ps.iter().copied() {
+            i2d[p2i[&(r, c)]] = r2x[r].max(r2n[c]);
         }
-
-        let row = row_map.get(&r).unwrap();
-        if let Some((_, row_values)) = row.range(1..a).next_back() {
-            for j in row_values.iter().copied() {
-                let (r, c, _) = rca[j];
-                let entry = dp.entry((r, c)).or_insert(0);
-                if v + 1 > *entry {
-                    *entry = v + 1;
-                    pq.push((Reverse(a), Reverse(v + 1), j))
-                }
-            }
-        }
-
-        let col = col_map.get(&c).unwrap();
-        if let Some((_, col_values)) = col.range(1..a).next_back() {
-            for j in col_values.iter().copied() {
-                let (r, c, _) = rca[j];
-                let entry = dp.entry((r, c)).or_insert(0);
-                if v + 1 > *entry {
-                    *entry = v + 1;
-                    pq.push((Reverse(a), Reverse(v + 1), j))
-                }
-            }
+        for (r, c) in ps.iter().copied() {
+            chmax!(r2x[r], i2d[p2i[&(r, c)]] + 1);
+            chmax!(r2n[c], i2d[p2i[&(r, c)]] + 1);
         }
     }
 
-    for (r, c, _) in rca {
-        println!("{}", *dp.get(&(r, c)).unwrap_or(&0));
+    for d in i2d {
+        println!("{}", d);
     }
 }
