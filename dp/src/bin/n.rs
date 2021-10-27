c7d18cd789e1366003da5e64f43a5c2ad625f526
bouzuya <m@bouzuya.net>
2021-10-27 22:02:14 +0900

    dp n

--- dp/src/bin/n.rs
+++ dp/src/bin/n.rs
@@ -1,12 +1,42 @@
-// TODO:
 use proconio::input;
-use proconio::marker::Usize1;
+
+fn f(
+    a: &[usize],
+    c: &[usize],
+    inf: usize,
+    memo: &mut [Vec<Option<usize>>],
+    l: usize,
+    r: usize,
+) -> usize {
+    if l == r {
+        return 0;
+    }
+
+    if let Some(v) = memo[l][r] {
+        return v;
+    }
+
+    let mut min = inf;
+    for m in l..r {
+        min = min.min(f(a, c, inf, memo, l, m) + f(a, c, inf, memo, m + 1, r) + c[r + 1] - c[l]);
+    }
+    memo[l][r] = Some(min);
+    min
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
+    let c = std::iter::once(0)
+        .chain(a.iter().scan(0, |acc, &i| {
+            *acc += i;
+            Some(*acc)
+        }))
+        .collect::<Vec<usize>>();
+    let inf = 1_000_000_000_000_000;
+    let mut memo = vec![vec![None; n]; n];
+    let ans = f(&a, &c, inf, &mut memo, 0, n - 1);
     println!("{}", ans);
 }
