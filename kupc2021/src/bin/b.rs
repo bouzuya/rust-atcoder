7b4d3771f49f9de1fc92eda37fccf17c659bebf8
bouzuya <m@bouzuya.net>
2021-11-04 23:15:28 +0900

    kupc2021 b

--- kupc2021/src/bin/b.rs
+++ kupc2021/src/bin/b.rs
@@ -1,10 +1,50 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+
+fn f(s: &mut [Vec<char>], n: usize, o: usize) {
+    if o > n {
+        return;
+    }
+    match n - 1 - o {
+        0 => {
+            s[o][o] = '.';
+        }
+        1 => {
+            s[o][o] = '#';
+            s[o][o + 1] = '.';
+            s[o + 1][o] = '.';
+            s[o + 1][o + 1] = '.';
+        }
+        _ => {
+            s[o][o] = '#';
+            for j in o + 2..n {
+                s[o][j] = '#';
+            }
+            for i in o + 2..n {
+                s[i][o] = '#';
+            }
+            for j in o..n {
+                s[o + 1][j] = '.';
+            }
+            for i in o..n {
+                s[i][o + 1] = '.';
+            }
+            f(s, n, o + 2)
+        }
+    }
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut s = vec![vec!['x'; n]; n];
+    f(&mut s, n, 0);
+
+    for i in 0..n {
+        for j in 0..n {
+            print!("{}", s[i][j]);
+        }
+        println!();
+    }
 }
