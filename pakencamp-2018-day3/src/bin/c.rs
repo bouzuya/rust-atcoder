56857be92ae57c123b597200605ba04817816445
bouzuya <m@bouzuya.net>
2021-11-15 21:38:26 +0900

    pakencamp-2018-day3 c

--- pakencamp-2018-day3/src/bin/c.rs
+++ pakencamp-2018-day3/src/bin/c.rs
@@ -1,10 +1,31 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        b: usize,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut used = vec![false; b];
+    used[0] = true;
+    used[1] = true;
+    for start in 2..b {
+        if used[start] {
+            continue;
+        }
+        used[start] = true;
+
+        let mut count = 0;
+        let mut x = start;
+        while x <= b {
+            x += x / 2;
+            if x < b {
+                used[x] = true;
+            }
+            count += 1;
+            if x == b {
+                println!("{}", count);
+                return;
+            }
+        }
+    }
+    println!("0");
 }
