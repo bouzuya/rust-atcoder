fb4c46a15e1fc5032671509ca18e51b9bec9344f
bouzuya <m@bouzuya.net>
2021-11-16 20:49:15 +0900

    pakencamp-2020-day2 c

--- pakencamp-2020-day2/src/bin/c.rs
+++ pakencamp-2020-day2/src/bin/c.rs
@@ -2,9 +2,25 @@ use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
-    };
-    let ans = n - a.len();
-    println!("{}", ans);
+        t: usize,
+    }
+    for _ in 0..t {
+        input! {
+            n: usize,
+            m: usize,
+            ab: [(Usize1, Usize1); m],
+        };
+        let mut is = vec![0; n];
+        let mut os = vec![0; n];
+        for (a, b) in ab {
+            os[a] += 1;
+            is[b] += 1;
+        }
+        let mut sum = 0_usize;
+        for (i, o) in is.into_iter().zip(os.into_iter()) {
+            sum += i.min(o);
+        }
+        let ans = m - sum;
+        println!("{}", ans);
+    }
 }
