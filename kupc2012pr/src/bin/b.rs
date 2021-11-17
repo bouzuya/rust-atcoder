49d71f1edcdbec5795da863198a486f4f7ef3b92
bouzuya <m@bouzuya.net>
2021-11-17 20:25:40 +0900

    kupc2012pr b

--- kupc2012pr/src/bin/b.rs
+++ kupc2012pr/src/bin/b.rs
@@ -1,10 +1,13 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        _k: usize,
+        mut a: [String; n],
     };
-    let ans = n - a.len();
+    a.sort();
+    a.reverse();
+    let ans = a.into_iter().collect::<Vec<String>>().join("");
     println!("{}", ans);
 }
