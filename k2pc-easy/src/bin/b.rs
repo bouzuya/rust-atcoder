11700172b5f98e204807d51e33079bdb0b2e999b
bouzuya <m@bouzuya.net>
2021-11-25 21:45:25 +0900

    k2pc-easy b

--- k2pc-easy/src/bin/b.rs
+++ k2pc-easy/src/bin/b.rs
@@ -1,10 +1,30 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        mut a: [usize; 10],
+        c: [Chars; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut xs = vec![0; 7];
+    let mut cs = vec![0; 7];
+    for c_i in c.iter() {
+        for (j, c_ij) in c_i.iter().copied().enumerate() {
+            match c_ij {
+                'X' => {
+                    cs[j] += 1;
+                    xs[j] = xs[j].max(cs[j]);
+                }
+                '-' => {
+                    cs[j] = 0;
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+    xs.sort();
+    a.sort();
+
+    let ans = xs.iter().zip(a.iter().skip(3)).all(|(x_i, a_i)| x_i <= a_i);
+    println!("{}", if ans { "YES" } else { "NO" });
 }
