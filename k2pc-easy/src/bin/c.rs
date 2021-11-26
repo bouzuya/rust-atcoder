d7a5e5c5e60faa33cf9c2c36eb23f2922176f239
bouzuya <m@bouzuya.net>
2021-11-26 22:20:45 +0900

    k2pc-easy c

--- k2pc-easy/src/bin/c.rs
+++ k2pc-easy/src/bin/c.rs
@@ -1,10 +1,59 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+
+fn f(n: usize) -> (usize, usize) {
+    for x in 1.. {
+        let sum = x * (x + 1) / 2;
+        if sum == n {
+            return (1, x);
+        } else if sum > n {
+            let offset = n - (x - 1) * x / 2 - 1;
+            return (x - offset, 1 + offset);
+        }
+    }
+    unreachable!()
+}
+
+fn g((a, b): (usize, usize)) -> usize {
+    let s = a + b;
+    if s == 2 {
+        return 1;
+    }
+    let n = s - 2;
+    let sum = n * (n + 1) / 2;
+    sum + b
+}
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        i: usize,
+        j: usize,
     };
-    let ans = n - a.len();
+    let (a_i, b_i) = f(i);
+    let (a_j, b_j) = f(j);
+    let x = (a_i + a_j, b_i + b_j);
+    let ans = g(x);
     println!("{}", ans);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn f_test() {
+        assert_eq!(f(1), (1, 1));
+        assert_eq!(f(2), (2, 1));
+        assert_eq!(f(3), (1, 2));
+        assert_eq!(f(4), (3, 1));
+        assert_eq!(f(5), (2, 2));
+    }
+
+    #[test]
+    fn g_test() {
+        assert_eq!(g((1, 1)), 1);
+        assert_eq!(g((2, 1)), 2);
+        assert_eq!(g((1, 2)), 3);
+        assert_eq!(g((3, 1)), 4);
+        assert_eq!(g((2, 2)), 5);
+    }
 }
