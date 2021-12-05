cf69db25d424ce26b4d8a61e3c15a1f1ed9297b5
bouzuya <m@bouzuya.net>
2021-12-05 22:35:35 +0900

    abc230 d, e

--- abc230/src/bin/d.rs
+++ abc230/src/bin/d.rs
@@ -7,24 +7,13 @@ fn main() {
         mut lr: [(usize, usize); n],
     };
     lr.sort_by_key(|&(a, b)| (b, a));
-    let mut ok = 1_000_000_000;
-    let mut ng = 0;
-    while ok - ng > 1 {
-        let x = (ok + ng) / 2;
-        let mut count = 1_usize;
-        let mut p = lr[0].1;
-        for (l, r) in lr.iter().skip(1).copied() {
-            if p + d - 1 < l {
-                count += 1;
-                p = r;
-            }
-        }
-        if count <= x {
-            ok = x;
-        } else {
-            ng = x;
+    let mut ans = 1;
+    let mut p = lr[0].1;
+    for (l, r) in lr.into_iter().skip(1) {
+        if p + d - 1 < l {
+            ans += 1;
+            p = r;
         }
     }
-    let ans = ok;
     println!("{}", ans);
 }
--- abc230/src/bin/e.rs
+++ abc230/src/bin/e.rs
@@ -1,10 +1,16 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
     };
-    let ans = n - a.len();
+    let mut ans = 0_usize;
+    let mut l = 1;
+    while l <= n {
+        let x = n / l;
+        let r = n / x + 1;
+        ans += (r - l) * x;
+        l = r;
+    }
     println!("{}", ans);
 }
