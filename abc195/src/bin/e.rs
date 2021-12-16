225e5adeb08a8f981e946efccc119d309f146442
bouzuya <m@bouzuya.net>
2021-12-16 21:37:24 +0900

    abc195 e

--- abc195/src/bin/e.rs
+++ abc195/src/bin/e.rs
@@ -1,11 +1,36 @@
-use proconio::input;
-use proconio::marker::Usize1;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        _n: usize,
+        s: Chars,
+        x: Chars,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut dp = vec![false; 7];
+    dp[0] = true;
+    for (s_i, x_i) in s.into_iter().zip(x.into_iter()).rev() {
+        let mut next = vec![false; 7];
+        let d = (s_i as u8 - b'0') as usize;
+        for r in 0..7 {
+            let d1 = (10 * r) % 7;
+            let d2 = (10 * r + d) % 7;
+            match x_i {
+                'T' => {
+                    if dp[d1] || dp[d2] {
+                        next[r] = true;
+                    }
+                }
+                'A' => {
+                    if dp[d1] && dp[d2] {
+                        next[r] = true;
+                    }
+                }
+                _ => unreachable!(),
+            }
+        }
+
+        dp = next;
+    }
+    let ans = dp[0];
+    println!("{}", if ans { "Takahashi" } else { "Aoki" });
 }
