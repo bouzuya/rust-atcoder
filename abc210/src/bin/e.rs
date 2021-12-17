664cbdb5b758b96af75e4033b68cddeb66eef8dd
bouzuya <m@bouzuya.net>
2021-12-17 22:24:01 +0900

    abc210 e

--- abc210/src/bin/e.rs
+++ abc210/src/bin/e.rs
@@ -1,10 +1,31 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        mut ac: [(usize, usize); m],
     };
-    let ans = n - a.len();
+    ac.sort_by_key(|&(_, c)| c);
+    let mut x_i = n;
+    let mut x_p = n;
+    let mut ans = 0_usize;
+    for (a_i, c_i) in ac {
+        x_i = gcd(x_i, a_i);
+        ans += c_i * (x_p - x_i);
+        x_p = x_i;
+    }
+    if x_i > 1 {
+        println!("{}", -1);
+        return;
+    }
     println!("{}", ans);
 }
