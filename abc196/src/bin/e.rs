2ba38fbcfe97882b16772228c84626429ed132cf
bouzuya <m@bouzuya.net>
2021-12-24 20:25:15 +0900

    abc196 e

--- abc196/src/bin/e.rs
+++ abc196/src/bin/e.rs
@@ -1,11 +1,32 @@
 use proconio::input;
-use proconio::marker::Usize1;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        at: [(i64, usize); n],
+        q: usize,
+        x: [i64; q],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let (mut low, mut high, mut add) = (-1 << 62, 1 << 62, 0);
+    for (a, t) in at {
+        match t {
+            1 => {
+                low += a;
+                high += a;
+                add += a;
+            }
+            2 => {
+                low = low.max(a);
+                high = high.max(a);
+            }
+            3 => {
+                low = low.min(a);
+                high = high.min(a);
+            }
+            _ => unreachable!(),
+        }
+    }
+    for x_i in x {
+        println!("{}", high.min(low.max(x_i + add)));
+    }
 }
