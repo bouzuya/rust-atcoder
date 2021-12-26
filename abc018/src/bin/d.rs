1507026dde716babb86350d22467d6bb8f3c6dcd
bouzuya <m@bouzuya.net>
2021-12-26 21:33:51 +0900

    abc018 d

--- abc018/src/bin/d.rs
+++ abc018/src/bin/d.rs
@@ -1,11 +1,30 @@
-use proconio::input;
-use proconio::marker::Usize1;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        p: usize,
+        q: usize,
+        r: usize,
+        xyz: [(Usize1, Usize1, usize); r],
     };
-    let ans = n - a.len();
+    let mut max = 0_usize;
+    for nset in 0_usize..(1 << n) {
+        if nset.count_ones() as usize > p {
+            continue;
+        }
+        let mut mscore = vec![0_usize; m];
+        for (x, y, z) in xyz.iter().copied() {
+            if (nset & (1 << x)) == 0 {
+                continue;
+            }
+            mscore[y] += z;
+        }
+        mscore.sort();
+        mscore.reverse();
+        max = max.max(mscore[0..q].iter().sum::<usize>());
+    }
+    let ans = max;
     println!("{}", ans);
 }
