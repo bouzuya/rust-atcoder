6d495c48344de9bdf95c9eea33ebc72cfe76c368
bouzuya <m@bouzuya.net>
2021-12-27 14:26:04 +0900

    abc013 d

--- abc013/src/bin/d.rs
+++ abc013/src/bin/d.rs
@@ -1,11 +1,32 @@
-use proconio::input;
-use proconio::marker::Usize1;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        d: usize,
+        a: [Usize1; m],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut db = vec![vec![n; n]; 32 + 1];
+    for i in 0..n {
+        db[0][i] = i;
+    }
+    for a_i in a.into_iter().rev() {
+        db[0].swap(a_i, a_i + 1);
+    }
+    for i in 0..32 {
+        for j in 0..n {
+            db[i + 1][j] = db[i][db[i][j]];
+        }
+    }
+
+    for k in 0..n {
+        let mut x = k;
+        for i in 0..32 {
+            if ((d >> i) & 1) == 1 {
+                x = db[i][x];
+            }
+        }
+        println!("{}", x + 1);
+    }
 }
