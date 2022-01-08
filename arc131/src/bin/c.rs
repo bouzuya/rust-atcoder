91059bfb16fa1e2a47ee134bb5d7a607e94e4faa
bouzuya <m@bouzuya.net>
2022-01-08 10:55:28 +0900

    arc131 c

--- arc131/src/bin/c.rs
+++ arc131/src/bin/c.rs
@@ -1,10 +1,11 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let x = a.iter().copied().fold(0, |acc, x| acc ^ x);
+    let ans = n % 2 != 0 || a.into_iter().any(|a_i| (a_i ^ x) == 0);
+    println!("{}", if ans { "Win" } else { "Lose" });
 }
