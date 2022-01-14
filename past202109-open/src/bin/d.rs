d22ac783c72a823a1511bf628a728d40c4934a86
bouzuya <m@bouzuya.net>
2022-01-14 22:45:11 +0900

    past202109-open d

--- past202109-open/src/bin/d.rs
+++ past202109-open/src/bin/d.rs
@@ -1,10 +1,32 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+
+fn divisors(n: usize) -> usize {
+    let mut count = 0;
+    for i in 1.. {
+        if i * i > n {
+            break;
+        }
+        if n % i == 0 {
+            count += 1;
+            if i != n / i {
+                count += 1;
+            }
+        }
+    }
+    count
+}
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        x: usize,
+        y: usize,
     };
-    let ans = n - a.len();
+    let d_x = divisors(x);
+    let d_y = divisors(y);
+    let ans = match d_x.cmp(&d_y) {
+        std::cmp::Ordering::Greater => 'X',
+        std::cmp::Ordering::Less => 'Y',
+        std::cmp::Ordering::Equal => 'Z',
+    };
     println!("{}", ans);
 }
