2c6f1ec2cb046aa6dc821ffe163f8bdd8a3bfe08
bouzuya <m@bouzuya.net>
2022-01-15 10:58:42 +0900

    past202109-open e

--- past202109-open/src/bin/e.rs
+++ past202109-open/src/bin/e.rs
@@ -1,10 +1,29 @@
-use proconio::{input, marker::Usize1};
+use std::collections::HashSet;
+
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        k: usize,
+        c: [i64; n],
+        p: [i64; n],
     };
-    let ans = n - a.len();
+    let mut cp = c
+        .into_iter()
+        .zip(p.into_iter())
+        .collect::<Vec<(i64, i64)>>();
+    cp.sort_by_key(|&(_, p)| p);
+    let mut ans = 0_i64;
+    let mut used = HashSet::new();
+    for (c, p) in cp {
+        if used.len() >= k {
+            break;
+        }
+        if used.insert(c) {
+            ans += p;
+        }
+    }
+    let ans = if used.len() >= k { ans } else { -1 };
     println!("{}", ans);
 }
