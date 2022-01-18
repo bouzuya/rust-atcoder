7d9dc62466eb29b32466bbdc80be7c29de3605e8
bouzuya <m@bouzuya.net>
2022-01-18 22:34:03 +0900

    past202109-open g

--- past202109-open/src/bin/g.rs
+++ past202109-open/src/bin/g.rs
@@ -1,10 +1,25 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        k: usize,
+        abc: [(usize, usize, usize); n],
     };
-    let ans = n - a.len();
+    let mut ng = 0_usize;
+    let mut ok = std::usize::MAX;
+    while ok - ng > 1 {
+        let x = ng + (ok - ng) / 2;
+        let mut count = 0;
+        for (a, b, c) in abc.iter().copied() {
+            count += (if x < b { 0 } else { (x - b) / c + 1 }).min(a);
+        }
+        if count >= k {
+            ok = x;
+        } else {
+            ng = x;
+        }
+    }
+    let ans = ok;
     println!("{}", ans);
 }
