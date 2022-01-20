2622d7be9d92d7f12940c3793fdedc5f17ff8733
bouzuya <m@bouzuya.net>
2022-01-20 09:24:36 +0900

    past202109-open i

--- past202109-open/src/bin/i.rs
+++ past202109-open/src/bin/i.rs
@@ -1,10 +1,36 @@
-use proconio::{input, marker::Usize1};
+use std::{cmp::Reverse, collections::BinaryHeap};
+
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
+    let mut count = 0;
+    let mut b = vec![];
+    for a_i in a {
+        let mut x = a_i;
+        while x % 2 == 0 {
+            x /= 2;
+            count += 1;
+        }
+        b.push(x);
+    }
+
+    let mut heap = b
+        .into_iter()
+        .map(Reverse)
+        .collect::<BinaryHeap<Reverse<usize>>>();
+    for _ in 0..count {
+        match heap.pop() {
+            Some(Reverse(x)) => {
+                heap.push(Reverse(x * 3));
+            }
+            None => unreachable!(),
+        }
+    }
+
+    let Reverse(ans) = heap.pop().unwrap();
     println!("{}", ans);
 }
