774a27a320c63e29495fde8622a0734cfbf11820
bouzuya <m@bouzuya.net>
2022-01-24 21:05:28 +0900

    abc236 d

--- abc236/src/bin/d.rs
+++ abc236/src/bin/d.rs
@@ -1,6 +1,50 @@
-// TLE
 use proconio::input;
-use superslice::Ext;
+
+fn dfs(
+    n: usize,
+    a: &[Vec<usize>],
+    ans: &mut usize,
+    pairs: &mut Vec<(usize, usize)>,
+    used: &mut Vec<bool>,
+    index: usize,
+) {
+    if index == 2 * n {
+        let mut b = 0_usize;
+        for (i, j) in pairs.iter().copied() {
+            b ^= a[i][j];
+        }
+        *ans = (*ans).max(b);
+        return;
+    }
+
+    if index % 2 == 0 {
+        for (l, used_l) in used.iter().copied().enumerate() {
+            if used_l {
+                continue;
+            }
+
+            pairs.push((l, 2 * n));
+            used[l] = true;
+            dfs(n, a, ans, pairs, used, index + 1);
+            pairs.pop();
+            used[l] = false;
+            break;
+        }
+    } else {
+        let l = pairs.pop().unwrap().0;
+        for r in l + 1..2 * n {
+            if used[r] {
+                continue;
+            }
+            pairs.push((l, r));
+            used[r] = true;
+            dfs(n, a, ans, pairs, used, index + 1);
+            pairs.pop();
+            used[r] = false;
+        }
+        pairs.push((l, 2 * n));
+    }
+}
 
 fn main() {
     input! {
@@ -24,44 +68,10 @@ fn main() {
         a2
     };
 
-    // for i in 0..2 * n {
-    //     for j in 0..2 * n {
-    //         print!("{} ", a[i][j]);
-    //     }
-    //     println!();
-    // }
+    let mut ans = 0_usize;
+    let mut pairs = vec![];
+    let mut used = vec![false; 2 * n];
+    dfs(n, &a, &mut ans, &mut pairs, &mut used, 0);
 
-    let mut ans = 0;
-    for bits in 0..1_usize << 2 * n {
-        if bits.count_ones() as usize != n {
-            continue;
-        }
-
-        let mut is = vec![];
-        let mut js = vec![];
-        for i in 0..2 * n {
-            if (bits >> i) & 1 == 1 {
-                is.push(i);
-            } else {
-                js.push(i);
-            }
-        }
-
-        let mut max = 0;
-
-        let mut pairs = (0..n).collect::<Vec<usize>>();
-        loop {
-            let mut b = 0;
-            for i in 0..n {
-                b ^= a[is[i]][js[pairs[i]]];
-            }
-            max = max.max(b);
-            if !pairs.next_permutation() {
-                break;
-            }
-        }
-
-        ans = ans.max(max);
-    }
     println!("{}", ans);
 }
