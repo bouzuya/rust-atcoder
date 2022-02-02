f94da579c4ba7ed3b0d81609f9b167ee2214908e
bouzuya <m@bouzuya.net>
2022-02-02 22:32:05 +0900

    math-and-algorithm 009-011

--- math-and-algorithm/src/bin/009.rs
+++ math-and-algorithm/src/bin/009.rs
@@ -1,10 +1,24 @@
-use proconio::{input, marker::Usize1};
+use std::collections::HashSet;
+
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        s: usize,
+        a: [usize; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut dp = HashSet::new();
+    dp.insert(0);
+    for a_i in a.iter().copied() {
+        let mut next = dp.clone();
+        for d in dp {
+            if d + a_i <= 10_000 {
+                next.insert(d + a_i);
+            }
+        }
+        dp = next;
+    }
+    let ans = dp.contains(&s);
+    println!("{}", if ans { "Yes" } else { "No" });
 }
--- math-and-algorithm/src/bin/011.rs
+++ math-and-algorithm/src/bin/011.rs
@@ -1,10 +1,37 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+
+fn sieve_of_eratosthenes(n: usize) -> Vec<usize> {
+    let mut p = vec![];
+    let mut b = vec![true; n + 1];
+    for i in 2.. {
+        if i * i > n {
+            for j in i..=n {
+                if b[j] {
+                    p.push(j);
+                }
+            }
+            break;
+        }
+        if b[i] {
+            p.push(i);
+            for j in (i + i..=n).step_by(i) {
+                b[j] = false;
+            }
+        }
+    }
+    p
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let ps = sieve_of_eratosthenes(n);
+    println!(
+        "{}",
+        ps.into_iter()
+            .map(|p_i| p_i.to_string())
+            .collect::<Vec<String>>()
+            .join(" ")
+    );
 }
