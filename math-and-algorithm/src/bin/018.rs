72d84ca6de9240e6a0dc4739b86d32a540ec8635
bouzuya <m@bouzuya.net>
2022-02-05 23:02:28 +0900

    math-and-algorithm 018

--- math-and-algorithm/src/bin/018.rs
+++ math-and-algorithm/src/bin/018.rs
@@ -1,10 +1,16 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
+
+    let mut count = vec![0_usize; 5];
+    for a_i in a {
+        count[a_i / 100] += 1;
+    }
+
+    let ans = (count[1] * count[4]) + (count[2] * count[3]);
     println!("{}", ans);
 }
