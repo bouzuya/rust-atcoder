0ed614e356d5efd01b062821fbf27eefbc44367e
bouzuya <m@bouzuya.net>
2022-02-08 21:28:18 +0900

    math-and-algorithm 027

--- math-and-algorithm/src/bin/027.rs
+++ math-and-algorithm/src/bin/027.rs
@@ -1,10 +1,55 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+
+fn merge_sort(a: &mut Vec<usize>, l: usize, r: usize) {
+    if r - l == 1 {
+        return;
+    }
+    let n = r - l;
+
+    let m = l + (r - l) / 2;
+    merge_sort(a, l, m);
+    merge_sort(a, m, r);
+
+    let mut i_l = l;
+    let mut i_r = m;
+    let mut c = vec![0; n];
+    for c_i in c.iter_mut() {
+        match (i_l == m, i_r == r) {
+            (false, false) => {
+                if a[i_l] <= a[i_r] {
+                    *c_i = a[i_l];
+                    i_l += 1;
+                } else {
+                    *c_i = a[i_r];
+                    i_r += 1;
+                }
+            }
+            (true, false) => {
+                *c_i = a[i_r];
+                i_r += 1;
+            }
+            (false, true) => {
+                *c_i = a[i_l];
+                i_l += 1;
+            }
+            (true, true) => unreachable!(),
+        }
+    }
+
+    for (i, c_i) in c.iter().copied().enumerate() {
+        a[l + i] = c_i;
+    }
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        mut a: [usize; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    merge_sort(&mut a, 0, n);
+
+    for (i, a_i) in a.iter().copied().enumerate() {
+        print!("{}{}", a_i, if i == n - 1 { '\n' } else { ' ' });
+    }
 }
