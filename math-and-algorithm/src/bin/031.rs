4407eda6b8fdd79355476ac64020cf5a705fc407
bouzuya <m@bouzuya.net>
2022-02-10 22:37:13 +0900

    math-and-algorithm 031

--- math-and-algorithm/src/bin/031.rs
+++ math-and-algorithm/src/bin/031.rs
@@ -1,10 +1,17 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
+    let mut dp = (0, 0);
+    for a_i in a {
+        let mut next = (0, 0);
+        next.0 = next.0.max(dp.0).max(dp.1);
+        next.1 = next.1.max(dp.0 + a_i);
+        dp = next;
+    }
+    let ans = dp.0.max(dp.1);
     println!("{}", ans);
 }
