c0090c91609d273bc967a9fb091603b8b1715511
bouzuya <m@bouzuya.net>
2022-02-12 22:29:10 +0900

    math-and-algorithm 034

--- math-and-algorithm/src/bin/034.rs
+++ math-and-algorithm/src/bin/034.rs
@@ -1,10 +1,21 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        xy: [(i64, i64); n],
     };
-    let ans = n - a.len();
+
+    let dist = |p1: (i64, i64), p2: (i64, i64)| -> f64 {
+        (((p1.0 - p2.0).pow(2) + (p1.1 - p2.1).pow(2)) as f64).sqrt()
+    };
+
+    let mut min = dist(xy[0], xy[1]);
+    for i in 0..n {
+        for j in i + 1..n {
+            min = min.min(dist(xy[i], xy[j]));
+        }
+    }
+    let ans = min;
     println!("{}", ans);
 }
