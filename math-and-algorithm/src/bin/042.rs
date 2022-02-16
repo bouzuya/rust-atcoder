9747212607edec50bbdd010e6124e58dac4db03c
bouzuya <m@bouzuya.net>
2022-02-16 21:14:05 +0900

    math-and-algorithm 042-043

--- math-and-algorithm/src/bin/042.rs
+++ math-and-algorithm/src/bin/042.rs
@@ -1,10 +1,17 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+
+fn arithmetic_series(a_1: usize, d: usize, n: usize) -> usize {
+    let a_n = a_1 + (n - 1) * d;
+    (a_1 + a_n) * n / 2
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
     };
-    let ans = n - a.len();
+    let mut ans = 0_usize;
+    for k in 1..=n {
+        ans += arithmetic_series(k, k, n / k);
+    }
     println!("{}", ans);
 }
--- math-and-algorithm/src/bin/043.rs
+++ math-and-algorithm/src/bin/043.rs
@@ -1,10 +1,32 @@
+use std::collections::VecDeque;
+
 use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        ab: [(Usize1, Usize1); m],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut edges = vec![vec![]; n];
+    for (a, b) in ab {
+        edges[a].push(b);
+        edges[b].push(a);
+    }
+
+    let mut used = vec![false; n];
+    let mut deque = VecDeque::new();
+    used[0] = true;
+    deque.push_back(0);
+    while let Some(u) = deque.pop_front() {
+        for v in edges[u].iter().copied() {
+            if used[v] {
+                continue;
+            }
+            used[v] = true;
+            deque.push_back(v);
+        }
+    }
+    let ans = !used.contains(&false);
+    println!("The graph is {}connected.", if ans { "" } else { "not " });
 }
