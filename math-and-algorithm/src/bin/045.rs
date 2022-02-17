78ecec3d7e8cd1ab5437b777632f315184d0d52b
bouzuya <m@bouzuya.net>
2022-02-18 08:29:15 +0900

    math-and-algorithm 045-046

--- math-and-algorithm/src/bin/045.rs
+++ math-and-algorithm/src/bin/045.rs
@@ -3,8 +3,17 @@ use proconio::{input, marker::Usize1};
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        ab: [(Usize1, Usize1); m],
     };
-    let ans = n - a.len();
+    let mut count = vec![0; n];
+    for (a, b) in ab {
+        match a.cmp(&b) {
+            std::cmp::Ordering::Less => count[b] += 1,
+            std::cmp::Ordering::Equal => {}
+            std::cmp::Ordering::Greater => count[a] += 1,
+        }
+    }
+    let ans = count.into_iter().filter(|c| c == &1).count();
     println!("{}", ans);
 }
--- math-and-algorithm/src/bin/046.rs
+++ math-and-algorithm/src/bin/046.rs
@@ -1,10 +1,42 @@
-use proconio::{input, marker::Usize1};
+use std::collections::VecDeque;
+
+use proconio::{
+    input,
+    marker::{Chars, Usize1},
+};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        large_r: usize,
+        large_c: usize,
+        s: (Usize1, Usize1),
+        g: (Usize1, Usize1),
+        c: [Chars; large_r],
     };
-    let ans = n - a.len();
+
+    let inf = large_r * large_c + 1;
+    let mut dist = vec![vec![inf; large_c]; large_r];
+    let mut deque = VecDeque::new();
+    deque.push_back((s.0, s.1, 0));
+    while let Some((y, x, d)) = deque.pop_front() {
+        let nd = d + 1;
+        let dir = vec![(-1, 0), (0, -1), (0, 1), (1, 0)];
+        for (dr, dc) in dir {
+            let (nr, nc) = (y as i64 + dr, x as i64 + dc);
+            if !(0..large_r as i64).contains(&nr) || !(0..large_c as i64).contains(&nc) {
+                continue;
+            }
+            let (nr, nc) = (nr as usize, nc as usize);
+            if c[nr][nc] != '.' {
+                continue;
+            }
+            if dist[nr][nc] != inf {
+                continue;
+            }
+            dist[nr][nc] = nd;
+            deque.push_back((nr, nc, nd));
+        }
+    }
+    let ans = dist[g.0][g.1];
     println!("{}", ans);
 }
