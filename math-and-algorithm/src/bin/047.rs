392f22d89e2253f9199782f61b2554ce167d5d74
bouzuya <m@bouzuya.net>
2022-02-18 20:45:50 +0900

    math-and-algorithm 047

--- math-and-algorithm/src/bin/047.rs
+++ math-and-algorithm/src/bin/047.rs
@@ -1,10 +1,42 @@
+use std::collections::VecDeque;
+
 use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        ab: [(Usize1, Usize1); m],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut edges = vec![vec![]; n];
+    for (a, b) in ab {
+        edges[a].push(b);
+        edges[b].push(a);
+    }
+
+    let mut c = vec![2; n];
+    for u in 0..n {
+        if c[u] != 2 {
+            continue;
+        }
+
+        let mut deque = VecDeque::new();
+        c[u] = 0;
+        deque.push_back(u);
+        while let Some(u) = deque.pop_front() {
+            for v in edges[u].iter().copied() {
+                let c_v = if c[u] == 0 { 1 } else { 0 };
+                if c[v] == 2 {
+                    c[v] = c_v;
+                    deque.push_back(v);
+                } else if c[v] != c_v {
+                    println!("No");
+                    return;
+                }
+            }
+        }
+    }
+
+    println!("Yes");
 }
