3b940936fdbf03b126fea1531d3bf7885f9f60e5
bouzuya <m@bouzuya.net>
2022-02-19 18:05:06 +0900

    math-and-algorithm 048-049

--- math-and-algorithm/src/bin/048.rs
+++ math-and-algorithm/src/bin/048.rs
@@ -1,10 +1,38 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+
+fn dijkstra(n: usize, inf: u64, e: &[Vec<(usize, u64)>], s: usize) -> Vec<u64> {
+    use std::{cmp::Reverse, collections::BinaryHeap};
+    let mut d = vec![inf; n];
+    let mut pq = BinaryHeap::new();
+    d[s] = 0;
+    pq.push(Reverse((0, s)));
+    while let Some(Reverse((w_u, u))) = pq.pop() {
+        if w_u > d[u] {
+            continue;
+        }
+        for (v, w_v) in e[u].iter().copied() {
+            let w = w_u + w_v;
+            if w < d[v] {
+                d[v] = w;
+                pq.push(Reverse((w, v)));
+            }
+        }
+    }
+    d
+}
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        k: usize,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut edges = vec![vec![]; k];
+    for i in 1..k {
+        edges[i % k].push(((10 * i) % k, 0));
+        edges[i % k].push(((i + 1) % k, 1));
+    }
+
+    let inf = 1_u64 << 60;
+    let dist = dijkstra(k, inf, &edges, 1);
+    println!("{}", dist[0] + 1);
 }
--- math-and-algorithm/src/bin/049.rs
+++ math-and-algorithm/src/bin/049.rs
@@ -1,10 +1,14 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
     };
-    let ans = n - a.len();
+    let modp = 1_000_000_007;
+    let mut dp = (1, 1);
+    for _ in 0..n - 2 {
+        dp = (dp.1, (dp.0 + dp.1) % modp);
+    }
+    let ans = dp.1;
     println!("{}", ans);
 }
