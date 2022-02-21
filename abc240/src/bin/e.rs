fd867bdd137fce493d51d724009a1aa9c6e74d44
bouzuya <m@bouzuya.net>
2022-02-22 08:53:24 +0900

    abc240 e

--- abc240/src/bin/e.rs
+++ abc240/src/bin/e.rs
@@ -1,4 +1,3 @@
-// WA
 use proconio::{input, marker::Usize1};
 
 fn adjacency_list(n: usize, uv: &[(usize, usize)]) -> Vec<Vec<usize>> {
@@ -10,18 +9,31 @@ fn adjacency_list(n: usize, uv: &[(usize, usize)]) -> Vec<Vec<usize>> {
     e
 }
 
-fn dfs(edges: &[Vec<usize>], ans: &mut Vec<(usize, usize)>, u: usize, p: usize, l: usize) -> usize {
-    let mut r = l;
+fn leaf_dfs(edges: &[Vec<usize>], leaf: &mut Vec<usize>, u: usize, p: usize) {
     let mut count = 0;
     for v in edges[u].iter().copied() {
         if v == p {
             continue;
         }
-        r = dfs(edges, ans, v, u, l + count);
+        leaf_dfs(edges, leaf, v, u);
         count += 1;
     }
-    ans[u] = (l, r);
-    r
+    if count == 0 {
+        leaf.push(u);
+    }
+}
+
+fn minmax_dfs(edges: &[Vec<usize>], ans: &mut Vec<(usize, usize)>, u: usize, p: usize) {
+    let (mut min, mut max) = ans[u];
+    for v in edges[u].iter().copied() {
+        if v == p {
+            continue;
+        }
+        minmax_dfs(edges, ans, v, u);
+        min = min.min(ans[v].0);
+        max = max.max(ans[v].1);
+    }
+    ans[u] = (min, max);
 }
 
 fn main() {
@@ -30,15 +42,17 @@ fn main() {
         uv: [(Usize1, Usize1); n - 1],
     };
 
-    let mut edges = adjacency_list(n, &uv);
-    for i in 0..n {
-        edges[i].sort();
-    }
+    let edges = adjacency_list(n, &uv);
 
-    let mut ans = vec![(0, 0); n];
-    dfs(&edges, &mut ans, 0, 0, 0);
-    let max = *ans.iter().map(|(a, b)| a.max(b)).max().unwrap();
+    let mut leaf = vec![];
+    leaf_dfs(&edges, &mut leaf, 0, 0);
+
+    let mut ans = vec![(n, 0); n];
+    for (i, leaf_i) in leaf.iter().copied().enumerate() {
+        ans[leaf_i] = (i + 1, i + 1);
+    }
+    minmax_dfs(&edges, &mut ans, 0, 0);
     for (a, b) in ans {
-        println!("{} {}", max - b + 1, max - a + 1);
+        println!("{} {}", a, b);
     }
 }
