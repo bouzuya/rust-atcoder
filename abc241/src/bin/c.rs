560a0ec7843186075837d1673a5a65ec23f5f1ec
bouzuya <m@bouzuya.net>
2022-02-27 16:59:20 +0900

    abc241 c

--- abc241/src/bin/c.rs
+++ abc241/src/bin/c.rs
@@ -1,103 +1,50 @@
-// WA
 use proconio::{input, marker::Chars};
 
-fn f(n: usize, s: &[Vec<char>]) -> bool {
-    for x in 0..n {
-        let y = 0;
-
-        let mut count = 0;
-        let mut r = 0;
-        for l in 0..n {
-            while y + r < n && x + r < n && (r - l < 6) {
-                if s[y + r][x + r] == '#' {
-                    count += 1;
-                }
-                r += 1;
-            }
-            if count >= 4 {
-                return true;
-            }
-            if r == l {
-                r += 1;
-            } else {
-                if s[y + l][x + l] == '#' {
-                    count -= 1;
-                }
-            }
-        }
-    }
-
+fn check_row(n: usize, s: &[Vec<char>]) -> bool {
     for y in 0..n {
-        let x = 0;
-
-        let mut count = 0;
-        let mut r = 0;
-        for l in 0..n {
-            while y + r < n && x + r < n && (r - l < 6) {
-                if s[y + r][x + r] == '#' {
+        for x in 0..=n - 6 {
+            let mut count = 0;
+            for i in 0..6 {
+                if s[y][x + i] == '#' {
                     count += 1;
                 }
-                r += 1;
             }
             if count >= 4 {
                 return true;
             }
-            if r == l {
-                r += 1;
-            } else {
-                if s[y + l][x + l] == '#' {
-                    count -= 1;
-                }
-            }
         }
     }
-
     false
 }
 
-fn g(n: usize, s: &[Vec<char>]) -> bool {
-    for i in 0..n {
-        let mut count = 0;
-        let mut r = 0;
-        for l in 0..n {
-            while r < n && (r - l < 6) {
-                if s[i][r] == '#' {
+fn check_col(n: usize, s: &[Vec<char>]) -> bool {
+    for x in 0..n {
+        for y in 0..=n - 6 {
+            let mut count = 0;
+            for i in 0..6 {
+                if s[y + i][x] == '#' {
                     count += 1;
                 }
-                r += 1;
             }
             if count >= 4 {
                 return true;
-            }
-            if r == l {
-                r += 1;
-            } else {
-                if s[i][l] == '#' {
-                    count -= 1;
-                }
             }
         }
     }
+    false
+}
 
-    for i in 0..n {
-        let mut count = 0;
-        let mut r = 0;
-        for l in 0..n {
-            while r < n && (r - l < 6) {
-                if s[r][i] == '#' {
+fn check_diagonal(n: usize, s: &[Vec<char>]) -> bool {
+    for x in 0..=n - 6 {
+        for y in 0..=n - 6 {
+            let mut count = 0;
+            for i in 0..6 {
+                if s[y + i][x + i] == '#' {
                     count += 1;
                 }
-                r += 1;
             }
             if count >= 4 {
                 return true;
-            }
-            if r == l {
-                r += 1;
-            } else {
-                if s[l][i] == '#' {
-                    count -= 1;
-                }
             }
         }
     }
@@ -107,22 +54,25 @@ fn g(n: usize, s: &[Vec<char>]) -> bool {
 fn main() {
     input! {
         n: usize,
-        mut s: [Chars; n],
+        s: [Chars; n],
     };
 
-    for _ in 0..4 {
-        if f(n, &s) || g(n, &s) {
-            println!("Yes");
-            return;
-        }
+    if check_row(n, &s) || check_col(n, &s) || check_diagonal(n, &s) {
+        println!("Yes");
+        return;
+    }
 
-        let mut t = vec![vec![' '; n]; n];
-        for i in 0..n {
-            for j in 0..n {
-                t[j][n - i - 1] = s[i][j];
-            }
+    let mut t = vec![vec![' '; n]; n];
+    for i in 0..n {
+        for j in 0..n {
+            t[j][n - i - 1] = s[i][j];
         }
-        s = t;
+    }
+    let s = t;
+
+    if check_row(n, &s) || check_col(n, &s) || check_diagonal(n, &s) {
+        println!("Yes");
+        return;
     }
 
     println!("No");
