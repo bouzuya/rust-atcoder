33d2e8a65df9ef88168e60d98425735b0cd7aa2b
bouzuya <m@bouzuya.net>
2022-03-03 21:06:07 +0900

    math-and-algorithm 066-067

--- math-and-algorithm/src/bin/066.rs
+++ math-and-algorithm/src/bin/066.rs
@@ -1,10 +1,21 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        n: i64,
+        k: i64
     };
-    let ans = n - a.len();
+    let mut count = 0_i64;
+    let k1 = k - 1;
+    for a in 1..=n {
+        for b in 1.max(a - k1)..=(a + k - 1).min(n) {
+            for c in 1.max(b - k1)..=(b + k - 1).min(n) {
+                if (a - c).abs() <= k1 {
+                    count += 1;
+                }
+            }
+        }
+    }
+    let ans = n.pow(3) - count;
     println!("{}", ans);
 }
--- math-and-algorithm/src/bin/067.rs
+++ math-and-algorithm/src/bin/067.rs
@@ -1,10 +1,36 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        h: usize,
+        w: usize,
+        a: [[usize; w]; h],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let sum_r = a
+        .iter()
+        .map(|a_i| a_i.iter().sum::<usize>())
+        .collect::<Vec<usize>>();
+    let sum_c = {
+        let mut s = vec![];
+        for j in 0..w {
+            let mut s_j = 0;
+            for i in 0..h {
+                s_j += a[i][j];
+            }
+            s.push(s_j);
+        }
+        s
+    };
+    let mut ans = vec![vec![0; w]; h];
+    for i in 0..h {
+        for j in 0..w {
+            ans[i][j] = sum_r[i] + sum_c[j] - a[i][j];
+        }
+    }
+
+    for i in 0..h {
+        for j in 0..w {
+            print!("{}{}", ans[i][j], if j == w - 1 { '\n' } else { ' ' });
+        }
+    }
 }
