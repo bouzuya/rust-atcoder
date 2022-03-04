928b65363f9aa2d9267505ee9ff824d6576eab53
bouzuya <m@bouzuya.net>
2022-03-04 20:56:11 +0900

    math-and-algorithm 068

--- math-and-algorithm/src/bin/068.rs
+++ math-and-algorithm/src/bin/068.rs
@@ -1,10 +1,36 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+
+fn gcd(n: usize, m: usize) -> usize {
+    if m == 0 {
+        n
+    } else {
+        gcd(m, n % m)
+    }
+}
+
+fn lcm(n: usize, m: usize) -> usize {
+    n * m / gcd(n, m)
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        k: usize,
+        v: [usize; k],
     };
-    let ans = n - a.len();
+
+    let mut ans = 0_i64;
+    for bits in 1..1 << k {
+        let mut is_even = true;
+        let mut x = 1_usize;
+        for (i, v_i) in v.iter().copied().enumerate() {
+            if ((bits >> i) & 1) == 1 {
+                x = lcm(x, v_i);
+                is_even = !is_even;
+            }
+        }
+        ans += if is_even { -1 } else { 1 } * ((n / x) as i64);
+    }
+
     println!("{}", ans);
 }
