784d5d89607401a0c3be125566fbfa91e890eea0
bouzuya <m@bouzuya.net>
2022-03-05 20:29:09 +0900

    math-and-algorithm 071

--- math-and-algorithm/src/bin/071.rs
+++ math-and-algorithm/src/bin/071.rs
@@ -15,14 +15,11 @@ fn main() {
             let (a2, b2, c2) = (a2 as f64, b2 as f64, c2 as f64);
             let x = (c2 * b1 - c1 * b2) / (a2 * b1 - a1 * b2);
             let y = (c2 * a1 - c1 * a2) / (b2 * a1 - b1 * a2);
-            let mut ok = true;
-            for (a3, b3, c3) in abc.iter().copied() {
-                if a3 as f64 * x + b3 as f64 * y > c3 as f64 {
-                    ok = false;
-                    break;
-                }
-            }
-            if ok {
+            if abc
+                .iter()
+                .copied()
+                .all(|(a, b, c)| a as f64 * x + b as f64 * y <= c as f64)
+            {
                 max = max.max(x + y);
             }
         }
