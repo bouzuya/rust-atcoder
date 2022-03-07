5a74dc4d775b8fc95074867e55a2317900cbb36a
bouzuya <m@bouzuya.net>
2022-03-07 21:19:19 +0900

    math-and-algorithm 074

--- math-and-algorithm/src/bin/074.rs
+++ math-and-algorithm/src/bin/074.rs
@@ -1,10 +1,24 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [i64; n],
     };
-    let ans = n - a.len();
+    //   1 2 3 4
+    // + 0 1 2 3
+    // - 3 2 1 0
+    //
+    // 2 - 1
+    // 3 - 1
+    // 4 - 1
+    // 3 - 2
+    // 4 - 2
+    // 4 - 3
+    let mut ans = 0_i64;
+    for (i, a_i) in a.iter().copied().enumerate() {
+        let i = i as i64;
+        ans += a_i * i - a_i * (n as i64 - 1 - i);
+    }
     println!("{}", ans);
 }
