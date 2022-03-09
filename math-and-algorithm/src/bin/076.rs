b5b3d8deb116071241ddf93d57aeb5f678b728fe
bouzuya <m@bouzuya.net>
2022-03-09 21:30:02 +0900

    math-and-algorithm 076-077

--- math-and-algorithm/src/bin/076.rs
+++ math-and-algorithm/src/bin/076.rs
@@ -1,10 +1,14 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        mut a: [i64; n],
     };
-    let ans = n - a.len();
+    a.sort();
+    let mut ans = 0_i64;
+    for (i, a_i) in a.iter().copied().enumerate() {
+        ans += a_i * i as i64 - a_i * (n as i64 - 1 - i as i64);
+    }
     println!("{}", ans);
 }
--- math-and-algorithm/src/bin/077.rs
+++ math-and-algorithm/src/bin/077.rs
@@ -1,10 +1,23 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+
+fn f(a: &[i64]) -> i64 {
+    let n = a.len() as i64;
+    let mut b = a.iter().copied().collect::<Vec<i64>>();
+    b.sort();
+    b.iter()
+        .copied()
+        .enumerate()
+        .map(|(i, b_i)| i as i64 * b_i - b_i * (n - 1 - i as i64))
+        .sum::<i64>()
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        xy: [(i64, i64); n],
     };
-    let ans = n - a.len();
+    let sum_x = f(&xy.iter().copied().map(|(x, _)| x).collect::<Vec<i64>>());
+    let sum_y = f(&xy.iter().copied().map(|(_, y)| y).collect::<Vec<i64>>());
+    let ans = sum_x + sum_y;
     println!("{}", ans);
 }
