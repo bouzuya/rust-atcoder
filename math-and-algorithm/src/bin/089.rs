9e1312d359543d09b102c7a385e985358cb68703
bouzuya <m@bouzuya.net>
2022-03-16 21:42:26 +0900

    math-and-algorithm 089-090

--- math-and-algorithm/src/bin/089.rs
+++ math-and-algorithm/src/bin/089.rs
@@ -1,10 +1,21 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        a: usize,
+        b: usize,
+        c: usize,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    // \log_2 a < b \log_2 c
+    // \log_2 a < \log_2 c^b
+    // a < c^b
+    let ans = if c == 1 {
+        false
+    } else {
+        match c.checked_pow(b.min(60) as u32) {
+            None => true,
+            Some(cb) => a < cb,
+        }
+    };
+    println!("{}", if ans { "Yes" } else { "No" });
 }
--- math-and-algorithm/src/bin/090.rs
+++ math-and-algorithm/src/bin/090.rs
@@ -1,10 +1,48 @@
-use proconio::{input, marker::Usize1};
+use std::collections::HashSet;
+
+use proconio::input;
+
+fn f(m: usize) -> usize {
+    m.to_string()
+        .chars()
+        .map(|c| (c as u8 - b'0') as usize)
+        .product::<usize>()
+}
+
+fn dfs(set: &mut HashSet<usize>, digits: &mut Vec<usize>, next: usize) {
+    if digits.len() == 11 {
+        return;
+    }
+
+    for d in next..=9 {
+        digits.push(d);
+        let f_m = f(digits
+            .iter()
+            .copied()
+            .map(|j| (j as u8 + b'0') as char)
+            .collect::<String>()
+            .parse::<usize>()
+            .unwrap());
+        set.insert(f_m);
+        dfs(set, digits, d);
+        digits.pop();
+    }
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        b: usize,
     };
-    let ans = n - a.len();
+    let mut digits = vec![];
+    let mut set = HashSet::new();
+    dfs(&mut set, &mut digits, 0);
+    let ans = set
+        .into_iter()
+        .filter(|f_m| {
+            let m = b + f_m;
+            (1..=n).contains(&m) && m - f(m) == b
+        })
+        .count();
     println!("{}", ans);
 }
