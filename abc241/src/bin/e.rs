6e7653e1eede73432222321dfa04db70c1a8c990
bouzuya <m@bouzuya.net>
2022-03-22 22:10:05 +0900

    abc241 e

--- abc241/src/bin/e.rs
+++ abc241/src/bin/e.rs
@@ -1,10 +1,53 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        k: usize,
+        a: [usize; n],
     };
-    let ans = n - a.len();
+
+    let (prefix_len, cycle_len) = {
+        let inf = n + 1;
+        let mut used = vec![inf; n];
+        let mut sum = 0_usize;
+        let mut index = 0_usize;
+        loop {
+            if used[sum % n] != inf {
+                break (used[sum % n], index - used[sum % n]);
+            }
+            used[sum % n] = index;
+            sum += a[sum % n];
+            index += 1;
+        }
+    };
+    let len = prefix_len + cycle_len;
+
+    let (_prefix_sum, cycle_sum) = {
+        let mut prefix_sum = 0_usize;
+        let mut sum = 0_usize;
+        for i in 0..len {
+            if i == prefix_len {
+                prefix_sum = sum;
+            }
+            sum += a[sum % n];
+        }
+        let cycle_sum = sum - prefix_sum;
+        (prefix_sum, cycle_sum)
+    };
+
+    let (sum, len) = if k <= prefix_len {
+        (0_usize, k)
+    } else {
+        (
+            ((k - prefix_len) / cycle_len) * cycle_sum,
+            prefix_len + (k - prefix_len) % cycle_len,
+        )
+    };
+    let mut ans = 0;
+    for _ in 0..len {
+        ans += a[ans % n];
+    }
+    ans += sum;
     println!("{}", ans);
 }
