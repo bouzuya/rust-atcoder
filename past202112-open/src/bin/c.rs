25965586574ae4810bd352ec387896dd5d603f8f
bouzuya <m@bouzuya.net>
2022-03-25 22:05:10 +0900

    past202112-open c

--- past202112-open/src/bin/c.rs
+++ past202112-open/src/bin/c.rs
@@ -1,10 +1,22 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        pv: [(char, String); n]
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut ids = vec![None; 6];
+    for (i, (p, v)) in pv.into_iter().enumerate() {
+        if v != "AC" {
+            continue;
+        }
+        let p = (p as u8 - b'A') as usize;
+        match ids[p] {
+            Some(_) => {}
+            None => ids[p] = Some(i),
+        }
+    }
+    for id in ids {
+        println!("{}", id.unwrap() + 1);
+    }
 }
