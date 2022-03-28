c5f15e8e0243098f68eb23be2910a5a6a0942a34
bouzuya <m@bouzuya.net>
2022-03-28 21:20:11 +0900

    abc245 e

--- abc245/src/bin/e.rs
+++ abc245/src/bin/e.rs
@@ -1,10 +1,56 @@
-use proconio::{input, marker::Usize1};
+use std::{cmp::Reverse, collections::BTreeMap};
+
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        a: [usize; n],
+        b: [usize; n],
+        c: [usize; m],
+        d: [usize; m],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    #[derive(Copy, Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
+    enum Type {
+        Box,
+        Chocolate,
+    }
+    let ab = a
+        .into_iter()
+        .zip(b.into_iter())
+        .map(|(a, b)| (a, b, Type::Chocolate));
+    let cd = c
+        .into_iter()
+        .zip(d.into_iter())
+        .map(|(c, d)| (c, d, Type::Box));
+    let mut abcd = ab.chain(cd).collect::<Vec<(usize, usize, Type)>>();
+    abcd.sort_by_key(|&(ac, bd, t)| (Reverse(ac), t, bd));
+    let mut boxes = BTreeMap::new();
+    for (ac, bd, t) in abcd {
+        match t {
+            Type::Box => {
+                let (_, d) = (ac, bd);
+                *boxes.entry(d).or_insert(0) += 1;
+            }
+            Type::Chocolate => {
+                let (_, b) = (ac, bd);
+                let mut remove = None;
+                if let Some((&d, count)) = boxes.range_mut(b..).next() {
+                    *count -= 1;
+                    if *count == 0 {
+                        remove = Some(d);
+                    }
+                } else {
+                    println!("No");
+                    return;
+                }
+                if let Some(d) = remove {
+                    boxes.remove(&d);
+                }
+            }
+        }
+    }
+
+    println!("Yes");
 }
