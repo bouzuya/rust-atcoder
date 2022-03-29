1a615e38baa74b093b14f11d676ddf3d32b12e9e
bouzuya <m@bouzuya.net>
2022-03-29 21:25:54 +0900

    past202112-open d

--- past202112-open/src/bin/d.rs
+++ past202112-open/src/bin/d.rs
@@ -1,10 +1,20 @@
-use proconio::{input, marker::Usize1};
+use std::cmp::Reverse;
+
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
+        b: [usize; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut ab = a
+        .into_iter()
+        .zip(b.into_iter())
+        .enumerate()
+        .collect::<Vec<_>>();
+    ab.sort_by_key(|&(i, (a_i, b_i))| (Reverse(a_i + b_i), Reverse(a_i), i));
+    for (i, _) in ab {
+        println!("{}", i + 1);
+    }
 }
