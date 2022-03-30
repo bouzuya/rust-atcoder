3a4b9745c8f0d5361ca71b7f45b7ee4e857fa394
bouzuya <m@bouzuya.net>
2022-03-30 20:47:15 +0900

    past202112-open e

--- past202112-open/src/bin/e.rs
+++ past202112-open/src/bin/e.rs
@@ -1,10 +1,25 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        n: Chars,
     };
-    let ans = n - a.len();
+    let is_left = |c: char| -> bool {
+        let x = (c as u8 - b'0') as usize;
+        x != 0 && x <= 5
+    };
+    let mut sum = 500_usize;
+    let mut p = n[0];
+    for c in n.into_iter().skip(1) {
+        sum += if p == c {
+            301
+        } else if is_left(p) == is_left(c) {
+            210
+        } else {
+            100
+        };
+        p = c;
+    }
+    let ans = sum;
     println!("{}", ans);
 }
