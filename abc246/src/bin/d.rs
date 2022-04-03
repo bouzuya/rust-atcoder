8803a0757cf7491b11fded40baaf6d653b8ed759
bouzuya <m@bouzuya.net>
2022-04-03 12:05:00 +0900

    abc246 d

--- abc246/src/bin/d.rs
+++ abc246/src/bin/d.rs
@@ -1,24 +1,37 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
     };
+
     if n == 0 {
-        println!("Yes");
+        println!("0");
         return;
     }
 
-    let mut y = 0;
+    let mut max_a = 1;
     for a in 1.. {
         if a * a * a >= n {
-            y = a;
+            max_a = a;
             break;
         }
     }
 
-    // WIP
-
-    let ans = n - a.len();
+    let f = |a: usize, b: usize| a.pow(3) + a.pow(2) * b + a * b.pow(2) + b.pow(3);
+    let mut max_b = max_a;
+    let mut min = 1 << 60;
+    for a in 0..=max_a {
+        for b in (a..=max_b).rev() {
+            let x = f(a, b);
+            if x >= n {
+                min = min.min(x);
+            } else {
+                max_b = b;
+                break;
+            }
+        }
+    }
+    let ans = min;
     println!("{}", ans);
 }
