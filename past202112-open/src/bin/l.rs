14fa13138a3aa29413aab44525fcc393dc28e641
bouzuya <m@bouzuya.net>
2022-04-09 23:51:21 +0900

    past202112-open l

--- past202112-open/src/bin/l.rs
+++ past202112-open/src/bin/l.rs
@@ -1,10 +1,25 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+use superslice::Ext;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        p: usize,
+        a: [usize; n],
     };
-    let ans = n - a.len();
+    let mut dp = vec![];
+    for (i, a_i) in a.into_iter().rev().enumerate() {
+        if !(i..=(p - (n - 1 - i))).contains(&a_i) {
+            continue;
+        }
+        let b_i = a_i - i;
+        let index = dp.upper_bound(&b_i);
+        if index < dp.len() {
+            dp[index] = b_i;
+        } else {
+            dp.push(b_i);
+        }
+    }
+    let ans = n - dp.len();
     println!("{}", ans);
 }
