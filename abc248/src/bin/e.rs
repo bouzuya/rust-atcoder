5beb80ba8907aed07d30579fa25abaebc0791b4d
bouzuya <m@bouzuya.net>
2022-04-18 21:18:01 +0900

    abc248 e

--- abc248/src/bin/e.rs
+++ abc248/src/bin/e.rs
@@ -1,7 +1,3 @@
-// TLE
-use std::collections::BTreeSet;
-
-use num::rational::Rational64;
 use proconio::input;
 
 fn main() {
@@ -15,45 +11,29 @@ fn main() {
         return;
     }
 
-    let mut set = BTreeSet::new();
+    let mut used = vec![vec![false; n]; n];
     let mut ans = 0_usize;
     for i in 0..n {
         for j in i + 1..n {
+            if used[i][j] {
+                continue;
+            }
             let (x_i, y_i) = xy[i];
             let (x_j, y_j) = xy[j];
-            let count = if x_j - x_i == 0 {
-                if !set.insert((
-                    0,
-                    Rational64::from_integer(x_i),
-                    Rational64::from_integer(0),
-                )) {
-                    continue;
+            let mut ps = vec![i, j];
+            for k in j + 1..n {
+                let (x_k, y_k) = xy[k];
+                if (y_k - y_i) * (x_j - x_i) == (y_j - y_i) * (x_k - x_i) {
+                    ps.push(k);
                 }
-                let mut count = 0_usize;
-                for k in 0..n {
-                    let (x_k, _) = xy[k];
-                    if x_k == x_i {
-                        count += 1;
-                    }
-                }
-                count
-            } else {
-                let a = Rational64::new(y_j - y_i, x_j - x_i);
-                let b = Rational64::from_integer(y_i) - a * Rational64::from_integer(x_i);
-                if !set.insert((1, a, b)) {
-                    continue;
-                }
-                let mut count = 0_usize;
-                for k in 0..n {
-                    let (x_k, y_k) = xy[k];
-                    if a * x_k + b == Rational64::from_integer(y_k) {
-                        count += 1;
-                    }
-                }
-                count
-            };
-            if count >= k {
+            }
+            if ps.len() >= k {
                 ans += 1;
+            }
+            for i in ps.iter().copied() {
+                for j in ps.iter().copied() {
+                    used[i][j] = true;
+                }
             }
         }
     }
