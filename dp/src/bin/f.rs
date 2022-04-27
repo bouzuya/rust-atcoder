b6db70820f2597a0466ca0add500a62b17139b28
bouzuya <m@bouzuya.net>
2022-04-27 21:05:37 +0900

    dp f, g, h

--- dp/src/bin/f.rs
+++ dp/src/bin/f.rs
@@ -1,10 +1,42 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        s: Chars,
+        t: Chars,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let n = s.len();
+    let m = t.len();
+    let mut dp = vec![vec![0; m + 1]; n + 1];
+    for i in 1..=n {
+        let s_i = s[i - 1];
+        for j in 1..=m {
+            let t_j = t[j - 1];
+            dp[i][j] = if s_i == t_j {
+                dp[i - 1][j - 1] + 1
+            } else {
+                dp[i - 1][j].max(dp[i][j - 1])
+            };
+        }
+    }
+
+    let mut ans = vec![];
+    let mut i = n;
+    let mut j = m;
+    while i > 0 && j > 0 {
+        if s[i - 1] == t[j - 1] {
+            ans.push(s[i - 1]);
+            i -= 1;
+            j -= 1;
+        } else if dp[i][j] == dp[i - 1][j] {
+            i -= 1;
+        } else if dp[i][j] == dp[i][j - 1] {
+            j -= 1;
+        } else {
+            unreachable!();
+        }
+    }
+    ans.reverse();
+
+    println!("{}", ans.iter().collect::<String>());
 }
--- dp/src/bin/g.rs
+++ dp/src/bin/g.rs
@@ -1,10 +1,38 @@
+use std::collections::VecDeque;
+
 use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        xy: [(Usize1, Usize1); m]
     };
-    let ans = n - a.len();
+    let mut count = vec![0; n];
+    let mut edges = vec![vec![]; n];
+    for (x, y) in xy.iter().copied() {
+        edges[y].push(x);
+        count[x] += 1;
+    }
+
+    let mut ans = vec![0; n];
+    let mut deque = count
+        .iter()
+        .copied()
+        .enumerate()
+        .filter(|&(_, c)| c == 0)
+        .map(|(i, _)| i)
+        .collect::<VecDeque<_>>();
+    while let Some(v) = deque.pop_front() {
+        for u in edges[v].iter().copied() {
+            count[u] -= 1;
+            if count[u] == 0 {
+                ans[u] = ans[v] + 1;
+                deque.push_back(u);
+            }
+        }
+    }
+
+    let ans = *ans.iter().max().unwrap();
     println!("{}", ans);
 }
