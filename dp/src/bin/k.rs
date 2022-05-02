f96188f57e75601d7ca52ceebc6bb00f725eaa44
bouzuya <m@bouzuya.net>
2022-05-02 19:21:46 +0900

    dp k, l

--- dp/src/bin/k.rs
+++ dp/src/bin/k.rs
@@ -1,10 +1,26 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        k: usize,
+        a: [usize; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut ans = vec![false; k + 1];
+    ans[0] = false;
+    for i in 1..=k {
+        let mut win = false;
+        for a_i in a.iter().copied() {
+            if i >= a_i {
+                if !ans[i - a_i] {
+                    win = true;
+                }
+            } else {
+                break;
+            }
+        }
+        ans[i] = win;
+    }
+    let ans = ans[k];
+    println!("{}", if ans { "First" } else { "Second" });
 }
--- dp/src/bin/l.rs
+++ dp/src/bin/l.rs
@@ -1,10 +1,42 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+
+fn f(memo: &mut Vec<Vec<Option<(i64, i64)>>>, a: &[i64], l: usize, r: usize) -> (i64, i64) {
+    if let Some((x, y)) = memo[l][r] {
+        return (x, y);
+    }
+
+    let (x, y) = if l + 1 == r {
+        let (x, y) = (a[l], a[r]);
+        if x > y {
+            (x, y)
+        } else {
+            (y, x)
+        }
+    } else {
+        let (x1, y1) = f(memo, a, l + 1, r);
+        let (x2, y2) = f(memo, a, l, r - 1);
+        if a[l] + y1 - x1 > a[r] + y2 - x1 {
+            (a[l] + y1, x1)
+        } else {
+            (a[r] + y2, x2)
+        }
+    };
+
+    memo[l][r] = Some((x, y));
+    (x, y)
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [i64; n],
     };
-    let ans = n - a.len();
+    if n == 1 {
+        println!("{}", a[0]);
+        return;
+    }
+    let mut memo = vec![vec![None; n + 1]; n + 1];
+    let (x, y) = f(&mut memo, &a, 0, n - 1);
+    let ans = x - y;
     println!("{}", ans);
 }
