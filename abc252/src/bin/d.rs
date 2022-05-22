c173654a17268fffb4b03e487ff6b0b4a94a991c
bouzuya <m@bouzuya.net>
2022-05-22 11:44:31 +0900

    abc252 d

--- abc252/src/bin/d.rs
+++ abc252/src/bin/d.rs
@@ -1,10 +1,32 @@
-use proconio::{input, marker::Usize1};
+use std::collections::BTreeMap;
+
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
+
+    let mut count = BTreeMap::new();
+    for a_i in a.iter().copied() {
+        *count.entry(a_i).or_insert(0_usize) += 1;
+    }
+
+    let mut curr = 0_usize;
+    let mut cumsum = BTreeMap::new();
+    for (&k, &v) in count.iter() {
+        *cumsum.entry(k).or_insert(0_usize) = curr;
+        curr += v;
+    }
+
+    let mut ans = 0_usize;
+    for a_j in a.iter().copied() {
+        let count_all = n;
+        let count_a_j = *count.get(&a_j).unwrap();
+        let count_a_i = *cumsum.get(&a_j).unwrap();
+        let count_a_k = count_all - count_a_i - count_a_j;
+        ans += count_a_i * count_a_k;
+    }
     println!("{}", ans);
 }
