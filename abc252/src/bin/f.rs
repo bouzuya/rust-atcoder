e46cc4b3c6ed39fdf16c7d8f314e0abfd17facc8
bouzuya <m@bouzuya.net>
2022-05-23 18:52:35 +0900

    abc252 f

--- abc252/src/bin/f.rs
+++ abc252/src/bin/f.rs
@@ -1,10 +1,35 @@
-use proconio::{input, marker::Usize1};
+use std::{cmp::Reverse, collections::BinaryHeap};
+
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        l: usize,
+        a: [usize; n],
     };
-    let ans = n - a.len();
+
+    let sum = a.iter().sum::<usize>();
+    let mut pq = BinaryHeap::new();
+    for a_i in a {
+        pq.push(Reverse(a_i));
+    }
+
+    if sum < l {
+        pq.push(Reverse(l - sum));
+    }
+
+    let mut ans = 0_usize;
+    loop {
+        match (pq.pop(), pq.pop()) {
+            (None, None) => unreachable!(),
+            (None, Some(_)) => unreachable!(),
+            (Some(Reverse(_)), None) => break,
+            (Some(Reverse(x)), Some(Reverse(y))) => {
+                ans += x + y;
+                pq.push(Reverse(x + y));
+            }
+        }
+    }
     println!("{}", ans);
 }
