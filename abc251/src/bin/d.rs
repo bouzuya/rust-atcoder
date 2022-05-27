038ca4a8f0a5908a66c0744b1a4fea684103846a
bouzuya <m@bouzuya.net>
2022-05-27 18:45:56 +0900

    abc251 d

--- abc251/src/bin/d.rs
+++ abc251/src/bin/d.rs
@@ -1,10 +1,20 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        _w: usize,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut ans = vec![];
+    for i in 0..3 {
+        let b = 100_usize.pow(i);
+        for j in 1..=99 {
+            ans.push(b * j);
+        }
+    }
+
+    println!("{}", ans.len());
+    for a in ans {
+        println!("{}", a);
+    }
 }
