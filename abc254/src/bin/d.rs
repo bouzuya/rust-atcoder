d33802824346cef7e1c40e96c22716a343b99563
bouzuya <m@bouzuya.net>
2022-06-05 21:28:16 +0900

    abc254 d

--- abc254/src/bin/d.rs
+++ abc254/src/bin/d.rs
@@ -1,10 +1,24 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
     };
-    let ans = n - a.len();
+    let mut ans = 0_usize;
+    for i in 1..=n {
+        let mut x = i;
+        for k2 in (2_usize..).map(|k| k.pow(2)) {
+            if k2 > x {
+                break;
+            }
+            while x % k2 == 0 {
+                x /= k2;
+            }
+        }
+        ans += (1_usize..)
+            .map(|k| k.pow(2))
+            .take_while(|&k2| x * k2 <= n)
+            .count();
+    }
     println!("{}", ans);
 }
