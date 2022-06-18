d1712a9f607ea2565e8a2311fe5ff99ca9629703
bouzuya <m@bouzuya.net>
2022-06-18 11:24:14 +0900

    agc016 b

--- agc016/src/bin/b.rs
+++ agc016/src/bin/b.rs
@@ -1,11 +1,27 @@
 use proconio::input;
-use proconio::marker::Usize1;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let max_a_i = a.iter().copied().max().unwrap();
+    let min_a_i = a.iter().copied().min().unwrap();
+    let ans = match max_a_i - min_a_i {
+        // ある色の帽子は 2 匹以上の猫に被られている
+        0 => {
+            let count_color = max_a_i;
+            a.iter().copied().all(|a_i| a_i == n - 1) || 2 * count_color <= n
+        }
+        // ある色の帽子は 1 匹の猫にしか被られていないことがある
+        1 => {
+            let count_color = max_a_i;
+            let count_max = a.iter().copied().filter(|a_i| a_i == &max_a_i).count();
+            let count_min = a.iter().copied().filter(|a_i| a_i == &min_a_i).count();
+            count_min < count_color && 2 * count_color <= count_max + 2 * count_min
+        }
+        _ => false,
+    };
+    println!("{}", if ans { "Yes" } else { "No" });
 }
