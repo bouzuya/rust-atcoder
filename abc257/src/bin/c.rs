29f44bcd11870a88fe61e9b894107c925167afb9
bouzuya <m@bouzuya.net>
2022-06-26 18:19:53 +0900

    abc257 c

--- abc257/src/bin/c.rs
+++ abc257/src/bin/c.rs
@@ -1,10 +1,31 @@
-use proconio::{input, marker::Usize1};
+use std::collections::BTreeMap;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        s: Chars,
+        w: [usize; n],
     };
-    let ans = n - a.len();
+
+    let mut map = BTreeMap::new();
+    for (w_i, s_i) in w
+        .iter()
+        .copied()
+        .zip(s.iter().copied())
+        .collect::<Vec<(usize, char)>>()
+    {
+        *map.entry(w_i).or_insert(0) += if s_i == '1' { -1 } else { 1 };
+    }
+
+    let count_ones = s.iter().copied().filter(|&s_i| s_i == '1').count() as i64;
+    let mut count = count_ones;
+    let mut max = count;
+    for d in map.values() {
+        count += d;
+        max = max.max(count);
+    }
+    let ans = max.max(s.iter().copied().filter(|&s_i| s_i == '0').count() as i64);
     println!("{}", ans);
 }
