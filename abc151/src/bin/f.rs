f377086950a78c1651eaf8d0cc6b5bc78981a625
bouzuya <m@bouzuya.net>
2022-07-07 21:04:49 +0900

    abc151 f

--- abc151/src/bin/f.rs
+++ abc151/src/bin/f.rs
@@ -1,11 +1,44 @@
 use proconio::input;
-use proconio::marker::Usize1;
+
+fn g(x: f64, y: f64, xy: &[(i64, i64)]) -> f64 {
+    let mut max = 0_f64;
+    for (x_i, y_i) in xy.iter().copied() {
+        max = max.max(((x - x_i as f64).powf(2_f64) + (y - y_i as f64).powf(2_f64)).sqrt());
+    }
+    max
+}
+
+fn f(x: f64, xy: &[(i64, i64)]) -> f64 {
+    let mut ly = 0_f64;
+    let mut ry = 1_000_f64;
+    for _ in 0..100 {
+        let nly = (ly * 2_f64 + ry) / 3_f64;
+        let nry = (ly + ry * 2_f64) / 3_f64;
+        if g(x, nly, xy) > g(x, nry, xy) {
+            ly = nly;
+        } else {
+            ry = nry;
+        }
+    }
+    g(x, ry, xy)
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        xy: [(i64, i64); n],
     };
-    let ans = n - a.len();
+    let mut lx = 0_f64;
+    let mut rx = 1_000_f64;
+    for _ in 0..100 {
+        let nlx = (lx * 2_f64 + rx) / 3_f64;
+        let nrx = (lx + rx * 2_f64) / 3_f64;
+        if f(nlx, &xy) > f(nrx, &xy) {
+            lx = nlx;
+        } else {
+            rx = nrx;
+        }
+    }
+    let ans = f(rx, &xy);
     println!("{}", ans);
 }
