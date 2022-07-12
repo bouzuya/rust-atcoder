b64243b80f60399da3a7cb0d3172954a7b3c9e37
bouzuya <m@bouzuya.net>
2022-07-12 19:02:21 +0900

    cf16-final d

--- cf16-final/src/bin/d.rs
+++ cf16-final/src/bin/d.rs
@@ -1,11 +1,38 @@
+use std::collections::HashMap;
+
 use proconio::input;
-use proconio::marker::Usize1;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        x: [usize; n],
     };
-    let ans = n - a.len();
+
+    let mut c = HashMap::new();
+    let mut r = vec![0_usize; m];
+    for x_i in x.iter().copied() {
+        r[x_i % m] += 1;
+        *c.entry(x_i).or_insert(0) += 1_usize;
+    }
+
+    let mut s = vec![0_usize; m];
+    for (x_i, count) in c {
+        s[x_i % m] += count / 2 * 2;
+    }
+
+    let mut ans = 0_usize;
+    for i in 0..=m / 2 {
+        let j = (m - i) % m;
+        if i == j {
+            let a = r[i] / 2;
+            ans += a;
+            r[i] -= a * 2;
+            continue;
+        }
+        let (i, j) = if r[i] < r[j] { (i, j) } else { (j, i) };
+        ans += r[i] + s[j].min(r[j] - r[i]) / 2;
+    }
+
     println!("{}", ans);
 }
