b9ba8b6fd4fbce861a22b9cb6f7ba573e45138f0
bouzuya <m@bouzuya.net>
2022-07-13 21:37:55 +0900

    abc176 e

--- abc176/src/bin/e.rs
+++ abc176/src/bin/e.rs
@@ -9,41 +9,39 @@ fn main() {
         m: usize,
         hw: [(Usize1, Usize1); m],
     };
+
     let mut set = HashSet::new();
     let mut map_r = HashMap::new();
     let mut map_c = HashMap::new();
     for (h_i, w_i) in hw {
         set.insert((h_i, w_i));
-        map_r.entry(h_i).or_insert_with(HashSet::new).insert(w_i);
-        map_c.entry(w_i).or_insert_with(HashSet::new).insert(h_i);
+        map_r.entry(h_i).or_insert_with(Vec::new).push(w_i);
+        map_c.entry(w_i).or_insert_with(Vec::new).push(h_i);
     }
 
-    let max_c = map_c.iter().map(|(_, s)| s.len()).max().unwrap();
-    let mut remove_keys = vec![];
-    for (w_i, s) in map_c.iter() {
-        if s.len() != max_c {
-            remove_keys.push(*w_i);
-            continue;
-        }
-    }
-    for k in remove_keys {
-        map_c.remove(&k);
-    }
+    let max_r = map_r.iter().map(|(_, v)| v.len()).max().unwrap();
+    let map_r = map_r
+        .into_iter()
+        .filter(|(_, v)| v.len() == max_r)
+        .map(|(k, _)| k)
+        .collect::<Vec<usize>>();
+    let max_c = map_c.iter().map(|(_, v)| v.len()).max().unwrap();
+    let map_c = map_c
+        .into_iter()
+        .filter(|(_, v)| v.len() == max_c)
+        .map(|(k, _)| k)
+        .collect::<Vec<usize>>();
 
-    let max_r = map_r.iter().map(|(_, s)| s.len()).max().unwrap();
-    for (h_i, set_w) in map_r.iter() {
-        if set_w.len() != max_r {
-            continue;
-        }
-
-        for (w_i, _) in map_c.iter() {
-            if !set.contains(&(*h_i, *w_i)) {
-                println!("{}", max_c + max_r);
+    for h_i in map_r {
+        for w_i in map_c.iter().copied() {
+            if !set.contains(&(h_i, w_i)) {
+                let ans = max_r + max_c;
+                println!("{}", ans);
                 return;
             }
         }
     }
 
-    let ans = max_c + max_r - 1;
+    let ans = max_r + max_c - 1;
     println!("{}", ans);
 }
