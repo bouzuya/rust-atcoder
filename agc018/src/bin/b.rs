34a43386d309c28196c15ea0ed52d766cbf85504
bouzuya <m@bouzuya.net>
2022-07-24 22:40:55 +0900

    agc018 b

--- agc018/src/bin/b.rs
+++ agc018/src/bin/b.rs
@@ -1,11 +1,31 @@
-use proconio::input;
-use proconio::marker::Usize1;
+use proconio::{input, marker::Usize1};
+
+fn f(a: &[Vec<usize>], set: &[bool]) -> Vec<usize> {
+    let mut count = vec![0; a[0].len()];
+    for a_i in a.iter() {
+        for a_ij in a_i.iter().copied() {
+            if set[a_ij] {
+                count[a_ij] += 1;
+                break;
+            }
+        }
+    }
+    count
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        a: [[Usize1; m]; n],
     };
-    let ans = n - a.len();
+    let mut set = vec![true; m];
+    let mut ans = n;
+    for _ in 0..m {
+        let count = f(&a, &set);
+        let max = count.iter().copied().max().unwrap();
+        ans = ans.min(max);
+        set[count.iter().copied().position(|c| c == max).unwrap()] = false;
+    }
     println!("{}", ans);
 }
