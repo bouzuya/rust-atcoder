5013f02246b9f7ab8bf40484faae7379ae89cb1e
bouzuya <m@bouzuya.net>
2022-08-28 16:03:54 +0900

    abc266 b, c

--- abc266/src/bin/c.rs
+++ abc266/src/bin/c.rs
@@ -1,10 +1,19 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        abcd: [(i64, i64); 4],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    for i in 0..4 {
+        let a = abcd[i];
+        let b = abcd[(i + 1) % 4];
+        let c = abcd[(i + 2) % 4];
+        let ab = (b.0 - a.0, b.1 - a.1);
+        let bc = (c.0 - b.0, c.1 - b.1);
+        if ab.0 * bc.1 - ab.1 * bc.0 < 0 {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
