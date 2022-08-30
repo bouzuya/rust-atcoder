f4edef05caddf37cca44f089c54994f77e44c523
bouzuya <m@bouzuya.net>
2022-08-30 18:29:56 +0900

    abc266 e

--- abc266/src/bin/e.rs
+++ abc266/src/bin/e.rs
@@ -1,10 +1,18 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
     };
-    let ans = n - a.len();
+
+    let mut ans = 3.5_f64;
+    for _ in 1..n {
+        let mut s = 0_f64;
+        for x in 1..=6 {
+            s += (x as f64).max(ans);
+        }
+        s /= 6_f64;
+        ans = s;
+    }
     println!("{}", ans);
 }
