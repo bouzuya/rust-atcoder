866f8788ca15888f93ff6fe55ac8c0db6871d621
bouzuya <m@bouzuya.net>
2022-09-04 21:25:43 +0900

    abc267 d

--- abc267/src/bin/d.rs
+++ abc267/src/bin/d.rs
@@ -1,10 +1,32 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+
+macro_rules! chmax {
+    ($max_v: expr, $v: expr) => {
+        if $v > $max_v {
+            $max_v = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        a: [i64; n],
     };
-    let ans = n - a.len();
+    let inf = 1_i64 << 60;
+    let mut dp = vec![-inf; m + 1];
+    for i in 0..n {
+        let mut next = dp.clone();
+        for j in 2..=m.min(i + 1) {
+            chmax!(next[j], dp[j - 1] + j as i64 * a[i]);
+        }
+        chmax!(next[1], a[i]);
+        dp = next;
+    }
+    let ans = dp[m];
     println!("{}", ans);
 }
