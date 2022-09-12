899251b7b266a4f6f071f5c5390e5c060ae6b418
bouzuya <m@bouzuya.net>
2022-09-12 18:27:45 +0900

    abc268 c

--- abc268/src/bin/c.rs
+++ abc268/src/bin/c.rs
@@ -1,10 +1,16 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        p: [usize; n],
     };
-    let ans = n - a.len();
+    let mut scores = vec![0_usize; n];
+    for (i, p_i) in p.iter().copied().enumerate() {
+        for d in -1..=1 {
+            scores[((n + p_i - i) as i64 + d) as usize % n] += 1;
+        }
+    }
+    let ans = scores.into_iter().max().unwrap();
     println!("{}", ans);
 }
