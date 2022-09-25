c145efb0652b33b0c00033974532acdffb7cbc70
bouzuya <m@bouzuya.net>
2022-09-25 21:01:50 +0900

    abc270 d

--- abc270/src/bin/d.rs
+++ abc270/src/bin/d.rs
@@ -1,10 +1,33 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+
+fn f(dp: &mut Vec<usize>, inf: usize, n: usize, a: &[usize], i: usize) {
+    if dp[i] != inf {
+        return;
+    }
+
+    let mut max = 0_usize;
+    for a_j in a.iter().copied() {
+        if a_j > i {
+            break;
+        }
+
+        f(dp, inf, n, a, i - a_j);
+        max = max.max(i - dp[i - a_j]);
+    }
+
+    dp[i] = max;
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        k: usize,
+        a: [usize; k],
     };
-    let ans = n - a.len();
+    let inf = 1 << 60;
+    let mut dp = vec![inf; n + 1];
+    dp[0] = 0;
+    f(&mut dp, inf, n, &a, n);
+    let ans = dp[n];
     println!("{}", ans);
 }
