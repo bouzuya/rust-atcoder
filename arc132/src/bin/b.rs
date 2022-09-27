849db2c46e0d726ee2afc4db2d098fc62c1a717d
bouzuya <m@bouzuya.net>
2022-09-27 21:53:34 +0900

    arc132 b

--- arc132/src/bin/b.rs
+++ arc132/src/bin/b.rs
@@ -1,10 +1,17 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        p: [usize; n],
     };
-    let ans = n - a.len();
+    let i = p.iter().position(|p_i| p_i == &1).unwrap();
+    let asc = p[(i + 1) % n] == 2;
+
+    let ans = if asc {
+        i.min(1 + 1 + n - 1 - i + 1)
+    } else {
+        1 + (n - 1 - i).min(1 + i)
+    };
     println!("{}", ans);
 }
