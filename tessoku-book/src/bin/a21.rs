a4641a7602ce23e6c2d27e313904f298cf9a95a7
bouzuya <m@bouzuya.net>
2022-10-11 19:46:34 +0900

    tessoku-book a21, b21

--- tessoku-book/src/bin/a21.rs
+++ tessoku-book/src/bin/a21.rs
@@ -1,38 +1,75 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
-fn f(memo: &mut Vec<Vec<usize>>, pa: &[(usize, usize)], inf: usize, l: usize, r: usize) -> usize {
-    if l >= r {
-        return 0;
-    }
+// fn f(memo: &mut Vec<Vec<usize>>, pa: &[(usize, usize)], inf: usize, l: usize, r: usize) -> usize {
+//     if l >= r {
+//         return 0;
+//     }
 
-    if memo[l][r] != inf {
-        return memo[l][r];
-    }
-    let left = f(memo, pa, inf, l + 1, r)
-        + if (l + 1..=r).contains(&pa[l].0) {
-            pa[l].1
-        } else {
-            0
-        };
-    let right = f(memo, pa, inf, l, r - 1)
-        + if (l..r).contains(&pa[r].0) {
-            pa[r].1
-        } else {
-            0
-        };
-    let score = left.max(right);
-    memo[l][r] = score;
-    score
-}
+//     if memo[l][r] != inf {
+//         return memo[l][r];
+//     }
+//     let left = f(memo, pa, inf, l + 1, r)
+//         + if (l + 1..=r).contains(&pa[l].0) {
+//             pa[l].1
+//         } else {
+//             0
+//         };
+//     let right = f(memo, pa, inf, l, r - 1)
+//         + if (l..r).contains(&pa[r].0) {
+//             pa[r].1
+//         } else {
+//             0
+//         };
+//     let score = left.max(right);
+//     memo[l][r] = score;
+//     score
+// }
+
+// fn main() {
+//     input! {
+//         n: usize,
+//         pa: [(Usize1, usize); n],
+//     };
+//     let inf = 1_usize << 60;
+//     let mut memo = vec![vec![inf; n + 1]; n + 1];
+//     memo[0][0] = 0;
+//     let ans = f(&mut memo, &pa, inf, 0, n - 1);
+//     println!("{}", ans);
+// }
 
 fn main() {
     input! {
         n: usize,
-        pa: [(Usize1, usize); n],
+        pa: [(usize, usize); n],
     };
-    let inf = 1_usize << 60;
-    let mut memo = vec![vec![inf; n + 1]; n + 1];
-    memo[0][0] = 0;
-    let ans = f(&mut memo, &pa, inf, 0, n - 1);
+    let mut dp = vec![vec![0; n + 2]; n + 2];
+    dp[1][n] = 0;
+    for len in (0..=n - 2).rev() {
+        for l in 1..=n - len {
+            let r = l + len;
+
+            let score1 = if l >= 2 && (l..=r).contains(&pa[l - 1 - 1].0) {
+                pa[l - 1 - 1].1
+            } else {
+                0
+            };
+
+            let score2 = if r < n && (l..=r).contains(&pa[r + 1 - 1].0) {
+                pa[r + 1 - 1].1
+            } else {
+                0
+            };
+
+            dp[l][r] = if l == 1 {
+                dp[l][r + 1] + score2
+            } else if r == n {
+                dp[l - 1][r] + score1
+            } else {
+                (dp[l][r + 1] + score2).max(dp[l - 1][r] + score1)
+            };
+        }
+    }
+
+    let ans = (1..=n).map(|i| dp[i][i]).max().unwrap();
     println!("{}", ans);
 }
--- tessoku-book/src/bin/b21.rs
+++ tessoku-book/src/bin/b21.rs
@@ -1,10 +1,31 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        s: Chars,
     };
-    let ans = n - a.len();
+    let mut dp = vec![vec![0; n]; n];
+    for len in 1..=n {
+        for l in 0..=n - len {
+            // [l, r]
+            let r = l + len - 1;
+
+            dp[l][r] = if l == r {
+                1
+            } else if l + 1 == r {
+                if s[l] == s[r] {
+                    2
+                } else {
+                    1
+                }
+            } else {
+                (dp[l + 1][r - 1] + if s[l] == s[r] { 2 } else { 0 })
+                    .max(dp[l][r - 1])
+                    .max(dp[l + 1][r])
+            };
+        }
+    }
+    let ans = dp[0][n - 1];
     println!("{}", ans);
 }
