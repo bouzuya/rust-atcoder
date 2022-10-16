7cd7a043a13883f675dad9a836a228f7ad45cc36
bouzuya <m@bouzuya.net>
2022-10-16 22:55:51 +0900

    tessoku-book a29

--- tessoku-book/src/bin/a29.rs
+++ tessoku-book/src/bin/a29.rs
@@ -1,10 +1,23 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        a: usize,
+        b: usize,
     };
-    let ans = n - a.len();
+
+    let p = 1_000_000_007;
+    let mut res = 1_usize;
+    let mut q = b;
+    let mut base = a;
+    while q != 0 {
+        if (q & 1) == 1 {
+            res *= base;
+            res %= p;
+        }
+        base = base.pow(2) % p;
+        q >>= 1;
+    }
+    let ans = res;
     println!("{}", ans);
 }
