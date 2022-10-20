fea1811a6580f5ea7e0f84973bb0b7e54a2193f0
bouzuya <m@bouzuya.net>
2022-10-20 19:20:54 +0900

    tessoku-book a34, b34

--- tessoku-book/src/bin/a34.rs
+++ tessoku-book/src/bin/a34.rs
@@ -1,10 +1,31 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        x: usize,
+        y: usize,
+        a: [usize; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let max_a_i = *a.iter().max().unwrap();
+
+    let mut grundy = vec![0; max_a_i + 1];
+    for i in 0..=max_a_i {
+        let mut transit = vec![false; 3];
+        if i >= x {
+            transit[grundy[i - x]] = true;
+        }
+        if i >= y {
+            transit[grundy[i - y]] = true;
+        }
+        grundy[i] = transit.iter().copied().position(|v| !v).unwrap();
+    }
+
+    let ans = a
+        .iter()
+        .copied()
+        .fold(0_usize, |acc, a_i| acc ^ grundy[a_i])
+        != 0;
+    println!("{}", if ans { "First" } else { "Second" });
 }
