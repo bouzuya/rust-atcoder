6ff6d47e0334e575d0d6e98293627e12ee158fd6
bouzuya <m@bouzuya.net>
2022-10-21 18:33:27 +0900

    tessoku-book a35

--- tessoku-book/src/bin/a35.rs
+++ tessoku-book/src/bin/a35.rs
@@ -1,10 +1,22 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
+
+    let mut dp = a;
+    for i in (1..n).rev() {
+        let mut parent = vec![0; dp.len() - 1];
+        for j in 0..parent.len() {
+            let l = dp[j];
+            let r = dp[j + 1];
+            parent[j] = if i % 2 == 0 { l.min(r) } else { l.max(r) };
+        }
+        dp = parent;
+    }
+
+    let ans = dp[0];
     println!("{}", ans);
 }
