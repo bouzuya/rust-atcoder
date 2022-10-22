b7dde58d05bba3afe17f9bc0b1e1d64d42b322db
bouzuya <m@bouzuya.net>
2022-10-22 11:11:46 +0900

    abc272 e

--- abc272/src/bin/e.rs
+++ abc272/src/bin/e.rs
@@ -1,10 +1,41 @@
-use proconio::{input, marker::Usize1};
+use std::collections::HashSet;
+
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        a: [i64; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut sets = vec![HashSet::new(); m];
+    for (i, a_i) in a.iter().copied().enumerate() {
+        let i = (i + 1) as i64;
+        let a_i = a_i + i;
+        let l = if a_i >= 0 { 0 } else { (-a_i + i - 1) / i } as usize;
+        let r = if a_i >= n as i64 {
+            0
+        } else {
+            (n as i64 - a_i + i - 1) / i
+        }
+        .min(m as i64)
+        .min(n as i64)
+        .max(0) as usize;
+
+        for j in l..r {
+            sets[j].insert((a_i + j as i64 * i) as usize);
+        }
+    }
+
+    for set in sets {
+        let mut b = vec![false; set.len() + 1];
+        for x in set.iter().copied() {
+            if x < set.len() {
+                b[x] = true;
+            }
+        }
+        let ans = b.iter().copied().position(|b_i| !b_i).unwrap();
+        println!("{}", ans);
+    }
 }
