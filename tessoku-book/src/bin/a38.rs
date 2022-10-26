19ab10242b29f2efabd478007ac19de81cce963d
bouzuya <m@bouzuya.net>
2022-10-26 19:22:18 +0900

    tessoku-book a38, b38

--- tessoku-book/src/bin/a38.rs
+++ tessoku-book/src/bin/a38.rs
@@ -1,10 +1,17 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
+        d: usize,
         n: usize,
-        a: [Usize1; n],
+        lrh: [(usize, usize, usize); n],
     };
-    let ans = n - a.len();
+    let mut max = vec![24; d + 1];
+    for (l, r, h) in lrh {
+        for j in l..=r {
+            max[j] = max[j].min(h);
+        }
+    }
+    let ans = max.into_iter().skip(1).sum::<usize>();
     println!("{}", ans);
 }
--- tessoku-book/src/bin/b38.rs
+++ tessoku-book/src/bin/b38.rs
@@ -1,10 +1,34 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        s: Chars,
     };
-    let ans = n - a.len();
+    let d = s
+        .into_iter()
+        .map(|s_i| match s_i {
+            'A' => 1,
+            'B' => -1,
+            _ => unreachable!(),
+        })
+        .collect::<Vec<i64>>();
+    let mut min = vec![1_i64; n];
+    for (i, d_i) in d.iter().copied().enumerate() {
+        if d_i > 0 {
+            if min[i] >= min[i + 1] {
+                min[i + 1] = min[i] + 1;
+            }
+        }
+    }
+    for (i, d_i) in d.iter().copied().enumerate().rev() {
+        if d_i < 0 {
+            if min[i] <= min[i + 1] {
+                min[i] = min[i].max(min[i + 1] + 1);
+            }
+        }
+    }
+
+    let ans = min.iter().sum::<i64>();
     println!("{}", ans);
 }
