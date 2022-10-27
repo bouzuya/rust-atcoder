d57538251de769a9cb3d1a5cc6c31ce4d5260283
bouzuya <m@bouzuya.net>
2022-10-27 21:04:17 +0900

    tessoku-book a39, b39

--- tessoku-book/src/bin/a39.rs
+++ tessoku-book/src/bin/a39.rs
@@ -1,10 +1,19 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        mut lr: [(usize, usize); n],
     };
-    let ans = n - a.len();
+    lr.sort_by_key(|&(l, r)| (r, l));
+    let mut count = 1_usize;
+    let mut prev = lr[0];
+    for (l, r) in lr.into_iter().skip(1) {
+        if prev.1 <= l {
+            count += 1;
+            prev = (l, r);
+        }
+    }
+    let ans = count;
     println!("{}", ans);
 }
--- tessoku-book/src/bin/b39.rs
+++ tessoku-book/src/bin/b39.rs
@@ -1,10 +1,26 @@
-use proconio::{input, marker::Usize1};
+use std::collections::BinaryHeap;
+
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        d: usize,
+        mut xy: [(usize, usize); n],
     };
-    let ans = n - a.len();
+    xy.sort_by_key(|&(x_i, _)| x_i);
+    let mut pq = BinaryHeap::new();
+    let mut sum = 0_usize;
+    let mut index = 0;
+    for d_i in 1..=d {
+        while index < n && xy[index].0 <= d_i {
+            pq.push(xy[index].1);
+            index += 1;
+        }
+        if let Some(x) = pq.pop() {
+            sum += x;
+        }
+    }
+    let ans = sum;
     println!("{}", ans);
 }
