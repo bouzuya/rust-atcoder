d45ce8c3766ec4c8d4b204ef42f33f53f5c74713
bouzuya <m@bouzuya.net>
2022-10-28 19:21:56 +0900

    tessoku-book a40, b40

--- tessoku-book/src/bin/a40.rs
+++ tessoku-book/src/bin/a40.rs
@@ -1,10 +1,25 @@
-use proconio::{input, marker::Usize1};
+use std::collections::HashMap;
+
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
+
+    let mut map = HashMap::new();
+    for a_i in a {
+        *map.entry(a_i).or_insert(0) += 1;
+    }
+
+    let mut ans = 0_usize;
+    for (_, v) in map {
+        if v < 3 {
+            continue;
+        }
+        ans += v * (v - 1) * (v - 2) / 6;
+    }
+
     println!("{}", ans);
 }
--- tessoku-book/src/bin/b40.rs
+++ tessoku-book/src/bin/b40.rs
@@ -1,10 +1,30 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
+    let mut count = vec![0_usize; 100];
+    for a_i in a {
+        count[a_i % 100] += 1;
+    }
+
+    let mut ans = 0_usize;
+    for x in 0..100 {
+        for y in x..100 {
+            if (x + y) % 100 == 0 {
+                ans += if x == y {
+                    if count[x] < 2 {
+                        0
+                    } else {
+                        count[x] * (count[x] - 1) / 2
+                    }
+                } else {
+                    count[x] * count[y]
+                };
+            }
+        }
+    }
     println!("{}", ans);
 }
