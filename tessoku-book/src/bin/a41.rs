d5d828e0c5fbc10c61dcc46d898ec50205fadfb7
bouzuya <m@bouzuya.net>
2022-10-30 21:31:57 +0900

    tessoku-book a41, b41

--- tessoku-book/src/bin/a41.rs
+++ tessoku-book/src/bin/a41.rs
@@ -1,10 +1,16 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        s: Chars,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut ok = false;
+    for i in 0..n - 2 {
+        if s[i] == s[i + 1] && s[i] == s[i + 2] {
+            ok = true;
+        }
+    }
+    let ans = ok;
+    println!("{}", if ans { "Yes" } else { "No" });
 }
--- tessoku-book/src/bin/b41.rs
+++ tessoku-book/src/bin/b41.rs
@@ -1,10 +1,27 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        mut x: usize,
+        mut y: usize,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut ans = vec![];
+    ans.push((x, y));
+    while !(x == 1 && y == 1) {
+        if x > y {
+            let d = x - y;
+            x = d;
+            ans.push((x, y));
+        } else {
+            let d = y - x;
+            y = d;
+            ans.push((x, y));
+        }
+    }
+    ans.pop();
+    ans.reverse();
+    println!("{}", ans.len());
+    for (x, y) in ans {
+        println!("{} {}", x, y);
+    }
 }
