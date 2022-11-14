3977a5d3ad323e5eac0cc4c7c5ca70a11480e0df
bouzuya <m@bouzuya.net>
2022-11-14 20:54:09 +0900

    past202206-open b

--- past202206-open/src/bin/b.rs
+++ past202206-open/src/bin/b.rs
@@ -1,10 +1,21 @@
-use proconio::{input, marker::Usize1};
+use std::collections::{BTreeMap, BTreeSet, HashMap};
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        s: Chars,
     };
-    let ans = n - a.len();
+    let n = s.len();
+    let mut map = HashMap::new();
+    for i in 0..n - 1 {
+        let t = format!("{}{}", s[i], s[i + 1]);
+        *map.entry(t).or_insert(0_usize) += 1;
+    }
+    let mut rev = BTreeMap::new();
+    for (t, count) in map {
+        rev.entry(count).or_insert_with(BTreeSet::new).insert(t);
+    }
+    let ans = rev.iter().last().unwrap().1.iter().next().unwrap();
     println!("{}", ans);
 }
