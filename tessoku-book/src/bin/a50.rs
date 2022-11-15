a63c6bc45d8efc5f8c259d7ffd1cb4e1f474a2c7
bouzuya <m@bouzuya.net>
2022-11-15 21:39:50 +0900

    tessoku-book a50 (1)

--- tessoku-book/src/bin/a50.rs
+++ tessoku-book/src/bin/a50.rs
@@ -1,10 +1,26 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+use rand::{thread_rng, Rng};
+
+// 4625995259
+fn f1(n: usize, _a: &[Vec<usize>]) -> Vec<(usize, usize, usize)> {
+    let mut ans = vec![];
+    let mut rng = thread_rng();
+    for _ in 0..1000 {
+        let i = rng.gen::<usize>() % n;
+        let j = rng.gen::<usize>() % n;
+        ans.push((i, j, 1));
+    }
+    ans
+}
 
 fn main() {
+    let n = 100_usize;
     input! {
-        n: usize,
-        a: [Usize1; n],
+        a: [[usize; n]; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let ans = f1(n, &a);
+    println!("{}", ans.len());
+    for (x, y, h) in ans {
+        println!("{} {} {}", x, y, h);
+    }
 }
