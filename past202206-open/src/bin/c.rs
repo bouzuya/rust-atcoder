7073259c78292a078f2944c853a43ad9690cf20f
bouzuya <m@bouzuya.net>
2022-11-20 19:19:44 +0900

    past202206-open c

--- past202206-open/src/bin/c.rs
+++ past202206-open/src/bin/c.rs
@@ -1,10 +1,23 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut ans = vec![];
+    for k in 1..=m {
+        let c = match n.checked_pow(k as u32) {
+            Some(nk) => {
+                if nk <= 1_000_000_000_usize {
+                    'o'
+                } else {
+                    'x'
+                }
+            }
+            None => 'x',
+        };
+        ans.push(c);
+    }
+    println!("{}", ans.iter().collect::<String>());
 }
