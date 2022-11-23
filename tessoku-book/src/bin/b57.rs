421d3d0eec55bf27e455459fe9717ccef2df7f9b
bouzuya <m@bouzuya.net>
2022-11-23 18:22:16 +0900

    tessoku-book b57

--- tessoku-book/src/bin/b57.rs
+++ tessoku-book/src/bin/b57.rs
@@ -1,10 +1,35 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        k: usize,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let m = 30;
+    let mut dp = vec![vec![0_usize; n + 1]; m + 1];
+    for i in 1..=n {
+        dp[0][i] = i - i
+            .to_string()
+            .chars()
+            .map(|c| (c as u8 - b'0') as usize)
+            .sum::<usize>();
+    }
+    for i in 1..=m {
+        for j in 0..=n {
+            dp[i][j] = dp[i - 1][dp[i - 1][j]];
+        }
+    }
+
+    for i in 1..=n {
+        let mut cur = i;
+        let mut cnt = k;
+        for dp_j in dp.iter() {
+            if (cnt & 1) != 0 {
+                cur = dp_j[cur];
+            }
+            cnt >>= 1;
+        }
+        println!("{}", cur);
+    }
 }
