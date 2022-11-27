289cbbf46b14dc94aee87f8de5410f0ef27f091f
bouzuya <m@bouzuya.net>
2022-11-27 20:00:50 +0900

    tessoku-book a60

--- tessoku-book/src/bin/a60.rs
+++ tessoku-book/src/bin/a60.rs
@@ -1,10 +1,28 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut stack = vec![];
+    for (d, a_d) in a.iter().copied().enumerate() {
+        let mut found = None;
+        while let Some((prev, i)) = stack.pop() {
+            if prev > a_d {
+                found = Some((prev, i));
+                break;
+            }
+        }
+        match found {
+            None => {
+                println!("-1");
+            }
+            Some((p, i)) => {
+                println!("{}", i + 1);
+                stack.push((p, i));
+            }
+        }
+        stack.push((a_d, d));
+    }
 }
