d4c606a403644e5e073d4e4596c3270538bbbb61
bouzuya <m@bouzuya.net>
2022-11-28 22:08:31 +0900

    tessoku-book a61-b61

--- tessoku-book/src/bin/a61.rs
+++ tessoku-book/src/bin/a61.rs
@@ -1,10 +1,31 @@
+use std::collections::BTreeSet;
+
 use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        ab: [(Usize1, Usize1); m],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut edges = vec![vec![]; n];
+    for (a, b) in ab {
+        edges[a].push(b);
+        edges[b].push(a);
+    }
+
+    for (k, edges_k) in edges.iter().enumerate() {
+        println!(
+            "{}: {{{}}}",
+            k + 1,
+            edges_k
+                .iter()
+                .map(|v| v + 1)
+                .collect::<BTreeSet<_>>()
+                .into_iter()
+                .map(|v| v.to_string())
+                .collect::<Vec<String>>()
+                .join(", ")
+        )
+    }
 }
--- tessoku-book/src/bin/b61.rs
+++ tessoku-book/src/bin/b61.rs
@@ -1,10 +1,26 @@
+use std::cmp::Reverse;
+
 use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        ab: [(Usize1, Usize1); m],
     };
-    let ans = n - a.len();
+    let mut edges = vec![0_usize; n];
+    for (a, b) in ab {
+        edges[a] += 1;
+        edges[b] += 1;
+    }
+
+    let mut count = edges
+        .iter()
+        .copied()
+        .enumerate()
+        .collect::<Vec<(usize, usize)>>();
+    count.sort_by_key(|&(_, c)| Reverse(c));
+
+    let ans = count[0].0 + 1;
     println!("{}", ans);
 }
