226fd23a0a0fac7a6f356c2f933a059117c83d78
bouzuya <m@bouzuya.net>
2022-11-30 18:58:31 +0900

    tessoku-book a63-b63

--- tessoku-book/src/bin/a63.rs
+++ tessoku-book/src/bin/a63.rs
@@ -1,10 +1,36 @@
+use std::collections::VecDeque;
+
 use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        ab: [(Usize1, Usize1); m]
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut edges = vec![vec![]; n];
+    for (a, b) in ab {
+        edges[a].push(b);
+        edges[b].push(a);
+    }
+
+    let inf = n + 1;
+    let mut dist = vec![inf; n];
+    let mut deque = VecDeque::new();
+    deque.push_back(0);
+    dist[0] = 0;
+    while let Some(u) = deque.pop_front() {
+        let d = dist[u];
+        for v in edges[u].iter().copied() {
+            if dist[v] == inf {
+                dist[v] = d + 1;
+                deque.push_back(v);
+            }
+        }
+    }
+
+    for d in dist {
+        println!("{}", if d == inf { -1 } else { d as i64 });
+    }
 }
--- tessoku-book/src/bin/b63.rs
+++ tessoku-book/src/bin/b63.rs
@@ -1,10 +1,43 @@
-use proconio::{input, marker::Usize1};
+use std::collections::VecDeque;
+
+use proconio::{
+    input,
+    marker::{Chars, Usize1},
+};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        h: usize,
+        w: usize,
+        s: (Usize1, Usize1),
+        g: (Usize1, Usize1),
+        c: [Chars; h],
     };
-    let ans = n - a.len();
+
+    let inf = h * w + 1;
+    let mut dist = vec![vec![inf; w]; h];
+    let mut deque = VecDeque::new();
+    deque.push_back(s);
+    dist[s.0][s.1] = 0;
+    while let Some((i, j)) = deque.pop_front() {
+        let dir = vec![(-1, 0), (0, -1), (0, 1), (1, 0)];
+        for (dr, dc) in dir {
+            let (nr, nc) = (i as i64 + dr, j as i64 + dc);
+            if !(0..h as i64).contains(&nr) || !(0..w as i64).contains(&nc) {
+                continue;
+            }
+            let (nr, nc) = (nr as usize, nc as usize);
+            if c[nr][nc] == '#' {
+                continue;
+            }
+            if dist[nr][nc] != inf {
+                continue;
+            }
+            deque.push_back((nr, nc));
+            dist[nr][nc] = dist[i][j] + 1;
+        }
+    }
+
+    let ans = dist[g.0][g.1];
     println!("{}", ans);
 }
