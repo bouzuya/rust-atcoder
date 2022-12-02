ff293abf8bab8d294fe26298ad2612213331eb0b
bouzuya <m@bouzuya.net>
2022-12-02 21:05:02 +0900

    tessoku-book a65-b65

--- tessoku-book/src/bin/a65.rs
+++ tessoku-book/src/bin/a65.rs
@@ -1,10 +1,30 @@
 use proconio::{input, marker::Usize1};
 
+fn dfs(dp: &mut Vec<usize>, edges: &[Vec<usize>], u: usize, p: usize) {
+    for v in edges[u].iter().copied() {
+        if v == p {
+            continue;
+        }
+        dfs(dp, edges, v, u);
+        dp[u] += dp[v] + 1;
+    }
+}
+
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [Usize1; n - 1],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut edges = vec![vec![]; n];
+    for (i, a_i) in a.iter().copied().enumerate() {
+        edges[i + 1].push(a_i);
+        edges[a_i].push(i + 1);
+    }
+
+    let mut dp = vec![0; n];
+    dfs(&mut dp, &edges, 0, 0);
+
+    for a in dp {
+        println!("{}", a);
+    }
 }
--- tessoku-book/src/bin/b65.rs
+++ tessoku-book/src/bin/b65.rs
@@ -1,10 +1,34 @@
 use proconio::{input, marker::Usize1};
 
+fn dfs(dp: &mut [usize], edges: &[Vec<usize>], u: usize, p: usize) {
+    let mut rank = 0_usize;
+    for v in edges[u].iter().copied() {
+        if v == p {
+            continue;
+        }
+        dfs(dp, edges, v, u);
+        rank = rank.max(dp[v] + 1);
+    }
+    dp[u] = rank;
+}
+
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        t: Usize1,
+        ab: [(Usize1, Usize1); n - 1],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut edges = vec![vec![]; n];
+    for (a, b) in ab {
+        edges[a].push(b);
+        edges[b].push(a);
+    }
+
+    let mut dp = vec![0; n];
+    dfs(&mut dp, &edges, t, t);
+
+    for a in dp {
+        println!("{}", a);
+    }
 }
