6c32c7f881d88c8090bbc71bf17cb6604adbb7d2
bouzuya <m@bouzuya.net>
2022-12-13 18:58:45 +0900

    tessoku-book a71, a72

--- tessoku-book/src/bin/a71.rs
+++ tessoku-book/src/bin/a71.rs
@@ -1,10 +1,19 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        mut a: [usize; n],
+        mut b: [usize; n],
     };
-    let ans = n - a.len();
+    a.sort();
+    b.sort();
+    b.reverse();
+
+    let ans = a
+        .into_iter()
+        .zip(b.into_iter())
+        .map(|(a_i, b_i)| a_i * b_i)
+        .sum::<usize>();
     println!("{}", ans);
 }
--- tessoku-book/src/bin/a72.rs
+++ tessoku-book/src/bin/a72.rs
@@ -1,10 +1,48 @@
-use proconio::{input, marker::Usize1};
+use std::collections::BinaryHeap;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        h: usize,
+        w: usize,
+        k: usize,
+        c: [Chars; h],
     };
-    let ans = n - a.len();
+
+    let mut ans = 0_usize;
+    for bits in 0_usize..1 << h {
+        let rows = bits.count_ones() as usize;
+        if rows > k {
+            continue;
+        }
+        let columns = k - rows;
+
+        let hs = (0..h)
+            .map(|i| ((bits >> i) & 1) == 1)
+            .collect::<Vec<bool>>();
+
+        let mut count_black = rows * w;
+        let mut pq = BinaryHeap::new();
+        for i in 0..w {
+            let mut b = 0_usize;
+            for j in 0..h {
+                if hs[j] {
+                    continue;
+                }
+                if c[j][i] == '#' {
+                    b += 1;
+                }
+            }
+            count_black += b;
+            pq.push(h - rows - b);
+        }
+
+        for _ in 0..columns {
+            count_black += pq.pop().unwrap();
+        }
+
+        ans = ans.max(count_black);
+    }
     println!("{}", ans);
 }
