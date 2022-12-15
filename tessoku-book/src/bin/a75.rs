57ae4b83b21661ca0a779e6b49ee684fd2c34529
bouzuya <m@bouzuya.net>
2022-12-15 20:18:10 +0900

    tessoku-book a75

--- tessoku-book/src/bin/a75.rs
+++ tessoku-book/src/bin/a75.rs
@@ -1,10 +1,32 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+
+macro_rules! chmax {
+    ($max_v: expr, $v: expr) => {
+        if $v > $max_v {
+            $max_v = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        mut td: [(usize, usize); n],
     };
-    let ans = n - a.len();
+    td.sort_by_key(|&(_, d)| d);
+
+    let mut dp = vec![vec![0; 1440 + 1]; n + 1];
+    for (i, (t, d)) in td.iter().copied().enumerate() {
+        for j in 0..=1440 {
+            if j + t <= d && j + t <= 1440 {
+                chmax!(dp[i + 1][j + t], dp[i][j] + 1);
+            }
+            chmax!(dp[i + 1][j], dp[i][j]);
+        }
+    }
+    let ans = dp[n].iter().max().unwrap();
     println!("{}", ans);
 }
