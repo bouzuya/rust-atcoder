bc43eee61296e0f7c52d755c5581792ee4c74a55
bouzuya <m@bouzuya.net>
2022-12-18 18:58:33 +0900

    tessoku-book a77

--- tessoku-book/src/bin/a77.rs
+++ tessoku-book/src/bin/a77.rs
@@ -1,10 +1,47 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        l: usize,
+        k: usize,
+        mut a: [usize; n],
     };
-    let ans = n - a.len();
+
+    a.push(l);
+
+    let mut b = vec![];
+    let mut prev = 0_usize;
+    for a_i in a.iter().copied() {
+        b.push(a_i - prev);
+        prev = a_i;
+    }
+
+    let mut ok = 1;
+    let mut ng = l;
+    while ng - ok > 1 {
+        let x = ok + (ng - ok) / 2;
+
+        let mut ys = vec![];
+        let mut cur = 0_usize;
+        for b_i in b.iter().copied() {
+            cur += b_i;
+            if cur >= x {
+                ys.push(cur);
+                cur = 0;
+            }
+        }
+        if cur > 0 {
+            let l = ys.len();
+            ys[l - 1] += cur;
+        }
+        if ys.len() > k {
+            ok = x;
+        } else {
+            ng = x;
+        }
+    }
+
+    let ans = ok;
     println!("{}", ans);
 }
