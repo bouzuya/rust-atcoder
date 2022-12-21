9de747e8f00e73551620e36ead065e3286500b68
bouzuya <m@bouzuya.net>
2022-12-21 22:01:55 +0900

    tessoku-book c09-c10

--- tessoku-book/src/bin/c09.rs
+++ tessoku-book/src/bin/c09.rs
@@ -1,10 +1,15 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
+    let mut dp = (0, 0);
+    for a_i in a {
+        let (p2, p1) = dp;
+        dp = (p2.max(p1), p2 + a_i);
+    }
+    let ans = dp.0.max(dp.1);
     println!("{}", ans);
 }
--- tessoku-book/src/bin/c10.rs
+++ tessoku-book/src/bin/c10.rs
@@ -1,10 +1,25 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        w: usize,
     };
-    let ans = n - a.len();
+
+    // 12 * 7^(w-1)
+    let p = 1_000_000_007_usize;
+    let mut b = 7_usize;
+    let mut x = w - 1;
+    let mut y = 12;
+    for _ in 0..60 {
+        if (x & 1) == 1 {
+            y *= b;
+            y %= p;
+        }
+        b *= b;
+        b %= p;
+        x >>= 1;
+    }
+
+    let ans = y;
     println!("{}", ans);
 }
