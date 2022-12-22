1f4bb45547206e4c1ba11561f669bdb298bbbc0a
bouzuya <m@bouzuya.net>
2022-12-22 22:13:09 +0900

    tessoku-book c11

--- tessoku-book/src/bin/c11.rs
+++ tessoku-book/src/bin/c11.rs
@@ -1,10 +1,32 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        k: usize,
+        a: [usize; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut ok = 1_f64;
+    let mut ng = 1_000_000_001_f64;
+    for _ in 0..100 {
+        let x = ok + (ng - ok) / 2_f64;
+        let mut sum = 0_usize;
+        for a_i in a.iter().copied() {
+            sum += (a_i as f64 / x).floor() as usize;
+        }
+        if sum >= k {
+            ok = x;
+        } else {
+            ng = x;
+        }
+    }
+
+    let mut ans = vec![];
+    for a_i in a {
+        ans.push((a_i as f64 / ok).floor());
+    }
+
+    for a in ans {
+        println!("{}", a);
+    }
 }
