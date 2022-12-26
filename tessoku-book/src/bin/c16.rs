316c02dbd656c72d056b93bdab10aef3b1a0d892
bouzuya <m@bouzuya.net>
2022-12-26 22:44:26 +0900

    tessoku-book c16

--- tessoku-book/src/bin/c16.rs
+++ tessoku-book/src/bin/c16.rs
@@ -1,6 +1,9 @@
-use std::collections::{BTreeMap, BTreeSet};
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    iter,
+};
 
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 macro_rules! chmax {
     ($max_v: expr, $v: expr) => {
@@ -18,93 +21,76 @@ fn main() {
         n: usize,
         m: usize,
         k: usize,
-        asbt: [(Usize1, usize, Usize1, usize); m],
+        asbt: [(usize, usize, usize, usize); m],
     };
+    let asbt = asbt
+        .iter()
+        .copied()
+        .map(|(a, s, b, t)| (a, s + 1, b, t + k + 1))
+        .collect::<Vec<_>>();
 
     let max_t = 1_000_000_000 + k;
-    let start = n;
+    let start = 0;
     let goal = n + 1;
-    let mut vertices = vec![];
-    for (a, s, b, t) in asbt.iter().copied() {
-        vertices.push((a, s));
-        vertices.push((b, t + k));
-    }
-    for i in 0..n {
-        vertices.push((i, 0));
-    }
-    vertices.push((start, 0));
-    vertices.push((goal, max_t + 1));
+    let vertices = asbt
+        .iter()
+        .copied()
+        .map(|(a, s, _, _)| (s, a))
+        .chain(asbt.iter().copied().map(|(_, _, b, t)| (t, b)))
+        .chain(iter::once((0, start)))
+        .chain(iter::once((max_t + 1, goal)))
+        .collect::<BTreeSet<_>>();
     let map = vertices
         .iter()
         .copied()
-        .collect::<BTreeSet<_>>()
-        .into_iter()
         .enumerate()
         .fold(BTreeMap::new(), |mut m, (i, k)| {
             m.insert(k, i);
             m
         });
 
-    let mut ts = vec![BTreeSet::new(); n];
-    let mut times = vec![vec![]; n];
+    let mut times = vec![BTreeSet::new(); n + 1];
     for (a, s, b, t) in asbt.iter().copied() {
-        ts[a].insert(s);
-        ts[b].insert(t + k);
-    }
-    for i in 0..n {
-        ts[i].insert(0);
-        times[i].extend(&ts[i]);
+        times[a].insert(s);
+        times[b].insert(t);
     }
 
     let mut edges = vec![vec![]; map.len()];
     for (a, s, b, t) in asbt {
-        let u = map[&(a, s)];
-        let v = map[&(b, t + k)];
+        let u = map[&(s, a)];
+        let v = map[&(t, b)];
         edges[v].push((u, 1));
     }
-    vertices.sort();
-    for i in 0..n {
-        let l = times[i].len();
-        for j in 0..l - 1 {
-            let u = map[(&(i, times[i][j]))];
-            let v = map[&(i, times[i][j + 1])];
+    for i in 1..=n {
+        let ts = times[i].iter().copied().collect::<Vec<usize>>();
+        if ts.is_empty() {
+            continue;
+        }
+
+        let u = map[&(0, start)];
+        let v = map[&(*ts.first().unwrap(), i)];
+        edges[v].push((u, 0));
+
+        for t in ts.windows(2) {
+            let u = map[&(t[0], i)];
+            let v = map[&(t[1], i)];
             edges[v].push((u, 0));
         }
-        let u = map[(&(i, times[i][l - 1]))];
-        let v = map[&(goal, max_t + 1)];
-        edges[v].push((u, 0));
-    }
-    for i in 0..n {
-        let u = map[&(start, 0)];
-        let v = map[&(i, 0)];
+
+        let u = map[&(*ts.last().unwrap(), i)];
+        let v = map[&(max_t + 1, goal)];
         edges[v].push((u, 0));
     }
 
-    let mut flat_times = vec![];
-    for i in 0..n {
-        flat_times.extend(
-            times[i]
-                .iter()
-                .copied()
-                .map(|t| (i, t))
-                .collect::<Vec<(usize, usize)>>(),
-        );
-    }
-
-    flat_times.sort_by_key(|&(_, t)| t);
-
+    let mut ans = 0_usize;
     let mut dp = vec![0_usize; map.len()];
-    for (i, t) in flat_times {
-        let u = map[&(i, t)];
-        for (v, w) in edges[u].iter().copied() {
-            chmax!(dp[u], dp[v] + w);
+    for (t, i) in vertices {
+        let v = map[&(t, i)];
+        for (u, w) in edges[v].iter().copied() {
+            chmax!(dp[v], dp[u] + w);
         }
-    }
-    let u = map[&(goal, max_t + 1)];
-    for (v, w) in edges[u].iter().copied() {
-        chmax!(dp[u], dp[v] + w);
+        ans = ans.max(dp[v]);
     }
 
-    let ans = dp[u];
     println!("{}", ans);
 }
