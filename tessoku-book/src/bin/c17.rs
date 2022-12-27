52fe0c330ec1d4cd7388c2cee31ece4b32c4f643
bouzuya <m@bouzuya.net>
2022-12-27 18:57:08 +0900

    tessoku-book c17

--- tessoku-book/src/bin/c17.rs
+++ tessoku-book/src/bin/c17.rs
@@ -1,10 +1,48 @@
-use proconio::{input, marker::Usize1};
+use std::collections::VecDeque;
+
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        q: usize,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut left = VecDeque::new();
+    let mut right = VecDeque::new();
+    for _ in 0..q {
+        input! {
+            t: char,
+        }
+
+        match t {
+            'A' => {
+                input! {
+                    x: String,
+                }
+                right.push_back(x);
+                if left.len() < right.len() {
+                    left.push_back(right.pop_front().unwrap());
+                }
+            }
+            'B' => {
+                input! {
+                    x: String,
+                }
+                left.push_back(x);
+                if left.len() > right.len() + 1 {
+                    right.push_front(left.pop_back().unwrap());
+                }
+            }
+            'C' => {
+                left.pop_front();
+                if left.len() < right.len() {
+                    left.push_back(right.pop_front().unwrap());
+                }
+            }
+            'D' => {
+                println!("{}", left.front().unwrap());
+            }
+            _ => unreachable!(),
+        }
+    }
 }
