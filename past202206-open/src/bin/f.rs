47e368a8308cd561c97366f334bb3566ef05a9f2
bouzuya <m@bouzuya.net>
2023-01-25 21:42:56 +0900

    past202206-open f

--- past202206-open/src/bin/f.rs
+++ past202206-open/src/bin/f.rs
@@ -2,9 +2,25 @@ use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
+        h: usize,
+        w: usize,
+        mut s: [[usize; w]; h],
         n: usize,
-        a: [Usize1; n],
+        rc: [(Usize1, Usize1); n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    for (r, c) in rc {
+        if s[r][c] == 0 {
+            continue;
+        }
+        for i in (0..r).rev() {
+            s[i + 1][c] = s[i][c];
+        }
+        s[0][c] = 0;
+    }
+
+    for i in 0..h {
+        for j in 0..w {
+            print!("{}{}", s[i][j], if j == w - 1 { '\n' } else { ' ' });
+        }
+    }
 }
