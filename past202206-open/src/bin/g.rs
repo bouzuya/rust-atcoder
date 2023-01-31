db69925aabc53c2cb564c0f8b78ce0823135dc68
bouzuya <m@bouzuya.net>
2023-01-31 22:10:39 +0900

    past202206-open g

--- past202206-open/src/bin/g.rs
+++ past202206-open/src/bin/g.rs
@@ -1,10 +1,33 @@
+use std::collections::VecDeque;
+
 use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        ab: [(Usize1, Usize1); n - 1],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut edges = vec![vec![]; n];
+    for (a, b) in ab.iter().copied() {
+        edges[a].push(b);
+        edges[b].push(a);
+    }
+
+    let mut visited = vec![false; n];
+    let mut deque = VecDeque::new();
+    visited[0] = true;
+    deque.push_back(0);
+    while let Some(u) = deque.pop_front() {
+        for v in edges[u].iter().copied() {
+            if visited[v] {
+                continue;
+            }
+            visited[v] = true;
+            deque.push_back(v);
+        }
+    }
+
+    let ans = visited.into_iter().all(|b| b);
+    println!("{}", if ans { "Yes" } else { "No" });
 }
