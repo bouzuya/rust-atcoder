96f334d65cacf51541f5e0e82913d2f26e138c4e
bouzuya <m@bouzuya.net>
2023-02-09 22:04:05 +0900

    agc038 a

--- agc038/src/bin/a.rs
+++ agc038/src/bin/a.rs
@@ -7,15 +7,13 @@ fn main() {
         a: usize,
         b: usize,
     };
-    for r in 0..h {
-        for c in 0..w {
+    for i in 0..h {
+        for j in 0..w {
             print!(
                 "{}",
-                match (r < b, c < a) {
-                    (true, true) => 0,
-                    (true, false) => 1,
-                    (false, true) => 1,
-                    (false, false) => 0,
+                match (i < b, j < a) {
+                    (true, true) | (false, false) => 0,
+                    (true, false) | (false, true) => 1,
                 }
             );
         }
