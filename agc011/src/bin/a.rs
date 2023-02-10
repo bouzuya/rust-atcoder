e55048e70a5257bf0f2687bd97f828a9341cafce
bouzuya <m@bouzuya.net>
2023-02-10 23:31:34 +0900

    agc011 a, b

--- agc011/src/bin/a.rs
+++ agc011/src/bin/a.rs
@@ -3,24 +3,24 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        c: usize,
+        capital_c: usize,
         k: usize,
         mut t: [usize; n],
     };
     t.sort();
 
-    let mut ans = 0;
-    let mut i_l = 0;
-    let mut i_r = 0;
-    while i_l < n {
-        while i_r + 1 < n && t[i_r + 1] <= t[i_l] + k {
-            i_r += 1;
+    let mut index = 0_usize;
+    let mut count = 0_usize;
+    while index < n {
+        let mut c = 0_usize;
+        let mut j = index;
+        while (j < n) && (t[j] <= t[index] + k) && (c < capital_c) {
+            c += 1;
+            j += 1;
         }
-        ans += 1;
-        i_l += c;
-        if i_l > i_r {
-            i_l = i_r + 1;
-        }
+        count += 1;
+        index = j;
     }
+    let ans = count;
     println!("{}", ans);
 }
--- agc011/src/bin/b.rs
+++ agc011/src/bin/b.rs
@@ -3,20 +3,20 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        mut a: [i64; n],
+        mut a: [usize; n],
     };
     a.sort();
-    let s = std::iter::once(0)
-        .chain(a.iter().scan(0, |acc, a_i| {
-            *acc += a_i;
+    let c = std::iter::once(0)
+        .chain(a.iter().scan(0, |acc, &i| {
+            *acc += i;
             Some(*acc)
         }))
-        .collect::<Vec<_>>();
-    let c = a
-        .iter()
-        .zip(s.iter())
-        .rposition(|(&a_i, &s_i)| s_i * 2 < a_i)
+        .collect::<Vec<usize>>();
+    let count = a
+        .into_iter()
+        .zip(c.into_iter())
+        .rposition(|(a_i, c_i)| a_i > c_i * 2)
         .unwrap_or(0);
-    let ans = n - c;
+    let ans = n - count;
     println!("{}", ans);
 }
