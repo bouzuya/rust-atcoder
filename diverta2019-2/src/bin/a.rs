676c26a5c434cf6896c65cf999a50a61d462b5e2
bouzuya <m@bouzuya.net>
2023-02-16 22:17:51 +0900

    diverta2019-2 a

--- diverta2019-2/src/bin/a.rs
+++ diverta2019-2/src/bin/a.rs
@@ -2,8 +2,8 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: i64,
-        k: i64,
+        n: usize,
+        k: usize,
     };
     let ans = if k == 1 { 0 } else { n - k };
     println!("{}", ans);
