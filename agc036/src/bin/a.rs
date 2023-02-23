7b5792cd1fb0f871f5a8f7db52f18802f5d66058
bouzuya <m@bouzuya.net>
2023-02-23 23:00:36 +0900

    agc036 a

--- agc036/src/bin/a.rs
+++ agc036/src/bin/a.rs
@@ -2,10 +2,10 @@ use proconio::input;
 
 fn main() {
     input! {
-        s: i64
+        s: usize,
     };
     let v = 1_000_000_000;
     let x = (v - (s % v)) % v;
     let y = (s + x) / v;
-    println!("{} {} {} {} {} {}", 0, 0, v, 1, x, y);
+    println!("0 0 {} 1 {} {}", v, x, y);
 }
