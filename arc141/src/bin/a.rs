509283535fcc079a9d4da4e192285165d44468be
bouzuya <m@bouzuya.net>
2023-03-03 23:40:28 +0900

    arc141 a

--- arc141/src/bin/a.rs
+++ arc141/src/bin/a.rs
@@ -1,10 +1,28 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        t: usize,
+        case: [usize; t],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    for capital_n in case {
+        let mut max = 1_usize;
+        for len in 2..=18 {
+            let mut ng = capital_n;
+            let mut ok = 1;
+            while ng - ok > 1 {
+                let m = ok + (ng - ok) / 2;
+                match m.to_string().repeat(len).parse::<usize>() {
+                    Ok(x) if x <= capital_n => ok = m,
+                    _ => ng = m,
+                }
+            }
+            match ok.to_string().repeat(len).parse::<usize>() {
+                Ok(n) if n <= capital_n => max = max.max(n),
+                _ => {}
+            }
+        }
+        println!("{}", max);
+    }
 }
