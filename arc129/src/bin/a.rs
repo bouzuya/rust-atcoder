0df244d2547cd8c0c62f5923ac9ab48c41fb05b6
bouzuya <m@bouzuya.net>
2023-03-05 23:09:55 +0900

    arc129 a

--- arc129/src/bin/a.rs
+++ arc129/src/bin/a.rs
@@ -1,10 +1,22 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        l: usize,
+        r: usize,
     };
-    let ans = n - a.len();
+
+    let mut ans = 0_usize;
+    for i in 0..60 {
+        if (n & (1 << i)) == 0 {
+            continue;
+        }
+
+        let u = (2_usize.pow((i + 1) as u32) - 1).min(r);
+        let l = 2_usize.pow(i as u32).max(l);
+        ans += u - l + 1;
+    }
+
     println!("{}", ans);
 }
