eef137939ffadcf908df9b36139357da0df775c2
bouzuya <m@bouzuya.net>
2023-03-07 22:43:15 +0900

    arc129 a

--- arc129/src/bin/a.rs
+++ arc129/src/bin/a.rs
@@ -8,13 +8,16 @@ fn main() {
     };
 
     let mut ans = 0_usize;
-    for i in 0..60 {
+    for i in 0..64 {
         if (n & (1 << i)) == 0 {
             continue;
         }
 
         let u = (2_usize.pow((i + 1) as u32) - 1).min(r);
         let l = 2_usize.pow(i as u32).max(l);
+        if u < l {
+            continue;
+        }
         ans += u - l + 1;
     }
 
