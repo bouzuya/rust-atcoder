1a7ee4f9db3269a88661601ff6fff4f99e314d83
bouzuya <m@bouzuya.net>
2023-04-06 21:55:38 +0900

    pastbook1 abc088 c, past #2 c

--- cargo-compete/pastbook1/src/bin/88.rs
+++ cargo-compete/pastbook1/src/bin/88.rs
@@ -1,1 +1,32 @@
-fn main() { todo!(); }
+use proconio::{input, marker::Chars};
+
+fn main() {
+    input! {
+        n: usize,
+        mut s: [Chars; n],
+    }
+
+    for i in (1..n).rev() {
+        for j in 0..2 * n - 1 {
+            if s[i][j] == 'X' {
+                for dj in -1..=1 {
+                    let pi = i - 1;
+                    let pj = j as i64 + dj;
+                    if (0..2 * n as i64 - 1).contains(&pj) {
+                        let pj = pj as usize;
+                        if s[pi][pj] == '#' {
+                            s[pi][pj] = 'X';
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for i in 0..n {
+        for j in 0..2 * n - 1 {
+            print!("{}", s[i][j]);
+        }
+        println!();
+    }
+}
