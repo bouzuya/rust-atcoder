8d2d972fb03f0ffed5fdd11e422ade966a8d13e8
bouzuya <m@bouzuya.net>
2023-04-08 22:46:49 +0900

    pastbook1 past #3 e

--- cargo-compete/pastbook1/src/bin/112.rs
+++ cargo-compete/pastbook1/src/bin/112.rs
@@ -1,1 +1,40 @@
-fn main() { todo!(); }
+use proconio::{input, marker::Usize1};
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        q: usize,
+        uv: [(Usize1, Usize1); m],
+        mut c: [usize; n],
+    }
+
+    let mut edges = vec![vec![]; n];
+    for (u, v) in uv.iter().copied() {
+        edges[u].push(v);
+        edges[v].push(u);
+    }
+
+    for _ in 0..q {
+        input! {
+            t: usize,
+            x: Usize1,
+        }
+        match t {
+            1 => {
+                println!("{}", c[x]);
+                for v in edges[x].iter().copied() {
+                    c[v] = c[x];
+                }
+            }
+            2 => {
+                input! {
+                    y: usize
+                }
+                println!("{}", c[x]);
+                c[x] = y;
+            }
+            _ => unreachable!(),
+        }
+    }
+}
