40becbdc428b78e662c3e5ac9a68675e7cfd9132
bouzuya <m@bouzuya.net>
2023-04-10 22:28:15 +0900

    pastbook1 past #1 e

--- cargo-compete/pastbook1/src/bin/123.rs
+++ cargo-compete/pastbook1/src/bin/123.rs
@@ -1,1 +1,61 @@
-fn main() { todo!(); }
+use std::collections::HashSet;
+
+use proconio::{input, marker::Usize1};
+
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+    }
+
+    let mut follows = vec![HashSet::new(); n];
+    let mut followers = vec![HashSet::new(); n];
+    for _ in 0..q {
+        input! {
+            t: usize,
+            a: Usize1,
+        }
+        match t {
+            1 => {
+                input! {
+                    b: Usize1,
+                }
+                follows[a].insert(b);
+                followers[b].insert(a);
+            }
+            2 => {
+                for b in followers[a].clone() {
+                    follows[a].insert(b);
+                    followers[b].insert(a);
+                }
+            }
+            3 => {
+                let mut bs = HashSet::new();
+                for x in follows[a].clone() {
+                    for b in follows[x].clone() {
+                        bs.insert(b);
+                    }
+                }
+                for b in bs {
+                    follows[a].insert(b);
+                    followers[b].insert(a);
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    for i in 0..n {
+        for j in 0..n {
+            print!(
+                "{}",
+                if i != j && follows[i].contains(&j) {
+                    "Y"
+                } else {
+                    "N"
+                }
+            );
+        }
+        println!();
+    }
+}
