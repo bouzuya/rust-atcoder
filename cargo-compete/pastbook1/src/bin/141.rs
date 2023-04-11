a3fb74a7c7775d36c78b30524b34d94300e1a355
bouzuya <m@bouzuya.net>
2023-04-11 20:57:22 +0900

    pastbook1 past #1 h

--- cargo-compete/pastbook1/src/bin/141.rs
+++ cargo-compete/pastbook1/src/bin/141.rs
@@ -1,1 +1,68 @@
-fn main() { todo!(); }
+use proconio::{input, marker::Usize1};
+
+fn main() {
+    input! {
+        n: usize,
+        mut c: [usize; n],
+        q: usize,
+    }
+
+    let mut count = 0_usize;
+    let mut all_count = 0_usize;
+    let mut all_min = c.iter().copied().min().unwrap();
+    let mut odd_count = 0_usize;
+    let mut odd_min = c
+        .iter()
+        .copied()
+        .enumerate()
+        .filter(|(i, _)| i % 2 == 0)
+        .map(|(_, c_i)| c_i)
+        .min()
+        .unwrap();
+    for _ in 0..q {
+        input! {
+            t: usize,
+        }
+        match t {
+            1 => {
+                input! {
+                    x: Usize1,
+                    a: usize,
+                }
+                let a_sum = a + all_count + if x % 2 == 0 { odd_count } else { 0 };
+                if c[x] >= a_sum {
+                    c[x] -= a;
+                    all_min = all_min.min(c[x]);
+                    if x % 2 == 0 {
+                        odd_min = odd_min.min(c[x]);
+                    }
+                    count += a;
+                }
+            }
+            2 => {
+                input! {
+                    a: usize,
+                }
+                if a <= odd_min {
+                    odd_min -= a;
+                    all_min = all_min.min(odd_min);
+                    odd_count += a;
+                    count += a * ((n + 1) / 2);
+                }
+            }
+            3 => {
+                input! {
+                    a: usize,
+                }
+                if a <= all_min {
+                    all_min -= a;
+                    odd_min -= a;
+                    all_count += a;
+                    count += a * n;
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+    println!("{}", count);
+}
