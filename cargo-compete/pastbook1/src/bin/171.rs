ee123fb5459882627a7e1bec442375789e6247a2
bouzuya <m@bouzuya.net>
2023-04-14 22:43:30 +0900

    pastbook1 abc114 c

--- cargo-compete/pastbook1/src/bin/171.rs
+++ cargo-compete/pastbook1/src/bin/171.rs
@@ -1,1 +1,32 @@
-fn main() { todo!(); }
+use proconio::input;
+
+fn f(n: usize, count: &mut usize, s: &mut Vec<char>) {
+    if s.len() >= 3 {
+        let x = s.iter().collect::<String>().parse::<usize>().unwrap();
+        if x > n {
+            return;
+        } else if s.iter().any(|c| c == &'3')
+            && s.iter().any(|c| c == &'5')
+            && s.iter().any(|c| c == &'7')
+        {
+            *count += 1;
+        }
+    }
+
+    for c in &['3', '5', '7'] {
+        s.push(*c);
+        f(n, count, s);
+        s.pop();
+    }
+}
+
+fn main() {
+    input! {
+        n: usize
+    }
+
+    let mut count = 0_usize;
+    let mut s = vec![];
+    f(n, &mut count, &mut s);
+    println!("{}", count);
+}
