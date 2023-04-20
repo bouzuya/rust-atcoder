a540bf0d594ed6b28d2335f0c043038a690f2399
bouzuya <m@bouzuya.net>
2023-04-20 20:47:11 +0900

    pastbook1 tdpc a

--- cargo-compete/pastbook1/src/bin/191.rs
+++ cargo-compete/pastbook1/src/bin/191.rs
@@ -1,1 +1,24 @@
-fn main() { todo!(); }
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        p: [usize; n],
+    }
+
+    let mut dp = vec![false; 100 * 100 + 1];
+    dp[0] = true;
+    for p_i in p {
+        let mut next = vec![false; 100 * 100 + 1];
+        for j in 0..dp.len() {
+            if !dp[j] {
+                continue;
+            }
+            next[j + p_i] = true;
+            next[j] = true;
+        }
+        dp = next;
+    }
+    let ans = dp.into_iter().filter(|b| *b).count();
+    println!("{}", ans);
+}
