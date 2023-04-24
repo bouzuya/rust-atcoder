7e971708f1b8a037a8fdff0be9b8652303d538fb
bouzuya <m@bouzuya.net>
2023-04-24 21:13:42 +0900

    pastbook1 abc026 c

--- cargo-compete/pastbook1/src/bin/198.rs
+++ cargo-compete/pastbook1/src/bin/198.rs
@@ -1,1 +1,28 @@
-fn main() { todo!(); }
+use proconio::{input, marker::Usize1};
+
+fn dfs(dp: &mut Vec<usize>, edges: &Vec<Vec<usize>>, u: usize) {
+    let mut cs = vec![];
+    for v in edges[u].iter().copied() {
+        dfs(dp, edges, v);
+        cs.push(dp[v]);
+    }
+    dp[u] = *cs.iter().max().unwrap_or(&0) + *cs.iter().min().unwrap_or(&0) + 1;
+}
+
+fn main() {
+    input! {
+        n: usize,
+        b: [Usize1; n - 1]
+    }
+
+    let mut edges = vec![vec![]; n];
+    for (i, b_i) in b.iter().copied().enumerate() {
+        edges[b_i].push(i + 1);
+    }
+
+    let mut dp = vec![0_usize; n];
+    dfs(&mut dp, &edges, 0);
+
+    let ans = dp[0];
+    println!("{}", ans);
+}
