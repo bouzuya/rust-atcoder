be051745ee9a104c53b20d1953f8d5315f6744d0
bouzuya <m@bouzuya.net>
2023-04-27 21:07:01 +0900

    pastbook1 past #1 i

--- cargo-compete/pastbook1/src/bin/215.rs
+++ cargo-compete/pastbook1/src/bin/215.rs
@@ -1,1 +1,42 @@
-fn main() { todo!(); }
+use proconio::{input, marker::Chars};
+
+macro_rules! chmin {
+    ($min_v: expr, $v: expr) => {
+        if $v < $min_v {
+            $min_v = $v;
+            true
+        } else {
+            false
+        }
+    };
+}
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        sc: [(Chars, usize); m],
+    }
+    let mut tc = vec![];
+    for (s, c) in sc {
+        let mut t = 0_usize;
+        for i in 0..n {
+            if s[i] == 'Y' {
+                t |= 1 << (n - i - 1);
+            }
+        }
+        tc.push((t, c));
+    }
+
+    let inf = 1_usize << 60;
+    let mut dp = vec![vec![inf; 1 << n]; m + 1];
+    dp[0][0] = 0_usize;
+    for (i, (t, c)) in tc.iter().copied().enumerate() {
+        for bits in 0..1 << n {
+            chmin!(dp[i + 1][bits], dp[i][bits]);
+            chmin!(dp[i + 1][bits | t], dp[i][bits] + c);
+        }
+    }
+    let ans = dp[m][(1 << n) - 1];
+    println!("{}", if ans == inf { -1 } else { ans as i64 });
+}
