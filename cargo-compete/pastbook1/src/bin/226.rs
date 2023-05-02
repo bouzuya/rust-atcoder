1a7406a4036fcd04a6e8f33b67f2e9d259bc9136
bouzuya <m@bouzuya.net>
2023-05-02 21:19:58 +0900

    pastbook1 past #3 c

--- cargo-compete/pastbook1/src/bin/226.rs
+++ cargo-compete/pastbook1/src/bin/226.rs
@@ -1,1 +1,17 @@
-fn main() { todo!(); }
+use proconio::input;
+
+fn main() {
+    input! {
+        a: usize,
+        r: usize,
+        n: usize,
+    }
+
+    let ans = r
+        .checked_pow(n as u32 - 1)
+        .and_then(|p| a.checked_mul(p))
+        .and_then(|x| if x > 1_000_000_000 { None } else { Some(x) })
+        .map(|x| x.to_string())
+        .unwrap_or("large".to_string());
+    println!("{}", ans);
+}
