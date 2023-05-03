0e48b5669873b1f59840c983e7649e23bf70a18f
bouzuya <m@bouzuya.net>
2023-05-03 20:14:50 +0900

    pastbook1 abc179 c

--- cargo-compete/pastbook1/src/bin/232.rs
+++ cargo-compete/pastbook1/src/bin/232.rs
@@ -1,1 +1,13 @@
-fn main() { todo!(); }
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+    }
+    let mut count = 0_usize;
+    for a in 1..n {
+        count += (n - 1) / a;
+    }
+    let ans = count;
+    println!("{}", ans);
+}
