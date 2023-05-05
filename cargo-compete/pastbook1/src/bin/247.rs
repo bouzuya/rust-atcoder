adcc19915d4cd396a9ee10e2a502cb726c75ac7f
bouzuya <m@bouzuya.net>
2023-05-05 22:57:29 +0900

    pastbook1 arc098 c

--- cargo-compete/pastbook1/src/bin/247.rs
+++ cargo-compete/pastbook1/src/bin/247.rs
@@ -1,1 +1,25 @@
-fn main() { todo!(); }
+use proconio::{input, marker::Chars};
+
+fn main() {
+    input! {
+        n: usize,
+        s: Chars,
+    }
+
+    let mut count_w = vec![0; n];
+    for i in 0..n {
+        count_w[i] = if i > 0 { count_w[i - 1] } else { 0 } + if s[i] == 'W' { 1 } else { 0 };
+    }
+    let mut count_e = vec![0; n];
+    for i in 0..n {
+        count_e[i] =
+            if i > 0 { count_e[i - 1] } else { 0 } + if s[n - 1 - i] == 'E' { 1 } else { 0 };
+    }
+    count_e.reverse();
+
+    let mut min = n;
+    for i in 0..n {
+        min = min.min(count_e[i] + count_w[i] - 1);
+    }
+    println!("{}", min);
+}
