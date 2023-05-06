39fcfc3013d0ef9b6ce5767393f6224362b304f0
bouzuya <m@bouzuya.net>
2023-05-06 22:37:08 +0900

    pastbook1 past #2 f

--- cargo-compete/pastbook1/src/bin/255.rs
+++ cargo-compete/pastbook1/src/bin/255.rs
@@ -1,1 +1,30 @@
-fn main() { todo!(); }
+use std::collections::BinaryHeap;
+
+use proconio::{input, marker::Usize1};
+
+fn main() {
+    input! {
+        n: usize,
+        mut ab: [(Usize1, usize); n],
+    }
+
+    ab.sort();
+
+    let mut sum = 0_usize;
+    let mut index = 0_usize;
+    let mut pq = BinaryHeap::new();
+    for k in 0..n {
+        while index < n {
+            let (a, b) = ab[index];
+            if a <= k {
+                pq.push(b);
+                index += 1;
+            } else {
+                break;
+            }
+        }
+
+        sum += pq.pop().unwrap();
+        println!("{}", sum);
+    }
+}
