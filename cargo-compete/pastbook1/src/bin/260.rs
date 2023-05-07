0bc81fc0cf0649cb93eea6140cee78db1b1555bf
bouzuya <m@bouzuya.net>
2023-05-07 22:16:19 +0900

    pastbook1 typical-algorithm b

--- cargo-compete/pastbook1/src/bin/260.rs
+++ cargo-compete/pastbook1/src/bin/260.rs
@@ -1,1 +1,22 @@
-fn main() { todo!(); }
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        mut ab: [(usize, usize); n],
+    }
+
+    ab.sort_by_key(|&(_, b)| b);
+
+    let mut count = 0_usize;
+    let mut cur = 0_usize;
+    for (a, b) in ab {
+        if cur < a {
+            cur = b;
+            count += 1;
+        }
+    }
+
+    let ans = count;
+    println!("{}", ans);
+}
