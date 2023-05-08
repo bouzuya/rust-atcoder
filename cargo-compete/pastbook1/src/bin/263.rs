f152c423b2f71521c0602c371b6d15db492a5931
bouzuya <m@bouzuya.net>
2023-05-08 21:20:28 +0900

    pastbook1 nikkei2019-qual c

--- cargo-compete/pastbook1/src/bin/263.rs
+++ cargo-compete/pastbook1/src/bin/263.rs
@@ -1,1 +1,22 @@
-fn main() { todo!(); }
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        mut ab: [(i64, i64); n],
+    }
+
+    ab.sort_by_key(|(a, b)| a + b);
+    ab.reverse();
+    let mut f = 0_i64;
+    let mut s = 0_i64;
+    for (i, (a, b)) in ab.iter().copied().enumerate() {
+        if i % 2 == 0 {
+            f += a;
+        } else {
+            s += b;
+        }
+    }
+    let ans = f - s;
+    println!("{}", ans);
+}
