c66bc72845bf2255c802d7ec0e337ac92afe22b1
bouzuya <m@bouzuya.net>
2023-05-10 20:56:27 +0900

    pastbook1 abc146 c

--- cargo-compete/pastbook1/src/bin/275.rs
+++ cargo-compete/pastbook1/src/bin/275.rs
@@ -1,1 +1,22 @@
-fn main() { todo!(); }
+use proconio::input;
+
+fn main() {
+    input! {
+        a: usize,
+        b: usize,
+        x: usize,
+    }
+
+    let mut ok = 0_usize;
+    let mut ng = x;
+    while ng - ok > 1 {
+        let n = ok + (ng - ok) / 2;
+        let d_n = n.to_string().len();
+        if a * n + b * d_n <= x && n <= 1_000_000_000 {
+            ok = n;
+        } else {
+            ng = n;
+        }
+    }
+    println!("{}", ok);
+}
