fe35f2ef8969c991079529d772001fcf86fd4f68
bouzuya <m@bouzuya.net>
2023-05-11 22:23:42 +0900

    pastbook1 arc050 b

--- cargo-compete/pastbook1/src/bin/278.rs
+++ cargo-compete/pastbook1/src/bin/278.rs
@@ -1,1 +1,23 @@
-fn main() { todo!(); }
+use proconio::input;
+
+fn main() {
+    input! {
+        r: usize,
+        b: usize,
+        x: usize,
+        y: usize,
+    }
+
+    let mut ok = 0_usize;
+    let mut ng = r + b;
+    while ng - ok > 1 {
+        let mid = ok + (ng - ok) / 2;
+        if (mid <= r) && (mid <= b) && (((r - mid) / (x - 1)) + ((b - mid) / (y - 1)) >= mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    let ans = ok;
+    println!("{}", ans);
+}
