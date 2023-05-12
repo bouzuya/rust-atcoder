4c9c37cf8b983bb7f0a029daea3b5143c961b8f5
bouzuya <m@bouzuya.net>
2023-05-12 21:44:54 +0900

    pastbook1 abc068 c

--- cargo-compete/pastbook1/src/bin/282.rs
+++ cargo-compete/pastbook1/src/bin/282.rs
@@ -1,1 +1,40 @@
-fn main() { todo!(); }
+use std::collections::VecDeque;
+
+use proconio::{input, marker::Usize1};
+
+fn adjacency_list(n: usize, uv: &[(usize, usize)]) -> Vec<Vec<usize>> {
+    let mut e = vec![vec![]; n];
+    for (u, v) in uv.iter().copied() {
+        e[u].push(v);
+        e[v].push(u);
+    }
+    e
+}
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(Usize1, Usize1); m],
+    }
+
+    let edges = adjacency_list(n, &ab);
+    let inf = n + 1;
+    let mut dist = vec![inf; n];
+    let mut deque = VecDeque::new();
+    deque.push_back(0);
+    dist[0] = 0;
+    while let Some(u) = deque.pop_front() {
+        for v in edges[u].iter().copied() {
+            if dist[v] == inf {
+                dist[v] = dist[u] + 1;
+                deque.push_back(v);
+            }
+        }
+    }
+    if dist[n - 1] == 2 {
+        println!("POSSIBLE");
+    } else {
+        println!("IMPOSSIBLE");
+    }
+}
