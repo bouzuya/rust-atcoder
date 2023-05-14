69bad76ae58282df073320991cd315e56da48f13
bouzuya <m@bouzuya.net>
2023-05-14 22:42:18 +0900

    pastbook1 typical-algorithm d

--- cargo-compete/pastbook1/src/bin/295.rs
+++ cargo-compete/pastbook1/src/bin/295.rs
@@ -1,1 +1,42 @@
-fn main() { todo!(); }
+use proconio::input;
+
+fn adjacency_list(n: usize, uvw: &[(usize, usize, usize)]) -> Vec<Vec<(usize, usize)>> {
+    let mut e = vec![vec![]; n];
+    for (u, v, w) in uvw.iter().copied() {
+        e[u].push((v, w));
+    }
+    e
+}
+
+fn dijkstra(n: usize, inf: usize, e: &[Vec<(usize, usize)>], s: usize) -> Vec<usize> {
+    use std::{cmp::Reverse, collections::BinaryHeap};
+    let mut d = vec![inf; n];
+    let mut pq = BinaryHeap::new();
+    d[s] = 0;
+    pq.push(Reverse((0, s)));
+    while let Some(Reverse((w_u, u))) = pq.pop() {
+        if w_u > d[u] {
+            continue;
+        }
+        for (v, w_v) in e[u].iter().copied() {
+            let w = w_u + w_v;
+            if w < d[v] {
+                d[v] = w;
+                pq.push(Reverse((w, v)));
+            }
+        }
+    }
+    d
+}
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        uvc: [(usize, usize, usize); m],
+    }
+
+    let edges = adjacency_list(n, &uvc);
+    let dist = dijkstra(n, 1_usize << 60, &edges, 0);
+    println!("{}", dist[n - 1]);
+}
