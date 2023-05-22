1d92207d6afe0858310eac91cf10f3b08be36cb2
bouzuya <m@bouzuya.net>
2023-05-22 22:14:12 +0900

    pastbook1 past #3 i

--- cargo-compete/pastbook1/src/bin/342.rs
+++ cargo-compete/pastbook1/src/bin/342.rs
@@ -1,1 +1,56 @@
-fn main() { todo!(); }
+use proconio::{input, marker::Usize1};
+
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+    }
+
+    let mut cols = (0..n).collect::<Vec<usize>>();
+    let mut rows = (0..n).collect::<Vec<usize>>();
+    let mut rotate = false;
+    for _ in 0..q {
+        input! {
+            t: usize,
+        }
+        match t {
+            1 => {
+                input! {
+                    a: Usize1,
+                    b: Usize1,
+                }
+                if rotate {
+                    cols.swap(a, b);
+                } else {
+                    rows.swap(a, b);
+                }
+            }
+            2 => {
+                input! {
+                    a: Usize1,
+                    b: Usize1,
+                }
+                if rotate {
+                    rows.swap(a, b);
+                } else {
+                    cols.swap(a, b);
+                }
+            }
+            3 => {
+                rotate = !rotate;
+            }
+            4 => {
+                input! {
+                    a: Usize1,
+                    b: Usize1,
+                }
+                if rotate {
+                    println!("{}", n * rows[b] + cols[a]);
+                } else {
+                    println!("{}", n * rows[a] + cols[b]);
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+}
