b945ebe9b26c98709d10a35723f86f567d5c6f57
bouzuya <m@bouzuya.net>
2023-05-24 22:25:50 +0900

    pastbook1 past #3 j

--- cargo-compete/pastbook1/src/bin/354.rs
+++ cargo-compete/pastbook1/src/bin/354.rs
@@ -1,1 +1,25 @@
-fn main() { todo!(); }
+use proconio::input;
+use superslice::Ext;
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        a: [usize; m],
+    }
+
+    let mut ans = vec![0_i64; m];
+    let mut kids = vec![0_usize; n];
+    for (i, a_i) in a.iter().copied().enumerate() {
+        let index = kids.upper_bound(&(a_i - 1));
+        if index > 0 {
+            kids[index - 1] = a_i;
+            ans[i] = (n - (index - 1)) as i64;
+        } else {
+            ans[i] = -1;
+        }
+    }
+    for a in ans {
+        println!("{}", a);
+    }
+}
