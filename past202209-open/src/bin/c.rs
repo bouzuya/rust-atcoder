a15755a4c6b26f58801779a24b3a141a2005ff5d
bouzuya <m@bouzuya.net>
2023-06-03 23:17:03 +0900

    past202209-open c

--- past202209-open/src/bin/c.rs
+++ past202209-open/src/bin/c.rs
@@ -1,10 +1,35 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+
+fn f(ans: &mut f64, selected: &mut Vec<usize>, p: &[Vec<usize>], k: usize, i_p: usize) {
+    if selected.len() == p.len() {
+        if k == selected.iter().copied().map(|s| s + 1).sum() {
+            *ans += selected
+                .iter()
+                .copied()
+                .enumerate()
+                .map(|(i, j)| p[i][j])
+                .product::<usize>() as f64
+                / 1_000_000_f64;
+        }
+        return;
+    }
+
+    for j in 0..6 {
+        selected.push(j);
+        f(ans, selected, p, k, i_p + 1);
+        selected.pop();
+    }
+}
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        p: [[usize; 6]; 3]
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    for k in 1..=18 {
+        let mut ans = 0_f64;
+        let mut selected = vec![];
+        f(&mut ans, &mut selected, &p, k, 0);
+        println!("{}", ans as f64);
+    }
 }
