a9efcd4fb9abc9cf5f20a51267e860a3ffe2f3b4
bouzuya <m@bouzuya.net>
2023-06-09 20:11:04 +0900

    past202209_d

--- past202209-open/src/bin/d.rs
+++ past202209-open/src/bin/d.rs
@@ -1,10 +1,28 @@
+use std::collections::HashSet;
+
 use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        uv: [(Usize1, Usize1); m],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut edges = HashSet::new();
+    for (u, v) in uv {
+        if !(0..n).contains(&u) || !(0..n).contains(&v) {
+            println!("No");
+            return;
+        }
+        if u == v {
+            println!("No");
+            return;
+        }
+        if !edges.insert((u.min(v), u.max(v))) {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
