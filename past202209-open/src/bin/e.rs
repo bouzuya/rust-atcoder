cccc3518da667f511fdd5d6f6cbb42c731cd177e
bouzuya <m@bouzuya.net>
2023-06-23 23:13:20 +0900

    past202209-open e

--- past202209-open/src/bin/e.rs
+++ past202209-open/src/bin/e.rs
@@ -1,10 +1,30 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
+        r: usize,
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        l: usize,
+        s: [usize; l],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut rounds = 1_usize;
+    let mut sum = 0_usize;
+    let mut times = 0_usize;
+    for s_i in s {
+        sum += s_i;
+        times += 1;
+        if rounds > r || sum > n || times > m {
+            println!("No");
+            return;
+        }
+        if sum == n || times == m {
+            rounds += 1;
+            sum = 0;
+            times = 0;
+        }
+    }
+    let ans = rounds == r + 1 && sum == 0 && times == 0;
+    println!("{}", if ans { "Yes" } else { "No" });
 }
