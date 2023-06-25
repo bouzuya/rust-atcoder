39e32dbb378a6a079312ebfbc9d9ae7a7af14db8
bouzuya <m@bouzuya.net>
2023-06-25 22:16:00 +0900

    abc306 f

--- abc306/src/bin/f.rs
+++ abc306/src/bin/f.rs
@@ -1,10 +1,93 @@
-use proconio::{input, marker::Usize1};
+use fenwicktree::*;
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        a: [[usize; m]; n],
     };
-    let ans = n - a.len();
+
+    let mut b = vec![];
+    for (i, a_i) in a.iter().enumerate() {
+        for a_ij in a_i {
+            b.push((a_ij, i));
+        }
+    }
+    b.sort();
+
+    let mut ans = 0_usize;
+    let mut bit = FenwickTree::new(n, 0_usize);
+    for (_, i) in b.iter().copied() {
+        bit.add(i, 1);
+        ans += bit.sum(i + 1, n);
+    }
+
+    ans += ((m + 1) * m / 2) * (n * (n - 1) / 2);
     println!("{}", ans);
+}
+
+//https://github.com/rust-lang-ja/ac-library-rs
+
+pub mod fenwicktree {
+    // Reference: https://en.wikipedia.org/wiki/Fenwick_tree
+    pub struct FenwickTree<T> {
+        n: usize,
+        ary: Vec<T>,
+        e: T,
+    }
+
+    impl<T: Clone + std::ops::AddAssign<T>> FenwickTree<T> {
+        pub fn new(n: usize, e: T) -> Self {
+            FenwickTree {
+                n,
+                ary: vec![e.clone(); n],
+                e,
+            }
+        }
+        pub fn accum(&self, mut idx: usize) -> T {
+            let mut sum = self.e.clone();
+            while idx > 0 {
+                sum += self.ary[idx - 1].clone();
+                idx &= idx - 1;
+            }
+            sum
+        }
+        /// performs data[idx] += val;
+        pub fn add<U: Clone>(&mut self, mut idx: usize, val: U)
+        where
+            T: std::ops::AddAssign<U>,
+        {
+            let n = self.n;
+            idx += 1;
+            while idx <= n {
+                self.ary[idx - 1] += val.clone();
+                idx += idx & idx.wrapping_neg();
+            }
+        }
+        /// Returns data[l] + ... + data[r - 1].
+        pub fn sum(&self, l: usize, r: usize) -> T
+        where
+            T: std::ops::Sub<Output = T>,
+        {
+            self.accum(r) - self.accum(l)
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn fenwick_tree_works() {
+            let mut bit = FenwickTree::new(5, 0i64);
+            // [1, 2, 3, 4, 5]
+            for i in 0..5 {
+                bit.add(i, i as i64 + 1);
+            }
+            assert_eq!(bit.sum(0, 5), 15);
+            assert_eq!(bit.sum(0, 4), 10);
+            assert_eq!(bit.sum(1, 3), 5);
+        }
+    }
 }
