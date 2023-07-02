e7467e0df8f0a7db4fcc6fc0eb55aeaa40339098
bouzuya <m@bouzuya.net>
2023-07-02 23:21:29 +0900

    abc296 e

--- abc296/src/bin/e.rs
+++ abc296/src/bin/e.rs
@@ -5,6 +5,29 @@ fn main() {
         n: usize,
         a: [Usize1; n],
     };
-    let ans = n - a.len();
+
+    let mut ord = vec![None; n];
+    let mut index = 1_usize;
+
+    let mut ans = 0_usize;
+    for i in 0..n {
+        if ord[i].is_some() {
+            continue;
+        }
+        let offset = index;
+        let mut v = i;
+        loop {
+            if let Some(l) = ord[v] {
+                if l >= offset {
+                    ans += index - l;
+                }
+                break;
+            }
+            ord[v] = Some(index);
+            v = a[v];
+            index += 1;
+        }
+    }
+
     println!("{}", ans);
 }
