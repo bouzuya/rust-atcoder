c5167b08fadadddecd8523c23d94add230bc60fd
bouzuya <m@bouzuya.net>
2023-07-20 21:05:14 +0900

    arc114 a, b

--- cargo-atcoder/contests/arc114/src/bin/a.rs
+++ cargo-atcoder/contests/arc114/src/bin/a.rs
@@ -1,6 +1,26 @@
-use std::cmp;
-
 use proconio::input;
+
+fn sieve_of_eratosthenes(n: usize) -> Vec<usize> {
+    let mut p = vec![];
+    let mut b = vec![true; n + 1];
+    for i in 2.. {
+        if i * i > n {
+            for j in i..=n {
+                if b[j] {
+                    p.push(j);
+                }
+            }
+            break;
+        }
+        if b[i] {
+            p.push(i);
+            for j in (i + i..=n).step_by(i) {
+                b[j] = false;
+            }
+        }
+    }
+    p
+}
 
 fn main() {
     input! {
@@ -8,29 +28,22 @@ fn main() {
         x: [usize; n],
     };
 
-    let mut ps = vec![];
-    for x in 2..=50 {
-        if ps.iter().all(|p_i| x % p_i != 0) {
-            ps.push(x);
-        }
-    }
+    let ps = sieve_of_eratosthenes(50);
     assert_eq!(ps.len(), 15);
 
-    let mut ans = ps.iter().product::<usize>();
+    let mut ans = ps.iter().copied().product::<usize>();
     for bits in 0..1 << ps.len() {
-        let mut qs = vec![];
-        for i in 0..ps.len() {
-            if (bits >> i) & 1 == 1 {
-                qs.push(ps[i]);
-            }
-        }
-
+        let qs = (0..ps.len())
+            .filter(|i| ((bits >> i) & 1) == 1)
+            .map(|i| ps[i])
+            .collect::<Vec<usize>>();
         if x.iter()
             .copied()
             .all(|x_i| qs.iter().copied().any(|q_i| x_i % q_i == 0))
         {
-            ans = cmp::min(ans, qs.iter().product::<usize>());
+            ans = ans.min(qs.iter().copied().product::<usize>());
         }
     }
+
     println!("{}", ans);
 }
