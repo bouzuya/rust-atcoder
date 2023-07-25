89503d9f26fc7d7fc1791e64ae79b238d8950ee3
bouzuya <m@bouzuya.net>
2023-07-26 06:44:52 +0900

    abc191 e

--- cargo-atcoder/contests/abc191/src/bin/e.rs
+++ cargo-atcoder/contests/abc191/src/bin/e.rs
@@ -1,10 +1,49 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        abc: [(Usize1, Usize1, usize); m]
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let edges = {
+        let mut edges = vec![vec![]; n];
+        for (u, v, w) in abc.iter().copied() {
+            edges[u].push((v, w));
+        }
+        edges
+    };
+    let inf = 1_usize << 60;
+
+    for s in 0..n {
+        let mut min_dist: Option<usize> = None;
+        let mut dist = vec![inf; n];
+        let mut pq = BinaryHeap::new();
+        dist[s] = 0;
+        pq.push(Reverse((0, s)));
+        while let Some(Reverse((w_u, u))) = pq.pop() {
+            if w_u > dist[u] {
+                continue;
+            }
+            for (v, w_v) in edges[u].iter().copied() {
+                if v == s {
+                    min_dist = Some(match min_dist {
+                        Some(m) => m.min(w_u + w_v),
+                        None => w_u + w_v,
+                    });
+                    continue;
+                }
+                let w = w_u + w_v;
+                if w < dist[v] {
+                    dist[v] = w;
+                    pq.push(Reverse((w, v)));
+                }
+            }
+        }
+
+        println!("{}", min_dist.map(|ans| ans as i64).unwrap_or(-1));
+    }
 }
