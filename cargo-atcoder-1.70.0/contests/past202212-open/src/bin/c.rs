871ecce47fdc94b85584e88b616deb09b9d667b5
bouzuya <m@bouzuya.net>
2023-09-27 23:18:43 +0900

    past202212-open c

--- cargo-atcoder-1.70.0/contests/past202212-open/src/bin/c.rs
+++ cargo-atcoder-1.70.0/contests/past202212-open/src/bin/c.rs
@@ -1,10 +1,20 @@
-use proconio::{input, marker::Usize1};
+use std::collections::HashSet;
+
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
+    let mut set = HashSet::new();
+    for i in 0..n {
+        for j in i + 1..n {
+            for k in j + 1..n {
+                set.insert(a[i] * a[j] * a[k]);
+            }
+        }
+    }
+    let ans = set.len();
     println!("{}", ans);
 }
