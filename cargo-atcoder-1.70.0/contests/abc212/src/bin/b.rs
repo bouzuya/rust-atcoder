73c082c0733cbbeb94a3916a02570f2fe91ca909
bouzuya <m@bouzuya.net>
2023-10-15 23:17:07 +0900

    abc212 b

--- cargo-atcoder-1.70.0/contests/abc212/src/bin/b.rs
+++ cargo-atcoder-1.70.0/contests/abc212/src/bin/b.rs
@@ -1,10 +1,20 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        x: Chars,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    if x.iter().copied().all(|c| c == x[0]) {
+        println!("Weak");
+        return;
+    }
+
+    for (p, x_i) in x.iter().copied().zip(x.iter().copied().skip(1)) {
+        if ((p as u8 - b'0') as usize + 1) % 10 != (x_i as u8 - b'0') as usize {
+            println!("Strong");
+            return;
+        }
+    }
+    println!("Weak");
 }
