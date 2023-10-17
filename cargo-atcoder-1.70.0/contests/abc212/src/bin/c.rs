3f94044282287a255ed888d94dd82d4b66c0a2e0
bouzuya <m@bouzuya.net>
2023-10-17 22:49:35 +0900

    abc212 c

--- cargo-atcoder-1.70.0/contests/abc212/src/bin/c.rs
+++ cargo-atcoder-1.70.0/contests/abc212/src/bin/c.rs
@@ -1,10 +1,25 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+use superslice::Ext;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        a: [i64; n],
+        mut b: [i64; m],
     };
-    let ans = n - a.len();
+    b.sort();
+    let mut min = (a[0] - b[0]).abs();
+    for a_i in a {
+        let r = b.lower_bound(&a_i);
+        let l = r.saturating_sub(1);
+        if let Some(b_r) = b.get(r) {
+            min = min.min((b_r - a_i).abs());
+        }
+        if let Some(b_l) = b.get(l) {
+            min = min.min((b_l - a_i).abs());
+        }
+    }
+    let ans = min;
     println!("{}", ans);
 }
