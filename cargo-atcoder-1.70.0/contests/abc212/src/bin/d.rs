3aeb9ff23f9f717578c839c359b719ef50de71aa
bouzuya <m@bouzuya.net>
2023-10-19 23:24:37 +0900

    abc212 d

--- cargo-atcoder-1.70.0/contests/abc212/src/bin/d.rs
+++ cargo-atcoder-1.70.0/contests/abc212/src/bin/d.rs
@@ -1,10 +1,36 @@
-use proconio::{input, marker::Usize1};
+use std::{cmp::Reverse, collections::BinaryHeap};
+
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        q: usize,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut pq = BinaryHeap::new();
+    let mut s = 0_i64;
+    for _ in 0..q {
+        input! {
+            t: usize,
+        }
+        match t {
+            1 => {
+                input! {
+                    x: i64,
+                }
+                pq.push(Reverse(x - s));
+            }
+            2 => {
+                input! {
+                    x: i64,
+                }
+                s += x;
+            }
+            3 => {
+                let Reverse(ans) = pq.pop().unwrap();
+                println!("{}", ans + s);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
