c4c1024f1bcb4bc3ea9618332dbde7d834a9c28f
bouzuya <m@bouzuya.net>
2023-10-29 23:14:12 +0900

    abc326 e

--- cargo-atcoder-1.70.0/contests/abc326/src/bin/e.rs
+++ cargo-atcoder-1.70.0/contests/abc326/src/bin/e.rs
@@ -1,10 +1,17 @@
-use proconio::{input, marker::Usize1};
+use ac_library::ModInt998244353 as ModInt;
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
-    };
-    let ans = n - a.len();
+        a: [usize; n],
+    }
+
+    let mut ans = ModInt::new(0);
+    let mut sum_p = ModInt::new(1) / n;
+    for a_i in a.iter().copied() {
+        ans += sum_p * a_i;
+        sum_p += sum_p / ModInt::new(n);
+    }
     println!("{}", ans);
 }
