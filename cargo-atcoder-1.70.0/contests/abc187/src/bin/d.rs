5131e1903ee21cc1d820b16e972c335b7e2864fa
bouzuya <m@bouzuya.net>
2023-11-02 23:44:25 +0900

    abc187 d

--- cargo-atcoder-1.70.0/contests/abc187/src/bin/d.rs
+++ cargo-atcoder-1.70.0/contests/abc187/src/bin/d.rs
@@ -1,10 +1,20 @@
-use proconio::{input, marker::Usize1};
+use std::cmp::Reverse;
+
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        mut ab: [(i64, i64); n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    ab.sort_by_key(|(a, b)| Reverse(2 * a + b));
+    let sum_a = ab.iter().map(|(a, _)| a).copied().sum::<i64>();
+    let mut s = -sum_a;
+    for (i, (a, b)) in ab.iter().copied().enumerate() {
+        s += 2 * a + b;
+        if s > 0 {
+            println!("{}", i + 1);
+            return;
+        }
+    }
 }
