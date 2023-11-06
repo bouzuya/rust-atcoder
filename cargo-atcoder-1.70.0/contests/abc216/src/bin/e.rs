2c46bf3ed5b3d41529b7c3792d1ab814f1599505
bouzuya <m@bouzuya.net>
2023-11-06 21:37:05 +0900

    abc216 e

--- cargo-atcoder-1.70.0/contests/abc216/src/bin/e.rs
+++ cargo-atcoder-1.70.0/contests/abc216/src/bin/e.rs
@@ -1,10 +1,49 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        k: usize,
+        a: [usize; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let sum_a = a.iter().copied().sum::<usize>();
+    if sum_a <= k {
+        let mut ans = 0_usize;
+        for a_i in a {
+            ans += (1 + a_i) * a_i / 2;
+        }
+        println!("{}", ans);
+        return;
+    }
+
+    let max_a = *a.iter().max().unwrap();
+
+    let mut ok = max_a;
+    let mut ng = 0;
+    while ok - ng > 1 {
+        let mid = ng + (ok - ng) / 2;
+        let mut cnt = 0_usize;
+        for a_i in a.iter().copied() {
+            cnt += a_i.saturating_sub(mid);
+        }
+        if cnt <= k {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+
+    let mut cnt = 0_usize;
+    let mut sum = 0_usize;
+    for a_i in a.iter().copied() {
+        let ok = ok + 1;
+        if a_i < ok {
+            continue;
+        }
+        cnt += a_i + 1 - ok;
+        sum += (ok + a_i) * (a_i + 1 - ok) / 2;
+    }
+
+    sum += ok * (sum_a.min(k) - cnt);
+    println!("{}", sum);
 }
