17e39a7e52b5776fdea32af83da5ff43df2fa473
bouzuya <m@bouzuya.net>
2023-11-08 22:21:15 +0900

    abc214 d

--- cargo-atcoder-1.70.0/contests/abc214/src/bin/d.rs
+++ cargo-atcoder-1.70.0/contests/abc214/src/bin/d.rs
@@ -1,10 +1,18 @@
+use ac_library::Dsu;
 use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        mut uvw: [(Usize1, Usize1, usize); n - 1],
     };
-    let ans = n - a.len();
+    uvw.sort_by_key(|&(_, _, w)| w);
+    let mut sum = 0_usize;
+    let mut dsu = Dsu::new(n);
+    for (u, v, w) in uvw {
+        sum += dsu.size(u) * dsu.size(v) * w;
+        dsu.merge(u, v);
+    }
+    let ans = sum;
     println!("{}", ans);
 }
