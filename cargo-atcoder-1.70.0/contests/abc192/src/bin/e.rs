91f67d0381528047603bb3963b5c12bbb043c682
bouzuya <m@bouzuya.net>
2023-11-10 22:20:20 +0900

    abc192 e

--- cargo-atcoder-1.70.0/contests/abc192/src/bin/e.rs
+++ cargo-atcoder-1.70.0/contests/abc192/src/bin/e.rs
@@ -1,10 +1,47 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        x: Usize1,
+        y: Usize1,
+        abtk: [(Usize1, Usize1, usize, usize); m]
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut edges = vec![vec![]; n];
+    for (a, b, t, k) in abtk {
+        edges[a].push((b, t, k));
+        edges[b].push((a, t, k));
+    }
+
+    let inf = 1_usize << 60;
+    let mut time = vec![inf; n];
+    let mut pq = BinaryHeap::new();
+    pq.push((Reverse(0_usize), x));
+    time[x] = 0_usize;
+    while let Some((Reverse(t), u)) = pq.pop() {
+        if t > time[u] {
+            continue;
+        }
+        for (v, t, k) in edges[u].iter().copied() {
+            let nt = if time[u] % k == 0 {
+                time[u]
+            } else {
+                time[u] + k - (time[u] % k)
+            } + t;
+            if nt < time[v] {
+                time[v] = nt;
+                pq.push((Reverse(nt), v));
+            }
+        }
+    }
+    let ans = time[y];
+    if ans == inf {
+        println!("-1");
+    } else {
+        println!("{}", ans);
+    }
 }
