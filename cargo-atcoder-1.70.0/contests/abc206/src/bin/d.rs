53a2bcee97692606a8755cf1aabc6372565cf365
bouzuya <m@bouzuya.net>
2023-11-14 21:15:57 +0900

    abc206 d

--- cargo-atcoder-1.70.0/contests/abc206/src/bin/d.rs
+++ cargo-atcoder-1.70.0/contests/abc206/src/bin/d.rs
@@ -1,10 +1,28 @@
-use proconio::{input, marker::Usize1};
+use std::collections::{BTreeMap, BTreeSet};
+
+use ac_library::Dsu;
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
+    let map = a
+        .iter()
+        .copied()
+        .collect::<BTreeSet<_>>()
+        .into_iter()
+        .enumerate()
+        .fold(BTreeMap::new(), |mut m, (i, k)| {
+            m.insert(k, i);
+            m
+        });
+    let mut dsu = Dsu::new(map.len());
+    for i in 0..n / 2 {
+        let (a_i, a_j) = (a[i], a[n - 1 - i]);
+        dsu.merge(map[&a_i], map[&a_j]);
+    }
+    let ans = map.len() - dsu.groups().len();
     println!("{}", ans);
 }
