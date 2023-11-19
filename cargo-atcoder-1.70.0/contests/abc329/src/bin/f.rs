caafe2fc0c300c0fba0ca4afa1442e71179f02f4
bouzuya <m@bouzuya.net>
2023-11-19 22:54:09 +0900

    abc329 f

--- cargo-atcoder-1.70.0/contests/abc329/src/bin/f.rs
+++ cargo-atcoder-1.70.0/contests/abc329/src/bin/f.rs
@@ -1,10 +1,34 @@
+use std::collections::HashSet;
+
 use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        q: usize,
+        c: [Usize1; n],
+        ab: [(Usize1, Usize1); q],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut boxes = vec![HashSet::new(); n];
+    for (i, c_i) in c.iter().copied().enumerate() {
+        boxes[i].insert(c_i);
+    }
+
+    for (a, b) in ab {
+        let (small, large) = if boxes[a].len() < boxes[b].len() {
+            (a, b)
+        } else {
+            (b, a)
+        };
+        boxes.push(HashSet::new());
+        boxes.swap(small, n);
+        for x in boxes.pop().unwrap() {
+            boxes[large].insert(x);
+        }
+        if large != b {
+            boxes.swap(small, large);
+        }
+        println!("{}", boxes[b].len());
+    }
 }
