4fa49a119e55dcfcfd2e444bd6f8e994c65db0c6
bouzuya <m@bouzuya.net>
2023-11-22 22:04:47 +0900

    past202303-open b

--- cargo-atcoder-1.70.0/contests/past202303-open/src/bin/b.rs
+++ cargo-atcoder-1.70.0/contests/past202303-open/src/bin/b.rs
@@ -1,10 +1,32 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        d: usize,
+        mut a: Chars,
+        mut b: Chars,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let len = a.len().max(b.len());
+    a.reverse();
+    b.reverse();
+    let mut c = 0;
+    let mut ans = vec![];
+    for i in 0..len {
+        if i == d {
+            ans.push('.');
+            continue;
+        }
+        let x = (*a.get(i).unwrap_or(&'0') as u8 - b'0') as usize;
+        let y = (*b.get(i).unwrap_or(&'0') as u8 - b'0') as usize;
+        ans.push((((x + y + c) % 10) as u8 + b'0') as char);
+        c = (x + y + c) / 10;
+    }
+    if c != 0 {
+        ans.push((c as u8 + b'0') as char);
+    }
+
+    ans.reverse();
+
+    println!("{}", ans.into_iter().collect::<String>());
 }
