2d8c6034d20d9d075c2c1aae5505b6fe5acab476
bouzuya <m@bouzuya.net>
2023-11-24 23:40:48 +0900

    past202303-open d

--- cargo-atcoder-1.70.0/contests/past202303-open/src/bin/d.rs
+++ cargo-atcoder-1.70.0/contests/past202303-open/src/bin/d.rs
@@ -1,10 +1,31 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        h: usize,
+        a: usize,
+        b: usize,
+        c: usize,
+        d: usize,
     };
-    let ans = n - a.len();
+
+    let mut min = 1 << 60;
+    for i in 0..100 {
+        let mut price = 0_usize;
+        let mut h = h;
+        for _ in 0..i {
+            price += d;
+            h = h.saturating_sub(c);
+            if h == 0 {
+                break;
+            }
+            h -= h / 2;
+        }
+        if h > 0 {
+            price += (h + a - 1) / a * b;
+        }
+        min = min.min(price);
+    }
+    let ans = min;
     println!("{}", ans);
 }
