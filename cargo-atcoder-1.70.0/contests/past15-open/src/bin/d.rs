d03f9db1054d202e4abc6b09e2440267d36ea347
bouzuya <m@bouzuya.net>
2023-12-08 21:46:45 +0900

    past15-open d

--- cargo-atcoder-1.70.0/contests/past15-open/src/bin/d.rs
+++ cargo-atcoder-1.70.0/contests/past15-open/src/bin/d.rs
@@ -1,10 +1,28 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        a: usize,
+        b: usize,
+        c: usize,
+        d: usize,
+        r: usize,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut ok = vec![0_i64; a.max(c) + r + r + 1];
+    for t in 0..=a.max(c) + r {
+        if t % d == 0 {
+            let (p, q) = if t < b { (a, a + r) } else { (c, c + r) };
+            if t < q {
+                ok[p.max(t)] += 1;
+                ok[q] -= 1;
+            }
+        }
+    }
+    for t in 0..a.max(c) + r + r {
+        ok[t + 1] += ok[t];
+    }
+
+    let ans = ok.iter().skip(c).take(r).all(|&x| x > 0);
+    println!("{}", if ans { "Yes" } else { "No" });
 }
