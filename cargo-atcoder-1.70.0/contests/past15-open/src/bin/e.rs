7289b89d4c041160948ee2dc1b04ff64665bd400
bouzuya <m@bouzuya.net>
2023-12-09 22:36:12 +0900

    past15-open e

--- cargo-atcoder-1.70.0/contests/past15-open/src/bin/e.rs
+++ cargo-atcoder-1.70.0/contests/past15-open/src/bin/e.rs
@@ -1,10 +1,21 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        k: usize,
+        a: [usize; n],
     };
-    let ans = n - a.len();
+    let mut sum = 0_usize;
+    for bits in 0..1 << n {
+        let is = (0..n)
+            .filter(|i| ((bits >> i) & 1) == 1)
+            .collect::<Vec<usize>>();
+        if is.len() != k {
+            continue;
+        }
+        sum += is.into_iter().map(|i| a[i]).sum::<usize>();
+    }
+    let ans = sum;
     println!("{}", ans);
 }
