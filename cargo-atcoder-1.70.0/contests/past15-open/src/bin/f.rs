adce903d9fa29ec49872a4fe4283fa0fa62c2fda
bouzuya <m@bouzuya.net>
2023-12-10 22:54:56 +0900

    past15-open f

--- cargo-atcoder-1.70.0/contests/past15-open/src/bin/f.rs
+++ cargo-atcoder-1.70.0/contests/past15-open/src/bin/f.rs
@@ -1,10 +1,28 @@
-use proconio::{input, marker::Usize1};
+use std::collections::BTreeMap;
+
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut map = BTreeMap::new();
+    for (i, a_i) in a.iter().copied().enumerate() {
+        map.entry(a_i).or_insert_with(Vec::new).push(i);
+    }
+
+    let mut b = vec![n; n];
+    let mut index = 0_usize;
+    for (_, is) in map {
+        for i in is {
+            b[i] = index;
+            index += 1;
+        }
+    }
+
+    for b_i in b {
+        print!("{} ", b_i + 1);
+    }
 }
