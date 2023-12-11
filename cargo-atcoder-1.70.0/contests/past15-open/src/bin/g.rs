937f395122a51dfd3c32aa9357bbc0dd07c68273
bouzuya <m@bouzuya.net>
2023-12-11 22:53:37 +0900

    past15-open g

--- cargo-atcoder-1.70.0/contests/past15-open/src/bin/g.rs
+++ cargo-atcoder-1.70.0/contests/past15-open/src/bin/g.rs
@@ -3,8 +3,26 @@ use proconio::{input, marker::Usize1};
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut abs = vec![];
+    for _ in 0..m {
+        input! {
+            k: usize,
+            ab: [(Usize1, usize); k],
+        }
+        abs.push(ab);
+    }
+
+    for bits in 0..1 << n {
+        let x = (0..n).map(|i| (bits >> i) & 1).collect::<Vec<usize>>();
+        if abs
+            .iter()
+            .all(|ab| ab.iter().copied().any(|(a, b)| x[a] == b))
+        {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
