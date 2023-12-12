c65a75912164cde902ea49b743a0e5a5029b223e
bouzuya <m@bouzuya.net>
2023-12-12 22:19:41 +0900

    past202212-open f

--- cargo-atcoder-1.70.0/contests/past202212-open/src/bin/f.rs
+++ cargo-atcoder-1.70.0/contests/past202212-open/src/bin/f.rs
@@ -1,10 +1,29 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: usize,
+        b: usize,
+        c: usize,
+        d: usize,
+        x: Chars,
     };
-    let ans = n - a.len();
+
+    let x = x
+        .into_iter()
+        .filter(|c| c != &'.')
+        .collect::<String>()
+        .parse::<usize>()
+        .unwrap();
+
+    let sum = a + 2 * b + 3 * c + 4 * d;
+    if sum * 1_000 <= x * n {
+        println!("0");
+        return;
+    }
+
+    let ans = ((1_000 * sum - x * n) + (x - 1000) - 1) / (x - 1000);
+
     println!("{}", ans);
 }
