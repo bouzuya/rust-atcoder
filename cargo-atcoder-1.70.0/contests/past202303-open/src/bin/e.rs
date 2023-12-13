e0d265dd5b373071dfe7405e4269a6a2b949e0d8
bouzuya <m@bouzuya.net>
2023-12-13 21:14:16 +0900

    past202303-open e

--- cargo-atcoder-1.70.0/contests/past202303-open/src/bin/e.rs
+++ cargo-atcoder-1.70.0/contests/past202303-open/src/bin/e.rs
@@ -1,10 +1,22 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        h: usize,
+        _w: usize,
+        s: [Chars; h],
+        t: [Chars; h],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let f = |a: &[char]| -> (usize, usize) {
+        a.iter().fold((0, 0), |(c1, c2), c| match c {
+            '#' => (c1 + 1, c2),
+            '.' => (c1, c2 + 1),
+            _ => unreachable!(),
+        })
+    };
+    let ans = s
+        .into_iter()
+        .zip(t.into_iter())
+        .all(|(s_i, t_i)| f(&s_i) == f(&t_i));
+    println!("{}", if ans { "Yes" } else { "No" });
 }
