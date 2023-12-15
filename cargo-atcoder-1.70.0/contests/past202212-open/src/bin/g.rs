c8cb88165e65c755e123f24660bb479aa860025d
bouzuya <m@bouzuya.net>
2023-12-15 22:06:43 +0900

    past202212-open g

--- cargo-atcoder-1.70.0/contests/past202212-open/src/bin/g.rs
+++ cargo-atcoder-1.70.0/contests/past202212-open/src/bin/g.rs
@@ -1,10 +1,24 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [i64; n],
     };
-    let ans = n - a.len();
+    let s = std::iter::once(0)
+        .chain(a.iter().scan(0, |acc, &i| {
+            *acc += i;
+            Some(*acc)
+        }))
+        .collect::<Vec<i64>>();
+
+    let mut max = a[0];
+    let mut min = s[0];
+    for s_i in s.iter().copied().skip(1) {
+        max = max.max(s_i - min);
+        min = min.min(s_i);
+    }
+
+    let ans = max;
     println!("{}", ans);
 }
