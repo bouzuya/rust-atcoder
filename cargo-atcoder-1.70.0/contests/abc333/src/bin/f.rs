fc4c08e346d94991a4c90c45d27ec6988c11777e
bouzuya <m@bouzuya.net>
2023-12-17 23:17:35 +0900

    abc333 f

--- cargo-atcoder-1.70.0/contests/abc333/src/bin/f.rs
+++ cargo-atcoder-1.70.0/contests/abc333/src/bin/f.rs
@@ -1,10 +1,39 @@
-use proconio::{input, marker::Usize1};
+use ac_library::ModInt998244353 as ModInt;
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let a = ModInt::new(2).inv();
+    let mut pa = vec![ModInt::new(1); n + 1];
+    for i in 0..n {
+        pa[i + 1] = pa[i] * a;
+    }
+
+    let mut dp = vec![ModInt::new(1); 1];
+    for k in 2..=n {
+        let mut next = vec![ModInt::new(0); k];
+        let x = (ModInt::new(2) - a.pow((k - 1) as u64)).inv();
+
+        for i in 0..k - 1 {
+            next[0] += pa[k - 1 - i] * dp[i];
+        }
+        for i in 0..k - 1 {
+            let mut now = next[i];
+            now -= dp[i] * pa[k - 1];
+            now *= a;
+            now += dp[i];
+            next[i + 1] = now;
+        }
+        for i in next.iter_mut() {
+            *i *= x;
+        }
+
+        dp = next;
+    }
+    for i in 0..n {
+        println!("{}", dp[i]);
+    }
 }
