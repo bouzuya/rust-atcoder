275ec85053294f450d92c7cc25eeede68891cf72
bouzuya <m@bouzuya.net>
2023-12-19 21:58:43 +0900

    abc330 c, d

--- cargo-atcoder-1.70.0/contests/abc330/src/bin/c.rs
+++ cargo-atcoder-1.70.0/contests/abc330/src/bin/c.rs
@@ -1,10 +1,36 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        d: usize,
     };
-    let ans = n - a.len();
+
+    let mut ans = 1_usize << 60;
+    for x in 1_usize.. {
+        let x2 = x.pow(2);
+        if x2 > d {
+            break;
+        }
+
+        let y2q = d - x2;
+        let mut ok = 1_usize;
+        let mut ng = 1_000_000_000_usize;
+        while ng - ok > 1 {
+            let mid = (ok + ng) / 2;
+            if mid.pow(2) <= y2q {
+                ok = mid;
+            } else {
+                ng = mid;
+            }
+        }
+
+        for y in [ok.saturating_sub(1), ok, ng, ng + 1].iter().copied() {
+            let y2 = y.pow(2);
+            let z2 = x2 + y2;
+            let a = z2.max(d) - z2.min(d);
+            ans = ans.min(a);
+        }
+    }
+
     println!("{}", ans);
 }
--- cargo-atcoder-1.70.0/contests/abc330/src/bin/d.rs
+++ cargo-atcoder-1.70.0/contests/abc330/src/bin/d.rs
@@ -1,10 +1,84 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        s: [Chars; n],
     };
-    let ans = n - a.len();
+
+    let mut rows = vec![vec![0; n + 1]; n];
+    for i in 0..n {
+        for j in 0..n {
+            rows[i][j + 1] = rows[i][j] + if s[i][j] == 'o' { 1 } else { 0 };
+        }
+    }
+
+    let mut rrows = vec![vec![0; n + 1]; n];
+    for i in 0..n {
+        for j in (0..n).rev() {
+            rrows[i][j] = rrows[i][j + 1] + if s[i][j] == 'o' { 1 } else { 0 };
+        }
+    }
+
+    let mut cols = vec![vec![0; n]; n + 1];
+    for j in 0..n {
+        for i in 0..n {
+            cols[i + 1][j] = cols[i][j] + if s[i][j] == 'o' { 1 } else { 0 };
+        }
+    }
+
+    let mut rcols = vec![vec![0; n]; n + 1];
+    for j in 0..n {
+        for i in (0..n).rev() {
+            rcols[i][j] = rcols[i + 1][j] + if s[i][j] == 'o' { 1 } else { 0 };
+        }
+    }
+
+    // for i in 0..=n {
+    //     for j in 0..n {
+    //         print!("{} ", rcols[i][j]);
+    //     }
+    //     println!();
+    // }
+
+    let mut ans = 0_usize;
+
+    // tl
+    for i in 0..n {
+        for j in 0..n {
+            if s[i][j] != 'o' {
+                continue;
+            }
+            ans += (rows[i][n] - rows[i][j + 1]) * (cols[n][j] - cols[i + 1][j]);
+        }
+    }
+    // tr
+    for i in 0..n {
+        for j in 1..n {
+            if s[i][j] != 'o' {
+                continue;
+            }
+            ans += (rrows[i][0] - rrows[i][j]) * (cols[n][j] - cols[i + 1][j]);
+        }
+    }
+    // br
+    for i in 1..n {
+        for j in 0..n {
+            if s[i][j] != 'o' {
+                continue;
+            }
+            ans += (rrows[i][0] - rrows[i][j]) * (rcols[0][j] - rcols[i][j]);
+        }
+    }
+    // bl
+    for i in 1..n {
+        for j in 0..n {
+            if s[i][j] != 'o' {
+                continue;
+            }
+            ans += (rows[i][n] - rows[i][j + 1]) * (rcols[0][j] - rcols[i][j]);
+        }
+    }
+
     println!("{}", ans);
 }
