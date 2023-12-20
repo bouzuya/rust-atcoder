0684e380568b352829c57b83592696d421847c3a
bouzuya <m@bouzuya.net>
2023-12-20 22:59:15 +0900

    abc330 e

--- cargo-atcoder-1.70.0/contests/abc330/src/bin/e.rs
+++ cargo-atcoder-1.70.0/contests/abc330/src/bin/e.rs
@@ -1,10 +1,35 @@
+use std::collections::{BTreeSet, HashMap};
+
 use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        q: usize,
+        mut a: [usize; n],
+        ix: [(Usize1, usize); q]
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut used = HashMap::new();
+    let mut unused = (0..=2 * 100_000).collect::<BTreeSet<usize>>();
+    for a_i in a.iter().copied() {
+        *used.entry(a_i).or_insert(0) += 1;
+        unused.remove(&a_i);
+    }
+
+    for (i, x) in ix {
+        let count = used.get_mut(&a[i]).unwrap();
+        *count -= 1;
+        if *count == 0 {
+            used.remove(&a[i]);
+            unused.insert(a[i]);
+        }
+
+        a[i] = x;
+        *used.entry(x).or_insert(0) += 1;
+        unused.remove(&x);
+
+        let mex = unused.iter().next().unwrap();
+        println!("{}", mex);
+    }
 }
