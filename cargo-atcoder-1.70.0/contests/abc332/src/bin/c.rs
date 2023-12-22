d8e1f0b330fc4bb3f753263e8d15172a89fbee48
bouzuya <m@bouzuya.net>
2023-12-22 23:06:00 +0900

    abc332 c

--- cargo-atcoder-1.70.0/contests/abc332/src/bin/c.rs
+++ cargo-atcoder-1.70.0/contests/abc332/src/bin/c.rs
@@ -1,10 +1,34 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        _n: usize,
+        m: usize,
+        s: Chars,
     };
-    let ans = n - a.len();
+
+    let mut ans = 0_usize;
+    let mut used_m = 0_usize;
+    let mut used_t = 0_usize;
+    for s_i in s {
+        match s_i {
+            '0' => {
+                used_m = 0;
+                used_t = 0;
+            }
+            '1' => {
+                if used_m < m {
+                    used_m += 1;
+                } else {
+                    used_t += 1;
+                }
+            }
+            '2' => {
+                used_t += 1;
+            }
+            _ => unreachable!(),
+        }
+        ans = ans.max(used_t);
+    }
     println!("{}", ans);
 }
