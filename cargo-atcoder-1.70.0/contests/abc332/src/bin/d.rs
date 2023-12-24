12128cd76b5cabd9456d0b9b4e14a7f845d22817
bouzuya <m@bouzuya.net>
2023-12-24 22:02:05 +0900

    abc332 d

--- cargo-atcoder-1.70.0/contests/abc332/src/bin/d.rs
+++ cargo-atcoder-1.70.0/contests/abc332/src/bin/d.rs
@@ -1,10 +1,57 @@
-use proconio::{input, marker::Usize1};
+use ac_library::FenwickTree;
+use proconio::input;
+use superslice::Ext;
+
+fn inversion_number(a: &[usize]) -> usize {
+    let mut count = 0;
+    let mut ft = FenwickTree::new(a.len(), 0);
+    for (i, &a_i) in a.iter().enumerate() {
+        ft.add(a_i, 1);
+        count += i - ft.accum(a_i);
+    }
+    count
+}
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        h: usize,
+        w: usize,
+        a: [[usize; w]; h],
+        b: [[usize; w]; h],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let inf = 1_000_000_000_usize;
+    let mut min = inf;
+    let mut is = (0..h).collect::<Vec<usize>>();
+    loop {
+        let mut js = (0..w).collect::<Vec<usize>>();
+
+        loop {
+            let mut ok = true;
+            for i in 0..h {
+                for j in 0..w {
+                    if a[is[i]][js[j]] != b[i][j] {
+                        ok = false;
+                    }
+                }
+            }
+            if ok {
+                min = min.min(inversion_number(&is) + inversion_number(&js));
+            }
+            if !js.next_permutation() {
+                break;
+            }
+        }
+
+        if !is.next_permutation() {
+            break;
+        }
+    }
+
+    if min == inf {
+        println!("-1");
+    } else {
+        let ans = min;
+        println!("{}", ans);
+    }
 }
