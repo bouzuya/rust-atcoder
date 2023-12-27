f6b4d30016a489d9b1f88125e7d4abccc4c3807f
bouzuya <m@bouzuya.net>
2023-12-27 22:18:17 +0900

    past16-open d

--- cargo-atcoder-1.70.0/contests/past16-open/src/bin/d.rs
+++ cargo-atcoder-1.70.0/contests/past16-open/src/bin/d.rs
@@ -1,10 +1,13 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let max = a.iter().copied().max().unwrap();
+    for a_i in a {
+        let ans = (1_000_000_000_f64 * a_i as f64 / max as f64).round();
+        println!("{}", ans)
+    }
 }
