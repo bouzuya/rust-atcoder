cb77e93cc340e23979b1f0f43a311b0de8ac594d
bouzuya <m@bouzuya.net>
2023-12-31 21:32:26 +0900

    past202004-open d, e

--- cargo-atcoder-1.70.0/contests/past202004-open/src/bin/d.rs
+++ cargo-atcoder-1.70.0/contests/past202004-open/src/bin/d.rs
@@ -1,10 +1,46 @@
-use proconio::{input, marker::Usize1};
+use std::collections::HashSet;
+
+use proconio::{input, marker::Chars};
+
+fn dfs(set: &mut HashSet<Vec<char>>, t: &mut Vec<char>) {
+    if t.len() == 3 {
+        return;
+    }
+    for c in (0..26)
+        .map(|j| (b'a' + j) as char)
+        .chain(std::iter::once('.'))
+    {
+        t.push(c);
+        set.insert(t.clone());
+        dfs(set, t);
+        t.pop();
+    }
+}
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        s: Chars,
     };
-    let ans = n - a.len();
+    let mut set = HashSet::new();
+    let mut t = vec![];
+    dfs(&mut set, &mut t);
+
+    let mut count = 0_usize;
+    for t in set {
+        if s.len() < t.len() {
+            continue;
+        }
+        for i in 0..s.len() + 1 - t.len() {
+            if t.iter()
+                .copied()
+                .enumerate()
+                .all(|(j, c)| c == '.' || s[i + j] == c)
+            {
+                count += 1;
+                break;
+            }
+        }
+    }
+    let ans = count;
     println!("{}", ans);
 }
--- cargo-atcoder-1.70.0/contests/past202004-open/src/bin/e.rs
+++ cargo-atcoder-1.70.0/contests/past202004-open/src/bin/e.rs
@@ -5,6 +5,17 @@ fn main() {
         n: usize,
         a: [Usize1; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    for i in 0..n {
+        let mut x = i;
+        let mut c = 0_usize;
+        loop {
+            x = a[x];
+            c += 1;
+            if x == i {
+                break;
+            }
+        }
+        println!("{}", c);
+    }
 }
