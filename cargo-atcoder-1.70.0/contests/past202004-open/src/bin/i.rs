b4b3aa48dd0d2a436480fbc1a830b5a2b97f989b
bouzuya <m@bouzuya.net>
2024-01-04 22:53:21 +0900

    past202004-open i

--- cargo-atcoder-1.70.0/contests/past202004-open/src/bin/i.rs
+++ cargo-atcoder-1.70.0/contests/past202004-open/src/bin/i.rs
@@ -1,10 +1,33 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; 2_usize.pow(n as u32)],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut ans = vec![0_usize; 2_usize.pow(n as u32)];
+    let mut ai = a
+        .iter()
+        .copied()
+        .enumerate()
+        .collect::<Vec<(usize, usize)>>();
+    for round in 1..=n {
+        let mut next = vec![];
+        for i in (0..ai.len()).step_by(2) {
+            if ai[i].1 < ai[i + 1].1 {
+                ans[ai[i].0] = round;
+                next.push(ai[i + 1]);
+            } else {
+                ans[ai[i + 1].0] = round;
+                next.push(ai[i]);
+            }
+        }
+        ai = next;
+        if ai.len() == 1 {
+            ans[ai[0].0] = round;
+        }
+    }
+    for a in ans {
+        println!("{}", a);
+    }
 }
