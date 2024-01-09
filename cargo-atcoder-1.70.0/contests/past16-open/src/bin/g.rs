a54a08faad6c2ef48ddac9965507d82216ef2df0
bouzuya <m@bouzuya.net>
2024-01-09 22:13:37 +0900

    past16-open g

--- cargo-atcoder-1.70.0/contests/past16-open/src/bin/g.rs
+++ cargo-atcoder-1.70.0/contests/past16-open/src/bin/g.rs
@@ -1,10 +1,38 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+
+fn dfs(a: &[usize]) -> usize {
+    if a.is_empty() {
+        return 1;
+    }
+    let mut count = 0_usize;
+    let (i, a_i) = (0, a[0]);
+    for j in 1..a.len() {
+        let a_j = a[j];
+        for k in j + 1..a.len() {
+            let a_k = a[k];
+            let (a_i, a_j) = (a_i.min(a_j), a_i.max(a_j));
+            let (a_j, a_k) = (a_j.min(a_k), a_j.max(a_k));
+            if a_i + a_j > a_k {
+                let b = a
+                    .iter()
+                    .copied()
+                    .enumerate()
+                    .filter(|(l, _)| l != &i && l != &j && l != &k)
+                    .map(|(_, x)| x)
+                    .collect::<Vec<usize>>();
+                count += dfs(&b);
+            }
+        }
+    }
+    count
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; 3 * n],
     };
-    let ans = n - a.len();
+
+    let ans = dfs(&a);
     println!("{}", ans);
 }
