3fd3fa1c50dd5ad5f60222f0d6a180d66cb57501
bouzuya <m@bouzuya.net>
2024-01-11 23:10:43 +0900

    past202005-open b

--- cargo-atcoder-1.70.0/contests/past202005-open/src/bin/b.rs
+++ cargo-atcoder-1.70.0/contests/past202005-open/src/bin/b.rs
@@ -2,9 +2,38 @@ use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        capital_n: usize,
+        capital_m: usize,
+        q: usize,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut count = vec![0_usize; capital_m];
+    let mut resolved = vec![vec![false; capital_m]; capital_n];
+    for _ in 0..q {
+        input! {
+            t: usize,
+        }
+
+        match t {
+            1 => {
+                input! {
+                    n: Usize1,
+                }
+                let ans = resolved[n]
+                    .iter()
+                    .enumerate()
+                    .map(|(i, r)| if *r { capital_n - count[i] } else { 0 })
+                    .sum::<usize>();
+                println!("{}", ans);
+            }
+            2 => {
+                input! {
+                    n: Usize1,
+                    m: Usize1,
+                }
+                resolved[n][m] = true;
+                count[m] += 1;
+            }
+            _ => unreachable!(),
+        }
+    }
 }
