686e0efe8f694ede4b7e233921d540b53b2422b6
bouzuya <m@bouzuya.net>
2024-01-12 20:50:53 +0900

    past202005-open c

--- cargo-atcoder-1.70.0/contests/past202005-open/src/bin/c.rs
+++ cargo-atcoder-1.70.0/contests/past202005-open/src/bin/c.rs
@@ -1,10 +1,30 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
+        a: usize,
+        r: usize,
         n: usize,
-        a: [Usize1; n],
     };
-    let ans = n - a.len();
+
+    if r == 1 {
+        let x = a * r;
+        if x > 10_usize.pow(9) {
+            println!("large");
+        } else {
+            println!("{}", x);
+        }
+        return;
+    }
+
+    let mut x = a;
+    for _ in 1..n {
+        x *= r;
+        if x > 10_usize.pow(9) {
+            println!("large");
+            return;
+        }
+    }
+    let ans = x;
     println!("{}", ans);
 }
