78c253b7ca045b4f0d3894ea1c9d5d99f1dcfe0c
bouzuya <m@bouzuya.net>
2024-01-13 22:16:48 +0900

    past202005-open d

--- cargo-atcoder-1.70.0/contests/past202005-open/src/bin/d.rs
+++ cargo-atcoder-1.70.0/contests/past202005-open/src/bin/d.rs
@@ -1,10 +1,43 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        s: [Chars; 5],
     };
-    let ans = n - a.len();
+
+    let lines = vec![
+        ".###..#..###.###.#.#.###.###.###.###.###.",
+        ".#.#.##....#...#.#.#.#...#.....#.#.#.#.#.",
+        ".#.#..#..###.###.###.###.###...#.###.###.",
+        ".#.#..#..#.....#...#...#.#.#...#.#.#...#.",
+        ".###.###.###.###...#.###.###...#.###.###.",
+    ]
+    .into_iter()
+    .map(|line| line.chars().collect::<Vec<char>>())
+    .collect::<Vec<Vec<char>>>();
+
+    let mut ans = vec![];
+    for i in 0..n {
+        let mut d = 0;
+        for j in 0..10 {
+            let mut ok = true;
+            for k in 0..5 {
+                for l in 0..4 {
+                    if s[k][4 * i + l] != lines[k][4 * j + l] {
+                        ok = false;
+                    }
+                }
+            }
+            if ok {
+                d = j;
+                break;
+            }
+        }
+
+        ans.push(d);
+    }
+
+    let ans = ans.into_iter().map(|d| d.to_string()).collect::<String>();
     println!("{}", ans);
 }
