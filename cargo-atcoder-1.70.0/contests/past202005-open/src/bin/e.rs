a9eb89dbbc797af43bd57b13d220f044c8bb3a43
bouzuya <m@bouzuya.net>
2024-01-14 23:21:21 +0900

    past202005-open e

--- cargo-atcoder-1.70.0/contests/past202005-open/src/bin/e.rs
+++ cargo-atcoder-1.70.0/contests/past202005-open/src/bin/e.rs
@@ -3,8 +3,37 @@ use proconio::{input, marker::Usize1};
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        q: usize,
+        uv: [(Usize1, Usize1); m],
+        mut c: [Usize1; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut edges = vec![vec![]; n];
+    for (u, v) in uv {
+        edges[u].push(v);
+        edges[v].push(u);
+    }
+
+    for _ in 0..q {
+        input! {
+            t: usize,
+            x: Usize1,
+        }
+        println!("{}", c[x] + 1);
+        match t {
+            1 => {
+                for v in edges[x].iter().copied() {
+                    c[v] = c[x];
+                }
+            }
+            2 => {
+                input! {
+                    y: Usize1,
+                }
+                c[x] = y;
+            }
+            _ => unreachable!(),
+        }
+    }
 }
