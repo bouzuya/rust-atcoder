59e5655a30fa0a520ac20cfda823f05d827205e6
bouzuya <m@bouzuya.net>
2024-01-19 22:39:40 +0900

    abc336 b, d

--- cargo-atcoder-1.70.0/contests/abc336/src/bin/b.rs
+++ cargo-atcoder-1.70.0/contests/abc336/src/bin/b.rs
@@ -4,13 +4,5 @@ fn main() {
     input! {
         n: usize,
     };
-    let mut count = 0_usize;
-    for i in 0..30 {
-        if n & (1 << i) != 0 {
-            break;
-        }
-        count += 1;
-    }
-    let ans = count;
-    println!("{}", ans);
+    println!("{}", n.trailing_zeros());
 }
--- cargo-atcoder-1.70.0/contests/abc336/src/bin/d.rs
+++ cargo-atcoder-1.70.0/contests/abc336/src/bin/d.rs
@@ -1,10 +1,30 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        a: [usize; n],
     };
-    let ans = n - a.len();
+
+    let mut b = vec![1; n];
+    let mut p = 0_usize;
+    for (i, a_i) in a.iter().copied().enumerate() {
+        p = (p + 1).min(a_i);
+        b[i] = p;
+    }
+
+    let mut c = vec![1; n];
+    let mut p = 0_usize;
+    for (i, a_i) in a.iter().copied().enumerate().rev() {
+        p = (p + 1).min(a_i);
+        c[i] = p;
+    }
+
+    let ans = b
+        .into_iter()
+        .zip(c.into_iter())
+        .map(|(b_i, c_i)| b_i.min(c_i))
+        .max()
+        .unwrap();
     println!("{}", ans);
 }
