3be65a420ca3a3a899a05f21b5019c8295004e54
bouzuya <m@bouzuya.net>
2024-01-21 21:26:02 +0900

    past202005-open i

--- cargo-atcoder-1.70.0/contests/past202005-open/src/bin/i.rs
+++ cargo-atcoder-1.70.0/contests/past202005-open/src/bin/i.rs
@@ -3,8 +3,50 @@ use proconio::{input, marker::Usize1};
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        q: usize,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut swap = false;
+    let mut rows = (0..n).collect::<Vec<usize>>();
+    let mut cols = (0..n).collect::<Vec<usize>>();
+    for _ in 0..q {
+        input! {
+            t: usize,
+        }
+        match t {
+            1 => {
+                input! {
+                    a: Usize1,
+                    b: Usize1,
+                }
+                if swap {
+                    cols.swap(a, b);
+                } else {
+                    rows.swap(a, b);
+                }
+            }
+            2 => {
+                input! {
+                    a: Usize1,
+                    b: Usize1,
+                }
+                if swap {
+                    rows.swap(a, b);
+                } else {
+                    cols.swap(a, b);
+                }
+            }
+            3 => {
+                swap = !swap;
+            }
+            4 => {
+                input! {
+                    a: Usize1,
+                    b: Usize1,
+                }
+                let (a, b) = if swap { (b, a) } else { (a, b) };
+                println!("{}", rows[a] * n + cols[b]);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
