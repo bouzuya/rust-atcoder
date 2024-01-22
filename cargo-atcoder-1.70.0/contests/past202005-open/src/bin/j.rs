1196a35bcfb3ed14b820eab0b940f61956b9ebb3
bouzuya <m@bouzuya.net>
2024-01-22 23:12:06 +0900

    past202005-open j

--- cargo-atcoder-1.70.0/contests/past202005-open/src/bin/j.rs
+++ cargo-atcoder-1.70.0/contests/past202005-open/src/bin/j.rs
@@ -1,10 +1,38 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        a: [usize; m],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut c = vec![0; n];
+    let mut ans = vec![];
+
+    for a_i in a {
+        if c[0] < a_i {
+            c[0] = a_i;
+            ans.push(0);
+            continue;
+        }
+        let mut ok = 0;
+        let mut ng = n;
+        while ng - ok > 1 {
+            let mid = (ok + ng) / 2;
+            if c[mid] >= a_i {
+                ok = mid;
+            } else {
+                ng = mid;
+            }
+        }
+        if ng == n {
+            ans.push(-1);
+        } else {
+            c[ng] = a_i;
+            ans.push(ng as i64);
+        }
+    }
+    for a in ans {
+        println!("{}", if a == -1 { -1 } else { a + 1 });
+    }
 }
