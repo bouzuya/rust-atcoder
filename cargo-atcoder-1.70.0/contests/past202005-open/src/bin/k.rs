f80b4a932dd0e79830904f3a1b872892a53761c3
bouzuya <m@bouzuya.net>
2024-01-23 23:03:34 +0900

    past202005-open k

--- cargo-atcoder-1.70.0/contests/past202005-open/src/bin/k.rs
+++ cargo-atcoder-1.70.0/contests/past202005-open/src/bin/k.rs
@@ -3,8 +3,27 @@ use proconio::{input, marker::Usize1};
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        q: usize,
+        ftx: [(Usize1, Usize1, Usize1); q],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut cb = vec![n; n];
+    let mut dt = (0..n).collect::<Vec<usize>>();
+
+    for (f, t, x) in ftx {
+        let (dt_f, dt_t, cb_x) = (dt[f], dt[t], cb[x]);
+        (dt[f], dt[t], cb[x]) = (cb_x, dt_f, dt_t);
+    }
+    let mut ans = vec![n; n];
+    for i in 0..n {
+        let mut p = dt[i];
+        while p < n {
+            ans[p] = i;
+            p = cb[p];
+        }
+    }
+
+    for a in ans {
+        println!("{}", a + 1);
+    }
 }
