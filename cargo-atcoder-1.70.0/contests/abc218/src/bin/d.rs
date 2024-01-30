716711ef0e01d99aafafcc40e51d65d3ad8af8f3
bouzuya <m@bouzuya.net>
2024-01-30 23:23:13 +0900

    abc218 d

--- cargo-atcoder-1.70.0/contests/abc218/src/bin/d.rs
+++ cargo-atcoder-1.70.0/contests/abc218/src/bin/d.rs
@@ -1,10 +1,25 @@
-use proconio::{input, marker::Usize1};
+use std::collections::HashSet;
+
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        xy: [(i64, i64); n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut ans = HashSet::new();
+    let set = xy.iter().collect::<std::collections::HashSet<_>>();
+    for i in 0..n {
+        let (x_i, y_i) = xy[i];
+        for j in i + 1..n {
+            let (x_j, y_j) = xy[j];
+            if x_i == x_j || y_i == y_j {
+                continue;
+            }
+            if set.contains(&(x_i, y_j)) && set.contains(&(x_j, y_i)) {
+                ans.insert((x_i.min(x_j), y_i.min(y_j), x_i.max(x_j), y_i.max(y_j)));
+            }
+        }
+    }
+    println!("{}", ans.len());
 }
