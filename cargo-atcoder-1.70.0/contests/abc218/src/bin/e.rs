0ca12cb9afe79441b31de9cfe109c6b9f66e312d
bouzuya <m@bouzuya.net>
2024-01-31 21:51:26 +0900

    abc218 e

--- cargo-atcoder-1.70.0/contests/abc218/src/bin/e.rs
+++ cargo-atcoder-1.70.0/contests/abc218/src/bin/e.rs
@@ -1,10 +1,27 @@
+use ac_library::Dsu;
 use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        mut abc: [(Usize1, Usize1, i64); m]
     };
-    let ans = n - a.len();
+
+    abc.sort_by_key(|&(_, _, c)| c);
+
+    let mut ans = abc.iter().copied().map(|(_, _, c)| c).sum::<i64>();
+    let mut dsu = Dsu::new(n);
+    for (a, b, c) in abc {
+        if dsu.same(a, b) {
+            if c < 0 {
+                ans -= c;
+            }
+            continue;
+        }
+        dsu.merge(a, b);
+        ans -= c;
+    }
+
     println!("{}", ans);
 }
