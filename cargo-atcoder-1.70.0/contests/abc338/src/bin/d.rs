1349d497375738bec928edfc0681841d6199f39f
bouzuya <m@bouzuya.net>
2024-02-02 23:15:00 +0900

    abc338 d

--- cargo-atcoder-1.70.0/contests/abc338/src/bin/d.rs
+++ cargo-atcoder-1.70.0/contests/abc338/src/bin/d.rs
@@ -3,8 +3,24 @@ use proconio::{input, marker::Usize1};
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        x: [Usize1; m],
     };
-    let ans = n - a.len();
+    let mut count = vec![0_i64; n + 1];
+    for (x_i, x_j) in x.iter().copied().zip(x.iter().copied().skip(1)) {
+        let (x_i, x_j) = if x_i < x_j { (x_i, x_j) } else { (x_j, x_i) };
+        let len_r = (x_j - x_i) as i64;
+        let len_l = n as i64 - len_r;
+        count[0] += len_r;
+        count[x_i] -= len_r;
+        count[x_i] += len_l;
+        count[x_j] -= len_l;
+        count[x_j] += len_r;
+        count[n] -= len_r;
+    }
+    for i in 0..n {
+        count[i + 1] += count[i];
+    }
+    let ans = count.into_iter().take(n).min().unwrap();
     println!("{}", ans);
 }
