50b4e9ef80c0608508b7064996c93227e3e1c610
bouzuya <m@bouzuya.net>
2024-02-04 22:58:52 +0900

    abc330 d

--- cargo-atcoder-1.70.0/contests/abc339/src/bin/d.rs
+++ cargo-atcoder-1.70.0/contests/abc339/src/bin/d.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::VecDeque;
 
 use proconio::{input, marker::Chars};
 
@@ -25,20 +25,13 @@ fn main() {
         }
     }
     let (p1, p2) = (p1.unwrap(), p2.unwrap());
-
-    let mut count = 0_usize;
-    let max = 6_000_000;
-    let mut dist = HashMap::new();
+    let inf = 1_usize << 60;
+    let mut dist = vec![vec![vec![vec![inf; n]; n]; n]; n];
     let mut deque = VecDeque::new();
     let (p1, p2) = (p1.min(p2), p1.max(p2));
-    dist.insert((p1, p2), 0_usize);
+    dist[p1.0][p1.1][p2.0][p2.1] = 0_usize;
     deque.push_back((p1, p2));
     while let Some((p1c, p2c)) = deque.pop_front() {
-        // 嘘解法
-        count += 1;
-        if count > max {
-            break;
-        }
         let dir = vec![(-1, 0), (0, -1), (0, 1), (1, 0)];
         for (dr, dc) in dir {
             let p1n = (
@@ -52,18 +45,25 @@ fn main() {
             );
             let p2n = if s[p2n.0][p2n.1] == '#' { p2c } else { p2n };
             let (p1n, p2n) = (p1n.min(p2n), p1n.max(p2n));
-            let d = dist[&(p1c, p2c)] + 1;
-            if p1n == p2n {
-                println!("{}", d);
-                return;
-            }
-            if dist.contains_key(&(p1n, p2n)) {
+            let d = dist[p1c.0][p1c.1][p2c.0][p2c.1] + 1;
+            if dist[p1n.0][p1n.1][p2n.0][p2n.1] != inf {
                 continue;
             }
-            dist.insert((p1n, p2n), d);
+            dist[p1n.0][p1n.1][p2n.0][p2n.1] = d;
+            if p1n == p2n {
+                continue;
+            }
             deque.push_back((p1n, p2n));
         }
     }
 
-    println!("-1");
+    let mut min = inf;
+    for i in 0..n {
+        for j in 0..n {
+            min = min.min(dist[i][j][i][j]);
+        }
+    }
+
+    let ans = if min == inf { -1 } else { min as i64 };
+    println!("{}", ans);
 }
