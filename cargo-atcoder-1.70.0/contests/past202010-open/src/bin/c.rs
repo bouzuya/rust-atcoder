6e52d2e20bbc5a6eb8885e945c0e39c96e1c8b5d
bouzuya <m@bouzuya.net>
2024-02-07 21:29:40 +0900

    past202010-open c

--- cargo-atcoder-1.70.0/contests/past202010-open/src/bin/c.rs
+++ cargo-atcoder-1.70.0/contests/past202010-open/src/bin/c.rs
@@ -1,10 +1,34 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        s: [Chars; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut a = vec![vec![0_usize; m]; n];
+    for i in 0..n {
+        for j in 0..m {
+            let mut count = 0_usize;
+            for di in -1..=1 {
+                for dj in -1..=1 {
+                    let (ni, nj) = (i as i64 + di, j as i64 + dj);
+                    if !(0..n as i64).contains(&ni) || !(0..m as i64).contains(&nj) {
+                        continue;
+                    }
+                    let (ni, nj) = (ni as usize, nj as usize);
+                    if s[ni][nj] == '#' {
+                        count += 1;
+                    }
+                }
+            }
+            a[i][j] = count;
+        }
+    }
+    for i in 0..n {
+        for j in 0..m {
+            print!("{}", a[i][j]);
+        }
+        println!();
+    }
 }
