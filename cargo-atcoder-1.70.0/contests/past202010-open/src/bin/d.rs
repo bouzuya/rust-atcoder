8b329a45f706a2b1780aa819e024fc5459e23b76
bouzuya <m@bouzuya.net>
2024-02-08 23:14:51 +0900

    past202010-open d

--- cargo-atcoder-1.70.0/contests/past202010-open/src/bin/d.rs
+++ cargo-atcoder-1.70.0/contests/past202010-open/src/bin/d.rs
@@ -1,10 +1,22 @@
-use proconio::{input, marker::Usize1};
+use std::collections::VecDeque;
+
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        _n: usize,
+        s: String,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut ss = s.split('#').collect::<VecDeque<&str>>();
+    let min_x = ss.pop_front().unwrap().len();
+    let min_y = ss.pop_back().unwrap().len();
+    let max = ss.iter().fold(min_x.max(min_y), |acc, x| {
+        acc.max(x.len().saturating_sub(min_x.min(min_y)))
+    });
+    let (x, y) = if min_x > min_y {
+        (max, min_y)
+    } else {
+        (min_x, max)
+    };
+    println!("{} {}", x, y);
 }
