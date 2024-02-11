f4e00bb82b49176efbe70f82704b9da9615ee75a
bouzuya <m@bouzuya.net>
2024-02-11 22:23:40 +0900

    abc340 b

--- cargo-atcoder-1.70.0/contests/abc340/src/bin/b.rs
+++ cargo-atcoder-1.70.0/contests/abc340/src/bin/b.rs
@@ -2,9 +2,27 @@ use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        q: usize,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut a = vec![];
+    for _ in 0..q {
+        input! {
+            t: usize
+        }
+        match t {
+            1 => {
+                input! {
+                    x: usize,
+                }
+                a.push(x);
+            }
+            2 => {
+                input! {
+                    k: Usize1,
+                }
+                println!("{}", a[a.len() - 1 - k]);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
