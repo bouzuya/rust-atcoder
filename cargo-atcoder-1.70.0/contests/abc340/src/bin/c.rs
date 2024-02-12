728ef8fd0b510becd54456bc05c173efc83a4528
bouzuya <m@bouzuya.net>
2024-02-12 21:57:31 +0900

    abc340 c

--- cargo-atcoder-1.70.0/contests/abc340/src/bin/c.rs
+++ cargo-atcoder-1.70.0/contests/abc340/src/bin/c.rs
@@ -1,10 +1,23 @@
-use proconio::{input, marker::Usize1};
+use std::collections::HashMap;
+
+use proconio::input;
+
+fn dfs(memo: &mut HashMap<usize, usize>, x: usize) -> usize {
+    if x == 0 || x == 1 {
+        return 0;
+    }
+    if let Some(&v) = memo.get(&x) {
+        return v;
+    }
+    let v = x + dfs(memo, x / 2) + dfs(memo, (x + 1) / 2);
+    memo.insert(x, v);
+    v
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
     };
-    let ans = n - a.len();
+    let ans = dfs(&mut HashMap::new(), n);
     println!("{}", ans);
 }
