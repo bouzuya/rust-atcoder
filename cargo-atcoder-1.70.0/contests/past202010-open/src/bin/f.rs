e217b127f962d56c7d0a49d5dfd93d5b5a6d693a
bouzuya <m@bouzuya.net>
2024-02-15 23:29:33 +0900

    past202010-open f

--- cargo-atcoder-1.70.0/contests/past202010-open/src/bin/f.rs
+++ cargo-atcoder-1.70.0/contests/past202010-open/src/bin/f.rs
@@ -1,10 +1,38 @@
-use proconio::{input, marker::Usize1};
+use std::{
+    cmp::Reverse,
+    collections::{BTreeMap, HashMap},
+};
+
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        k: usize,
+        s: [String; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut map = HashMap::new();
+    for s_i in s {
+        *map.entry(s_i).or_insert(0_usize) += 1;
+    }
+
+    let mut map2 = BTreeMap::new();
+    for (s, c) in map {
+        map2.entry(Reverse(c)).or_insert_with(Vec::new).push(s);
+    }
+
+    let mut sum = 0_usize;
+    for (_, ss) in map2.into_iter() {
+        if sum + ss.len() < k {
+            sum += ss.len();
+            continue;
+        }
+        if ss.len() == 1 {
+            println!("{}", ss[0]);
+        } else {
+            println!("AMBIGUOUS");
+        }
+        return;
+    }
 }
