12bcec4bfaee142860117a39c4b1c77378e75fc6
bouzuya <m@bouzuya.net>
2024-02-18 22:46:07 +0900

    abc341 d

--- cargo-atcoder-1.70.0/contests/abc341/src/bin/d.rs
+++ cargo-atcoder-1.70.0/contests/abc341/src/bin/d.rs
@@ -1,10 +1,33 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
+
+fn gcd(n: usize, m: usize) -> usize {
+    if m == 0 {
+        n
+    } else {
+        gcd(m, n % m)
+    }
+}
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        m: usize,
+        k: usize,
     };
-    let ans = n - a.len();
+    let lcm = n * m / gcd(n, m);
+    let mut ng = 0;
+    let mut ok = 1_usize << 60;
+    while ok - ng > 1 {
+        let mid = (ok + ng) / 2;
+        let cn = mid / n;
+        let cm = mid / m;
+        let cl = mid / lcm;
+        if cn + cm - 2 * cl >= k {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    let ans = ok;
     println!("{}", ans);
 }
