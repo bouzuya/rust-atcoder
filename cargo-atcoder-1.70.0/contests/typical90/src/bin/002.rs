e329c6c4acd88f3c4be1ca6ab5e27316a8275010
bouzuya <m@bouzuya.net>
2024-02-20 23:23:59 +0900

    typical90 002

--- cargo-atcoder-1.70.0/contests/typical90/src/bin/002.rs
+++ cargo-atcoder-1.70.0/contests/typical90/src/bin/002.rs
@@ -1,10 +1,42 @@
-use proconio::{input, marker::Usize1};
+use std::collections::BTreeSet;
+
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut ans = BTreeSet::new();
+    for bits in 0..1 << n {
+        let cs = (0..n)
+            .map(|i| if ((bits >> i) & 1) == 1 { ')' } else { '(' })
+            .collect::<Vec<char>>();
+        let mut ok = true;
+        let mut stack = vec![];
+        for c in cs.iter().copied() {
+            match c {
+                '(' => stack.push(c),
+                ')' => match stack.pop() {
+                    None => {
+                        ok = false;
+                        break;
+                    }
+                    Some(p) => {
+                        if p != '(' {
+                            ok = false;
+                            break;
+                        }
+                    }
+                },
+                _ => unreachable!(),
+            }
+        }
+        ok &= stack.is_empty();
+        if ok {
+            ans.insert(cs.into_iter().collect::<String>());
+        }
+    }
+    for a in ans {
+        println!("{}", a);
+    }
 }
