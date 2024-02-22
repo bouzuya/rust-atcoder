1f52f8418b8900876a9e70ad1c1048e14efd368f
bouzuya <m@bouzuya.net>
2024-02-22 22:01:18 +0900

    typical90 004

--- cargo-atcoder-1.70.0/contests/typical90/src/bin/004.rs
+++ cargo-atcoder-1.70.0/contests/typical90/src/bin/004.rs
@@ -1,10 +1,22 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [Usize1; n],
+        h: usize,
+        w: usize,
+        a: [[usize; w]; h],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let rows = a
+        .iter()
+        .map(|r| r.iter().sum::<usize>())
+        .collect::<Vec<usize>>();
+    let cols = (0..w)
+        .map(|j| (0..h).map(|i| a[i][j]).sum::<usize>())
+        .collect::<Vec<usize>>();
+    for i in 0..h {
+        for j in 0..w {
+            let ans = rows[i] + cols[j] - a[i][j];
+            print!("{}{}", ans, if j == w - 1 { "\n" } else { " " });
+        }
+    }
 }
