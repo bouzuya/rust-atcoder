2800fd48625ba9b03236ca44df47b0343b5b0943
bouzuya <m@bouzuya.net>
2024-02-26 23:16:18 +0900

    typical90 010

--- cargo-atcoder-1.70.0/contests/typical90/src/bin/010.rs
+++ cargo-atcoder-1.70.0/contests/typical90/src/bin/010.rs
@@ -3,8 +3,20 @@ use proconio::{input, marker::Usize1};
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        cp: [(Usize1, usize); n],
+        q: usize,
+        lr: [(Usize1, usize); q],
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+
+    let mut sum = vec![vec![0; n + 1]; 2];
+    for (i, (c, p)) in cp.iter().copied().enumerate() {
+        sum[c][i + 1] = sum[c][i] + p;
+        sum[1 - c][i + 1] = sum[1 - c][i];
+    }
+
+    for (l, r) in lr {
+        let a = sum[0][r] - sum[0][l];
+        let b = sum[1][r] - sum[1][l];
+        println!("{} {}", a, b);
+    }
 }
