1adf95a87d2e8c0e25119127180b49ff1760d2b5
bouzuya <m@bouzuya.net>
2024-02-27 22:40:45 +0900

    typical90 006

--- cargo-atcoder-1.70.0/contests/typical90/src/bin/006.rs
+++ cargo-atcoder-1.70.0/contests/typical90/src/bin/006.rs
@@ -1,10 +1,25 @@
-use proconio::{input, marker::Usize1};
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        k: usize,
+        s: Chars,
     };
-    let ans = n - a.len();
-    println!("{}", ans);
+    let mut ans = vec![];
+    let mut l = 0_usize;
+    for j in (1..=k).rev() {
+        let r = n + 1 - j;
+        let min_i = s[l..r]
+            .iter()
+            .copied()
+            .enumerate()
+            .min_by_key(|&(_, c)| c)
+            .map(|(i, _)| l + i)
+            .unwrap();
+        ans.push(s[min_i]);
+        l = min_i + 1;
+    }
+
+    println!("{}", ans.into_iter().collect::<String>());
 }
