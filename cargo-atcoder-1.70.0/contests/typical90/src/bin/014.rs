946cb7e9590a7fff43dfdea70a033a98594e13cc
bouzuya <m@bouzuya.net>
2024-03-01 22:57:34 +0900

    typical90 014

--- cargo-atcoder-1.70.0/contests/typical90/src/bin/014.rs
+++ cargo-atcoder-1.70.0/contests/typical90/src/bin/014.rs
@@ -1,10 +1,17 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        mut a: [i64; n],
+        mut b: [i64; n],
     };
-    let ans = n - a.len();
+    a.sort();
+    b.sort();
+    let ans = a
+        .into_iter()
+        .zip(b.into_iter())
+        .map(|(a_i, b_i)| (a_i - b_i).abs())
+        .sum::<i64>();
     println!("{}", ans);
 }
