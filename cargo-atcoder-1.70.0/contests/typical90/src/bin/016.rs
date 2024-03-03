24aa4f964d67a46137644bc8b93c88a41df8e50e
bouzuya <m@bouzuya.net>
2024-03-03 23:00:52 +0900

    typical90 016

--- cargo-atcoder-1.70.0/contests/typical90/src/bin/016.rs
+++ cargo-atcoder-1.70.0/contests/typical90/src/bin/016.rs
@@ -1,10 +1,29 @@
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
-    };
-    let ans = n - a.len();
+        a: usize,
+        b: usize,
+        c: usize,
+    }
+
+    let mut min = 9999;
+    for x in 0..=9999 {
+        for y in 0..=9999 {
+            if a * x + b * y > n {
+                break;
+            }
+            if (n - a * x - b * y) % c != 0 {
+                continue;
+            }
+            let z = (n - a * x - b * y) / c;
+            if x + y + z > 9999 {
+                continue;
+            }
+            min = min.min(x + y + z);
+        }
+    }
+    let ans = min;
     println!("{}", ans);
 }
