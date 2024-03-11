baed17bb734b25df2188a1357df1386a4320f7a1
bouzuya <m@bouzuya.net>
2024-03-11 21:44:22 +0900

    adt_easy_20240307_2 e

--- cargo-atcoder-1.70.0/contests/adt_easy_20240307_2/src/bin/e.rs
+++ cargo-atcoder-1.70.0/contests/adt_easy_20240307_2/src/bin/e.rs
@@ -1,11 +1,20 @@
-// TODO
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [Usize1; n],
+        mut a: [usize; n],
     };
-    let ans = n - a.len();
+    a.sort();
+    let sum = a.iter().sum::<usize>();
+    let avg = sum / n;
+    let avg2 = sum - avg * n;
+    let ans = vec![avg; n - avg2]
+        .into_iter()
+        .chain(vec![avg + 1; avg2])
+        .zip(a)
+        .map(|(x, y)| (x as i64 - y as i64).unsigned_abs() as usize)
+        .sum::<usize>()
+        / 2;
     println!("{}", ans);
 }
